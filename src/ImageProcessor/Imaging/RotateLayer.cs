50f75c0fc5939f69f4e5b8390e0745e8256f4955
JimBobSquarePants <james_south@hotmail.com>
2012-08-19 15:50:40 +0100

    Updated rotate method and Nuget manifests
    Former-commit-id: 718461336432ed3936dbf9b8a30d4231186267c0

---
+++ src/ImageProcessor/Imaging/RotateLayer.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RotateLayer.cs" company="James South">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ImageProcessor.Imaging
+{
+    #region Using
+    using System.Drawing;
+    #endregion
+
+    /// <summary>
+    /// Enacapsulates the properties required to rotate an image.
+    /// </summary>
+    internal class RotateLayer
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RotateLayer"/> class.
+        /// </summary>
+        public RotateLayer()
+        {
+            this.BackgroundColor = Color.Transparent;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the angle at which to rotate the image.
+        /// </summary>
+        public int Angle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the background color.
+        /// </summary>
+        public Color BackgroundColor { get; set; }
+        #endregion
+    }
+}

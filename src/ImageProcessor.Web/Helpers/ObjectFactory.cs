23643866fd9f8df4f694aabf32be9477f0f4e6c0
James South <james_south@hotmail.com>
2013-03-05 17:20:24 +0000

    cache updates
    Former-commit-id: 8f3bec7fd268acc12ceabe64541a3ffda5b25901

--- src/ImageProcessor.Web/Helpers/ObjectFactory.cs
+++ src/ImageProcessor.Web/Helpers/ObjectFactory.cs
@@ -17,13 +17,13 @@ namespace ImageProcessor.Web.Helpers
             Type type = ctor.DeclaringType;
             ParameterInfo[] paramsInfo = ctor.GetParameters();
 
-            //create a single param of type object[]
+            // Create a single param of type object[]
             ParameterExpression param = Expression.Parameter(typeof(object[]), "args");
 
             Expression[] argsExp = new Expression[paramsInfo.Length];
 
-            //pick each arg from the params array 
-            //and create a typed expression of them
+            // Pick each arg from the params array 
+            // and create a typed expression for them
             for (int i = 0; i < paramsInfo.Length; i++)
             {
                 Expression index = Expression.Constant(i);
@@ -36,15 +36,15 @@ namespace ImageProcessor.Web.Helpers
                 argsExp[i] = paramCastExp;
             }
 
-            //make a NewExpression that calls the
-            //ctor with the args we just created
+            // Make a NewExpression that calls the
+            // ctor with the args we just created
             NewExpression newExp = Expression.New(ctor, argsExp);
 
-            //create a lambda with the New
-            //Expression as body and our param object[] as arg
+            // Create a lambda with the New
+            // Expression as body and our param object[] as arg
             LambdaExpression lambda = Expression.Lambda(typeof(ObjectActivator<T>), newExp, param);
 
-            //compile it
+            // Compile it
             ObjectActivator<T> compiled = (ObjectActivator<T>)lambda.Compile();
             return compiled;
         }

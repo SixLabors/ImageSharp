e1b950bb5d31013f4420ad3fc80761e8ec6b1ec2
James South <james_south@hotmail.com>
2013-03-19 17:30:44 +0000

    Adding unit tests.
    Former-commit-id: 9221e0553a40b8e5cb8c3b2b031703e7a072a206

---
+++ src/ImageProcessor.Tests/RegularExpressionUnitTests.cs
@@ -0,0 +1,81 @@
+﻿namespace ImageProcessor.Tests
+{
+    #region Using
+    using System;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.IO;
+    using System.Text.RegularExpressions;
+    using ImageProcessor.Imaging;
+    using ImageProcessor.Processors;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    #endregion
+
+    [TestClass]
+    public class RegularExpressionUnitTests
+    {
+        #region Regular Expression Tests
+        [TestMethod]
+        public void TestAlphaRegex()
+        {
+            string querystring = "alpha=56";
+            int expected = 56;
+
+            Alpha alpha = new Alpha();
+            alpha.MatchRegexIndex(querystring);
+
+            int actual = alpha.DynamicParameter;
+
+            Assert.AreEqual<int>(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFormatRegex()
+        {
+            string querystring = "format=gif";
+            string expected = "gif";
+
+            Format format = new Format();
+            format.MatchRegexIndex(querystring);
+
+            string actual = format.DynamicParameter;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestQualityRegex()
+        {
+            string querystring = "quality=56";
+            int expected = 56;
+
+            Quality quality = new Quality();
+            quality.MatchRegexIndex(querystring);
+
+            int actual = quality.DynamicParameter;
+
+            Assert.AreEqual<int>(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestRotateRegex()
+        {
+            string querystring = "rotate=270";
+            RotateLayer expected = new RotateLayer
+            {
+                Angle = 270,
+                BackgroundColor = Color.Transparent
+            };
+
+            Rotate rotate = new Rotate();
+            rotate.MatchRegexIndex(querystring);
+
+            RotateLayer actual = rotate.DynamicParameter;
+
+            Debug.Print("{0}{1}", actual.Angle, actual.BackgroundColor);
+
+            Assert.AreEqual<RotateLayer>(expected, actual);
+        }
+        #endregion
+    }
+}

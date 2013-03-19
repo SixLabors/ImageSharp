b2d92df8447e7d330ce7b7879ca5235641c5d926
JimBobSquarePants <james_south@hotmail.com>
2013-03-19 21:40:17 +0000

    Prepping for 2.0.0 release
    Former-commit-id: b59ab2bde9a0cce9543992d66cc3297dee8efb45

--- src/ImageProcessor/Imaging/Filters/HiSatchMatrixFilter.cs
+++ src/ImageProcessor/Imaging/Filters/HiSatchMatrixFilter.cs
@@ -19,10 +19,10 @@ namespace ImageProcessor.Imaging.Filters
     /// <summary>
     /// Encapsulates methods with which to add a high saturated filter to an image.
     /// </summary>
-    class HiSatchMatrixFilter : IMatrixFilter
+    internal class HiSatchMatrixFilter : IMatrixFilter
     {
         /// <summary>
-        /// Gets the <see cref="T:System.Drawing.Imaging.ColoMatrix"/> for this filter instance.
+        /// Gets the <see cref="T:System.Drawing.Imaging.ColorMatrix"/> for this filter instance.
         /// </summary>
         public ColorMatrix Matrix
         {

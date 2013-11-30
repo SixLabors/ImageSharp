92e2394c23e58395271be083afa748f6ab4a166c
James South <james_south@hotmail.com>
2013-11-30 16:24:35 +0000

    Improved memory usage
    Former-commit-id: 8547d9f8f2fcb4b3fda6e5f3b8373994c4ba5dda

--- src/ImageProcessor.Web/NET45/HttpModules/ImageProcessingModule.cs
+++ src/ImageProcessor.Web/NET45/HttpModules/ImageProcessingModule.cs
@@ -1,9 +1,12 @@
-﻿// -----------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="ImageProcessingModule.cs" company="James South">
-//     Copyright (c) James South.
-//     Licensed under the Apache License, Version 2.0.
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
 // </copyright>
-// -----------------------------------------------------------------------
+// <summary>
+//   Processes any image requests within the web application.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ImageProcessor.Web.HttpModules
 {

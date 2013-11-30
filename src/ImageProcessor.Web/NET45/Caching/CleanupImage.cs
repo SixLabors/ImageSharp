573a93b667b51cce5e85d60d68926698459fe5ff
James South <james_south@hotmail.com>
2013-11-30 16:24:35 +0000

    Improved memory usage
    Former-commit-id: 24d4ee157e9b5acb2e72dad453e07aa18a6a561a

---
+++ src/ImageProcessor.Web/NET45/Caching/CleanupImage.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CleanupImage.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Describes a cached image for cleanup.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Web.Caching
+{
+    #region Using
+    using System;
+    #endregion
+
+    /// <summary>
+    /// Describes a cached image for cleanup
+    /// </summary>
+    public sealed class CleanupImage
+    {
+        /// <summary>
+        /// Gets or sets the value of the cached image.
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Gets or sets when the cached image should expire from the cache.
+        /// </summary>
+        public DateTime ExpiresUtc { get; set; }
+    }
+}

371b92c71ef160eba72b07d75893b5e5c8cc0edf
James South <james_south@hotmail.com>
2013-12-02 23:33:32 +0000

    Tweaks and tidy up
    Former-commit-id: 36cfa7389463c1afaf11c4cdc6f7d0db2ae419ea

--- src/ImageProcessor/Processors/IGraphicsProcessor.cs
+++ src/ImageProcessor/Processors/IGraphicsProcessor.cs
@@ -1,9 +1,12 @@
-﻿// -----------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="IGraphicsProcessor.cs" company="James South">
-//     Copyright (c) James South.
-//     Licensed under the Apache License, Version 2.0.
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
 // </copyright>
-// -----------------------------------------------------------------------
+// <summary>
+//   Defines properties and methods for ImageProcessor Plugins.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ImageProcessor.Processors
 {

ace7a5c03a44f8ad6a743f5587c38f6f14857329
James South <james_south@hotmail.com>
2013-12-02 23:33:32 +0000

    Tweaks and tidy up
    Former-commit-id: 75771789b7de2a884b77c0f16c37220212ba382a

--- src/ImageProcessor/Processors/Quality.cs
+++ src/ImageProcessor/Processors/Quality.cs
@@ -1,9 +1,12 @@
-﻿// -----------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Quality.cs" company="James South">
-//     Copyright (c) James South.
-//     Licensed under the Apache License, Version 2.0.
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
 // </copyright>
-// -----------------------------------------------------------------------
+// <summary>
+//   Encapsulates methods to change the quality component of the image.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ImageProcessor.Processors
 {
@@ -11,11 +14,10 @@ namespace ImageProcessor.Processors
     using System.Collections.Generic;
     using System.Drawing;
     using System.Text.RegularExpressions;
-    using ImageProcessor.Helpers.Extensions; 
     #endregion
 
     /// <summary>
-    /// TODO: Update summary.
+    /// Encapsulates methods to change the quality component of the image.
     /// </summary>
     public class Quality : IGraphicsProcessor
     {

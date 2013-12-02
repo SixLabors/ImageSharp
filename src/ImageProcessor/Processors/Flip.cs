c4e00551d04ef9becba9c70e1db5eff826bd6700
James South <james_south@hotmail.com>
2013-12-02 23:33:32 +0000

    Tweaks and tidy up
    Former-commit-id: 4502c2362bd1451661d13c7e667f7cce11f5045a

--- src/ImageProcessor/Processors/Flip.cs
+++ src/ImageProcessor/Processors/Flip.cs
@@ -1,9 +1,12 @@
-﻿// -----------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Flip.cs" company="James South">
-//     Copyright (c) James South.
-//     Licensed under the Apache License, Version 2.0.
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
 // </copyright>
-// -----------------------------------------------------------------------
+// <summary>
+//   Flips an image horizontally or vertically.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ImageProcessor.Processors
 {

20764204554510b3ac63e06ef88738818df05c71
James South <james_south@hotmail.com>
2013-12-02 23:33:32 +0000

    Tweaks and tidy up
    Former-commit-id: 75771789b7de2a884b77c0f16c37220212ba382a

--- src/ImageProcessor/Processors/RoundedCorners.cs
+++ src/ImageProcessor/Processors/RoundedCorners.cs
@@ -1,9 +1,12 @@
-﻿// -----------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="RoundedCorners.cs" company="James South">
-//     Copyright (c) James South.
-//     Licensed under the Apache License, Version 2.0.
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
 // </copyright>
-// -----------------------------------------------------------------------
+// <summary>
+//   Encapsulates methods to add rounded corners to an image.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ImageProcessor.Processors
 {

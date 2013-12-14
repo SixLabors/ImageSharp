89dc095718a1cecbb940a7ade67c8e18ecc00e9f
James South <james_south@hotmail.com>
2013-12-14 10:17:57 +0000

    Refactoring contstrain method
    Former-commit-id: befb487247ed2520ace3a7ac7d5e31a81413476d

--- src/ImageProcessor/Processors/Resize.cs
+++ src/ImageProcessor/Processors/Resize.cs
@@ -15,6 +15,8 @@ namespace ImageProcessor.Processors
     using System;
     using System.Collections.Generic;
     using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Drawing.Imaging;
     using System.Text;
     using System.Text.RegularExpressions;
     using ImageProcessor.Helpers.Extensions;
@@ -25,12 +27,12 @@ namespace ImageProcessor.Processors
     /// <summary>
     /// Resizes an image to the given dimensions.
     /// </summary>
-    public class Resize : ResizeBase
+    public class Resize : IGraphicsProcessor
     {
         /// <summary>
         /// The regular expression to search strings for.
         /// </summary>
-        private static readonly Regex QueryRegex = new Regex(@"((width|height)=\d+)|(mode=(pad|stretch|crop|constrain))|(anchor=(top|bottom|left|right|center))|(bgcolor=([0-9a-fA-F]{3}){1,2})", RegexOptions.Compiled);
+        private static readonly Regex QueryRegex = new Regex(@"((width|height)=\d+)|(mode=(pad|stretch|crop|max))|(anchor=(top|bottom|left|right|center))|(bgcolor=([0-9a-fA-F]{3}){1,2})", RegexOptions.Compiled);
 
         /// <summary>
         /// The regular expression to search strings for the size attribute.
@@ -40,7 +42,7 @@ namespace ImageProcessor.Processors
         /// <summary>
         /// The regular expression to search strings for the mode attribute.
         /// </summary>
-        private static readonly Regex ModeRegex = new Regex(@"mode=(pad|stretch|crop|constrain)", RegexOptions.Compiled);
+        private static readonly Regex ModeRegex = new Regex(@"mode=(pad|stretch|crop|max)", RegexOptions.Compiled);
 
         /// <summary>
         /// The regular expression to search strings for the anchor attribute.
@@ -56,7 +58,7 @@ namespace ImageProcessor.Processors
         /// <summary>
         /// Gets the regular expression to search strings for.
         /// </summary>
-        public override Regex RegexPattern
+        public Regex RegexPattern
         {
             get
             {
@@ -67,7 +69,7 @@ namespace ImageProcessor.Processors
         /// <summary>
         /// Gets or sets DynamicParameter.
         /// </summary>
-        public override dynamic DynamicParameter
+        public dynamic DynamicParameter
         {
             get;
             set;
@@ -76,16 +78,16 @@ namespace ImageProcessor.Processors
         /// <summary>
         /// Gets the order in which this processor is to be used in a chain.
         /// </summary>
-        public override int SortOrder
+        public int SortOrder
         {
             get;
-            protected set;
+            private set;
         }
 
         /// <summary>
         /// Gets or sets any additional settings required by the processor.
         /// </summary>
-        public override Dictionary<string, string> Settings
+        public Dictionary<string, string> Settings
         {
             get;
             set;
@@ -100,7 +102,7 @@ namespace ImageProcessor.Processors
         /// <returns>
         /// The zero-based starting position in the original string where the captured substring was found.
         /// </returns>
-        public override int MatchRegexIndex(string queryString)
+        public int MatchRegexIndex(string queryString)
         {
             int index = 0;
 
@@ -151,7 +153,7 @@ namespace ImageProcessor.Processors
         /// <returns>
         /// The processed image from the current instance of the <see cref="T:ImageProcessor.ImageFactory"/> class.
         /// </returns>
-        public override Image ProcessImage(ImageFactory factory)
+        public Image ProcessImage(ImageFactory factory)
         {
             int width = this.DynamicParameter.Size.Width ?? 0;
             int height = this.DynamicParameter.Size.Height ?? 0;
@@ -164,17 +166,227 @@ namespace ImageProcessor.Processors
             int.TryParse(this.Settings["MaxWidth"], out defaultMaxWidth);
             int.TryParse(this.Settings["MaxHeight"], out defaultMaxHeight);
 
-            if (mode == ResizeMode.Constrain)
-            {
-                // Just use the old constrain plugin to handle the resize.
-                var constrainSettings = new Dictionary<string, string> { { "MaxWidth", defaultMaxWidth.ToString("G") }, { "MaxHeight", defaultMaxHeight.ToString("G") } };
-                Constrain constrain = new Constrain { DynamicParameter = new Size(width, height), Settings = constrainSettings };
-                return constrain.ProcessImage(factory);
-            }
-
             return this.ResizeImage(factory, width, height, defaultMaxWidth, defaultMaxHeight, backgroundColor, mode, anchor);
         }
         #endregion
+
+        /// <summary>
+        /// The resize image.
+        /// </summary>
+        /// <param name="factory">
+        /// The the current instance of the <see cref="T:ImageProcessor.ImageFactory"/> class containing
+        /// the image to process.
+        /// </param>
+        /// <param name="width">
+        /// The width to resize the image to.
+        /// </param>
+        /// <param name="height">
+        /// The height to resize the image to.
+        /// </param>
+        /// <param name="defaultMaxWidth">
+        /// The default max width to resize the image to.
+        /// </param>
+        /// <param name="defaultMaxHeight">
+        /// The default max height to resize the image to.
+        /// </param>
+        /// <param name="backgroundColor">
+        /// The background color to pad the image with.
+        /// </param>
+        /// <param name="resizeMode">
+        /// The mode with which to resize the image.
+        /// </param>
+        /// <param name="anchorPosition">
+        /// The anchor position to place the image at.
+        /// </param>
+        /// <returns>
+        /// The processed image from the current instance of the <see cref="T:ImageProcessor.ImageFactory"/> class.
+        /// </returns>
+        private Image ResizeImage(
+            ImageFactory factory,
+            int width,
+            int height,
+            int defaultMaxWidth,
+            int defaultMaxHeight,
+            Color backgroundColor,
+            ResizeMode resizeMode = ResizeMode.Pad,
+            AnchorPosition anchorPosition = AnchorPosition.Center)
+        {
+            Bitmap newImage = null;
+            Image image = factory.Image;
+
+            try
+            {
+                int sourceWidth = image.Width;
+                int sourceHeight = image.Height;
+
+                int destinationWidth = width;
+                int destinationHeight = height;
+
+                int maxWidth = defaultMaxWidth > 0 ? defaultMaxWidth : int.MaxValue;
+                int maxHeight = defaultMaxHeight > 0 ? defaultMaxHeight : int.MaxValue;
+
+                // Fractional variants for preserving aspect ratio.
+                double percentHeight = Math.Abs(height / (double)sourceHeight);
+                double percentWidth = Math.Abs(width / (double)sourceWidth);
+
+                int destinationX = 0;
+                int destinationY = 0;
+
+                // Change the destination rectangle coordinates if padding and 
+                // there has been a set width and height.
+                if (resizeMode == ResizeMode.Pad && width > 0 && height > 0)
+                {
+                    double ratio;
+
+                    if (percentHeight < percentWidth)
+                    {
+                        ratio = percentHeight;
+                        destinationX = (int)((width - (sourceWidth * ratio)) / 2);
+                        destinationWidth = (int)Math.Floor(sourceWidth * percentHeight);
+                    }
+                    else
+                    {
+                        ratio = percentWidth;
+                        destinationY = (int)((height - (sourceHeight * ratio)) / 2);
+                        destinationHeight = (int)Math.Floor(sourceHeight * percentWidth);
+                    }
+                }
+
+                // Change the destination rectangle coordinates if cropping and 
+                // there has been a set width and height.
+                if (resizeMode == ResizeMode.Crop && width > 0 && height > 0)
+                {
+                    double ratio;
+
+                    if (percentHeight < percentWidth)
+                    {
+                        ratio = percentWidth;
+
+                        switch (anchorPosition)
+                        {
+                            case AnchorPosition.Top:
+                                destinationY = 0;
+                                break;
+                            case AnchorPosition.Bottom:
+                                destinationY = (int)(height - (sourceHeight * ratio));
+                                break;
+                            default:
+                                destinationY = (int)((height - (sourceHeight * ratio)) / 2);
+                                break;
+                        }
+
+                        destinationHeight = (int)Math.Floor(sourceHeight * percentWidth);
+                    }
+                    else
+                    {
+                        ratio = percentHeight;
+
+                        switch (anchorPosition)
+                        {
+                            case AnchorPosition.Left:
+                                destinationX = 0;
+                                break;
+                            case AnchorPosition.Right:
+                                destinationX = (int)(width - (sourceWidth * ratio));
+                                break;
+                            default:
+                                destinationX = (int)((width - (sourceWidth * ratio)) / 2);
+                                break;
+                        }
+
+                        destinationWidth = (int)Math.Floor(sourceWidth * percentHeight);
+                    }
+                }
+
+                // Constrain the image to fit the maximum possible height or width.
+                if (resizeMode == ResizeMode.Max)
+                {
+                    if (sourceWidth > width || sourceHeight > height)
+                    {
+                        double ratio = Math.Abs(height / width);
+                        double sourceRatio = Math.Abs(sourceHeight / sourceWidth);
+
+                        if (sourceRatio < ratio)
+                        {
+                            height = 0;
+                        }
+                        else
+                        {
+                            width = 0;
+                        }
+                    }
+                }
+
+                // If height or width is not passed we assume that the standard ratio is to be kept.
+                if (height == 0)
+                {
+                    destinationHeight = (int)Math.Floor(sourceHeight * percentWidth);
+                    height = destinationHeight;
+                }
+
+                if (width == 0)
+                {
+                    destinationWidth = (int)Math.Floor(sourceWidth * percentHeight);
+                    width = destinationWidth;
+                }
+
+                if (width > 0 && height > 0 && width <= maxWidth && height <= maxHeight)
+                {
+                    // Don't use an object initializer here.
+                    // ReSharper disable once UseObjectOrCollectionInitializer
+                    newImage = new Bitmap(width, height, PixelFormat.Format32bppPArgb);
+                    newImage.Tag = image.Tag;
+
+                    using (Graphics graphics = Graphics.FromImage(newImage))
+                    {
+                        // We want to use two different blending algorithms for enlargement/shrinking.
+                        // Bicubic is better enlarging for whilst Bilinear is better for shrinking.
+                        // http://www.codinghorror.com/blog/2007/07/better-image-resizing.html
+                        if (image.Width < destinationWidth && image.Height < destinationHeight)
+                        {
+                            // We are making it larger.
+                            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                            graphics.CompositingQuality = CompositingQuality.HighQuality;
+                        }
+                        else
+                        {
+                            // We are making it smaller.
+                            graphics.SmoothingMode = SmoothingMode.None;
+                            graphics.InterpolationMode = InterpolationMode.HighQualityBilinear;
+                            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                            graphics.CompositingQuality = CompositingQuality.HighQuality;
+                        }
+
+                        // An unwanted border appears when using InterpolationMode.HighQualityBicubic to resize the image
+                        // as the algorithm appears to be pulling averaging detail from surFlooring pixels beyond the edge 
+                        // of the image. Using the ImageAttributes class to specify that the pixels beyond are simply mirror 
+                        // images of the pixels within solves this problem.
+                        using (ImageAttributes wrapMode = new ImageAttributes())
+                        {
+                            wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                            graphics.Clear(backgroundColor);
+                            Rectangle destRect = new Rectangle(destinationX, destinationY, destinationWidth, destinationHeight);
+                            graphics.DrawImage(image, destRect, 0, 0, sourceWidth, sourceHeight, GraphicsUnit.Pixel, wrapMode);
+                        }
+
+                        // Reassign the image.
+                        image.Dispose();
+                        image = newImage;
+                    }
+                }
+            }
+            catch
+            {
+                if (newImage != null)
+                {
+                    newImage.Dispose();
+                }
+            }
+
+            return image;
+        }
 
         /// <summary>
         /// Returns the correct <see cref="Size"/> for the given string.
@@ -247,8 +459,8 @@ namespace ImageProcessor.Processors
                         return ResizeMode.Stretch;
                     case "crop":
                         return ResizeMode.Crop;
-                    case "constrain":
-                        return ResizeMode.Constrain;
+                    case "max":
+                        return ResizeMode.Max;
                     default:
                         return ResizeMode.Pad;
                 }

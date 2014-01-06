d279b09697e1ca38d2e7787740ce6800f187f459
James South <james_south@hotmail.com>
2014-01-06 16:11:52 +0000

    v1.8.1.0 & v3.0.0.3
    Former-commit-id: 798df2c6253c9e4cbdee9293ed29689db5c1f3a9

--- src/ImageProcessor/Properties/AssemblyInfo.cs
+++ src/ImageProcessor/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Security;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.8.1.0")]
+[assembly: AssemblyFileVersion("1.8.1.0")]
 

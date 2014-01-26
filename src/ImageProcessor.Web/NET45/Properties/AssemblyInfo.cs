6b2370535d552be0deff32ed3330e375ad54fb4f
James South <james_south@hotmail.com>
2014-01-26 12:29:09 +0000

    v1.8.2 & v3.0.1
    Fixes #28
    Fixes #18
    
    
    Former-commit-id: a78ac08c9ec13857ca317b8801375db41ac09702

--- src/ImageProcessor.Web/NET45/Properties/AssemblyInfo.cs
+++ src/ImageProcessor.Web/NET45/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.0.3")]
-[assembly: AssemblyFileVersion("3.0.0.3")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
 

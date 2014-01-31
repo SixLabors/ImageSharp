89679669a90ab20c80a333946c8af8ff1bf8f8c4
James South <james_south@hotmail.com>
2014-01-31 17:33:46 +0000

    Adding webforms site
    Former-commit-id: fa3653cdab0c812dc54f534f48c2944cd4ab8e6f

---
+++ src/TestWebsites/NET45/Test_Website_Webforms_NET45/Account/OpenAuthProviders.ascx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Microsoft.AspNet.Membership.OpenAuth;
+
+namespace Test_Website_Webforms_NET45.Account
+{
+    public partial class OpenAuthProviders : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (IsPostBack)
+            {
+                var provider = Request.Form["provider"];
+                if (provider == null)
+                {
+                    return;
+                }
+
+                var redirectUrl = "~/Account/RegisterExternalLogin";
+                if (!String.IsNullOrEmpty(ReturnUrl))
+                {
+                    var resolvedReturnUrl = ResolveUrl(ReturnUrl);
+                    redirectUrl += "?ReturnUrl=" + HttpUtility.UrlEncode(resolvedReturnUrl);
+                }
+
+                OpenAuth.RequestAuthentication(provider, redirectUrl);
+            }
+        }
+
+
+
+        public string ReturnUrl { get; set; }
+
+
+        public IEnumerable<ProviderDetails> GetProviderNames()
+        {
+            return OpenAuth.AuthenticationClients.GetAll();
+        }
+
+    }
+}
---
+++ src/TestWebsites/NET45/Test_Website_Webforms_NET45/Site.Mobile.Master.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Test_Website_Webforms_NET45
+{
+    public partial class Site_Mobile : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

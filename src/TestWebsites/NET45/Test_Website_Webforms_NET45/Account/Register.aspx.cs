1e3b6a85e406940f11999f86803eaf4a6c4cd64d
James South <james_south@hotmail.com>
2014-01-31 17:33:46 +0000

    Adding webforms site
    Former-commit-id: 2be964138cead6a87ffd6bafc6f4c809aa5f8bee

---
+++ src/TestWebsites/NET45/Test_Website_Webforms_NET45/Account/Register.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Membership.OpenAuth;
+
+namespace Test_Website_Webforms_NET45.Account
+{
+    public partial class Register : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RegisterUser.ContinueDestinationPageUrl = Request.QueryString["ReturnUrl"];
+        }
+
+        protected void RegisterUser_CreatedUser(object sender, EventArgs e)
+        {
+            FormsAuthentication.SetAuthCookie(RegisterUser.UserName, createPersistentCookie: false);
+
+            string continueUrl = RegisterUser.ContinueDestinationPageUrl;
+            if (!OpenAuth.IsLocalUrl(continueUrl))
+            {
+                continueUrl = "~/";
+            }
+            Response.Redirect(continueUrl);
+        }
+    }
+}

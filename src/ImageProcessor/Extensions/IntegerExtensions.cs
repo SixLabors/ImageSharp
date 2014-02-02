4bd8ff952f76618a7024698c3b557bdad895b9b6
James South <james_south@hotmail.com>
2014-02-02 12:06:17 +0000

    More refactoring
    Former-commit-id: cbd2713de7d6378edc260d6f35f50dd106e39e5a

---
+++ src/ImageProcessor/Extensions/IntegerExtensions.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IntegerExtensions.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Encapsulates a series of time saving extension methods to the <see cref="T:System.Int32" /> class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Extensions
+{
+    /// <summary>
+    /// Encapsulates a series of time saving extension methods to the <see cref="T:System.Int32"/> class.
+    /// </summary>
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Converts an <see cref="T:System.Int32"/> value into a valid <see cref="T:System.Byte"/>.
+        /// <remarks>
+        /// If the value given is less than 0 or greater than 255, the value will be constrained into
+        /// those restricted ranges.
+        /// </remarks>
+        /// </summary>
+        /// <param name="integer">
+        /// The <see cref="T:System.Int32"/> to convert.
+        /// </param>
+        /// <returns>
+        /// The <see cref="T:System.Byte"/>.
+        /// </returns>
+        public static byte ToByte(this int integer)
+        {
+            return ((double)integer).ToByte();
+        }
+    }
+}

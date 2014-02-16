43e35bcbef335cb8b52c79e1f339555ed53054c9
James South <james_south@hotmail.com>
2014-02-16 16:48:58 +0000

    Adding Mvc5 Test
    Former-commit-id: 870a388fa6d189f229ab5f64ce22d5100455a563

---
+++ src/TestWebsites/NET45/Test_Website_MVC5_NET45/Models/IdentityModels.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Test_Website_MVC5_NET45.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}

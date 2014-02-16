e5e3b8cfffe377ba2e1c588210cfc7589961d7a0
James South <james_south@hotmail.com>
2014-02-16 16:48:58 +0000

    Adding Mvc5 Test
    Former-commit-id: 987f70d48f171d5287cc9c3822dfea0346e53a24

---
+++ src/TestWebsites/NET45/Test_Website_MVC5_NET45/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_Website_MVC5_NET45.Startup))]
+namespace Test_Website_MVC5_NET45
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

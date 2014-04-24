3355bc29a8c13bb24cf48eaa93964ea0a70ae205
James South <james_south@hotmail.com>
2014-04-24 22:56:26 +0100

    First success!!
    Code is an absolute mess just now.
    
    
    Former-commit-id: 3e06fce0239f8ef7fe29080b152b742f7581c914

---
+++ src/ImageProcessorConsole/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessorConsole
+{
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+
+    using ImageProcessor;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+            // ReSharper disable once AssignNullToNotNullAttribute
+            string resolvedPath = Path.Combine(Path.GetDirectoryName(path), "images");
+            DirectoryInfo di = new DirectoryInfo(resolvedPath);
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+
+            FileInfo[] files = di.GetFiles("*.gif");
+
+            foreach (FileInfo fileInfo in files)
+            {
+                byte[] photoBytes = File.ReadAllBytes(fileInfo.FullName);
+
+                // ImageProcessor
+                using (MemoryStream inStream = new MemoryStream(photoBytes))
+                {
+                    using (ImageFactory imageFactory = new ImageFactory())
+                    {
+                        Size size = new Size(150, 150);
+                        ImageFormat format = ImageFormat.Gif;
+
+                        // Load, resize, set the format and quality and save an image.
+                        imageFactory.Load(inStream)
+                            .Constrain(size)
+                            .Format(format)
+                            .Save(Path.Combine(Path.GetDirectoryName(path), "output", fileInfo.Name));
+                    }
+                }
+            }
+        }
+    }
+}

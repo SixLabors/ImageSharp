363bda1bfd7f4be1cb39d3732c597baed321228f
James South <james_south@hotmail.com>
2014-05-06 16:09:19 +0100

    Finalising animated gif and improving filters
    Former-commit-id: 0feb975e5a07d50e812cd113e3335fe6b7a4271d

---
+++ src/ImageProcessor/Imaging/Filters/MatrixFilterRegexAttribute.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MatrixFilterRegexAttribute.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   The filter attribute for identifying matrix filter properties.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Imaging.Filters
+{
+    using System;
+
+    /// <summary>
+    /// The filter attribute for identifying matrix filter properties.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MatrixFilterRegexAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MatrixFilterRegexAttribute"/> class.
+        /// </summary>
+        /// <param name="regexIdentifier">
+        /// The regex identifier.
+        /// </param>
+        public MatrixFilterRegexAttribute(string regexIdentifier)
+        {
+            this.RegexIdentifier = regexIdentifier;
+        }
+
+        /// <summary>
+        /// Gets the regex identifier.
+        /// </summary>
+        public string RegexIdentifier { get; private set; }
+    }
+}

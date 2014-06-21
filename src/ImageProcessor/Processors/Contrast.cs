22a5bc0c383d192d0b3959c7e1cddb3da0b59f9f
James South <james_south@hotmail.com>
2014-06-21 17:24:56 +0100

    Fixing Unit tests.
    Former-commit-id: 238822ef315612de5201703a61709a325c629a4a

--- src/ImageProcessor/Processors/Contrast.cs
+++ src/ImageProcessor/Processors/Contrast.cs
@@ -20,6 +20,14 @@ namespace ImageProcessor.Processors
     public class Contrast : IGraphicsProcessor
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="Contrast"/> class.
+        /// </summary>
+        public Contrast()
+        {
+            this.Settings = new Dictionary<string, string>();
+        }
+
+        /// <summary>
         /// Gets or sets DynamicParameter.
         /// </summary>
         public dynamic DynamicParameter

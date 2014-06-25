6aac1054d7390252b420bea99c9fea6589a0fcde
Thomas Broust <thomas.broust@gmail.com>
2014-06-25 23:26:59 +0200

    Creates a new simple test class for image loading
    Former-commit-id: 8add7ad0e49c1fdcf5da9090e6de2d7546b63d52

--- src/ImageProcessor.UnitTests/ImageFactoryUnitTests.cs
+++ src/ImageProcessor.UnitTests/ImageFactoryUnitTests.cs
@@ -1,15 +1,43 @@
-﻿using NUnit.Framework;
-using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ImageFactoryUnitTests.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Unit tests for the ImageFactory (loading of images)
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace ImageProcessor.UnitTests
 {
-    [TestFixture()]
+    using System;
+    using System.IO;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test harness for the image factory
+    /// </summary>
+    [TestFixture]
     public class ImageFactoryUnitTests
     {
-        [Test()]
-        public void TestCase()
+        /// <summary>
+        /// The path to the binary's folder
+        /// </summary>
+        private readonly string localPath = Path.GetDirectoryName(new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+
+        /// <summary>
+        /// Tests the loading of image from a file
+        /// </summary>
+        [Test]
+        public void TestLoadImageFromFile()
         {
+            var testPhoto = Path.Combine(this.localPath, "Images/Chrysanthemum.jpg");
+            using (ImageFactory imageFactory = new ImageFactory())
+            {
+                imageFactory.Load(testPhoto);
+                Assert.AreEqual(testPhoto, imageFactory.ImagePath);
+                Assert.AreEqual("image/jpeg", imageFactory.MimeType);
+            }
         }
     }
 }
-

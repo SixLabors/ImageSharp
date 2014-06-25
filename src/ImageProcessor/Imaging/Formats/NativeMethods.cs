0f07e259c1c8f84071429c8bfa81dfcb9d186518
James South <james_south@hotmail.com>
2014-06-25 19:02:07 +0100

    Funky Nuget stuff
    Still can't copy unmanaged binaries across.
    
    
    Former-commit-id: 7f67e907cd075b5397b0d4dc0b0727262596e620

--- src/ImageProcessor/Imaging/Formats/NativeMethods.cs
+++ src/ImageProcessor/Imaging/Formats/NativeMethods.cs
@@ -37,8 +37,29 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// 1 if success, otherwise error code returned in the case of (a) formatting error(s).
         /// </returns>
-        [DllImport("libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int WebPGetInfo(IntPtr data, uint dataSize, out int width, out int height);
+        [DllImport("x86\\libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPGetInfo")]
+        public static extern int WebPGetInfo86(IntPtr data, uint dataSize, out int width, out int height);
+
+        /// <summary>
+        /// Validate the WebP image header and retrieve the image height and width. Pointers *width and *height can be passed NULL if deemed irrelevant
+        /// </summary>
+        /// <param name="data">
+        /// Pointer to WebP image data
+        /// </param>
+        /// <param name="dataSize">
+        /// This is the size of the memory block pointed to by data containing the image data
+        /// </param>
+        /// <param name="width">
+        /// The width range is limited currently from 1 to 16383
+        /// </param>
+        /// <param name="height">
+        /// The height range is limited currently from 1 to 16383
+        /// </param>
+        /// <returns>
+        /// 1 if success, otherwise error code returned in the case of (a) formatting error(s).
+        /// </returns>
+        [DllImport("x64\\libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPGetInfo")]
+        public static extern int WebPGetInfo64(IntPtr data, uint dataSize, out int width, out int height);
 
         /// <summary>
         /// Decode WEBP image pointed to by *data and returns BGR samples into a pre-allocated buffer
@@ -61,8 +82,32 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// output_buffer if function succeeds; NULL otherwise
         /// </returns>
-        [DllImport("libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr WebPDecodeBGRAInto(IntPtr data, uint dataSize, IntPtr outputBuffer, int outputBufferSize, int outputStride);
+        [DllImport("x86\\libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPDecodeBGRAInto")]
+        public static extern IntPtr WebPDecodeBGRAInto86(IntPtr data, uint dataSize, IntPtr outputBuffer, int outputBufferSize, int outputStride);
+
+        /// <summary>
+        /// Decode WEBP image pointed to by *data and returns BGR samples into a pre-allocated buffer
+        /// </summary>
+        /// <param name="data">
+        /// Pointer to WebP image data
+        /// </param>
+        /// <param name="dataSize">
+        /// This is the size of the memory block pointed to by data containing the image data
+        /// </param>
+        /// <param name="outputBuffer">
+        /// Pointer to decoded WebP image
+        /// </param>
+        /// <param name="outputBufferSize">
+        /// Size of allocated buffer
+        /// </param>
+        /// <param name="outputStride">
+        /// Specifies the distance between scan-lines
+        /// </param>
+        /// <returns>
+        /// output_buffer if function succeeds; NULL otherwise
+        /// </returns>
+        [DllImport("x64\\libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPDecodeBGRAInto")]
+        public static extern IntPtr WebPDecodeBGRAInto64(IntPtr data, uint dataSize, IntPtr outputBuffer, int outputBufferSize, int outputStride);
 
         /// <summary>
         /// Lossy encoding images pointed to by *data in WebP format
@@ -88,8 +133,35 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// Size of WebP Image
         /// </returns>
-        [DllImport("libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int WebPEncodeBGRA(IntPtr rgb, int width, int height, int stride, float qualityFactor, out IntPtr output);
+        [DllImport("x86\\libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPEncodeBGRA")]
+        public static extern int WebPEncodeBGRA86(IntPtr rgb, int width, int height, int stride, float qualityFactor, out IntPtr output);
+
+        /// <summary>
+        /// Lossy encoding images pointed to by *data in WebP format
+        /// </summary>
+        /// <param name="rgb">
+        /// Pointer to RGB image data
+        /// </param>
+        /// <param name="width">
+        /// The width range is limited currently from 1 to 16383
+        /// </param>
+        /// <param name="height">
+        /// The height range is limited currently from 1 to 16383
+        /// </param>
+        /// <param name="stride">
+        /// The stride.
+        /// </param>
+        /// <param name="qualityFactor">
+        /// Ranges from 0 (lower quality) to 100 (highest quality). Controls the loss and quality during compression
+        /// </param>
+        /// <param name="output">
+        /// output_buffer with WebP image
+        /// </param>
+        /// <returns>
+        /// Size of WebP Image
+        /// </returns>
+        [DllImport("x64\\libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPEncodeBGRA")]
+        public static extern int WebPEncodeBGRA64(IntPtr rgb, int width, int height, int stride, float qualityFactor, out IntPtr output);
 
         /// <summary>
         /// Frees the unmanaged memory.
@@ -100,8 +172,20 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// 1 if success, otherwise error code returned in the case of (a) error(s).
         /// </returns>
-        [DllImport("libwebp.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int WebPFree(IntPtr pointer); 
+        [DllImport("x86\\libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPFree")]
+        public static extern int WebPFree86(IntPtr pointer);
+
+        /// <summary>
+        /// Frees the unmanaged memory.
+        /// </summary>
+        /// <param name="pointer">
+        /// The pointer.
+        /// </param>
+        /// <returns>
+        /// 1 if success, otherwise error code returned in the case of (a) error(s).
+        /// </returns>
+        [DllImport("x64\\libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPFree")]
+        public static extern int WebPFree64(IntPtr pointer);
         #endregion
     }
 }

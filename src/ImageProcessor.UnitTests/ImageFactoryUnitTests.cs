56f68aadad27f7e546416677043856784ee1734c
Thomas Broust <thomas.broust@gmail.com>
2014-06-25 22:48:16 +0200

    Stylecop all the things!
    Former-commit-id: ff6d069372a9627856a3afc06b581eac8fd24f56

---
+++ src/ImageProcessor.UnitTests/ImageFactoryUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ImageProcessor.UnitTests
+{
+    [TestFixture()]
+    public class ImageFactoryUnitTests
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
+

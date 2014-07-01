a8289fba948b613172252e41ebdd28cbf1b2b5db
James South <james_south@hotmail.com>
2014-07-01 22:42:16 +0100

    v1.9.4, v3.2.9, v1.1.3
    Former-commit-id: c038bd08676e26026efa8a6ea25e39ffdfd222e9

--- src/ImageProcessor.Web/NET45/Properties/AssemblyInfo.cs
+++ src/ImageProcessor.Web/NET45/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using ImageProcessor.Web.HttpModules;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.2.8.0")]
-[assembly: AssemblyFileVersion("3.2.8.0")]
+[assembly: AssemblyVersion("3.2.9.0")]
+[assembly: AssemblyFileVersion("3.2.9.0")]

95b66aa6556944073583c88c1969f3153c845887
James South <james_south@hotmail.com>
2014-07-06 18:58:51 +0100

    Fixing references.
    Former-commit-id: cd78e3ba30aed1803c400e128a9e4ba0b5090681

--- src/ImageProcessor.Web.UnitTests/Extensions/StringExtensionsUnitTests.cs
+++ src/ImageProcessor.Web.UnitTests/Extensions/StringExtensionsUnitTests.cs
@@ -11,6 +11,8 @@
 namespace ImageProcessor.Web.UnitTests.Extensions
 {
     using System;
+    using System.Collections.Generic;
+
     using ImageProcessor.Web.Extensions;
     using NUnit.Framework;
 
@@ -20,6 +22,66 @@ namespace ImageProcessor.Web.UnitTests.Extensions
     [TestFixture]
     public class StringExtensionsUnitTests
     {
+        /// <summary>
+        /// Tests the passing to an integer array
+        /// </summary>
+        [Test]
+        public void TestToIntegerArray()
+        {
+            Dictionary<string, int[]> data = new Dictionary<string, int[]>
+            {
+                {
+                    "123-456,78-90",
+                    new[] { 123, 456, 78, 90 }
+                },
+                {
+                    "87390174,741897498,74816,748297,57355",
+                    new[]
+                        {
+                            87390174, 741897498, 74816,
+                            748297, 57355
+                        }
+                },
+                { "1-2-3", new[] { 1, 2, 3 } }
+            };
+
+            foreach (KeyValuePair<string, int[]> item in data)
+            {
+                int[] result = item.Key.ToPositiveIntegerArray();
+                Assert.AreEqual(item.Value, result);
+            }
+        }
+
+        /// <summary>
+        /// Tests the passing to an float array
+        /// </summary>
+        [Test]
+        public void TestToFloatArray()
+        {
+            Dictionary<string, float[]> data = new Dictionary<string, float[]>
+            {
+                {
+                    "12.3-4.56,78-9.0",
+                    new[] { 12.3F, 4.56F, 78, 9 }
+                },
+                {
+                    "87390.174,7.41897498,748.16,748297,5.7355",
+                    new[]
+                        {
+                            87390.174F, 7.41897498F,
+                            748.16F, 748297, 5.7355F
+                        }
+                },
+                { "1-2-3", new float[] { 1, 2, 3 } }
+            };
+
+            foreach (KeyValuePair<string, float[]> item in data)
+            {
+                float[] result = item.Key.ToPositiveFloatArray();
+                Assert.AreEqual(item.Value, result);
+            }
+        }
+
         /// <summary>
         /// Tests the MD5 fingerprint
         /// </summary>

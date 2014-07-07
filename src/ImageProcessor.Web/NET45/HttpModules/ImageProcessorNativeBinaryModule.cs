0f1faa0ced180b4a58197d6e6a342cd70730046c
James South <james_south@hotmail.com>
2014-07-07 08:21:28 +0100

    Reorganizing native binaries
    Former-commit-id: f372775134cb759ae27a29d8703e11e8336403f1

---
+++ src/ImageProcessor.Web/NET45/HttpModules/ImageProcessorNativeBinaryModule.cs
@@ -0,0 +1,159 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ImageProcessorNativeBinaryModule.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   The image processing native binary module.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Web.HttpModules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Web;
+
+    using ImageProcessor.Web.Helpers;
+
+    /// <summary>
+    /// Controls the loading and unloading of any native binaries required by ImageProcessor.Web.
+    /// </summary>
+    public sealed class ImageProcessorNativeBinaryModule : IHttpModule
+    {
+        /// <summary>
+        /// Whether the process is running in 64bit mode. Used for calling the correct dllimport method.
+        /// </summary>
+        private static readonly bool Is64Bit = Environment.Is64BitProcess;
+
+        /// <summary>
+        /// The object to lock against.
+        /// </summary>
+        private static readonly object SyncRoot = new object();
+
+        /// <summary>
+        /// The native binaries.
+        /// </summary>
+        private static readonly List<IntPtr> NativeBinaries = new List<IntPtr>();
+
+        /// <summary>
+        /// A value indicating whether this instance of the given entity has been disposed.
+        /// </summary>
+        /// <value><see langword="true"/> if this instance has been disposed; otherwise, <see langword="false"/>.</value>
+        /// <remarks>
+        /// If the entity is disposed, it must not be disposed a second
+        /// time. The isDisposed field is set the first time the entity
+        /// is disposed. If the isDisposed field is true, then the Dispose()
+        /// method will not dispose again. This help not to prolong the entity's
+        /// life in the Garbage Collector.
+        /// </remarks>
+        private bool isDisposed;
+
+        /// <summary>
+        /// Disposes of the resources (other than memory) used by the module that implements 
+        /// <see cref="T:System.Web.IHttpModule" />.
+        /// </summary>
+        public void Dispose()
+        {
+            if (this.isDisposed)
+            {
+                return;
+            }
+
+            // Call the appropriate methods to clean up
+            // unmanaged resources here.
+            lock (SyncRoot)
+            {
+                this.FreeNativeBinaries();
+            }
+
+            // Note disposing is done.
+            this.isDisposed = true;
+        }
+
+        /// <summary>
+        /// Initializes a module and prepares it to handle requests.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.Web.HttpApplication" /> that provides access to 
+        /// the methods, properties, and events common to all application objects within an ASP.NET application</param>
+        public void Init(HttpApplication context)
+        {
+        }
+
+        /// <summary>
+        /// Loads any native ImageProcessor binaries.
+        /// </summary>
+        public void LoadNativeBinaries()
+        {
+            lock (SyncRoot)
+            {
+                this.RegisterNativeBinaries();
+            }
+        }
+
+        /// <summary>
+        /// Registers any native binaries.
+        /// </summary>
+        /// <exception cref="ApplicationException">
+        /// Thrown when a native binary cannot be loaded.
+        /// </exception>
+        private void RegisterNativeBinaries()
+        {
+            if (NativeBinaries.Any())
+            {
+                return;
+            }
+
+            string folder = Is64Bit ? "x64" : "x86";
+            string sourcePath = HttpContext.Current.Server.MapPath("~/bin/" + folder);
+            string targetBasePath = new Uri(Assembly.GetExecutingAssembly().Location).LocalPath;
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(sourcePath);
+            if (directoryInfo.Exists)
+            {
+                foreach (FileInfo fileInfo in directoryInfo.EnumerateFiles("*.dll"))
+                {
+                    if (fileInfo.Name.ToUpperInvariant().StartsWith("IMAGEPROCESSOR"))
+                    {
+                        IntPtr pointer;
+                        string targetPath = Path.GetFullPath(Path.Combine(targetBasePath, "..\\" + folder + "\\" + fileInfo.Name));
+                        File.Copy(sourcePath, targetPath, true);
+
+                        try
+                        {
+                            // Load the binary into memory.
+                            pointer = NativeMethods.LoadLibrary(targetPath);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new ApplicationException(ex.Message);
+                        }
+
+                        if (pointer == IntPtr.Zero)
+                        {
+                            throw new ApplicationException("Cannot load " + fileInfo.Name);
+                        }
+
+                        NativeBinaries.Add(pointer);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Frees the reference to the native binaries.
+        /// </summary>
+        private void FreeNativeBinaries()
+        {
+            foreach (IntPtr nativeBinary in NativeBinaries)
+            {
+                // According to http://stackoverflow.com/a/2445558/427899 you need to call this twice.
+                NativeMethods.FreeLibrary(nativeBinary);
+                NativeMethods.FreeLibrary(nativeBinary);
+            }
+        }
+    }
+}
--- src/ImageProcessor/Imaging/Formats/NativeMethods.cs
+++ src/ImageProcessor/Imaging/Formats/NativeMethods.cs
@@ -43,8 +43,82 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// 1 if success, otherwise error code returned in the case of (a) formatting error(s).
         /// </returns>
-        [DllImport("libwebp32.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPGetInfo")]
-        public static extern int WebPGetInfo86(IntPtr data, uint dataSize, out int width, out int height);
+        public static int WebPGetInfo(IntPtr data, uint dataSize, out int width, out int height)
+        {
+            return Is64Bit ? WebPGetInfo64(data, dataSize, out width, out height)
+                           : WebPGetInfo86(data, dataSize, out width, out height);
+        }
+
+        /// <summary>
+        /// Decode WEBP image pointed to by *data and returns BGR samples into a pre-allocated buffer
+        /// </summary>
+        /// <param name="data">
+        /// Pointer to WebP image data
+        /// </param>
+        /// <param name="dataSize">
+        /// This is the size of the memory block pointed to by data containing the image data
+        /// </param>
+        /// <param name="outputBuffer">
+        /// Pointer to decoded WebP image
+        /// </param>
+        /// <param name="outputBufferSize">
+        /// Size of allocated buffer
+        /// </param>
+        /// <param name="outputStride">
+        /// Specifies the distance between scan-lines
+        /// </param>
+        /// <returns>
+        /// output_buffer if function succeeds; NULL otherwise
+        /// </returns>
+        public static IntPtr WebPDecodeBGRAInto(IntPtr data, uint dataSize, IntPtr outputBuffer, int outputBufferSize, int outputStride)
+        {
+            return Is64Bit ? WebPDecodeBGRAInto64(data, dataSize, outputBuffer, outputBufferSize, outputStride)
+                           : WebPDecodeBGRAInto86(data, dataSize, outputBuffer, outputBufferSize, outputStride);
+        }
+
+        /// <summary>
+        /// Lossy encoding images pointed to by *data in WebP format
+        /// </summary>
+        /// <param name="rgb">
+        /// Pointer to RGB image data
+        /// </param>
+        /// <param name="width">
+        /// The width range is limited currently from 1 to 16383
+        /// </param>
+        /// <param name="height">
+        /// The height range is limited currently from 1 to 16383
+        /// </param>
+        /// <param name="stride">
+        /// The stride.
+        /// </param>
+        /// <param name="qualityFactor">
+        /// Ranges from 0 (lower quality) to 100 (highest quality). Controls the loss and quality during compression
+        /// </param>
+        /// <param name="output">
+        /// output_buffer with WebP image
+        /// </param>
+        /// <returns>
+        /// Size of WebP Image
+        /// </returns>
+        public static int WebPEncodeBGRA(IntPtr rgb, int width, int height, int stride, float qualityFactor, out IntPtr output)
+        {
+            return Is64Bit ? WebPEncodeBGRA64(rgb, width, height, stride, qualityFactor, out output)
+                           : WebPEncodeBGRA86(rgb, width, height, stride, qualityFactor, out output);
+        }
+
+        /// <summary>
+        /// Frees the unmanaged memory.
+        /// </summary>
+        /// <param name="pointer">
+        /// The pointer.
+        /// </param>
+        /// <returns>
+        /// 1 if success, otherwise error code returned in the case of (a) error(s).
+        /// </returns>
+        public static int WebPFree(IntPtr pointer)
+        {
+            return Is64Bit ? WebPFree64(pointer) : WebPFree86(pointer);
+        }
 
         /// <summary>
         /// Validate the WebP image header and retrieve the image height and width. Pointers *width and *height can be passed NULL if deemed irrelevant
@@ -64,8 +138,29 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// 1 if success, otherwise error code returned in the case of (a) formatting error(s).
         /// </returns>
-        [DllImport("libwebp64.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPGetInfo")]
-        public static extern int WebPGetInfo64(IntPtr data, uint dataSize, out int width, out int height);
+        [DllImport("x86\\imageprocessor.libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPGetInfo")]
+        private static extern int WebPGetInfo86(IntPtr data, uint dataSize, out int width, out int height);
+
+        /// <summary>
+        /// Validate the WebP image header and retrieve the image height and width. Pointers *width and *height can be passed NULL if deemed irrelevant
+        /// </summary>
+        /// <param name="data">
+        /// Pointer to WebP image data
+        /// </param>
+        /// <param name="dataSize">
+        /// This is the size of the memory block pointed to by data containing the image data
+        /// </param>
+        /// <param name="width">
+        /// The width range is limited currently from 1 to 16383
+        /// </param>
+        /// <param name="height">
+        /// The height range is limited currently from 1 to 16383
+        /// </param>
+        /// <returns>
+        /// 1 if success, otherwise error code returned in the case of (a) formatting error(s).
+        /// </returns>
+        [DllImport("x64\\imageprocessor.libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPGetInfo")]
+        private static extern int WebPGetInfo64(IntPtr data, uint dataSize, out int width, out int height);
 
         /// <summary>
         /// Decode WEBP image pointed to by *data and returns BGR samples into a pre-allocated buffer
@@ -88,8 +183,8 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// output_buffer if function succeeds; NULL otherwise
         /// </returns>
-        [DllImport("libwebp32.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPDecodeBGRAInto")]
-        public static extern IntPtr WebPDecodeBGRAInto86(IntPtr data, uint dataSize, IntPtr outputBuffer, int outputBufferSize, int outputStride);
+        [DllImport("x86\\imageprocessor.libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPDecodeBGRAInto")]
+        private static extern IntPtr WebPDecodeBGRAInto86(IntPtr data, uint dataSize, IntPtr outputBuffer, int outputBufferSize, int outputStride);
 
         /// <summary>
         /// Decode WEBP image pointed to by *data and returns BGR samples into a pre-allocated buffer
@@ -112,8 +207,8 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// output_buffer if function succeeds; NULL otherwise
         /// </returns>
-        [DllImport("libwebp64.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPDecodeBGRAInto")]
-        public static extern IntPtr WebPDecodeBGRAInto64(IntPtr data, uint dataSize, IntPtr outputBuffer, int outputBufferSize, int outputStride);
+        [DllImport("x64\\imageprocessor.libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPDecodeBGRAInto")]
+        private static extern IntPtr WebPDecodeBGRAInto64(IntPtr data, uint dataSize, IntPtr outputBuffer, int outputBufferSize, int outputStride);
 
         /// <summary>
         /// Lossy encoding images pointed to by *data in WebP format
@@ -139,8 +234,8 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// Size of WebP Image
         /// </returns>
-        [DllImport("libwebp32.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPEncodeBGRA")]
-        public static extern int WebPEncodeBGRA86(IntPtr rgb, int width, int height, int stride, float qualityFactor, out IntPtr output);
+        [DllImport("x86\\imageprocessor.libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPEncodeBGRA")]
+        private static extern int WebPEncodeBGRA86(IntPtr rgb, int width, int height, int stride, float qualityFactor, out IntPtr output);
 
         /// <summary>
         /// Lossy encoding images pointed to by *data in WebP format
@@ -166,8 +261,8 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// Size of WebP Image
         /// </returns>
-        [DllImport("libwebp64.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPEncodeBGRA")]
-        public static extern int WebPEncodeBGRA64(IntPtr rgb, int width, int height, int stride, float qualityFactor, out IntPtr output);
+        [DllImport("x64\\imageprocessor.libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPEncodeBGRA")]
+        private static extern int WebPEncodeBGRA64(IntPtr rgb, int width, int height, int stride, float qualityFactor, out IntPtr output);
 
         /// <summary>
         /// Frees the unmanaged memory.
@@ -178,8 +273,8 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// 1 if success, otherwise error code returned in the case of (a) error(s).
         /// </returns>
-        [DllImport("libwebp32.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPFree")]
-        public static extern int WebPFree86(IntPtr pointer);
+        [DllImport("x86\\imageprocessor.libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPFree")]
+        private static extern int WebPFree86(IntPtr pointer);
 
         /// <summary>
         /// Frees the unmanaged memory.
@@ -190,8 +285,8 @@ namespace ImageProcessor.Imaging.Formats
         /// <returns>
         /// 1 if success, otherwise error code returned in the case of (a) error(s).
         /// </returns>
-        [DllImport("libwebp64.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPFree")]
-        public static extern int WebPFree64(IntPtr pointer);
+        [DllImport("x64\\imageprocessor.libwebp.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "WebPFree")]
+        private static extern int WebPFree64(IntPtr pointer);
         #endregion
     }
 }

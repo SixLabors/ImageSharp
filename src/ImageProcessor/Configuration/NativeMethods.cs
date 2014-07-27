10042812498de21e5c5c0db7e220da73033ff4b0
James South <james_south@hotmail.com>
2014-07-27 23:07:11 +0100

    Adding Linux native dll loading
    Former-commit-id: bf4c36486c3e3db4883d766162c4cf5c3c32da74

--- src/ImageProcessor/Configuration/NativeMethods.cs
+++ src/ImageProcessor/Configuration/NativeMethods.cs
@@ -40,5 +40,33 @@ namespace ImageProcessor.Configuration
         /// <returns>If the function succeeds, the return value is nonzero; otherwise zero.</returns>
         [DllImport("kernel32", CharSet = CharSet.Auto)]
         public static extern bool FreeLibrary(IntPtr hModule);
+
+        /// <summary>
+        /// Loads the specified module into the address space of the calling process. 
+        /// The specified module may cause other modules to be loaded.
+        /// </summary>
+        /// <param name="libname">
+        /// The name of the module. This can be either a library module or 
+        /// an executable module. 
+        /// </param>
+        /// <param name="flags">
+        /// The flag indicating whether to load the library immediately or lazily.
+        /// </param>
+        /// <returns>
+        /// If the function succeeds, the return value is a handle to the module; otherwise null.
+        /// </returns>
+        [System.Runtime.InteropServices.DllImport("libdl")]
+        public static extern IntPtr dlopen(string libname, int flags);
+
+        /// <summary>
+        /// Frees the loaded dynamic-link library (DLL) module and, if necessary, decrements its reference count. 
+        /// When the reference count reaches zero, the module is unloaded from the address space of the calling 
+        /// process and the handle is no longer valid.
+        /// </summary>
+        /// <param name="hModule">A handle to the loaded library module. 
+        /// The LoadLibrary, LoadLibraryEx, GetModuleHandle, or GetModuleHandleEx function returns this handle.</param>
+        /// <returns>If the function succeeds, the return value is nonzero; otherwise zero.</returns>
+        [System.Runtime.InteropServices.DllImport("libdl")]
+        public static extern int dlclose(IntPtr hModule);
     }
 }

76b38f3b75221f0f2a62fa45617a543b296a7ced
James South <james_south@hotmail.com>
2014-08-19 18:56:11 +0100

    Better error handling in bootstrapper
    Former-commit-id: 005a7e795e74e6ccb3ca5e3c6a40b4c2770f67e6

--- src/ImageProcessorConsole/Program.cs
+++ src/ImageProcessorConsole/Program.cs
@@ -40,8 +40,8 @@ namespace ImageProcessorConsole
                 di.Create();
             }
 
-			IEnumerable<FileInfo> files = GetFilesByExtensions(di, ".webp");
-            //IEnumerable<FileInfo> files = GetFilesByExtensions(di, ".gif", ".webp", ".bmp", ".jpg", ".png");
+            //IEnumerable<FileInfo> files = GetFilesByExtensions(di, ".jpg");
+            IEnumerable<FileInfo> files = GetFilesByExtensions(di, ".gif", ".webp", ".bmp", ".jpg", ".png");
 
             foreach (FileInfo fileInfo in files)
             {

6528edf44b23040f5a99028c134bbb9dc7032d90
James South <james_south@hotmail.com>
2014-08-19 19:54:57 +0100

    Adding WebP Nuget package
    Former-commit-id: c654cb3e844d25a4d9499dc35e0590e7258c0972

--- build/Build.bat
+++ build/Build.bat
@@ -3,19 +3,22 @@
 SET version=2.0.0.0
 SET webversion=4.0.0.0
 SET webconfigversion=2.0.0.0
+SET webppluginversion=1.0.0.0
 
-ECHO Building ImageProcessor %version%, ImageProcess.Web %webversion% and ImageProcess.Web.Config %webconfigversion%
+ECHO Building ImageProcessor %version%, ImageProcessor.Web %webversion%, ImageProcessor.Web.Config %webconfigversion%, and ImageProcessor.Plugins.WebP %webppluginversion%
 
 ECHO Removing _BuildOutput directory so everything is nice and clean
 RD _BuildOutput /q /s
 
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build.ImageProcessor.proj" /p:BUILD_RELEASE=%version% /p:BUILD_COMMENT=%comment%
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build.ImageProcessor.Web.proj" /p:BUILD_RELEASE=%webversion% /p:BUILD_COMMENT=%comment%
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build.ImageProcessor.Plugins.WebP.proj" /p:BUILD_RELEASE=%webppluginversion% /p:BUILD_COMMENT=%comment%
 
 ECHO Packing the NuGet release files
 ..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.nuspec -Version %version%
 ..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.Web.nuspec -Version %webversion%
 ..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.Web.Config.nuspec -Version %webconfigversion%
+..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.Plugins.WebP.nuspec -Version %webppluginversion%
 PAUSE
 
 IF ERRORLEVEL 1 GOTO :showerror
--- src/Plugins/ImageProcessor/ImageProcessor.Plugins.WebP/Properties/AssemblyInfo.cs
+++ src/Plugins/ImageProcessor/ImageProcessor.Plugins.WebP/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImageProcessor.Plugins.WebP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds support to ImageProcessor for the WebP image format.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("James South")]
 [assembly: AssemblyProduct("ImageProcessor.Plugins.WebP")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  James South")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

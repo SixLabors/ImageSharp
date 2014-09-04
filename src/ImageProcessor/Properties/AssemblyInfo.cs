ef2c12069101da2d3db6b8cd5afd681a70c643cc
James South <james_south@hotmail.com>
2014-09-04 14:38:40 +0100

    ImageProcessor v2.1.0 WebP v1.0.1
    Former-commit-id: 3bff6b668da8a4106e9b749eda099f1ebe53ac13

--- src/ImageProcessor/Properties/AssemblyInfo.cs
+++ src/ImageProcessor/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// --------------------------------------------------------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="James South">
 //   Copyright (c) James South.
 //   Licensed under the Apache License, Version 2.0.
@@ -40,5 +40,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]

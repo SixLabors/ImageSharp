f459c89fcb8d2abbf8c5d8a5b7f5d0dd9eaec66f
James South <james_south@hotmail.com>
2014-09-17 18:22:52 +0100

    Adding YCbCr (not correct yet)
    Former-commit-id: 7da7253860f658b378cd2a57415b0cc693755fa9

--- src/ImageProcessor.UnitTests/Imaging/ColorUnitTests.cs
+++ src/ImageProcessor.UnitTests/Imaging/ColorUnitTests.cs
@@ -10,6 +10,7 @@
 
 namespace ImageProcessor.UnitTests.Imaging
 {
+    using System.Diagnostics.CodeAnalysis;
     using System.Drawing;
     using ImageProcessor.Imaging.Colors;
     using NUnit.Framework;
@@ -45,17 +46,78 @@ namespace ImageProcessor.UnitTests.Imaging
         }
 
         /// <summary>
-        /// Test conversion to and from a HSLA color.
+        /// Test conversion to and from a <see cref="HslaColor"/>.
         /// </summary>
+        /// <param name="expected">
+        /// The expected output.
+        /// </param>
         [Test]
-        public void TestHSLAConversion()
+        [TestCase("#FEFFFE")]
+        [TestCase("#000000")]
+        [TestCase("#CCFF33")]
+        [TestCase("#990000")]
+        [TestCase("#5C955C")]
+        [TestCase("#5C5C95")]
+        [TestCase("#3F3F66")]
+        [TestCase("#FFFFBB")]
+        [TestCase("#FF002B")]
+        [TestCase("#00ABFF")]
+        public void TestHslaConversion(string expected)
         {
-            const string Hex = "#FEFFFE";
-
-            Color color = ColorTranslator.FromHtml(Hex);
+            Color color = ColorTranslator.FromHtml(expected);
             HslaColor hslaColor = HslaColor.FromColor(color);
-            string outPut = ColorTranslator.ToHtml(hslaColor);
-            Assert.AreEqual(Hex, outPut);
+            string result = ColorTranslator.ToHtml(hslaColor);
+            Assert.AreEqual(expected, result);
         }
+
+        /// <summary>
+        /// Test conversion to and from a <see cref="RgbaColor"/>.
+        /// </summary>
+        /// <param name="expected">
+        /// The expected output.
+        /// </param>
+        [Test]
+        [TestCase("#FEFFFE")]
+        [TestCase("#000000")]
+        [TestCase("#CCFF33")]
+        [TestCase("#990000")]
+        [TestCase("#5C955C")]
+        [TestCase("#5C5C95")]
+        [TestCase("#3F3F66")]
+        [TestCase("#FFFFBB")]
+        [TestCase("#FF002B")]
+        [TestCase("#00ABFF")]
+        public void TestRgbaConversion(string expected)
+        {
+            Color color = ColorTranslator.FromHtml(expected);
+            RgbaColor rgbaColor = RgbaColor.FromColor(color);
+            string result = ColorTranslator.ToHtml(rgbaColor);
+            Assert.AreEqual(expected, result);
+        }
+
+        ///// <summary>
+        ///// Test conversion to and from a <see cref="RgbaColor"/>.
+        ///// </summary>
+        ///// <param name="expected">
+        ///// The expected output.
+        ///// </param>
+        //[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "Reviewed. Suppression is OK here."),Test]
+        //[TestCase("#FEFFFE")]
+        //[TestCase("#000000")]
+        //[TestCase("#CCFF33")]
+        //[TestCase("#990000")]
+        //[TestCase("#5C955C")]
+        //[TestCase("#5C5C95")]
+        //[TestCase("#3F3F66")]
+        //[TestCase("#FFFFBB")]
+        //[TestCase("#FF002B")]
+        //[TestCase("#00ABFF")]
+        //public void TestYCbCrConversion(string expected)
+        //{
+        //    Color color = ColorTranslator.FromHtml(expected);
+        //    YCbCrColor yCbCrColor = YCbCrColor.FromColor(color);
+        //    string result = ColorTranslator.ToHtml(yCbCrColor);
+        //    Assert.AreEqual(expected, result);
+        //}
     }
 }

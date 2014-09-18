52bf1dc8d0304f7d3278179486b3d9cfbf8ad16a
James South <james_south@hotmail.com>
2014-09-18 15:39:09 +0100

    Fixing YcbCr
    Former-commit-id: dded3e8a633a37d26d1232adf358910043a52914

--- src/ImageProcessor.UnitTests/Imaging/ColorUnitTests.cs
+++ src/ImageProcessor.UnitTests/Imaging/ColorUnitTests.cs
@@ -25,7 +25,7 @@ namespace ImageProcessor.UnitTests.Imaging
         /// Tests the <see cref="RgbaColor"/> struct equality operators.
         /// </summary>
         [Test]
-        public void TestRGBAEquality()
+        public void TestRgbaEquality()
         {
             RgbaColor first = RgbaColor.FromColor(Color.White);
             RgbaColor second = RgbaColor.FromColor(Color.White);
@@ -34,13 +34,25 @@ namespace ImageProcessor.UnitTests.Imaging
         }
 
         /// <summary>
-        /// Tests the <see cref="RgbaColor"/> struct equality operators.
+        /// Tests the <see cref="HslaColor"/> struct equality operators.
         /// </summary>
         [Test]
-        public void TestHSLAEquality()
+        public void TestHslaEquality()
         {
             HslaColor first = HslaColor.FromColor(Color.White);
             HslaColor second = HslaColor.FromColor(Color.White);
+
+            Assert.AreEqual(first, second);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="YCbCrColor"/> struct equality operators.
+        /// </summary>
+        [Test]
+        public void TestYCbCrEquality()
+        {
+            YCbCrColor first = YCbCrColor.FromColor(Color.White);
+            YCbCrColor second = YCbCrColor.FromColor(Color.White);
 
             Assert.AreEqual(first, second);
         }
@@ -52,9 +64,13 @@ namespace ImageProcessor.UnitTests.Imaging
         /// The expected output.
         /// </param>
         [Test]
+        [TestCase("#FFFFFF")]
         [TestCase("#FEFFFE")]
+        [TestCase("#F0F8FF")]
         [TestCase("#000000")]
         [TestCase("#CCFF33")]
+        [TestCase("#00FF00")]
+        [TestCase("#FF00FF")]
         [TestCase("#990000")]
         [TestCase("#5C955C")]
         [TestCase("#5C5C95")]
@@ -77,9 +93,13 @@ namespace ImageProcessor.UnitTests.Imaging
         /// The expected output.
         /// </param>
         [Test]
+        [TestCase("#FFFFFF")]
         [TestCase("#FEFFFE")]
+        [TestCase("#F0F8FF")]
         [TestCase("#000000")]
         [TestCase("#CCFF33")]
+        [TestCase("#00FF00")]
+        [TestCase("#FF00FF")]
         [TestCase("#990000")]
         [TestCase("#5C955C")]
         [TestCase("#5C5C95")]
@@ -95,29 +115,33 @@ namespace ImageProcessor.UnitTests.Imaging
             Assert.AreEqual(expected, result);
         }
 
-        ///// <summary>
-        ///// Test conversion to and from a <see cref="RgbaColor"/>.
-        ///// </summary>
-        ///// <param name="expected">
-        ///// The expected output.
-        ///// </param>
-        //[SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "Reviewed. Suppression is OK here."),Test]
-        //[TestCase("#FEFFFE")]
-        //[TestCase("#000000")]
-        //[TestCase("#CCFF33")]
-        //[TestCase("#990000")]
-        //[TestCase("#5C955C")]
-        //[TestCase("#5C5C95")]
-        //[TestCase("#3F3F66")]
-        //[TestCase("#FFFFBB")]
-        //[TestCase("#FF002B")]
-        //[TestCase("#00ABFF")]
-        //public void TestYCbCrConversion(string expected)
-        //{
-        //    Color color = ColorTranslator.FromHtml(expected);
-        //    YCbCrColor yCbCrColor = YCbCrColor.FromColor(color);
-        //    string result = ColorTranslator.ToHtml(yCbCrColor);
-        //    Assert.AreEqual(expected, result);
-        //}
+        /// <summary>
+        /// Test conversion to and from a <see cref="RgbaColor"/>.
+        /// </summary>
+        /// <param name="expected">
+        /// The expected output.
+        /// </param>
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "Reviewed. Suppression is OK here."), Test]
+        [TestCase("#FFFFFF")]
+        [TestCase("#FEFFFE")]
+        [TestCase("#F0F8FF")]
+        [TestCase("#000000")]
+        [TestCase("#CCFF33")]
+        [TestCase("#00FF00")]
+        [TestCase("#FF00FF")]
+        [TestCase("#990000")]
+        [TestCase("#5C955C")]
+        [TestCase("#5C5C95")]
+        [TestCase("#3F3F66")]
+        [TestCase("#FFFFBB")]
+        [TestCase("#FF002B")]
+        [TestCase("#00ABFF")]
+        public void TestYCbCrConversion(string expected)
+        {
+            Color color = ColorTranslator.FromHtml(expected);
+            YCbCrColor yCbCrColor = YCbCrColor.FromColor(color);
+            string result = ColorTranslator.ToHtml(yCbCrColor);
+            Assert.AreEqual(expected, result);
+        }
     }
 }

a3ba89e7a04c7a30961f24bbde0cac3a11a269e2
Thomas Broust <tbroust-trepia@ventadis.fr>
2014-09-19 11:25:35 +0200

    Replaces the old-school batch file with a shiny new PSake build script
    Former-commit-id: 1fa7d934e56afe551654425637151a8f1b19bc5f

--- build/Build.bat
+++ build/Build.bat
@@ -1,34 +1,3 @@
-@ECHO OFF
+@echo off
 
-SET version=2.0.1.0
-SET webversion=4.0.0.0
-SET webconfigversion=2.0.0.0
-SET webppluginversion=1.0.1.0
-SET cairpluginversion=1.0.0.0
-
-ECHO Building ImageProcessor %version%, ImageProcessor.Web %webversion%, ImageProcessor.Web.Config %webconfigversion%
-
-ECHO Building ImageProcessor.Plugins.WebP %webppluginversion%, ImageProcessor.Plugins.Cair %cairpluginversion%
-
-ECHO Removing _BuildOutput directory so everything is nice and clean
-RD _BuildOutput /q /s
-
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build.ImageProcessor.proj" /p:BUILD_RELEASE=%version% /p:BUILD_COMMENT=%comment%
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build.ImageProcessor.Web.proj" /p:BUILD_RELEASE=%webversion% /p:BUILD_COMMENT=%comment%
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build.ImageProcessor.Plugins.WebP.proj" /p:BUILD_RELEASE=%webppluginversion% /p:BUILD_COMMENT=%comment%
-%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build.ImageProcessor.Plugins.Cair.proj" /p:BUILD_RELEASE=%cairpluginversion% /p:BUILD_COMMENT=%comment%
-
-ECHO Packing the NuGet release files
-..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.nuspec -Version %version%
-..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.Web.nuspec -Version %webversion%
-..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.Web.Config.nuspec -Version %webconfigversion%
-..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.Plugins.WebP.nuspec -Version %webppluginversion%
-..\src\.nuget\NuGet.exe pack NuSpecs\ImageProcessor.Plugins.Cair.nuspec -Version %cairpluginversion%
-PAUSE
-
-IF ERRORLEVEL 1 GOTO :showerror
-
-GOTO :EOF
-
-:showerror
-PAUSE
+powershell "Import-Module %~dp0\psake.psm1 ; Invoke-Psake %~dp0\build.ps1 ; exit $LASTEXITCODE"

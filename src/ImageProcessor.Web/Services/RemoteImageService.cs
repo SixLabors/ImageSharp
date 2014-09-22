0251c3d5fe8a172fd8a3dc8763d83cc8b7b5194a
James South <james_south@hotmail.com>
2014-09-22 17:20:11 +0100

    Initial commit
    TODO: Remote setting plus further integration.
    
    Former-commit-id: 360cab7a4ce988c81d10657df549496990d059c7

---
+++ src/ImageProcessor.Web/Services/RemoteImageService.cs
@@ -0,0 +1,105 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RemoteImageService.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   The remote image service.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Web.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    using ImageProcessor.Web.Helpers;
+
+    /// <summary>
+    /// The remote image service.
+    /// </summary>
+    public class RemoteImageService : IImageService
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RemoteImageService"/> class.
+        /// </summary>
+        public RemoteImageService()
+        {
+            this.Settings = new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Gets the key for the given implementation.
+        /// <remarks>
+        /// This value is used as a prefix for any image requests that should use this service.
+        /// </remarks>
+        /// </summary>
+        public string Key
+        {
+            get
+            {
+                return "remote.axd";
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the image service requests files from
+        /// the locally based file system.
+        /// </summary>
+        public bool IsFileLocalService
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets any additional settings required by the service.
+        /// </summary>
+        public Dictionary<string, string> Settings { get; set; }
+
+        /// <summary>
+        /// Gets the image using the given identifier.
+        /// </summary>
+        /// <param name="id">
+        /// The value identifying the image to fetch.
+        /// </param>
+        /// <returns>
+        /// The <see cref="System.Byte"/> array containing the image data.
+        /// </returns>
+        public async Task<byte[]> GetImage(object id)
+        {
+            Uri uri = new Uri(id.ToString());
+            RemoteFile remoteFile = new RemoteFile(uri, false);
+            byte[] buffer = { };
+
+            // Prevent response blocking.
+            WebResponse webResponse = await remoteFile.GetWebResponseAsync().ConfigureAwait(false);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (WebResponse response = webResponse)
+                {
+                    using (Stream responseStream = response.GetResponseStream())
+                    {
+                        if (responseStream != null)
+                        {
+                            responseStream.CopyTo(memoryStream);
+
+                            // Reset the position of the stream to ensure we're reading the correct part.
+                            memoryStream.Position = 0;
+
+                            buffer = memoryStream.ToArray();
+                        }
+                    }
+                }
+            }
+
+            return buffer;
+        }
+    }
+}

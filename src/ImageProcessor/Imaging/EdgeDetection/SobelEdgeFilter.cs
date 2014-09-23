e727871f87220ccb7bc2c3b0df91d932b6e0f71c
James South <james_south@hotmail.com>
2014-09-23 23:14:04 +0100

    Edge detection experiment.
    Former-commit-id: 6770319d4943080addcf9a4d6128477963a6bb7a

---
+++ src/ImageProcessor/Imaging/EdgeDetection/SobelEdgeFilter.cs
@@ -0,0 +1,27 @@
+﻿namespace ImageProcessor.Imaging.EdgeDetection
+{
+    public class SobelEdgeFilter : IEdgeFilter
+    {
+        public double[,] HorizontalMatrix
+        {
+            get
+            {
+                return new double[,] 
+                { { -1,  0,  1, }, 
+                  { -2,  0,  2, }, 
+                  { -1,  0,  1, }, };
+            }
+        }
+
+        public double[,] VerticalMatrix
+        {
+            get
+            {
+                return new double[,] 
+                { {  1,  2,  1, }, 
+                  {  0,  0,  0, }, 
+                  { -1, -2, -1, }, };
+            }
+        }
+    }
+}

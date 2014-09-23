44982a33e9b706824567d4387f70a50c800508ed
James South <james_south@hotmail.com>
2014-09-23 23:14:04 +0100

    Edge detection experiment.
    Former-commit-id: ea07e17c2325d6db5a7f85ae489dc96900c41475

---
+++ src/ImageProcessor/Processors/DetectEdges.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessor.Processors
+{
+    using System.Drawing;
+
+    using ImageProcessor.Common.Exceptions;
+    using ImageProcessor.Imaging.EdgeDetection;
+
+    public class DetectEdges : IGraphicsProcessor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DetectEdges"/> class.
+        /// </summary>
+        public DetectEdges()
+        {
+            this.Settings = new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// Gets or sets the dynamic parameter.
+        /// </summary>
+        public dynamic DynamicParameter
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets any additional settings required by the processor.
+        /// </summary>
+        public Dictionary<string, string> Settings
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Processes the image.
+        /// </summary>
+        /// <param name="factory">
+        /// The current instance of the <see cref="T:ImageProcessor.ImageFactory"/> class containing
+        /// the image to process.
+        /// </param>
+        /// <returns>
+        /// The processed image from the current instance of the <see cref="T:ImageProcessor.ImageFactory"/> class.
+        /// </returns>
+        public Image ProcessImage(ImageFactory factory)
+        {
+            Bitmap newImage = null;
+            Image image = factory.Image;
+            IEdgeFilter filter = this.DynamicParameter;
+            try
+            {
+                ConvolutionFilter convolutionFilter = new ConvolutionFilter(filter);
+                newImage = convolutionFilter.ProcessFilter((Bitmap)image);
+
+                image.Dispose();
+                image = newImage;
+            }
+            catch (Exception ex)
+            {
+                if (newImage != null)
+                {
+                    newImage.Dispose();
+                }
+
+                throw new ImageProcessingException("Error processing image with " + this.GetType().Name, ex);
+            }
+
+            return image;
+        }
+    }
+}

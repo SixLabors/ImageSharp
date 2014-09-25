70787cae7fa8e8a1c84bd5e8b69729a8d7e22470
James South <james_south@hotmail.com>
2014-09-25 16:16:32 +0100

    Adding more operators
    Former-commit-id: c7a21e360951af3de40b7e90291858f4db798724

---
+++ src/ImageProcessor/Imaging/EdgeDetection/KayyaliEdgeFilter.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KayyaliEdgeFilter.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   The Kayyali operator filter.
+//   <see href="http://edgedetection.webs.com/" />
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Imaging.EdgeDetection
+{
+    /// <summary>
+    /// The Kayyali operator filter.
+    /// <see href="http://edgedetection.webs.com/"/>
+    /// </summary>
+    public class KayyaliEdgeFilter : IEdgeFilter
+    {
+        /// <summary>
+        /// Gets the horizontal gradient operator.
+        /// </summary>
+        public double[,] HorizontalGradientOperator
+        {
+            get
+            {
+                return new double[,] 
+                {
+                    { 6, 0, -6 }, 
+                    { 0, 0, 0 }, 
+                    { -6, 0, 6 }
+                };
+            }
+        }
+
+        /// <summary>
+        /// Gets the vertical gradient operator.
+        /// </summary>
+        public double[,] VerticalGradientOperator
+        {
+            get
+            {
+                return new double[,] 
+                {
+                    { -6, 0, 6 }, 
+                    { 0, 0, 0 }, 
+                    { 6, 0, -6 }
+                };
+            }
+        }
+    }
+}

7a063b21a4cb83950d77728c1864c50f3ca972df
James South <james_south@hotmail.com>
2014-09-26 15:11:29 +0100

    Fix file name
    Former-commit-id: 29da9dd5c73ef16d7e028933f048c0b11568be3d

--- src/ImageProcessor/Processors/EntropyCrop.cs
+++ src/ImageProcessor/Processors/EntropyCrop.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AutoCrop.cs" company="James South">
+// <copyright file="EntropyCrop.cs" company="James South">
 //   Copyright (c) James South.
 //   Licensed under the Apache License, Version 2.0.
 // </copyright>

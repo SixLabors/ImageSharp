39d7395efea05dfe1dfe2fb7d9830dccff66ea32
Thomas Broust <thomas.broust@gmail.com>
2014-10-02 12:08:57 +0200

    Adds a batch to generate all documentation at once
    Former-commit-id: f23e5faf9a3c092172141fb7a9caa3d11abf5808

---
+++ document.bat
@@ -0,0 +1,5 @@
+.\build\tools\docu\docu.exe .\build\_BuildOutput\ImageProcessor\lib\net45\ImageProcessor.dll --output=.\build\_BuildOutput\Help\docu
+
+.\build\tools\doxygen\doxygen.exe .\Doxyfile
+
+.\build\tools\nocco\nocco.exe .\src\ImageProcessor\*.cs

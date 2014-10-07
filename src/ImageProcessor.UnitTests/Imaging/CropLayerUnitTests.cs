319b61daaad934ba3b9eccbd485c922272deae3e
Thomas Broust <thomas.broust@gmail.com>
2014-10-07 13:56:23 +0200

    Refactors the tests to extract the bitmap tester, so that it can be used by other tests
    Former-commit-id: 8089c857d0304156db615b3c04a93d4bbc68cf18
    Former-commit-id: 3d80374aa22017a633cf27f92376611fb35e57d3

---
+++ src/ImageProcessor.UnitTests/Imaging/CropLayerUnitTests.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CropLayerUnitTests.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ImageProcessor.UnitTests.Imaging
+{
+    using ImageProcessor.Imaging;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test harness for the <see cref="CropLayer" /> class
+    /// </summary>
+    [TestFixture]
+    public class CropLayerUnitTests
+    {
+        /// <summary>
+        /// Tests that the constructor saves the provided data
+        /// </summary>
+        [Test]
+        [TestCase(10.5F, 11.2F, 15.6F, 108.9F, CropMode.Percentage)]
+        [TestCase(15.1F, 20.7F, 65.8F, 156.7F, CropMode.Pixels)]
+        public void ConstructorSavesData(float left, float top, float right, float bottom, CropMode mode)
+        {
+            CropLayer cl = new CropLayer(left, top, right, bottom, mode);
+
+            cl.Left.Should().Be(left);
+            cl.Top.Should().Be(top);
+            cl.Right.Should().Be(right);
+            cl.Bottom.Should().Be(bottom);
+            cl.CropMode.Should().Be(mode);
+        }
+    }
+}

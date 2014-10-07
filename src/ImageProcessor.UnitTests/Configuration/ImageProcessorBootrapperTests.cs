88733076750e96ffda73c6553e4d5577e9c6364f
Thomas Broust <thomas.broust@gmail.com>
2014-10-07 10:04:09 +0200

    Adds unit test for boostrapper
    Former-commit-id: a52ae1a608a1968c34eb4e0d958e609a3f3481aa
    Former-commit-id: b592043e7ba58b60625f0fe344966ef8aa6e65c9

---
+++ src/ImageProcessor.UnitTests/Configuration/ImageProcessorBootrapperTests.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ImageProcessorBootstrapperTests.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ImageProcessor.UnitTests.Configuration
+{
+    using System;
+    using System.Linq;
+
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    using ImageProcessor.Configuration;
+
+    /// <summary>
+    /// Test harness for the ImageProcessor bootstrapper tests
+    /// </summary>
+    [TestFixture]
+    public class ImageProcessorBootrapperTests
+    {
+        [Test]
+        public void Singleton_is_instantiated()
+        {
+            ImageProcessorBootstrapper.Instance.SupportedImageFormats.Count().Should().BeGreaterThan(0, "because there should be supported image formats");
+
+            ImageProcessorBootstrapper.Instance.NativeBinaryFactory.Is64BitEnvironment.Should().Be(Environment.Is64BitProcess);
+        }
+    }
+}

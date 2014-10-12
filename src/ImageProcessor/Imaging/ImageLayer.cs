de77cc17d7b2071270f86a56aa360090dbf90ac3
James South <james_south@hotmail.com>
2014-10-12 23:13:37 +0100

    Adding Image Layer
    Former-commit-id: d373222acce3ea1884db6993330e7775b9347da9
    Former-commit-id: 37c67ed3b3f5ca29a641f11227771cf46711b91f

---
+++ src/ImageProcessor/Imaging/ImageLayer.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace ImageProcessor.Imaging
+{
+    using System.Drawing;
+
+    /// <summary>
+    /// Encapsulates the properties required to add an image layer to an image.
+    /// </summary>
+    public class ImageLayer
+    {
+        /// <summary>
+        /// The opacity at which to render the text.
+        /// </summary>
+        private int opacity = 100;
+
+        /// <summary>
+        /// The position to start creating the text from.
+        /// </summary>
+        private Point position = Point.Empty;
+
+        /// <summary>
+        /// Gets or sets the image.
+        /// </summary>
+        public Image Image { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size.
+        /// </summary>
+        public Size Size { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Opacity of the text layer.
+        /// </summary>
+        public int Opacity
+        {
+            get { return this.opacity; }
+            set { this.opacity = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the Position of the text layer.
+        /// </summary>
+        public Point Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+    }
+}

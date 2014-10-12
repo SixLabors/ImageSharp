08e7cba53428eef0a26c1d55fec03781cc66ded9
James South <james_south@hotmail.com>
2014-10-12 13:00:47 +0100

    Refactoring Resize and adding initial Overlay code
    Former-commit-id: 8a64a99d8ce9e7a5d8f0dd53d3e35b3a3e429ffd
    Former-commit-id: ae1ebe17653c064f157847d7a82226535a27a4c9

--- src/ImageProcessor/Processors/Resize.cs
+++ src/ImageProcessor/Processors/Resize.cs
@@ -13,10 +13,7 @@ namespace ImageProcessor.Processors
     using System;
     using System.Collections.Generic;
     using System.Drawing;
-    using System.Drawing.Drawing2D;
-    using System.Drawing.Imaging;
     using System.Globalization;
-    using System.Linq;
 
     using ImageProcessor.Common.Exceptions;
     using ImageProcessor.Imaging;
@@ -31,6 +28,7 @@ namespace ImageProcessor.Processors
         /// </summary>
         public Resize()
         {
+            this.RestrictedSizes = new List<Size>();
             this.Settings = new Dictionary<string, string>();
         }
 
@@ -69,297 +67,33 @@ namespace ImageProcessor.Processors
         /// </returns>
         public Image ProcessImage(ImageFactory factory)
         {
-            int width = this.DynamicParameter.Size.Width ?? 0;
-            int height = this.DynamicParameter.Size.Height ?? 0;
-            ResizeMode mode = this.DynamicParameter.ResizeMode;
-            AnchorPosition anchor = this.DynamicParameter.AnchorPosition;
-            bool upscale = this.DynamicParameter.Upscale;
-            float[] centerCoordinates = this.DynamicParameter.CenterCoordinates;
-
-            int defaultMaxWidth;
-            int defaultMaxHeight;
-
-            int.TryParse(this.Settings["MaxWidth"], NumberStyles.Any, CultureInfo.InvariantCulture, out defaultMaxWidth);
-            int.TryParse(this.Settings["MaxHeight"], NumberStyles.Any, CultureInfo.InvariantCulture, out defaultMaxHeight);
-
-            return this.ResizeImage(factory, width, height, defaultMaxWidth, defaultMaxHeight, this.RestrictedSizes, mode, anchor, upscale, centerCoordinates);
-        }
-
-        /// <summary>
-        /// The resize image.
-        /// </summary>
-        /// <param name="factory">
-        /// The current instance of the <see cref="T:ImageProcessor.ImageFactory"/> class containing
-        /// the image to process.
-        /// </param>
-        /// <param name="width">
-        /// The width to resize the image to.
-        /// </param>
-        /// <param name="height">
-        /// The height to resize the image to.
-        /// </param>
-        /// <param name="defaultMaxWidth">
-        /// The default max width to resize the image to.
-        /// </param>
-        /// <param name="defaultMaxHeight">
-        /// The default max height to resize the image to.
-        /// </param>
-        /// <param name="restrictedSizes">
-        /// A <see cref="List{Size}"/> containing image resizing restrictions.
-        /// </param>
-        /// <param name="resizeMode">
-        /// The mode with which to resize the image.
-        /// </param>
-        /// <param name="anchorPosition">
-        /// The anchor position to place the image at.
-        /// </param>
-        /// <param name="upscale">
-        /// Whether to allow up-scaling of images. (Default true)
-        /// </param>
-        /// <param name="centerCoordinates">
-        /// If the resize mode is crop, you can set a specific center coordinate, use as alternative to anchorPosition
-        /// </param>
-        /// <returns>
-        /// The processed image from the current instance of the <see cref="T:ImageProcessor.ImageFactory"/> class.
-        /// </returns>
-        private Image ResizeImage(
-            ImageFactory factory,
-            int width,
-            int height,
-            int defaultMaxWidth,
-            int defaultMaxHeight,
-            List<Size> restrictedSizes,
-            ResizeMode resizeMode = ResizeMode.Pad,
-            AnchorPosition anchorPosition = AnchorPosition.Center,
-            bool upscale = true,
-            float[] centerCoordinates = null)
-        {
             Bitmap newImage = null;
             Image image = factory.Image;
 
             try
             {
-                int sourceWidth = image.Width;
-                int sourceHeight = image.Height;
+                ResizeLayer resizeLayer = this.DynamicParameter;
+                
+                // Augment the layer with the extra information.
+                resizeLayer.RestrictedSizes = this.RestrictedSizes;
+                Size maxSize = new Size();
 
-                int destinationWidth = width;
-                int destinationHeight = height;
+                int maxWidth;
+                int maxHeight;
+                int.TryParse(this.Settings["MaxWidth"], NumberStyles.Any, CultureInfo.InvariantCulture, out maxWidth);
+                int.TryParse(this.Settings["MaxHeight"], NumberStyles.Any, CultureInfo.InvariantCulture, out maxHeight);
 
-                int maxWidth = defaultMaxWidth > 0 ? defaultMaxWidth : int.MaxValue;
-                int maxHeight = defaultMaxHeight > 0 ? defaultMaxHeight : int.MaxValue;
+                maxSize.Width = maxHeight;
+                maxSize.Height = maxHeight;
 
-                // Fractional variants for preserving aspect ratio.
-                double percentHeight = Math.Abs(height / (double)sourceHeight);
-                double percentWidth = Math.Abs(width / (double)sourceWidth);
+                resizeLayer.MaxSize = maxSize;
 
-                int destinationX = 0;
-                int destinationY = 0;
+                Resizer resizer = new Resizer(resizeLayer);
+                newImage = (Bitmap)resizer.ResizeImage(image);
 
-                // Change the destination rectangle coordinates if padding and
-                // there has been a set width and height.
-                if (resizeMode == ResizeMode.Pad && width > 0 && height > 0)
-                {
-                    double ratio;
-
-                    if (percentHeight < percentWidth)
-                    {
-                        ratio = percentHeight;
-                        destinationX = Convert.ToInt32((width - (sourceWidth * ratio)) / 2);
-                        destinationWidth = Convert.ToInt32(sourceWidth * percentHeight);
-                    }
-                    else
-                    {
-                        ratio = percentWidth;
-                        destinationY = Convert.ToInt32((height - (sourceHeight * ratio)) / 2);
-                        destinationHeight = Convert.ToInt32(sourceHeight * percentWidth);
-                    }
-                }
-
-                // Change the destination rectangle coordinates if cropping and
-                // there has been a set width and height.
-                if (resizeMode == ResizeMode.Crop && width > 0 && height > 0)
-                {
-                    double ratio;
-
-                    if (percentHeight < percentWidth)
-                    {
-                        ratio = percentWidth;
-
-                        if (centerCoordinates != null && centerCoordinates.Any())
-                        {
-                            double center = -(ratio * sourceHeight) * centerCoordinates[0];
-                            destinationY = (int)center + (height / 2);
-
-                            if (destinationY > 0)
-                            {
-                                destinationY = 0;
-                            }
-
-                            if (destinationY < (int)(height - (sourceHeight * ratio)))
-                            {
-                                destinationY = (int)(height - (sourceHeight * ratio));
-                            }
-                        }
-                        else
-                        {
-                            switch (anchorPosition)
-                            {
-                                case AnchorPosition.Top:
-                                    destinationY = 0;
-                                    break;
-                                case AnchorPosition.Bottom:
-                                    destinationY = (int)(height - (sourceHeight * ratio));
-                                    break;
-                                default:
-                                    destinationY = (int)((height - (sourceHeight * ratio)) / 2);
-                                    break;
-                            }
-                        }
-
-                        destinationHeight = (int)Math.Ceiling(sourceHeight * percentWidth);
-                    }
-                    else
-                    {
-                        ratio = percentHeight;
-
-                        if (centerCoordinates != null && centerCoordinates.Any())
-                        {
-                            double center = -(ratio * sourceWidth) * centerCoordinates[1];
-                            destinationX = (int)center + (width / 2);
-
-                            if (destinationX > 0)
-                            {
-                                destinationX = 0;
-                            }
-
-                            if (destinationX < (int)(width - (sourceWidth * ratio)))
-                            {
-                                destinationX = (int)(width - (sourceWidth * ratio));
-                            }
-                        }
-                        else
-                        {
-                            switch (anchorPosition)
-                            {
-                                case AnchorPosition.Left:
-                                    destinationX = 0;
-                                    break;
-                                case AnchorPosition.Right:
-                                    destinationX = (int)(width - (sourceWidth * ratio));
-                                    break;
-                                default:
-                                    destinationX = (int)((width - (sourceWidth * ratio)) / 2);
-                                    break;
-                            }
-                        }
-
-                        destinationWidth = (int)Math.Ceiling(sourceWidth * percentHeight);
-                    }
-                }
-
-                // Constrain the image to fit the maximum possible height or width.
-                if (resizeMode == ResizeMode.Max)
-                {
-                    // If either is 0, we don't need to figure out orientation
-                    if (width > 0 && height > 0)
-                    {
-                        // Integers must be cast to doubles to get needed precision
-                        double ratio = (double)height / width;
-                        double sourceRatio = (double)sourceHeight / sourceWidth;
-
-                        if (sourceRatio < ratio)
-                        {
-                            height = 0;
-                        }
-                        else
-                        {
-                            width = 0;
-                        }
-                    }
-                }
-
-                // If height or width is not passed we assume that the standard ratio is to be kept.
-                if (height == 0)
-                {
-                    destinationHeight = Convert.ToInt32(sourceHeight * percentWidth);
-                    height = destinationHeight;
-                }
-
-                if (width == 0)
-                {
-                    destinationHeight = Convert.ToInt32(sourceHeight * percentWidth);
-                    width = destinationWidth;
-                }
-
-                // Restrict sizes
-                if (restrictedSizes != null && restrictedSizes.Any())
-                {
-                    bool reject = true;
-                    foreach (Size restrictedSize in restrictedSizes)
-                    {
-                        if (restrictedSize.Height == 0 || restrictedSize.Width == 0)
-                        {
-                            if (restrictedSize.Width == width || restrictedSize.Height == height)
-                            {
-                                reject = false;
-                            }
-                        }
-                        else if (restrictedSize.Width == width && restrictedSize.Height == height)
-                        {
-                            reject = false;
-                        }
-                    }
-
-                    if (reject)
-                    {
-                        return image;
-                    }
-                }
-
-                if (width > 0 && height > 0 && width <= maxWidth && height <= maxHeight)
-                {
-                    // Exit if upscaling is not allowed.
-                    if ((width > sourceWidth || height > sourceHeight) && upscale == false && resizeMode != ResizeMode.Stretch)
-                    {
-                        return image;
-                    }
-
-                    newImage = new Bitmap(width, height, PixelFormat.Format32bppPArgb);
-
-                    using (Graphics graphics = Graphics.FromImage(newImage))
-                    {
-                        // We want to use two different blending algorithms for enlargement/shrinking.
-                        if (image.Width < destinationWidth && image.Height < destinationHeight)
-                        {
-                            // We are making it larger.
-                            graphics.SmoothingMode = SmoothingMode.AntiAlias;
-                        }
-                        else
-                        {
-                            // We are making it smaller.
-                            graphics.SmoothingMode = SmoothingMode.None;
-                        }
-
-                        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                        graphics.CompositingQuality = CompositingQuality.HighQuality;
-
-                        // An unwanted border appears when using InterpolationMode.HighQualityBicubic to resize the image
-                        // as the algorithm appears to be pulling averaging detail from surrounding pixels beyond the edge
-                        // of the image. Using the ImageAttributes class to specify that the pixels beyond are simply mirror
-                        // images of the pixels within solves this problem.
-                        using (ImageAttributes wrapMode = new ImageAttributes())
-                        {
-                            wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                            Rectangle destRect = new Rectangle(destinationX, destinationY, destinationWidth, destinationHeight);
-                            graphics.DrawImage(image, destRect, 0, 0, sourceWidth, sourceHeight, GraphicsUnit.Pixel, wrapMode);
-                        }
-
-                        // Reassign the image.
-                        image.Dispose();
-                        image = newImage;
-                    }
-                }
+                // Reassign the image.
+                image.Dispose();
+                image = newImage;
             }
             catch (Exception ex)
             {

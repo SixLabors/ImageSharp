4df6b439b618db5ec4a8a331ba1a800811378cc6
James South <james_south@hotmail.com>
2014-11-04 19:19:28 +0000

    Begin Wu Quantizer
    Former-commit-id: 8aa426759eabc8845903f8217fff4fd6917b14d1
    Former-commit-id: 4bcfd2269bd88fc66cf958e04da831cf917a6295

---
+++ src/ImageProcessor/Imaging/Quantizers/WuQuantizer/IWuQuantizer.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace nQuant
+{
+    public interface IWuQuantizer
+    {
+        Image QuantizeImage(Bitmap image, int alphaThreshold, int alphaFader);
+    }
+}
---
+++ src/ImageProcessor/Imaging/Quantizers/WuQuantizer/WuQuantizer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace nQuant
+{
+    public class WuQuantizer : WuQuantizerBase, IWuQuantizer
+    {
+         protected override QuantizedPalette GetQuantizedPalette(int colorCount, ColorData data, Box[] cubes, int alphaThreshold)
+         {
+            int pixelsCount = data.Pixels.Length;
+            Lookup[] lookups = BuildLookups(cubes, data);
+
+            var alphas = new int[colorCount + 1];
+            var reds = new int[colorCount + 1];
+            var greens = new int[colorCount + 1];
+            var blues = new int[colorCount + 1];
+            var sums = new int[colorCount + 1];
+            var palette = new QuantizedPalette(pixelsCount);
+
+            IList<Pixel> pixels = data.Pixels;
+
+            Dictionary<int, byte> cachedMaches = new Dictionary<int, byte>();
+
+            for (int pixelIndex = 0; pixelIndex < pixelsCount; pixelIndex++)
+            {
+                Pixel pixel = pixels[pixelIndex];
+
+                if (pixel.Alpha > alphaThreshold)
+                {
+                    byte bestMatch;
+                    int argb = pixel.Argb;
+
+                    if (!cachedMaches.TryGetValue(argb, out bestMatch))
+                    {
+                        int bestDistance = int.MaxValue;
+
+                        for (int lookupIndex = 0; lookupIndex < lookups.Length; lookupIndex++)
+                        {
+                            Lookup lookup = lookups[lookupIndex];
+                            var deltaAlpha = pixel.Alpha - lookup.Alpha;
+                            var deltaRed = pixel.Red - lookup.Red;
+                            var deltaGreen = pixel.Green - lookup.Green;
+                            var deltaBlue = pixel.Blue - lookup.Blue;
+
+                            int distance = deltaAlpha * deltaAlpha + deltaRed * deltaRed + deltaGreen * deltaGreen + deltaBlue * deltaBlue;
+
+                            if (distance >= bestDistance)
+                                continue;
+
+                            bestDistance = distance;
+                            bestMatch = (byte)lookupIndex;
+                        }
+
+                        cachedMaches[argb] = bestMatch;
+                    }
+
+                    alphas[bestMatch] += pixel.Alpha;
+                    reds[bestMatch] += pixel.Red;
+                    greens[bestMatch] += pixel.Green;
+                    blues[bestMatch] += pixel.Blue;
+                    sums[bestMatch]++;
+
+                    palette.PixelIndex[pixelIndex] = bestMatch;
+                }
+                else
+                {
+                    palette.PixelIndex[pixelIndex] = AlphaColor;
+                }
+            }
+
+            for (var paletteIndex = 0; paletteIndex < colorCount; paletteIndex++)
+            {
+                if (sums[paletteIndex] > 0)
+                {
+                    alphas[paletteIndex] /= sums[paletteIndex];
+                    reds[paletteIndex] /= sums[paletteIndex];
+                    greens[paletteIndex] /= sums[paletteIndex];
+                    blues[paletteIndex] /= sums[paletteIndex];
+                }
+
+                var color = Color.FromArgb(alphas[paletteIndex], reds[paletteIndex], greens[paletteIndex], blues[paletteIndex]);
+                palette.Colors.Add(color);
+            }
+
+            palette.Colors.Add(Color.FromArgb(0, 0, 0, 0));
+
+            return palette;
+        }
+    }
+}

9046e98cabb0656a383875d6bf4c477c0126470d
James South <james_south@hotmail.com>
2014-11-06 00:37:42 +0000

    Moar optimisations plus start tidy up
    Former-commit-id: ba829a443adb63d264bb579d2ac24f4e6c70957e
    Former-commit-id: f96810374836964ff35aae91687006373b1beae7

--- src/ImageProcessor/Imaging/Quantizers/WuQuantizer/IWuQuantizer.cs
+++ src/ImageProcessor/Imaging/Quantizers/WuQuantizer/IWuQuantizer.cs
@@ -1,9 +1,40 @@
-﻿using System.Drawing;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IWuQuantizer.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   The WuQuantizer interface.
+//   Adapted from <see href="https://github.com/drewnoakes" />
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-namespace nQuant
+namespace ImageProcessor.Imaging.Quantizers.WuQuantizer
 {
+    using System.Drawing;
+
+    /// <summary>
+    /// The WuQuantizer interface.
+    /// Adapted from <see href="https://github.com/drewnoakes" />
+    /// </summary>
     public interface IWuQuantizer
     {
+        /// <summary>
+        /// Quantizes the given image.
+        /// </summary>
+        /// <param name="image">
+        /// The 32 bit per pixel <see cref="Image"/>.
+        /// </param>
+        /// <param name="alphaThreshold">
+        /// The alpha threshold. All colors with an alpha value less than this will be 
+        /// considered fully transparent
+        /// </param>
+        /// <param name="alphaFader">
+        /// The alpha fader. Alpha values will be normalized to the nearest multiple of this value.
+        /// </param>
+        /// <returns>
+        /// The quantized <see cref="Image"/>.
+        /// </returns>
         Image QuantizeImage(Bitmap image, int alphaThreshold, int alphaFader);
     }
 }

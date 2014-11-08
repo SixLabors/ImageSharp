6fed528f1f0d7856df191b7935d2efa036b1119b
James South <james_south@hotmail.com>
2014-11-08 10:16:06 +0000

    Adding missed interface
    Former-commit-id: 3dcdd087c3e70b7aef92425bc9c6ba0fe96dd292
    Former-commit-id: fd5dd1856cf371005622acebc9e49b3d866f87a0

---
+++ src/ImageProcessor/Imaging/Formats/IQuantizableImageFormat.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IIndexedImageFormat.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   The IQuantizableImageFormat interface for identifying quantizable image formats.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Imaging.Formats
+{
+    using ImageProcessor.Imaging.Quantizers;
+
+    /// <summary>
+    /// The IndexedImageFormat interface for identifying quantizable image formats.
+    /// </summary>
+    public interface IQuantizableImageFormat
+    {
+        /// <summary>
+        /// Gets or sets the quantizer for reducing the image palette.
+        /// </summary>
+        IQuantizer Quantizer { get; set; }
+    }
+}

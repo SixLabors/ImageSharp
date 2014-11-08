57ce024be6ba70285a9705216371460a09687e71
James South <james_south@hotmail.com>
2014-11-08 18:59:24 +0000

    Much better extension parsing.
    Former-commit-id: dca8fe116ea18b6736004a02a867b3ddf51ef6ec
    Former-commit-id: b83ce4b340735af13d8cd0a8ed8484a0d282ba8e

--- src/ImageProcessor/Configuration/NativeBinaryFactory.cs
+++ src/ImageProcessor/Configuration/NativeBinaryFactory.cs
@@ -163,7 +163,7 @@ namespace ImageProcessor.Configuration
             this.Dispose(true);
 
             // This object will be cleaned up by the Dispose method.
-            // Therefore, you should call GC.SupressFinalize to
+            // Therefore, you should call GC.SuppressFinalize to
             // take this object off the finalization queue 
             // and prevent finalization code for this object
             // from executing a second time.

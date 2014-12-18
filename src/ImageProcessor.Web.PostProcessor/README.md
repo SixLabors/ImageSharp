28aae731a02ae63e4be01f671e15f8038f61b4ea
James South <james_south@hotmail.com>
2014-12-18 00:40:53 +0000

    Adding Web PostProcessor project.
    Former-commit-id: e865fcd09f0aa9249ba9f1116fcf027bd3e48e14
    Former-commit-id: 530db416ac1747fb424c93ab286efb673af738c7

---
+++ src/ImageProcessor.Web.PostProcessor/README.md
@@ -0,0 +1,13 @@
+#Resource locations
+
+###gifsicle
+http://www.lcdf.org/gifsicle/
+
+###jpegtran
+http://jpegclub.org/jpegtran/
+
+###optipng
+http://optipng.sourceforge.net/
+
+###pngout
+http://advsys.net/ken/utils.htm
--- src/ImageProcessor.Web/Properties/AssemblyInfo.cs
+++ src/ImageProcessor.Web/Properties/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.1.3.0")]
-[assembly: AssemblyFileVersion("4.1.3.0")]
+[assembly: AssemblyVersion("4.1.4.0")]
+[assembly: AssemblyFileVersion("4.1.4.0")]

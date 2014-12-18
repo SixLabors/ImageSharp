290c2204fb4c56c32e9eebd293a93d7aa1aca050
James South <james_south@hotmail.com>
2014-12-18 11:46:22 +0000

    4.1.4 and 1.0.0
    Updates ImageProcessor.Web to 4.1.4 and add
    ImageProcessor.Web.PostProcessor 1.0.0
    
    
    Former-commit-id: 40a13bd8426a8680eb93b9d676a4d635baad066a
    Former-commit-id: 2e08bf7acea5664ab93d7705d54ac4e896fbebad

--- src/ImageProcessor.Web.PostProcessor/ApplicationEvents.cs
+++ src/ImageProcessor.Web.PostProcessor/ApplicationEvents.cs
@@ -1,10 +1,10 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ImageProcessingModule.cs" company="James South">
+// <copyright file="ApplicationEvents.cs" company="James South">
 //   Copyright (c) James South.
 //   Licensed under the Apache License, Version 2.0.
 // </copyright>
 // <summary>
-//   PostProcesses any image requests within the web application.
+//   Binds the PostProcessor to process any image requests within the web application.
 //   Many thanks to Azure Image Optimizer <see href="https://github.com/ligershark/AzureJobs"/>
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
@@ -12,25 +12,38 @@
 using System.Web;
 
 [assembly: PreApplicationStartMethod(typeof(ImageProcessor.Web.PostProcessor.ApplicationEvents), "Start")]
+
 namespace ImageProcessor.Web.PostProcessor
 {
     using ImageProcessor.Web.Helpers;
     using ImageProcessor.Web.HttpModules;
 
     /// <summary>
-    /// PostProcesses any image requests within the web application.
+    /// Binds the PostProcessor to process any image requests within the web application.
     /// Many thanks to Azure Image Optimizer <see href="https://github.com/ligershark/AzureJobs"/>
     /// </summary>
     public static class ApplicationEvents
     {
+        /// <summary>
+        /// The initial startup method.
+        /// </summary>
         public static void Start()
         {
-            ImageProcessingModule.OnPostProcessing += PostProcess;
+            ImageProcessingModule.OnPostProcessing += PostProcessAsync;
         }
 
-        private static async void PostProcess(object sender, PostProcessingEventArgs e)
+        /// <summary>
+        /// Asynchronously post-processes cached images.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event.
+        /// </param>
+        /// <param name="e">
+        /// An <see cref="PostProcessingEventArgs">EventArgs</see> that contains the event data.
+        /// </param>
+        private static async void PostProcessAsync(object sender, PostProcessingEventArgs e)
         {
-            await PostProcessor.PostProcessImage(e.CachedImagePath);
+            await PostProcessor.PostProcessImageAsync(e.CachedImagePath);
         }
     }
 }

dc0363871bd0095b1a2f14574f323ad89f939f5f
James South <james_south@hotmail.com>
2014-12-18 00:40:53 +0000

    Adding Web PostProcessor project.
    Former-commit-id: e865fcd09f0aa9249ba9f1116fcf027bd3e48e14
    Former-commit-id: 530db416ac1747fb424c93ab286efb673af738c7

---
+++ src/ImageProcessor.Web.PostProcessor/ApplicationEvents.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ImageProcessingModule.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   PostProcesses any image requests within the web application.
+//   Many thanks to Azure Image Optimizer <see href="https://github.com/ligershark/AzureJobs"/>
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Web;
+
+[assembly: PreApplicationStartMethod(typeof(ImageProcessor.Web.PostProcessor.ApplicationEvents), "Start")]
+namespace ImageProcessor.Web.PostProcessor
+{
+    using ImageProcessor.Web.Helpers;
+    using ImageProcessor.Web.HttpModules;
+
+    /// <summary>
+    /// PostProcesses any image requests within the web application.
+    /// Many thanks to Azure Image Optimizer <see href="https://github.com/ligershark/AzureJobs"/>
+    /// </summary>
+    public static class ApplicationEvents
+    {
+        public static void Start()
+        {
+            ImageProcessingModule.OnPostProcessing += PostProcess;
+        }
+
+        private static async void PostProcess(object sender, PostProcessingEventArgs e)
+        {
+            await PostProcessor.PostProcessImage(e.CachedImagePath);
+        }
+    }
+}

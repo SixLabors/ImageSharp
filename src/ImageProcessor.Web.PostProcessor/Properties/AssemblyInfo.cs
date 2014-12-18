9c3720af37764f1967b22daf9f66677fb55b64a3
James South <james_south@hotmail.com>
2014-12-18 11:46:22 +0000

    4.1.4 and 1.0.0
    Updates ImageProcessor.Web to 4.1.4 and add
    ImageProcessor.Web.PostProcessor 1.0.0
    
    
    Former-commit-id: b4673f7535226a904874127d8b07b7a6622f130d
    Former-commit-id: c06846d205f0f651b754e45ba03d3095b33ca58c

--- src/ImageProcessor.Web.PostProcessor/Properties/AssemblyInfo.cs
+++ src/ImageProcessor.Web.PostProcessor/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImageProcessor.Web.PostProcessor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Post-processes cached images to further reduce file size.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("James South")]
 [assembly: AssemblyProduct("ImageProcessor.Web.PostProcessor")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright � James South")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

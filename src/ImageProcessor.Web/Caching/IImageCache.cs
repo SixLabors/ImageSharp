0d4ba02ef515529fbfe80b17c8ee063d0eba74a1
James South <james_south@hotmail.com>
2015-02-16 18:28:56 +0000

    Begin decoupling cache.
    Former-commit-id: 282fdc30f4a813fe8fe759a21debfb6142676f52
    Former-commit-id: 39a6b41dea2323461008da07120c387169913523

---
+++ src/ImageProcessor.Web/Caching/IImageCache.cs
@@ -0,0 +1,26 @@
+﻿
+namespace ImageProcessor.Web.Caching
+{
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Web;
+
+    public interface IImageCache
+    {
+        string CachedPath { get; }
+
+        int MaxAge { get; }
+
+        Task<bool> IsNewOrUpdatedAsync();
+
+        Task AddImageToCacheAsync(Stream stream);
+
+        Task TrimCacheAsync();
+
+        Task<string> CreateCachedFileName();
+
+        void RewritePath(HttpContext context);
+
+        //void SetHeaders(HttpContext context);
+    }
+}

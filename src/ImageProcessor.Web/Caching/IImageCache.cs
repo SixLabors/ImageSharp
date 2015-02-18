12153042d38cd4336b691ff63bc5e6cf7d0f7e4a
James South <james_south@hotmail.com>
2015-02-18 10:46:01 +0000

    Adding code comments
    Former-commit-id: 678bf6525da8ce7752a88361701c2b49f64c1080
    Former-commit-id: 0e7e798aa76710971d62d38c90a1f5817a291d47

--- src/ImageProcessor.Web/Caching/IImageCache.cs
+++ src/ImageProcessor.Web/Caching/IImageCache.cs
@@ -1,4 +1,13 @@
-﻿
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IImageCache.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Defines properties and methods for allowing caching of images to different sources.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace ImageProcessor.Web.Caching
 {
     using System.Collections.Generic;
@@ -6,6 +15,9 @@ namespace ImageProcessor.Web.Caching
     using System.Threading.Tasks;
     using System.Web;
 
+    /// <summary>
+    ///  Defines properties and methods for allowing caching of images to different sources.
+    /// </summary>
     public interface IImageCache
     {
         /// <summary>
@@ -13,20 +25,60 @@ namespace ImageProcessor.Web.Caching
         /// </summary>
         Dictionary<string, string> Settings { get; set; }
 
+        /// <summary>
+        /// Gets the path to the cached image.
+        /// </summary>
         string CachedPath { get; }
 
+        /// <summary>
+        /// Gets the maximum number of days to store the image.
+        /// </summary>
         int MaxDays { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether the image is new or updated in an asynchronous manner.
+        /// </summary>
+        /// <returns>
+        /// The asynchronous <see cref="Task"/> returning the value.
+        /// </returns>
         Task<bool> IsNewOrUpdatedAsync();
 
+        /// <summary>
+        /// Adds the image to the cache in an asynchronous manner.
+        /// </summary>
+        /// <param name="stream">
+        /// The stream containing the image data.
+        /// </param>
+        /// <param name="contentType">
+        /// The content type of the image.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/> representing an asynchronous operation.
+        /// </returns>
         Task AddImageToCacheAsync(Stream stream, string contentType);
 
+        /// <summary>
+        /// Trims the cache of any expired items in an asynchronous manner.
+        /// </summary>
+        /// <returns>
+        /// The asynchronous <see cref="Task"/> representing an asynchronous operation.
+        /// </returns>
         Task TrimCacheAsync();
 
+        /// <summary>
+        /// Gets a string identifying the cached file name.
+        /// </summary>
+        /// <returns>
+        /// The asynchronous <see cref="Task"/> returning the value.
+        /// </returns>
         Task<string> CreateCachedFileName();
 
+        /// <summary>
+        /// Rewrites the path to point to the cached image.
+        /// </summary>
+        /// <param name="context">
+        /// The <see cref="HttpContext"/> encapsulating all information about the request.
+        /// </param>
         void RewritePath(HttpContext context);
-
-        //void SetHeaders(HttpContext context);
     }
 }

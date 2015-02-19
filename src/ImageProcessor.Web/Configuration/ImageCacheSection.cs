4dc07f36d77127fde706884f93aa843030a2fe0f
James South <james_south@hotmail.com>
2015-02-19 17:29:39 +0000

    New Releases
    ImageProcessor v2.2.0.0
    ImageProcessor.Web v4.2.0.0
    ImageProcessor.Web.PostProcessor v1.0.2.0
    ImageProcessor.Web.Config v2.2.0.0
    ImageProcessor.Web.AzureBlobCache v1.0.0.0
    
    
    Former-commit-id: dbc295c25fbcea2d328b8dea80b45742f31fd241
    Former-commit-id: 59bc4ee9d9bbf4c426e8bb35caafb88634266ff3

--- src/ImageProcessor.Web/Configuration/ImageCacheSection.cs
+++ src/ImageProcessor.Web/Configuration/ImageCacheSection.cs
@@ -66,7 +66,7 @@ namespace ImageProcessor.Web.Configuration
                 return imageCacheSection;
             }
 
-            string section = ResourceHelpers.ResourceAsString("ImageProcessor.Web.Configuration.Resources.cache.config");
+            string section = ResourceHelpers.ResourceAsString("ImageProcessor.Web.Configuration.Resources.cache.config.transform");
             XmlReader reader = new XmlTextReader(new StringReader(section));
             imageCacheSection = new ImageCacheSection();
             imageCacheSection.DeserializeSection(reader);

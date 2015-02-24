2af1ed6d5f3103929413b2593656809f95d273f6
Thomas Broust <thomas.broust@gmail.com>
2015-02-24 17:49:20 +0100

    Adds empty classes for rotation
    Former-commit-id: dac0dff3e2b0dc374a36a0d15573bdd36cc3cc17
    Former-commit-id: 90eb30bcae5952f5d7727add156ce443476a631a

---
+++ src/ImageProcessor/Processors/RotateInside.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Rotate.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Encapsulates methods to rotate an image.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Processors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+
+    /// <summary>
+    /// Encapsulates the methods to rotate the inside of an image
+    /// </summary>
+    public class RotateInside : IGraphicsProcessor
+    {
+        /// <summary>
+        /// Gets or sets the DynamicParameter.
+        /// </summary>
+        public dynamic DynamicParameter { get; set; }
+
+        /// <summary>
+        /// Gets or sets any additional settings required by the processor.
+        /// </summary>
+        public Dictionary<string, string> Settings { get; set; }
+
+        /// <summary>
+        /// Processes the image.
+        /// </summary>
+        /// <param name="factory">The current instance of the <see cref="T:ImageProcessor.ImageFactory" /> class containing
+        /// the image to process.</param>
+        /// <returns>
+        /// The processed image from the current instance of the <see cref="T:ImageProcessor.ImageFactory" /> class.
+        /// </returns>
+        public Image ProcessImage(ImageFactory factory)
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+    }
+}

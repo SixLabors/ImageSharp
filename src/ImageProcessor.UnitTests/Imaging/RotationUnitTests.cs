b1caeefb2ae79d2d9059d5cfc23708d75be8c89b
Thomas Broust <thomas.broust@gmail.com>
2015-02-25 10:04:03 +0100

    Changes the method to return a factor instead of a percentage
    Former-commit-id: 0e668b5b5f0b4321eb3386656f53693f6189d51a
    Former-commit-id: b1bc22cb28de7e3d6ff33d018886b67b38625715

--- src/ImageProcessor.UnitTests/Imaging/RotationUnitTests.cs
+++ src/ImageProcessor.UnitTests/Imaging/RotationUnitTests.cs
@@ -18,5 +18,16 @@
             result.Width.Should().Be(expectedWidth, "because the rotated width should have been calculated");
             result.Height.Should().Be(expectedHeight, "because the rotated height should have been calculated");
         }
+
+        [Test]
+        [TestCase(100, 100, 45, 1.41f)]
+        [TestCase(100, 100, 15, 1.22f)]
+        [TestCase(100, 200, 45, 2.12f)]
+        public void RotationZoomIsCalculated(int imageWidth, int imageHeight, float angle, float expected)
+        {
+            float result = Rotation.ZoomAfterRotation(imageWidth, imageHeight, angle);
+
+            result.Should().BeApproximately(expected, 0.01f, "because the zoom level after rotation should have been calculated");
+        }
     }
 }

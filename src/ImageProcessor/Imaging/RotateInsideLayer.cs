23256a22734476d484c0d9cd988397c2b144d612
Thomas Broust <thomas.broust@gmail.com>
2015-02-25 10:51:24 +0100

    Adds a 'rotate layer' to provide a new 'resize' parameter
    Former-commit-id: 66a434058f74a07417c362e272e47a1bfbbbb616
    Former-commit-id: 2ae4e1140ccad579bc0585f13a923f78be3dea06

---
+++ src/ImageProcessor/Imaging/RotateInsideLayer.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ImageLayer.cs" company="James South">
+//   Copyright (c) James South.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Encapsulates the properties required to add an rotation layer to an image.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Imaging
+{
+    /// <summary>
+    /// A rotation layer to apply an inside rotation to an image
+    /// </summary>
+    public class RotateInsideLayer
+    {
+        /// <summary>
+        /// Gets or sets the rotation angle.
+        /// </summary>
+        public float Angle { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to keep the image dimensions.
+        /// If set to true, the image is zoomed inside the area.
+        /// If set to false, the area is resized to match the rotated image.
+        /// </summary>
+        public bool KeepImageDimensions { get; set; }
+    }
+}

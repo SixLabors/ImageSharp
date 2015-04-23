ed4fdd4585cef3a7867591e8bd1ab5ceb26dd204
James South <james_south@hotmail.com>
2015-04-23 11:00:08 +0100

    v2.2.4
    Former-commit-id: 3e954b29e484b52877dd60a236c66304efed28d1
    Former-commit-id: 53040ec892ac114d86e45a42b781c502c10b0ca2
    Former-commit-id: 86a745576a72eab820d800aead0ceb5e98be0297

--- src/ImageProcessor/Properties/AssemblyInfo.cs
+++ src/ImageProcessor/Properties/AssemblyInfo.cs
@@ -41,8 +41,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.3.0")]
-[assembly: AssemblyFileVersion("2.2.3.0")]
+[assembly: AssemblyVersion("2.2.4.0")]
+[assembly: AssemblyFileVersion("2.2.4.0")]
 
 [assembly: InternalsVisibleTo("ImageProcessor.UnitTests")]
 [assembly: InternalsVisibleTo("ImageProcessor.Web")]

b4fda7ed5e877e63d44863f7995ff31cea2cf25a
James South <james_south@hotmail.com>
2015-04-24 18:52:14 +0100

    Fresh start
    Former-commit-id: df0ff1f50d8da42a8d6cf1b22e6b7c6e3ba64c04
    Former-commit-id: 0897a11d33346b3e5c43e1462ab1f73504d540ce
    Former-commit-id: 0ef884ebdd225218d55b88e3627df037761b81c0

--- src/ImageProcessor/Properties/AssemblyInfo.cs
+++ src/ImageProcessor/Properties/AssemblyInfo.cs
@@ -1,48 +1,30 @@
-// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="James South">
-//   Copyright (c) James South.
-//   Licensed under the Apache License, Version 2.0.
-// </copyright>
-// <summary>
-//   AssemblyInfo.cs
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
+﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImageProcessor")]
 [assembly: AssemblyDescription("A library for on-the-fly processing of image files written in C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("James South")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImageProcessor")]
-[assembly: AssemblyCopyright("Copyright � James South")]
+[assembly: AssemblyCopyright("Copyright © James South 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bdaae9bd-0dc8-4b06-8722-e2e0c9a74301")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.4.0")]
-[assembly: AssemblyFileVersion("2.2.4.0")]
-
-[assembly: InternalsVisibleTo("ImageProcessor.UnitTests")]
-[assembly: InternalsVisibleTo("ImageProcessor.Web")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]

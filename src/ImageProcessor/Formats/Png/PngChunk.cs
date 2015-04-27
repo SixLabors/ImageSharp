fd7b03385529ad8d8c192180c8a3e63b0fd8ce3a
James South <james_south@hotmail.com>
2015-04-28 00:07:29 +0100

    Import png encoder and begin gif
    Former-commit-id: da82a46559b9964bf02563bd0dd102bf68f8ff10
    Former-commit-id: a71fb9608d926ad5fc48b6dc780a100641c00763
    Former-commit-id: 4dabf320c3ef95ae3c27f437f7551c2a737c87cc

---
+++ src/ImageProcessor/Formats/Png/PngChunk.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PngChunk.cs" company="James South">
+//   Copyright © James South and contributors.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Stores header information about a chunk.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Formats
+{
+    /// <summary>
+    /// Stores header information about a chunk.
+    /// </summary>
+    internal sealed class PngChunk
+    {
+        /// <summary>
+        /// Gets or sets the length.
+        /// An unsigned integer giving the number of bytes in the chunk's 
+        /// data field. The length counts only the data field, not itself, 
+        /// the chunk type code, or the CRC. Zero is a valid length
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// Gets or sets the chunk type as string with 4 chars.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data bytes appropriate to the chunk type, if any. 
+        /// This field can be of zero length. 
+        /// </summary>
+        public byte[] Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets a CRC (Cyclic Redundancy Check) calculated on the preceding bytes in the chunk, 
+        /// including the chunk type code and chunk data fields, but not including the length field. 
+        /// The CRC is always present, even for chunks containing no data
+        /// </summary>
+        public uint Crc { get; set; }
+    }
+}
---
+++ src/ImageProcessor/Formats/Png/TrueColorReader.cs
@@ -0,0 +1,81 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TrueColorReader.cs" company="James South">
+//   Copyright © James South and contributors.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Color reader for reading true colors from a png file. Only colors
+//   with 24 or 32 bit (3 or 4 bytes) per pixel are supported at the moment.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Formats
+{
+    /// <summary>
+    /// Color reader for reading true colors from a png file. Only colors
+    /// with 24 or 32 bit (3 or 4 bytes) per pixel are supported at the moment.
+    /// </summary>
+    public sealed class TrueColorReader : IColorReader
+    {
+        /// <summary>
+        /// Whether t also read the alpha channel.
+        /// </summary>
+        private readonly bool useAlpha;
+
+        /// <summary>
+        /// The current row.
+        /// </summary>
+        private int row;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrueColorReader"/> class.
+        /// </summary>
+        /// <param name="useAlpha">if set to <c>true</c> the color reader will also read the
+        /// alpha channel from the scanline.</param>
+        public TrueColorReader(bool useAlpha)
+        {
+            this.useAlpha = useAlpha;
+        }
+
+        /// <summary>
+        /// Reads the specified scanline.
+        /// </summary>
+        /// <param name="scanline">The scanline.</param>
+        /// <param name="pixels">The pixels, where the colors should be stored in RGBA format.</param>
+        /// <param name="header">The header, which contains information about the png file, like
+        /// the width of the image and the height.</param>
+        public void ReadScanline(byte[] scanline, byte[] pixels, PngHeader header)
+        {
+            int offset;
+
+            byte[] newScanline = scanline.ToArrayByBitsLength(header.BitDepth);
+
+            if (this.useAlpha)
+            {
+                for (int x = 0; x < newScanline.Length; x += 4)
+                {
+                    offset = ((this.row * header.Width) + (x >> 2)) * 4;
+
+                    pixels[offset + 0] = newScanline[x + 2];
+                    pixels[offset + 1] = newScanline[x + 1];
+                    pixels[offset + 2] = newScanline[x + 0];
+                    pixels[offset + 3] = newScanline[x + 3];
+                }
+            }
+            else
+            {
+                for (int x = 0; x < newScanline.Length / 3; x++)
+                {
+                    offset = ((this.row * header.Width) + x) * 4;
+
+                    pixels[offset + 0] = newScanline[(x * 3) + 2];
+                    pixels[offset + 1] = newScanline[(x * 3) + 1];
+                    pixels[offset + 2] = newScanline[(x * 3) + 0];
+                    pixels[offset + 3] = 255;
+                }
+            }
+
+            this.row++;
+        }
+    }
+}

4c083a3ff9b629a2358a3d45e86a5718057eaf62
James South <james_south@hotmail.com>
2015-04-28 00:07:29 +0100

    Import png encoder and begin gif
    Former-commit-id: 28605ca292741e3de1e36caef5a4a14b5b425618
    Former-commit-id: 45c538d36742adbd6487910e0fba74da1cccf30a
    Former-commit-id: 2d524bbb0d9835d5688c762b215a612cc8d9d530

---
+++ src/ImageProcessor/Formats/Png/PngDecoderCore.cs
@@ -0,0 +1,554 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PngDecoderCore.cs" company="James South">
+//   Copyright © James South and contributors.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Performs the png decoding operation.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Formats
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using ICSharpCode.SharpZipLib.Checksums;
+    using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+
+    /// <summary>
+    /// Performs the png decoding operation.
+    /// </summary>
+    internal class PngDecoderCore
+    {
+        /// <summary>
+        /// The maximum chunk size.
+        /// </summary>
+        private const int MaxChunkSize = 1024 * 1024;
+
+        /// <summary>
+        /// The dictionary of available color types.
+        /// </summary>
+        private static readonly Dictionary<int, PngColorTypeInformation> ColorTypes
+            = new Dictionary<int, PngColorTypeInformation>();
+
+        /// <summary>
+        /// The image to decode.
+        /// </summary>
+        private Image currentImage;
+
+        /// <summary>
+        /// The stream to decode to.
+        /// </summary>
+        private Stream currentStream;
+
+        /// <summary>
+        /// The png header.
+        /// </summary>
+        private PngHeader header;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="PngDecoderCore"/> class.
+        /// </summary>
+        static PngDecoderCore()
+        {
+            ColorTypes.Add(
+                0,
+                new PngColorTypeInformation(
+                    1,
+                    new[] { 1, 2, 4, 8 },
+                    (p, a) => new GrayscaleReader(false)));
+
+            ColorTypes.Add(
+                2,
+                new PngColorTypeInformation(
+                    3,
+                    new[] { 8 },
+                    (p, a) => new TrueColorReader(false)));
+
+            ColorTypes.Add(
+                3,
+                new PngColorTypeInformation(
+                    1,
+                    new[] { 1, 2, 4, 8 },
+                    (p, a) => new PaletteIndexReader(p, a)));
+
+            ColorTypes.Add(
+                4,
+                new PngColorTypeInformation(
+                    2,
+                    new[] { 8 },
+                    (p, a) => new GrayscaleReader(true)));
+
+            ColorTypes.Add(
+                6,
+                new PngColorTypeInformation(
+                    4,
+                    new[] { 8 },
+                    (p, a) => new TrueColorReader(true)));
+        }
+
+        /// <summary>
+        /// Decodes the stream to the image.
+        /// </summary>
+        /// <param name="image">The image to decode to.</param>
+        /// <param name="stream">The stream containing image data. </param>
+        /// <exception cref="ImageFormatException">
+        /// Thrown if the stream does not contain and end chunk.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the image is larger than the maximum allowable size.
+        /// </exception>
+        public void Decode(Image image, Stream stream)
+        {
+            this.currentImage = image;
+            this.currentStream = stream;
+            this.currentStream.Seek(8, SeekOrigin.Current);
+
+            bool isEndChunkReached = false;
+
+            byte[] palette = null;
+            byte[] paletteAlpha = null;
+
+            using (MemoryStream dataStream = new MemoryStream())
+            {
+                PngChunk currentChunk;
+                while ((currentChunk = this.ReadChunk()) != null)
+                {
+                    if (isEndChunkReached)
+                    {
+                        throw new ImageFormatException("Image does not end with end chunk.");
+                    }
+
+                    if (currentChunk.Type == PngChunkTypes.Header)
+                    {
+                        this.ReadHeaderChunk(currentChunk.Data);
+                        this.ValidateHeader();
+                    }
+                    else if (currentChunk.Type == PngChunkTypes.Physical)
+                    {
+                        this.ReadPhysicalChunk(currentChunk.Data);
+                    }
+                    else if (currentChunk.Type == PngChunkTypes.Data)
+                    {
+                        dataStream.Write(currentChunk.Data, 0, currentChunk.Data.Length);
+                    }
+                    else if (currentChunk.Type == PngChunkTypes.Palette)
+                    {
+                        palette = currentChunk.Data;
+                    }
+                    else if (currentChunk.Type == PngChunkTypes.PaletteAlpha)
+                    {
+                        paletteAlpha = currentChunk.Data;
+                    }
+                    else if (currentChunk.Type == PngChunkTypes.Text)
+                    {
+                        this.ReadTextChunk(currentChunk.Data);
+                    }
+                    else if (currentChunk.Type == PngChunkTypes.End)
+                    {
+                        isEndChunkReached = true;
+                    }
+                }
+
+                if (this.header.Width > ImageBase.MaxWidth || this.header.Height > ImageBase.MaxHeight)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        string.Format(
+                            "The input png '{0}x{1}' is bigger then the max allowed size '{2}x{3}'",
+                            this.header.Width,
+                            this.header.Height,
+                            ImageBase.MaxWidth,
+                            ImageBase.MaxHeight));
+                }
+
+                byte[] pixels = new byte[this.header.Width * this.header.Height * 4];
+
+                PngColorTypeInformation colorTypeInformation = ColorTypes[this.header.ColorType];
+
+                if (colorTypeInformation != null)
+                {
+                    IColorReader colorReader = colorTypeInformation.CreateColorReader(palette, paletteAlpha);
+
+                    this.ReadScanlines(dataStream, pixels, colorReader, colorTypeInformation);
+                }
+
+                image.SetPixels(this.header.Width, this.header.Height, pixels);
+            }
+        }
+
+        /// <summary>
+        /// Reads the data chunk containing physical dimension data.
+        /// </summary>
+        /// <param name="data">The data containing physical data.</param>
+        private void ReadPhysicalChunk(byte[] data)
+        {
+            Array.Reverse(data, 0, 4);
+            Array.Reverse(data, 4, 4);
+
+            this.currentImage.DensityX = BitConverter.ToInt32(data, 0) / 39.3700787d;
+            this.currentImage.DensityY = BitConverter.ToInt32(data, 4) / 39.3700787d;
+        }
+
+        /// <summary>
+        /// Calculates the scanline length.
+        /// </summary>
+        /// <param name="colorTypeInformation">The color type information.</param>
+        /// <returns>The <see cref="int"/> representing the length.</returns>
+        private int CalculateScanlineLength(PngColorTypeInformation colorTypeInformation)
+        {
+            int scanlineLength = this.header.Width * this.header.BitDepth * colorTypeInformation.ChannelsPerColor;
+
+            int amount = scanlineLength % 8;
+            if (amount != 0)
+            {
+                scanlineLength += 8 - amount;
+            }
+
+            return scanlineLength / 8;
+        }
+
+        /// <summary>
+        /// Calculates a scanline step.
+        /// </summary>
+        /// <param name="colorTypeInformation">The color type information.</param>
+        /// <returns>The <see cref="int"/> representing the length of each step.</returns>
+        private int CalculateScanlineStep(PngColorTypeInformation colorTypeInformation)
+        {
+            int scanlineStep = 1;
+
+            if (this.header.BitDepth >= 8)
+            {
+                scanlineStep = (colorTypeInformation.ChannelsPerColor * this.header.BitDepth) / 8;
+            }
+
+            return scanlineStep;
+        }
+
+        /// <summary>
+        /// Reads the scanlines within the image.
+        /// </summary>
+        /// <param name="dataStream">The <see cref="MemoryStream"/> containing data.</param>
+        /// <param name="pixels">
+        /// The <see cref="T:byte[]"/> containing pixel data.</param>
+        /// <param name="colorReader">The color reader.</param>
+        /// <param name="colorTypeInformation">The color type information.</param>
+        private void ReadScanlines(MemoryStream dataStream, byte[] pixels, IColorReader colorReader, PngColorTypeInformation colorTypeInformation)
+        {
+            dataStream.Position = 0;
+
+            int scanlineLength = this.CalculateScanlineLength(colorTypeInformation);
+            int scanlineStep = this.CalculateScanlineStep(colorTypeInformation);
+
+            byte[] lastScanline = new byte[scanlineLength];
+            byte[] currentScanline = new byte[scanlineLength];
+            int filter = 0, column = -1;
+
+            using (InflaterInputStream compressedStream = new InflaterInputStream(dataStream))
+            {
+                int readByte;
+                while ((readByte = compressedStream.ReadByte()) >= 0)
+                {
+                    if (column == -1)
+                    {
+                        filter = readByte;
+
+                        column++;
+                    }
+                    else
+                    {
+                        currentScanline[column] = (byte)readByte;
+
+                        byte a;
+                        byte b;
+                        byte c;
+
+                        if (column >= scanlineStep)
+                        {
+                            a = currentScanline[column - scanlineStep];
+                            c = lastScanline[column - scanlineStep];
+                        }
+                        else
+                        {
+                            a = 0;
+                            c = 0;
+                        }
+
+                        b = lastScanline[column];
+
+                        if (filter == 1)
+                        {
+                            currentScanline[column] = (byte)(currentScanline[column] + a);
+                        }
+                        else if (filter == 2)
+                        {
+                            currentScanline[column] = (byte)(currentScanline[column] + b);
+                        }
+                        else if (filter == 3)
+                        {
+                            currentScanline[column] = (byte)(currentScanline[column] + (byte)((a + b) / 2));
+                        }
+                        else if (filter == 4)
+                        {
+                            currentScanline[column] = (byte)(currentScanline[column] + PathPredicator(a, b, c));
+                        }
+
+                        column++;
+
+                        if (column == scanlineLength)
+                        {
+                            colorReader.ReadScanline(currentScanline, pixels, this.header);
+                            column = -1;
+
+                            Utils.Swap(ref currentScanline, ref lastScanline);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// TODO: Document this
+        /// </summary>
+        /// <param name="a">The a.</param>
+        /// <param name="b">The b.</param>
+        /// <param name="c">The c.</param>
+        /// <returns>
+        /// The <see cref="byte"/>.
+        /// </returns>
+        private static byte PathPredicator(byte a, byte b, byte c)
+        {
+            byte predicator;
+
+            int p = a + b - c;
+            int pa = Math.Abs(p - a);
+            int pb = Math.Abs(p - b);
+            int pc = Math.Abs(p - c);
+
+            if (pa <= pb && pa <= pc)
+            {
+                predicator = a;
+            }
+            else if (pb <= pc)
+            {
+                predicator = b;
+            }
+            else
+            {
+                predicator = c;
+            }
+
+            return predicator;
+        }
+
+        /// <summary>
+        /// Reads a text chunk containing image properties from the data.
+        /// </summary>
+        /// <param name="data">The <see cref="T:byte[]"/> containing  data.</param>
+        private void ReadTextChunk(byte[] data)
+        {
+            int zeroIndex = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == 0)
+                {
+                    zeroIndex = i;
+                    break;
+                }
+            }
+
+            string name = System.Text.Encoding.Unicode.GetString(data, 0, zeroIndex);
+            string value = System.Text.Encoding.Unicode.GetString(data, zeroIndex + 1, data.Length - zeroIndex - 1);
+
+            this.currentImage.Properties.Add(new ImageProperty(name, value));
+        }
+
+        /// <summary>
+        /// Reads a header chunk from the data.
+        /// </summary>
+        /// <param name="data">The <see cref="T:byte[]"/> containing  data.</param>
+        private void ReadHeaderChunk(byte[] data)
+        {
+            this.header = new PngHeader();
+
+            Array.Reverse(data, 0, 4);
+            Array.Reverse(data, 4, 4);
+
+            this.header.Width = BitConverter.ToInt32(data, 0);
+            this.header.Height = BitConverter.ToInt32(data, 4);
+
+            this.header.BitDepth = data[8];
+            this.header.ColorType = data[9];
+            this.header.FilterMethod = data[11];
+            this.header.InterlaceMethod = data[12];
+            this.header.CompressionMethod = data[10];
+        }
+
+        /// <summary>
+        /// Validates the png header.
+        /// </summary>
+        /// <exception cref="ImageFormatException">
+        /// Thrown if the image does pass validation.
+        /// </exception>
+        private void ValidateHeader()
+        {
+            if (!ColorTypes.ContainsKey(this.header.ColorType))
+            {
+                throw new ImageFormatException("Color type is not supported or not valid.");
+            }
+
+            if (!ColorTypes[this.header.ColorType].SupportedBitDepths.Contains(this.header.BitDepth))
+            {
+                throw new ImageFormatException("Bit depth is not supported or not valid.");
+            }
+
+            if (this.header.FilterMethod != 0)
+            {
+                throw new ImageFormatException("The png specification only defines 0 as filter method.");
+            }
+
+            if (this.header.InterlaceMethod != 0)
+            {
+                throw new ImageFormatException("Interlacing is not supported.");
+            }
+        }
+
+        /// <summary>
+        /// Reads a chunk from the stream.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="PngChunk"/>.
+        /// </returns>
+        private PngChunk ReadChunk()
+        {
+            PngChunk chunk = new PngChunk();
+
+            if (this.ReadChunkLength(chunk) == 0)
+            {
+                return null;
+            }
+
+            byte[] typeBuffer = this.ReadChunkType(chunk);
+
+            this.ReadChunkData(chunk);
+            this.ReadChunkCrc(chunk, typeBuffer);
+
+            return chunk;
+        }
+
+        /// <summary>
+        /// Reads the cycle redundancy chunk from the data.
+        /// </summary>
+        /// <param name="chunk">The chunk.</param>
+        /// <param name="typeBuffer">The type buffer.</param>
+        /// <exception cref="ImageFormatException">
+        /// Thrown if the input stream is not valid or corrupt.
+        /// </exception>
+        private void ReadChunkCrc(PngChunk chunk, byte[] typeBuffer)
+        {
+            byte[] crcBuffer = new byte[4];
+
+            int numBytes = this.currentStream.Read(crcBuffer, 0, 4);
+            if (numBytes.IsBetween(1, 3))
+            {
+                throw new ImageFormatException("Image stream is not valid!");
+            }
+
+            Array.Reverse(crcBuffer);
+
+            chunk.Crc = BitConverter.ToUInt32(crcBuffer, 0);
+
+            Crc32 crc = new Crc32();
+            crc.Update(typeBuffer);
+            crc.Update(chunk.Data);
+
+            if (crc.Value != chunk.Crc)
+            {
+                throw new ImageFormatException("CRC Error. PNG Image chunk is corrupt!");
+            }
+        }
+
+        /// <summary>
+        /// Reads the chunk data from the stream.
+        /// </summary>
+        /// <param name="chunk">The chunk.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if the chunk length exceeds the maximum allowable size.
+        /// </exception>
+        private void ReadChunkData(PngChunk chunk)
+        {
+            if (chunk.Length > MaxChunkSize)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format(
+                    "Png chunk size '{0}' exceeds the '{1}'",
+                    chunk.Length,
+                    MaxChunkSize));
+            }
+
+            chunk.Data = new byte[chunk.Length];
+            this.currentStream.Read(chunk.Data, 0, chunk.Length);
+        }
+
+        /// <summary>
+        /// Identifies the chunk type from the chunk.
+        /// </summary>
+        /// <param name="chunk">The chunk.</param>
+        /// <returns>
+        /// The <see cref="T:byte[]"/> containing identifying information.
+        /// </returns>
+        /// <exception cref="ImageFormatException">
+        /// Thrown if the input stream is not valid.
+        /// </exception>
+        private byte[] ReadChunkType(PngChunk chunk)
+        {
+            byte[] typeBuffer = new byte[4];
+
+            int numBytes = this.currentStream.Read(typeBuffer, 0, 4);
+            if (numBytes.IsBetween(1, 3))
+            {
+                throw new ImageFormatException("Image stream is not valid!");
+            }
+
+            char[] chars = new char[4];
+            chars[0] = (char)typeBuffer[0];
+            chars[1] = (char)typeBuffer[1];
+            chars[2] = (char)typeBuffer[2];
+            chars[3] = (char)typeBuffer[3];
+
+            chunk.Type = new string(chars);
+
+            return typeBuffer;
+        }
+
+        /// <summary>
+        /// Calculates the length of the given chunk.
+        /// </summary>
+        /// <param name="chunk">he chunk.</param>
+        /// <returns>
+        /// The <see cref="int"/> representing the chunk length.
+        /// </returns>
+        /// <exception cref="ImageFormatException">
+        /// Thrown if the input stream is not valid.
+        /// </exception>
+        private int ReadChunkLength(PngChunk chunk)
+        {
+            byte[] lengthBuffer = new byte[4];
+
+            int numBytes = this.currentStream.Read(lengthBuffer, 0, 4);
+            if (numBytes.IsBetween(1, 3))
+            {
+                throw new ImageFormatException("Image stream is not valid!");
+            }
+
+            Array.Reverse(lengthBuffer);
+
+            chunk.Length = BitConverter.ToInt32(lengthBuffer, 0);
+
+            return numBytes;
+        }
+    }
+}
---
+++ src/ImageProcessor/ImageProperty.cs
@@ -0,0 +1,167 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ImageProperty.cs" company="James South">
+//   Copyright © James South and contributors.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Stores meta information about a image, like the name of the author,
+//   the copyright information, the date, where the image was created
+//   or some other information.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor
+{
+    using System;
+
+    /// <summary>
+    /// Stores meta information about a image, like the name of the author,
+    /// the copyright information, the date, where the image was created
+    /// or some other information.
+    /// </summary>
+    public struct ImageProperty : IEquatable<ImageProperty>
+    {
+        /// <summary>
+        /// The name of this <see cref="ImageProperty"/> indicating which kind of 
+        /// information this property stores.
+        /// </summary>
+        /// <example>
+        /// Typical properties are the author, copyright
+        /// information or other meta information.
+        /// </example>
+        public string Name;
+
+        /// <summary>
+        /// The value of this <see cref="ImageProperty"/>.
+        /// </summary>
+        public string Value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageProperty"/> struct.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the property.
+        /// </param>
+        /// <param name="value">
+        /// The value of the property.
+        /// </param>
+        public ImageProperty(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Compares two <see cref="ImageProperty"/> objects. The result specifies whether the values
+        /// of the <see cref="ImageProperty.Name"/> or <see cref="ImageProperty.Value"/> properties of the two
+        /// <see cref="ImageProperty"/> objects are equal.
+        /// </summary>
+        /// <param name="left">
+        /// The <see cref="ImageProperty"/> on the left side of the operand.
+        /// </param>
+        /// <param name="right">
+        /// The <see cref="ImageProperty"/> on the right side of the operand.
+        /// </param>
+        /// <returns>
+        /// True if the current left is equal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        public static bool operator ==(ImageProperty left, ImageProperty right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Compares two <see cref="ImageProperty"/> objects. The result specifies whether the values
+        /// of the <see cref="ImageProperty.Name"/> or <see cref="ImageProperty.Value"/> properties of the two
+        /// <see cref="ImageProperty"/> objects are unequal.
+        /// </summary>
+        /// <param name="left">
+        /// The <see cref="ImageProperty"/> on the left side of the operand.
+        /// </param>
+        /// <param name="right">
+        /// The <see cref="ImageProperty"/> on the right side of the operand.
+        /// </param>
+        /// <returns>
+        /// True if the current left is unequal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        public static bool operator !=(ImageProperty left, ImageProperty right)
+        {
+            return !left.Equals(right);
+        }
+
+        /// <summary>
+        /// Indicates whether this instance and a specified object are equal.
+        /// </summary>
+        /// <param name="obj">
+        /// The object to compare with the current instance. 
+        /// </param>
+        /// <returns>
+        /// true if <paramref name="obj"/> and this instance are the same type and represent the 
+        /// same value; otherwise, false. 
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ImageProperty))
+            {
+                return false;
+            }
+
+            ImageProperty other = (ImageProperty)obj;
+
+            return other.Name == this.Name && other.Value == this.Value;
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A 32-bit signed integer that is the hash code for this instance.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.GetHashCode(this);
+        }
+
+        /// <summary>
+        /// Returns the fully qualified type name of this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.String"/> containing a fully qualified type name.
+        /// </returns>
+        public override string ToString()
+        {
+            return "{Name=" + this.Name + ",Value=" + this.Value + "}";
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <returns>
+        /// True if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(ImageProperty other)
+        {
+            return this.Name.Equals(other.Name) && this.Value.Equals(other.Value);
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <param name="imageProperty">
+        /// The instance of <see cref="ImageProperty"/> to return the hash code for.
+        /// </param>
+        /// <returns>
+        /// A 32-bit signed integer that is the hash code for this instance.
+        /// </returns>
+        private int GetHashCode(ImageProperty imageProperty)
+        {
+            unchecked
+            {
+                int hashCode = imageProperty.Name.GetHashCode();
+                hashCode = (hashCode * 397) ^ imageProperty.Value.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}

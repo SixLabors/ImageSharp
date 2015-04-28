45898e9827ba16bd392c6d4db19fb7c6354fa886
James South <james_south@hotmail.com>
2015-04-28 20:51:49 +0100

    Import and cleanup bitmap format.
    Former-commit-id: 3695394604ca491864f134a6f0e95d288ec9eb12
    Former-commit-id: bd3dcffd03c9ff0c75f231cba67fb07375fd0ca0
    Former-commit-id: 584a931185b0f4d0d86f33d9a14018381599d62b

---
+++ src/ImageProcessor/Formats/Bmp/BmpDecoder.cs
@@ -0,0 +1,94 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BmpDecoder.cs" company="James South">
+//   Copyright © James South and contributors.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Image decoder for generating an image out of a Windows bitmap stream.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Formats
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Image decoder for generating an image out of a Windows bitmap stream.
+    /// </summary>
+    /// <remarks>
+    /// Does not support the following formats at the moment:
+    /// <list type="bullet">
+    ///    <item>JPG</item>
+    ///    <item>PNG</item>
+    ///    <item>RLE4</item>
+    ///    <item>RLE8</item>
+    ///    <item>BitFields</item>
+    /// </list>
+    /// Formats will be supported in a later releases. We advise always 
+    /// to use only 24 Bit Windows bitmaps.
+    /// </remarks>
+    public class BmpDecoder : IImageDecoder
+    {
+        /// <summary>
+        /// Gets the size of the header for this image type.
+        /// </summary>
+        /// <value>The size of the header.</value>
+        public int HeaderSize
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether the <see cref="IImageDecoder"/> supports the specified
+        /// file header.
+        /// </summary>
+        /// <param name="extension">The <see cref="string"/> containing the file extension.</param>
+        /// <returns>
+        /// True if the decoder supports the file extension; otherwise, false.
+        /// </returns>
+        public bool IsSupportedFileExtension(string extension)
+        {
+            Guard.NotNullOrEmpty(extension, "extension");
+
+            extension = extension.StartsWith(".") ? extension.Substring(1) : extension;
+
+            return extension.Equals("BMP", StringComparison.OrdinalIgnoreCase)
+                || extension.Equals("DIP", StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether the <see cref="IImageDecoder"/> supports the specified
+        /// file header.
+        /// </summary>
+        /// <param name="header">The <see cref="T:byte[]"/> containing the file header.</param>
+        /// <returns>
+        /// True if the decoder supports the file header; otherwise, false.
+        /// </returns>
+        public bool IsSupportedFileFormat(byte[] header)
+        {
+            bool isBmp = false;
+            if (header.Length >= 2)
+            {
+                isBmp =
+                    header[0] == 0x42 && // B
+                    header[1] == 0x4D;   // M
+            }
+
+            return isBmp;
+        }
+
+        /// <summary>
+        /// Decodes the image from the specified stream to the <see cref="ImageBase"/>.
+        /// </summary>
+        /// <param name="image">The <see cref="ImageBase"/> to decode to.</param>
+        /// <param name="stream">The <see cref="Stream"/> containing image data.</param>
+        public void Decode(Image image, Stream stream)
+        {
+            new BmpDecoderCore().Decode(image, stream);
+        }
+    }
+}
---
+++ src/ImageProcessor/Formats/Bmp/BmpEncoder.cs
@@ -0,0 +1,180 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BmpEncoder.cs" company="James South">
+//   Copyright © James South and contributors.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Image encoder for writing an image to a stream as a Windows bitmap.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Formats
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Image encoder for writing an image to a stream as a Windows bitmap.
+    /// </summary>
+    /// <remarks>The encoder can currently only write 24-bit rgb images to streams.</remarks>
+    public class BmpEncoder : IImageEncoder
+    {
+        /// <summary>
+        /// Gets or sets the quality of output for images.
+        /// </summary>
+        /// <remarks>Png is a lossless format so this is not used in this encoder.</remarks>
+        public int Quality { get; set; }
+
+        /// <summary>
+        /// Gets the default file extension for this encoder.
+        /// </summary>
+        public string Extension
+        {
+            get { return "BMP"; }
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether the <see cref="IImageEncoder"/> supports the specified
+        /// file header.
+        /// </summary>
+        /// <param name="extension">The <see cref="string"/> containing the file extension.</param>
+        /// <returns>
+        /// True if the decoder supports the file extension; otherwise, false.
+        /// </returns>
+        public bool IsSupportedFileExtension(string extension)
+        {
+            Guard.NotNullOrEmpty(extension, "extension");
+
+            extension = extension.StartsWith(".") ? extension.Substring(1) : extension;
+
+            return extension.Equals("BMP", StringComparison.OrdinalIgnoreCase)
+                || extension.Equals("DIP", StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Encodes the image to the specified stream from the <see cref="ImageBase"/>.
+        /// </summary>
+        /// <param name="image">The <see cref="ImageBase"/> to encode from.</param>
+        /// <param name="stream">The <see cref="Stream"/> to encode the image data to.</param>
+        public void Encode(ImageBase image, Stream stream)
+        {
+            Guard.NotNull(image, "image");
+            Guard.NotNull(stream, "stream");
+
+            int rowWidth = image.Width;
+
+            int amount = (image.Width * 3) % 4;
+            if (amount != 0)
+            {
+                rowWidth += 4 - amount;
+            }
+
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            BmpFileHeader fileHeader = new BmpFileHeader
+            {
+                Type = 19778,
+                Offset = 54,
+                FileSize = 54 + (image.Height * rowWidth * 3)
+            };
+            WriteHeader(writer, fileHeader);
+
+            BmpInfoHeader infoHeader = new BmpInfoHeader
+            {
+                HeaderSize = 40,
+                Height = image.Height,
+                Width = image.Width,
+                BitsPerPixel = 24,
+                Planes = 1,
+                Compression = BmpCompression.RGB,
+                ImageSize = image.Height * rowWidth * 3,
+                ClrUsed = 0,
+                ClrImportant = 0
+            };
+
+            WriteInfo(writer, infoHeader);
+
+            WriteImage(writer, image);
+
+            writer.Flush();
+        }
+
+        /// <summary>
+        /// Writes the pixel data to the binary stream.
+        /// </summary>
+        /// <param name="writer">
+        /// The <see cref="BinaryWriter"/> containing the stream to write to.
+        /// </param>
+        /// <param name="image">
+        /// The <see cref="ImageBase"/> containing pixel data.
+        /// </param>
+        private static void WriteImage(BinaryWriter writer, ImageBase image)
+        {
+            int amount = (image.Width * 3) % 4;
+            if (amount != 0)
+            {
+                amount = 4 - amount;
+            }
+
+            byte[] data = image.Pixels;
+
+            for (int y = image.Height - 1; y >= 0; y--)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    int offset = ((y * image.Width) + x) * 4;
+
+                    writer.Write(data[offset + 0]);
+                    writer.Write(data[offset + 1]);
+                    writer.Write(data[offset + 2]);
+                }
+
+                for (int i = 0; i < amount; i++)
+                {
+                    writer.Write((byte)0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes the bitmap header data to the binary stream.
+        /// </summary>
+        /// <param name="writer">
+        /// The <see cref="BinaryWriter"/> containing the stream to write to.
+        /// </param>
+        /// <param name="fileHeader">
+        /// The <see cref="BmpFileHeader"/> containing the header data.
+        /// </param>
+        private static void WriteHeader(BinaryWriter writer, BmpFileHeader fileHeader)
+        {
+            writer.Write(fileHeader.Type);
+            writer.Write(fileHeader.FileSize);
+            writer.Write(fileHeader.Reserved);
+            writer.Write(fileHeader.Offset);
+        }
+
+        /// <summary>
+        /// Writes the bitmap information to the binary stream.
+        /// </summary>
+        /// <param name="writer">
+        /// The <see cref="BinaryWriter"/> containing the stream to write to.
+        /// </param>
+        /// <param name="infoHeader">
+        /// The <see cref="BmpFileHeader"/> containing the detailed information about the image.
+        /// </param>
+        private static void WriteInfo(BinaryWriter writer, BmpInfoHeader infoHeader)
+        {
+            writer.Write(infoHeader.HeaderSize);
+            writer.Write(infoHeader.Width);
+            writer.Write(infoHeader.Height);
+            writer.Write(infoHeader.Planes);
+            writer.Write(infoHeader.BitsPerPixel);
+            writer.Write((int)infoHeader.Compression);
+            writer.Write(infoHeader.ImageSize);
+            writer.Write(infoHeader.XPelsPerMeter);
+            writer.Write(infoHeader.YPelsPerMeter);
+            writer.Write(infoHeader.ClrUsed);
+            writer.Write(infoHeader.ClrImportant);
+        }
+    }
+}

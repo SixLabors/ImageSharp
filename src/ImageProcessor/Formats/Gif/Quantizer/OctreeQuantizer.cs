83cb74cb565af32d177a58a677bf2a7c0cb0436b
James South <james_south@hotmail.com>
2015-04-30 23:33:24 +0100

    A bit more gif encoder work
    Not going well :frowning:
    
    
    Former-commit-id: 2e8d3e9b1125bb7b480a93f95237e92688b6c262
    Former-commit-id: 6556b12027896b88e2541f39589c6bed1f5bc67f
    Former-commit-id: 8ac88db36c67226e335b97273c78e4eeef27d9e4

---
+++ src/ImageProcessor/Formats/Gif/Quantizer/OctreeQuantizer.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OctreeQuantizer.cs" company="James South">
+//   Copyright © James South and contributors.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// <summary>
+//   Encapsulates methods to calculate the color palette of an image using an Octree pattern.
+//   <see href="http://msdn.microsoft.com/en-us/library/aa479306.aspx" />
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImageProcessor.Formats
+{
+    /// <summary>
+    /// Encapsulates methods to calculate the color palette of an image using an Octree pattern.
+    /// <see href="http://msdn.microsoft.com/en-us/library/aa479306.aspx"/>
+    /// </summary>
+    public class OctreeQuantizer : Quantizer
+    {
+        /// <summary>
+        /// Maximum allowed color depth
+        /// </summary>
+        private readonly int maxColors;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OctreeQuantizer"/> class.
+        /// </summary>
+        /// <remarks>
+        /// The Octree quantizer is a two pass algorithm. The initial pass sets up the Octree,
+        /// the second pass quantizes a color based on the nodes in the tree.
+        /// <para>
+        /// Defaults to return a maximum of 255 colors plus transparency with 8 significant bits.
+        /// </para>
+        /// </remarks>
+        public OctreeQuantizer()
+            : this(255, 8)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OctreeQuantizer"/> class. 
+        /// </summary>
+        /// <remarks>
+        /// The Octree quantizer is a two pass algorithm. The initial pass sets up the Octree,
+        /// the second pass quantizes a color based on the nodes in the tree
+        /// </remarks>
+        /// <param name="maxColors">The maximum number of colors to return</param>
+        /// <param name="maxColorBits">The number of significant bits</param>
+        public OctreeQuantizer(int maxColors, int maxColorBits)
+            : base(false)
+        {
+            Guard.LessEquals(maxColors, 255, "maxColors");
+        }
+    }
+}

30f627ca8347b320c5fc2143ce0ab8836f6ba392
James South <james_south@hotmail.com>
2015-05-04 16:01:04 +0100

    Enable jpeg
    Former-commit-id: cf9da933af92eb4a4d461778e14774aa0fe17f1f
    Former-commit-id: 607f049c7e81cca8305b334955cc9189cc6558fd
    Former-commit-id: 60ce8be67e4e654dde7c263b6c4070513063ed67

--- src/ImageProcessor/Formats/Jpg/LibJpeg/IDecompressDestination.cs
+++ src/ImageProcessor/Formats/Jpg/LibJpeg/IDecompressDestination.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-
-using BitMiracle.LibJpeg.Classic;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IDecompressDestination.cs" company="James South">
+//   Copyright © James South and contributors.
+//   Licensed under the Apache License, Version 2.0.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace BitMiracle.LibJpeg
 {
+    using System.IO;
+
     /// <summary>
     /// Common interface for processing of decompression.
     /// </summary>
-    interface IDecompressDestination
+    internal interface IDecompressDestination
     {
         /// <summary>
-        /// Strean with decompressed data
+        /// Gets the stream with decompressed data.
         /// </summary>
         Stream Output
         {
@@ -21,208 +23,29 @@ namespace BitMiracle.LibJpeg
         }
 
         /// <summary>
-        /// Implementor of this interface should process image properties received from decompressor.
+        /// Sets the image attributes.
         /// </summary>
-        /// <param name="parameters">Image properties</param>
+        /// <param name="parameters">
+        /// The <see cref="LoadedImageAttributes"/> containing attributes.
+        /// </param>
         void SetImageAttributes(LoadedImageAttributes parameters);
 
         /// <summary>
-        /// Called before decompression
+        /// Begins writing. Called before decompression
         /// </summary>
         void BeginWrite();
 
         /// <summary>
-        /// It called during decompression - pass row of pixels from JPEG
+        /// Processes the given row of pixels.
         /// </summary>
-        /// <param name="row"></param>
+        /// <param name="row">
+        /// The <see cref="T:byte[]"/> representing the row.
+        /// </param>
         void ProcessPixelsRow(byte[] row);
 
         /// <summary>
-        /// Called after decompression
+        /// Ends writing. Called after decompression
         /// </summary>
         void EndWrite();
-    }
-
-    /// <summary>
-    /// Holds parameters of image for decompression (IDecomressDesination)
-    /// </summary>
-    class LoadedImageAttributes
-    {
-        private Colorspace m_colorspace;
-        private bool m_quantizeColors;
-        private int m_width;
-        private int m_height;
-        private int m_componentsPerSample;
-        private int m_components;
-        private int m_actualNumberOfColors;
-        private byte[][] m_colormap;
-        private DensityUnit m_densityUnit;
-        private int m_densityX;
-        private int m_densityY;
-
-        /* Decompression processing parameters --- these fields must be set before
-         * calling jpeg_start_decompress().  Note that jpeg_read_header() initializes
-         * them to default values.
-         */
-
-        // colorspace for output
-        public Colorspace Colorspace
-        {
-            get
-            {
-                return m_colorspace;
-            }
-            internal set
-            {
-                m_colorspace = value;
-            }
-        }
-
-        // true=colormapped output wanted
-        public bool QuantizeColors
-        {
-            get
-            {
-                return m_quantizeColors;
-            }
-            internal set
-            {
-                m_quantizeColors = value;
-            }
-        }
-
-        /* Description of actual output image that will be returned to application.
-         * These fields are computed by jpeg_start_decompress().
-         * You can also use jpeg_calc_output_dimensions() to determine these values
-         * in advance of calling jpeg_start_decompress().
-         */
-
-        // scaled image width
-        public int Width
-        {
-            get
-            {
-                return m_width;
-            }
-            internal set
-            {
-                m_width = value;
-            }
-        }
-
-        // scaled image height
-        public int Height
-        {
-            get
-            {
-                return m_height;
-            }
-            internal set
-            {
-                m_height = value;
-            }
-        }
-
-        // # of color components in out_color_space
-        public int ComponentsPerSample
-        {
-            get
-            {
-                return m_componentsPerSample;
-            }
-            internal set
-            {
-                m_componentsPerSample = value;
-            }
-        }
-
-        // # of color components returned. it is 1 (a colormap index) when 
-        // quantizing colors; otherwise it equals out_color_components.
-        public int Components
-        {
-            get
-            {
-                return m_components;
-            }
-            internal set
-            {
-                m_components = value;
-            }
-        }
-
-        /* When quantizing colors, the output colormap is described by these fields.
-         * The application can supply a colormap by setting colormap non-null before
-         * calling jpeg_start_decompress; otherwise a colormap is created during
-         * jpeg_start_decompress or jpeg_start_output.
-         * The map has out_color_components rows and actual_number_of_colors columns.
-         */
-
-        // number of entries in use
-        public int ActualNumberOfColors
-        {
-            get
-            {
-                return m_actualNumberOfColors;
-            }
-            internal set
-            {
-                m_actualNumberOfColors = value;
-            }
-        }
-
-        // The color map as a 2-D pixel array
-        public byte[][] Colormap
-        {
-            get
-            {
-                return m_colormap;
-            }
-            internal set
-            {
-                m_colormap = value;
-            }
-        }
-
-        // These fields record data obtained from optional markers 
-        // recognized by the JPEG library.
-
-        // JFIF code for pixel size units
-        public DensityUnit DensityUnit
-        {
-            get
-            {
-                return m_densityUnit;
-            }
-            internal set
-            {
-                m_densityUnit = value;
-            }
-        }
-
-        // Horizontal pixel density
-        public int DensityX
-        {
-            get
-            {
-                return m_densityX;
-            }
-            internal set
-            {
-                m_densityX = value;
-            }
-        }
-
-        // Vertical pixel density
-        public int DensityY
-        {
-            get
-            {
-                return m_densityY;
-            }
-            internal set
-            {
-                m_densityY = value;
-            }
-        }
     }
 }

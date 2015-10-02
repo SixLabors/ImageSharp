d442194f7c3935d40e1c3ffb7bb41326a72541f1
James Jackson-South <james_south@hotmail.com>
2015-10-03 01:04:16 +1000

    Working Gif Encoder :tada:
    Former-commit-id: cbd69b2e1405f6ba4dd766177ce70f9f8bf08433
    Former-commit-id: 18b60afa438ff00a9e4d03b10ee19876d0132563
    Former-commit-id: 24c2a39bca3156adf35295a0a9bbeb5c01fd1cff

--- tests/ImageProcessor.Tests/Formats/EncoderDecoderTests.cs
+++ tests/ImageProcessor.Tests/Formats/EncoderDecoderTests.cs
@@ -11,14 +11,14 @@
     public class EncoderDecoderTests
     {
         [Theory]
-        //[InlineData("TestImages/Car.bmp")]
-        //[InlineData("TestImages/Portrait.png")]
-        //[InlineData("../../TestImages/Formats/Jpg/Backdrop.jpg")]
+        [InlineData("../../TestImages/Formats/Jpg/Backdrop.jpg")]
         [InlineData("../../TestImages/Formats/Gif/a.gif")]
-        //[InlineData("../../TestImages/Formats/Gif/leaf.gif")]
-        //[InlineData("TestImages/Windmill.gif")]
-        //[InlineData("../../TestImages/Formats/Bmp/Car.bmp")]
-        //[InlineData("../../TestImages/Formats/Png/cmyk.png")]
+        [InlineData("../../TestImages/Formats/Gif/leaf.gif")]
+        [InlineData("../../TestImages/Formats/Gif/ani.gif")]
+        [InlineData("../../TestImages/Formats/Gif/ani2.gif")]
+        [InlineData("../../TestImages/Formats/Gif/giphy.gif")]
+        [InlineData("../../TestImages/Formats/Bmp/Car.bmp")]
+        [InlineData("../../TestImages/Formats/Png/cmyk.png")]
         public void DecodeThenEncodeImageFromStreamShouldSucceed(string filename)
         {
             if (!Directory.Exists("Encoded"))
@@ -29,7 +29,7 @@
             FileStream stream = File.OpenRead(filename);
             Stopwatch watch = Stopwatch.StartNew();
             Image image = new Image(stream);
-
+            
             string encodedFilename = "Encoded/" + Path.GetFileName(filename);
 
             //if (!image.IsAnimated)
@@ -62,8 +62,8 @@
 
         [Theory]
         [InlineData("../../TestImages/Formats/Jpg/Backdrop.jpg")]
-        [InlineData("../../TestImages/Formats/Bmp/Car.bmp")]
-        [InlineData("../../TestImages/Formats/Png/cmyk.png")]
+        //[InlineData("../../TestImages/Formats/Bmp/Car.bmp")]
+        //[InlineData("../../TestImages/Formats/Png/cmyk.png")]
         public void QuantizedImageShouldPreserveMaximumColorPrecision(string filename)
         {
             if (!Directory.Exists("Quantized"))
@@ -74,7 +74,7 @@
             Image image = new Image(File.OpenRead(filename));
             IQuantizer quantizer = new OctreeQuantizer();
             QuantizedImage quantizedImage = quantizer.Quantize(image);
-
+            var pixel = quantizedImage.Pixels;
             using (FileStream output = File.OpenWrite($"Quantized/{ Path.GetFileName(filename) }"))
             {
                 IImageEncoder encoder = Image.Encoders.First(e => e.IsSupportedFileExtension(Path.GetExtension(filename)));

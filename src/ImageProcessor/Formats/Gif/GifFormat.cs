f15f1323c0780b5ed87f3c876a078fb31a77869e
James Jackson-South <james_south@hotmail.com>
2015-10-04 00:42:49 +1000

    Make things more testable.
    Former-commit-id: 53bdb09258dc39b852dda3679b991887e8204f98
    Former-commit-id: 64f1fe6f18af8d8bb053c59e9fa999cf7c675524
    Former-commit-id: b889f3dc5a64b8968a52d0a5cf7af2d9cf37579c

---
+++ src/ImageProcessor/Formats/Gif/GifFormat.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="GifFormat.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Formats
+{
+    /// <summary>
+    /// Encapsulates the means to encode and decode gif images.
+    /// </summary>
+    public class GifFormat : IImageFormat
+    {
+        /// <inheritdoc/>
+        public IImageDecoder Decoder => new GifDecoder();
+
+        /// <inheritdoc/>
+        public IImageEncoder Encoder => new GifEncoder();
+    }
+}
--- src/ImageProcessor/Formats/IImageFormat.cs
+++ src/ImageProcessor/Formats/IImageFormat.cs
@@ -1,9 +1,23 @@
-﻿namespace ImageProcessor.Encoders
+﻿// <copyright file="IImageFormat.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Formats
 {
+    /// <summary>
+    /// Encapsulates a supported image format, providing means to encode and decode an image.
+    /// </summary>
     public interface IImageFormat
     {
+        /// <summary>
+        /// Gets the image encoder for encoding an image from a stream.
+        /// </summary>
         IImageEncoder Encoder { get; }
 
+        /// <summary>
+        /// Gets the image decoder for decoding an image from a stream.
+        /// </summary>
         IImageDecoder Decoder { get; }
     }
 }

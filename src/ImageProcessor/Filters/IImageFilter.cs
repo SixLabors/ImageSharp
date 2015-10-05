1ecefab37b5500e5f7dd5e453ad8f10a75d29cde
James Jackson-South <james_south@hotmail.com>
2015-10-05 20:23:24 +1100

    First rough filter API attempt
    Former-commit-id: 6941eb37bf679465857517bfd16ebd910801f430
    Former-commit-id: b1fd2d8e4d90eb282201ed81a088e0f7b9c72278
    Former-commit-id: 5d1db23e65e6c613a151b8afa3c6b30453206b0a

---
+++ src/ImageProcessor/Filters/IImageFilter.cs
@@ -0,0 +1,34 @@
+﻿namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// Image processing filter interface.
+    /// </summary>
+    /// <remarks>
+    /// The interface defines the set of methods, which should be
+    /// provided by all image processing filters. Methods of this interface
+    /// manipulate the original image.
+    /// </remarks>
+    public interface IImageFilter
+    {
+        /// <summary>
+        /// Apply filter to an image at the area of the specified rectangle.
+        /// </summary>
+        /// <param name="target">Target image to apply filter to.</param>
+        /// <param name="source">The source image. Cannot be null.</param>
+        /// <param name="rectangle">The rectangle, which defines the area of the
+        /// image where the filter should be applied to.</param>
+        /// <remarks>The method keeps the source image unchanged and returns the
+        /// the result of image processing filter as new image.</remarks>
+        /// <exception cref="System.ArgumentNullException">
+        ///     <paramref name="target"/>
+        ///     is null.
+        ///     - or -
+        ///     <paramref name="source"/>
+        ///     is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// <paramref name="rectangle"/> doesnt fit the dimension of the image.
+        /// </exception>
+        void Apply(ImageBase target, ImageBase source, Rectangle rectangle);
+    }
+}

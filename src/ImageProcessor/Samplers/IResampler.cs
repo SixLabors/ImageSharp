4688eaa5f6467c844a7fb14aefe195d662e52d49
James Jackson-South <james_south@hotmail.com>
2015-10-07 08:00:23 +1100

    Begin add resize
    Not working correctly just now. Y calc is wrong.
    
    
    Former-commit-id: 21b81ada54ca20a5ba1f08cb0fce9c57dd7237b2
    Former-commit-id: b9800debdc5306d6935be95808bef72b34c2b9bc
    Former-commit-id: b06159adfd6ec098e13f9509a14b0f5bb3d8369e

---
+++ src/ImageProcessor/Samplers/IResampler.cs
@@ -0,0 +1,22 @@
+﻿namespace ImageProcessor.Samplers
+{
+    /// <summary>
+    /// Encasulates an interpolation algorithm for resampling images.
+    /// </summary>
+    public interface IResampler
+    {
+        /// <summary>
+        /// Gets the radius in which to sample pixels.
+        /// </summary>
+        double Radius { get; }
+
+        /// <summary>
+        /// Gets the result of the interpolation algorithm.
+        /// </summary>
+        /// <param name="x">The value to process.</param>
+        /// <returns>
+        /// The <see cref="double"/>
+        /// </returns>
+        double GetValue(double x);
+    }
+}

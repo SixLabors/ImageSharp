82bac0b7c777c4f688464004ae006b6a79dd397e
James Jackson-South <james_south@hotmail.com>
2015-10-07 18:41:58 +1100

    Add Lanczos3
    Former-commit-id: 083ac391afe84f4305517c0ad4ea783bec1732d8
    Former-commit-id: 1fd26a05db26d7573be02dae89937816beaec508
    Former-commit-id: 68aafabac8f507618fa366b86540e4030b66db47

---
+++ src/ImageProcessor/Samplers/Lanczos3Resampler.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="BicubicResampler.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Samplers
+{
+    /// <summary>
+    /// The function implements the Lanczos kernel algorithm as described on
+    /// <see href="https://en.wikipedia.org/wiki/Lanczos_resampling#Algorithm">Wikipedia</see>
+    /// </summary>
+    public class Lanczos3Resampler : IResampler
+    {
+        /// <inheritdoc/>
+        public double Radius => 3;
+
+        /// <inheritdoc/>
+        public double GetValue(double x)
+        {
+            if (x < 0)
+            {
+                x = -x;
+            }
+
+            if (x < 3)
+            {
+                return ImageMaths.SinC(x) * ImageMaths.SinC(x / 3f);
+            }
+
+            return 0;
+        }
+    }
+}

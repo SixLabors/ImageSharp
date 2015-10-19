19858262804ee5506d8dbd91ae285eb3ae88ac49
James Jackson-South <james_south@hotmail.com>
2015-10-19 17:53:36 +1100

    Adding some resamplers
    Former-commit-id: 81377f2d574fe5778b5a24c968c53bd967e8f08d
    Former-commit-id: 0dbdf15a7d41f51a7ca109a1b6fc39954c6cfa69
    Former-commit-id: 48d223ff221f92f49f12685d44c09839bb76b2e4

---
+++ src/ImageProcessor/Samplers/Resamplers/BoxResampler.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="BoxResampler.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Samplers
+{
+    /// <summary>
+    /// The function implements the box (nearest neighbour) algorithm.
+    /// </summary>
+    public class BoxResampler : IResampler
+    {
+        /// <inheritdoc/>
+        public double Radius => 0.5;
+
+        /// <inheritdoc/>
+        public double GetValue(double x)
+        {
+            if (x < 0)
+            {
+                x = -x;
+            }
+
+            if (x <= 0.5)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ src/ImageProcessor/Samplers/Resamplers/RobidouxSharpResampler.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="RobidouxSharpResampler.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Samplers
+{
+    /// <summary>
+    /// The function implements the Robidoux Sharp algorithm.
+    /// <see href="http://www.imagemagick.org/Usage/filter/#robidoux"/>
+    /// </summary>
+    public class RobidouxSharpResampler : IResampler
+    {
+        /// <inheritdoc/>
+        public double Radius => 2;
+
+        /// <inheritdoc/>
+        public double GetValue(double x)
+        {
+            const double B = 0.2620;
+            const double C = 0.3690;
+
+            return ImageMaths.GetBcValue(x, B, C);
+        }
+    }
+}

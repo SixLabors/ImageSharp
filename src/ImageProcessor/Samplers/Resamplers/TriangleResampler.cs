d4d1e00b9ed7b6f9f8c1e0637498467329bc8591
James Jackson-South <james_south@hotmail.com>
2015-10-19 17:53:36 +1100

    Adding some resamplers
    Former-commit-id: 173de39596036ccb0970ae195d47874cb5610db6
    Former-commit-id: 156b60bd82d6e2d49c0733aa5954206417d63501
    Former-commit-id: 6639ef8d33a006707c408193122d001189eec15d

---
+++ src/ImageProcessor/Samplers/Resamplers/TriangleResampler.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="TriangleResampler.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Samplers
+{
+    /// <summary>
+    /// The function implements the triangle (bilinear) algorithm.
+    /// </summary>
+    public class TriangleResampler : IResampler
+    {
+        /// <inheritdoc/>
+        public double Radius => 1;
+
+        /// <inheritdoc/>
+        public double GetValue(double x)
+        {
+            if (x < 0)
+            {
+                x = -x;
+            }
+
+            if (x < 1)
+            {
+                return 1 - x;
+            }
+
+            return 0;
+        }
+    }
+}

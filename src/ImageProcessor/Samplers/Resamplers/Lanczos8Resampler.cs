a4986afc31cff886cb8ed112506fc1fa4726780e
James Jackson-South <james_south@hotmail.com>
2015-10-20 17:53:13 +1100

    Add new test image for rewrite
    Former-commit-id: 9ddf97eb0300834d945db627e4c500e67fbf1fb7
    Former-commit-id: c9a893530a085724596af1054492d125dc0355ea
    Former-commit-id: 3266f3ce095fd3f865d0e540e6096f794de70c4f

---
+++ src/ImageProcessor/Samplers/Resamplers/Lanczos8Resampler.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="Lanczos3Resampler.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Samplers
+{
+    /// <summary>
+    /// The function implements the Lanczos kernel algorithm as described on
+    /// <see href="https://en.wikipedia.org/wiki/Lanczos_resampling#Algorithm">Wikipedia</see>
+    /// </summary>
+    public class Lanczos8Resampler : IResampler
+    {
+        /// <inheritdoc/>
+        public double Radius => 8;
+
+        /// <inheritdoc/>
+        public double GetValue(double x)
+        {
+            if (x < 0)
+            {
+                x = -x;
+            }
+
+            if (x < 8)
+            {
+                return ImageMaths.SinC(x) * ImageMaths.SinC(x / 8f);
+            }
+
+            return 0;
+        }
+    }
+}

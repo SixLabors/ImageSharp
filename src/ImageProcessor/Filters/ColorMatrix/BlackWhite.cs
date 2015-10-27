0cef737078f6bb466f5edd2dd8e5ef7c0e4f315f
James Jackson-South <james_south@hotmail.com>
2015-10-27 12:58:11 +1100

    Add ColorMatrix functionality
    Former-commit-id: 741ed5325553c7e521375e2b6168cfd345fe69d5
    Former-commit-id: f290ab071672406c5f3d846cedea2e7c280329c0
    Former-commit-id: 1ca475830cd7732a17fb3f407418189aea692e8e

---
+++ src/ImageProcessor/Filters/ColorMatrix/BlackWhite.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="BlackWhite.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// Converts the colors of the image to their black and white equivalent.
+    /// </summary>
+    public class BlackWhite : ColorMatrixFilter
+    {
+        /// <summary>
+        /// The BlackWhite matrix.
+        /// TODO: Calculate a matrix that works in the linear color space.
+        /// </summary>
+        private static readonly ColorMatrix Matrix = new ColorMatrix(
+            new[]
+                {
+                    new[] { 1.5f, 1.5f, 1.5f, 0, 0 },
+                    new[] { 1.5f, 1.5f, 1.5f, 0, 0 },
+                    new[] { 1.5f, 1.5f, 1.5f, 0, 0 },
+                    new float[] { 0, 0, 0, 1, 0 },
+                    new float[] { -1, -1, -1, 0, 1 }
+                });
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BlackWhite"/> class.
+        /// </summary>
+        public BlackWhite()
+            : base(Matrix, false)
+        {
+        }
+    }
+}
---
+++ src/ImageProcessor/Filters/ColorMatrix/Invert.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="Invert.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// Inverts the colors of the image.
+    /// </summary>
+    public class Invert : ColorMatrixFilter
+    {
+        /// <summary>
+        /// The inversion matrix.
+        /// TODO: With gamma adjustment enabled this leaves the image too bright.
+        /// </summary>
+        private static readonly ColorMatrix Matrix = new ColorMatrix(
+            new[]
+                {
+                    new float[] { -1, 0, 0, 0, 0 },
+                    new float[] { 0, -1, 0, 0, 0 },
+                    new float[] { 0, 0, -1, 0, 0 },
+                    new float[] { 0, 0, 0, 1, 0 },
+                    new float[] { 1, 1, 1, 0, 1 }
+                });
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Invert"/> class.
+        /// </summary>
+        public Invert()
+            : base(Matrix, false)
+        {
+        }
+    }
+}

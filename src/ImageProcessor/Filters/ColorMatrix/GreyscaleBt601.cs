ed60538dc66cc3790114b91ecbdcac2862b270b9
James Jackson-South <james_south@hotmail.com>
2015-10-27 12:58:11 +1100

    Add ColorMatrix functionality
    Former-commit-id: b2ef211d179e3a8ce57b9b02f98afc5292007e4f
    Former-commit-id: c7eb2f1a65beef56dee7cff467ca3c67a5ebc27c
    Former-commit-id: ff0dc95c61e1a2b39e342ff18d0c8c9da0a1a3c7

---
+++ src/ImageProcessor/Filters/ColorMatrix/GreyscaleBt601.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="GreyscaleBt601.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// Converts the colors of the image to greyscale applying the formula as specified by
+    /// ITU-R Recommendation BT.601 <see href="https://en.wikipedia.org/wiki/Luma_%28video%29#Rec._601_luma_versus_Rec._709_luma_coefficients"/>.
+    /// </summary>
+    public class GreyscaleBt601 : ColorMatrixFilter
+    {
+        /// <summary>
+        /// The inversion matrix.
+        /// </summary>
+        private static readonly ColorMatrix Matrix = new ColorMatrix(
+            new[]
+                {
+                    new float[] { 0.299f, 0.299f, 0.299f, 0, 0 },
+                    new float[] { 0.587f, 0.587f, 0.587f, 0, 0 },
+                    new float[] { 0.114f, 0.114f, 0.114f, 0, 0 },
+                    new float[] { 0, 0, 0, 1, 0 },
+                    new float[] { 0, 0, 0, 0, 1 }
+                });
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GreyscaleBt601"/> class.
+        /// </summary>
+        public GreyscaleBt601()
+            : base(Matrix, true)
+        {
+        }
+    }
+}
---
+++ src/ImageProcessor/Filters/ColorMatrix/GreyscaleBt709.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="GreyscaleBt709.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// Converts the colors of the image to greyscale applying the formula as specified by
+    /// ITU-R Recommendation BT.709 <see href="https://en.wikipedia.org/wiki/Rec._709#Luma_coefficients"/>.
+    /// </summary>
+    public class GreyscaleBt709 : ColorMatrixFilter
+    {
+        /// <summary>
+        /// The inversion matrix.
+        /// </summary>
+        private static readonly ColorMatrix Matrix = new ColorMatrix(
+            new[]
+                {
+                    new float[] { 0.2126f, 0.2126f, 0.2126f, 0, 0 },
+                    new float[] { 0.7152f, 0.7152f, 0.7152f, 0, 0 },
+                    new float[] { 0.0722f, 0.0722f, 0.0722f, 0, 0 },
+                    new float[] { 0, 0, 0, 1, 0 },
+                    new float[] { 0, 0, 0, 0, 1 }
+                });
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GreyscaleBt709"/> class.
+        /// </summary>
+        public GreyscaleBt709()
+            : base(Matrix, true)
+        {
+        }
+    }
+}

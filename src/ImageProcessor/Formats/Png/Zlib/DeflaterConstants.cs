bf2ee4dd9bac8668784e761f35e46efe8553cae6
James Jackson-South <james_south@hotmail.com>
2015-10-28 13:18:25 +1100

    Moar cleanup
    Former-commit-id: 2768412a12ebae48de30635184a4e213d1033346
    Former-commit-id: 033c6cc52ceec936e5c596f2dbe365d644d09a66
    Former-commit-id: 7e94075a33f258c94fd9d1d06098c64f7ca7d783

--- src/ImageProcessor/Formats/Png/Zlib/DeflaterConstants.cs
+++ src/ImageProcessor/Formats/Png/Zlib/DeflaterConstants.cs
@@ -1,4 +1,9 @@
-﻿namespace ImageProcessor.Formats
+﻿// <copyright file="DeflaterConstants.cs" company="James South">
+// Copyright © James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Formats
 {
     using System;
 
@@ -8,138 +13,133 @@
     public class DeflaterConstants
     {
         /// <summary>
-        /// Set to true to enable debugging
-        /// </summary>
-        public const bool DEBUGGING = false;
-
-        /// <summary>
         /// Written to Zip file to identify a stored block
         /// </summary>
-        public const int STORED_BLOCK = 0;
+        public const int StoredBlock = 0;
 
         /// <summary>
         /// Identifies static tree in Zip file
         /// </summary>
-        public const int STATIC_TREES = 1;
+        public const int StaticTrees = 1;
 
         /// <summary>
         /// Identifies dynamic tree in Zip file
         /// </summary>
-        public const int DYN_TREES = 2;
+        public const int DynTrees = 2;
 
         /// <summary>
         /// Header flag indicating a preset dictionary for deflation
         /// </summary>
-        public const int PRESET_DICT = 0x20;
+        public const int PresetDict = 0x20;
 
         /// <summary>
         /// Sets internal buffer sizes for Huffman encoding
         /// </summary>
-        public const int DEFAULT_MEM_LEVEL = 8;
+        public const int DefaultMemLevel = 8;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int MAX_MATCH = 258;
+        /// </summary>
+        public const int MaxMatch = 258;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int MIN_MATCH = 3;
+        /// </summary>
+        public const int MinMatch = 3;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int MAX_WBITS = 15;
+        /// </summary>
+        public const int MaxWbits = 15;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int WSIZE = 1 << MAX_WBITS;
+        /// </summary>
+        public const int Wsize = 1 << MaxWbits;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int WMASK = WSIZE - 1;
+        /// </summary>
+        public const int Wmask = Wsize - 1;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int HASH_BITS = DEFAULT_MEM_LEVEL + 7;
+        /// </summary>
+        public const int HashBits = DefaultMemLevel + 7;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int HASH_SIZE = 1 << HASH_BITS;
+        /// </summary>
+        public const int HashSize = 1 << HashBits;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int HASH_MASK = HASH_SIZE - 1;
+        /// </summary>
+        public const int HashMask = HashSize - 1;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int HASH_SHIFT = (HASH_BITS + MIN_MATCH - 1) / MIN_MATCH;
+        /// </summary>
+        public const int HashShift = (HashBits + MinMatch - 1) / MinMatch;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int MIN_LOOKAHEAD = MAX_MATCH + MIN_MATCH + 1;
+        /// </summary>
+        public const int MinLookahead = MaxMatch + MinMatch + 1;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int MAX_DIST = WSIZE - MIN_LOOKAHEAD;
+        /// </summary>
+        public const int MaxDist = Wsize - MinLookahead;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int PENDING_BUF_SIZE = 1 << (DEFAULT_MEM_LEVEL + 8);
+        /// </summary>
+        public const int PendingBufSize = 1 << (DefaultMemLevel + 8);
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public static int MAX_BLOCK_SIZE = Math.Min(65535, PENDING_BUF_SIZE - 5);
+        /// </summary>
+        public const int Deflatestored = 0;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int DEFLATE_STORED = 0;
+        /// </summary>
+        public const int Deflatefast = 1;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int DEFLATE_FAST = 1;
+        /// </summary>
+        public const int Deflateslow = 2;
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public const int DEFLATE_SLOW = 2;
+        /// </summary>
+        public static int MaxBlockSize => Math.Min(65535, PendingBufSize - 5);
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public static int[] GOOD_LENGTH = { 0, 4, 4, 4, 4, 8, 8, 8, 32, 32 };
+        /// </summary>
+        public static int[] GoodLength => new[] { 0, 4, 4, 4, 4, 8, 8, 8, 32, 32 };
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public static int[] MAX_LAZY = { 0, 4, 5, 6, 4, 16, 16, 32, 128, 258 };
+        /// </summary>
+        public static int[] MaxLazy => new[] { 0, 4, 5, 6, 4, 16, 16, 32, 128, 258 };
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public static int[] NICE_LENGTH = { 0, 8, 16, 32, 16, 32, 128, 128, 258, 258 };
+        /// </summary>
+        public static int[] NiceLength => new[] { 0, 8, 16, 32, 16, 32, 128, 128, 258, 258 };
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public static int[] MAX_CHAIN = { 0, 4, 8, 32, 16, 32, 128, 256, 1024, 4096 };
+        /// </summary>
+        public static int[] MaxChain => new[] { 0, 4, 8, 32, 16, 32, 128, 256, 1024, 4096 };
 
         /// <summary>
         /// Internal compression engine constant
-        /// </summary>		
-        public static int[] COMPR_FUNC = { 0, 1, 1, 1, 1, 2, 2, 2, 2, 2 };
+        /// </summary>
+        public static int[] ComprFunc => new[] { 0, 1, 1, 1, 1, 2, 2, 2, 2, 2 };
     }
 }

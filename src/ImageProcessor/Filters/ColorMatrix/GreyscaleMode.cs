c07c01a29efb38eb2626b28f3ea06a90d1672031
James Jackson-South <james_south@hotmail.com>
2015-11-01 19:48:36 +1100

    Add more filter methods to API
    Former-commit-id: d4780ca73ce16b8c9274d881ae7798f6e4988e59
    Former-commit-id: 8bd63c9e392fead7e065614385bd73551f2cfdde
    Former-commit-id: 04b6f741e5ce9928a6d3406ec65e31fb7f9bbcd3

---
+++ src/ImageProcessor/Filters/ColorMatrix/GreyscaleMode.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="GreyscaleMode.cs" company="James South">
+// Copyright (c) James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// Provides enumeration over the various greyscale methods available.
+    /// </summary>
+    public enum GreyscaleMode
+    {
+        /// <summary>
+        /// ITU-R Recommendation BT.709
+        /// </summary>
+        Bt709,
+
+        /// <summary>
+        /// ITU-R Recommendation BT.601
+        /// </summary>
+        Bt601
+    }
+}

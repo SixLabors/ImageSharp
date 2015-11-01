f470a68de0c12b9d8cc8235ebe60067a127143b5
James Jackson-South <james_south@hotmail.com>
2015-11-01 19:48:36 +1100

    Add more filter methods to API
    Former-commit-id: d4780ca73ce16b8c9274d881ae7798f6e4988e59
    Former-commit-id: 8bd63c9e392fead7e065614385bd73551f2cfdde
    Former-commit-id: 04b6f741e5ce9928a6d3406ec65e31fb7f9bbcd3

--- src/ImageProcessor/Common/Exceptions/ImageFormatException.cs
+++ src/ImageProcessor/Common/Exceptions/ImageFormatException.cs
@@ -1,13 +1,7 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ImageFormatException.cs" company="James South">
-//   Copyright (c) James South and contributors.
-//   Licensed under the Apache License, Version 2.0.
+﻿// <copyright file="ImageFormatException.cs" company="James South">
+// Copyright (c) James South and contributors.
+// Licensed under the Apache License, Version 2.0.
 // </copyright>
-// <summary>
-//   The exception that is thrown when the library tries to load
-//   an image, which has an invalid format.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
 
 namespace ImageProcessor
 {
@@ -27,7 +21,7 @@ namespace ImageProcessor
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ImageFormatException"/> class with the name of the 
+        /// Initializes a new instance of the <see cref="ImageFormatException"/> class with the name of the
         /// parameter that causes this exception.
         /// </summary>
         /// <param name="errorMessage">The error message that explains the reason for this exception.</param>
@@ -37,11 +31,11 @@ namespace ImageProcessor
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ImageFormatException"/> class with a specified 
+        /// Initializes a new instance of the <see cref="ImageFormatException"/> class with a specified
         /// error message and the exception that is the cause of this exception.
         /// </summary>
         /// <param name="errorMessage">The error message that explains the reason for this exception.</param>
-        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) 
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic)
         /// if no inner exception is specified.</param>
         public ImageFormatException(string errorMessage, Exception innerException)
             : base(errorMessage, innerException)

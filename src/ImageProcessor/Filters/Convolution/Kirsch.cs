bd51a9d35d6d808390e778181089e143d9051957
James Jackson-South <james_south@hotmail.com>
2015-11-05 00:43:05 +1100

    Add convolution filters
    Former-commit-id: c0578b87f4443ee3508e743b2ff56ce190bc9dbc
    Former-commit-id: fd223f0a81941eafdd858f5da687079bf7cf0116
    Former-commit-id: 1627f2d73ebb9b1067473fa82e2edd566299a0a4

---
+++ src/ImageProcessor/Filters/Convolution/Kirsch.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Kirsch.cs" company="James South">
+// Copyright (c) James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// The Kirsch operator filter.
+    /// <see href="http://en.wikipedia.org/wiki/Kirsch_operator"/>
+    /// </summary>
+    public class Kirsch : Convolution2DFilter
+    {
+        /// <summary>
+        /// Gets the horizontal gradient operator.
+        /// </summary>
+        public override float[,] KernelX => new float[,]
+        {
+            { 5, 5, 5 },
+            { -3, 0, -3 },
+            { -3, -3, -3 }
+        };
+
+        /// <summary>
+        /// Gets the vertical gradient operator.
+        /// </summary>
+        public override float[,] KernelY => new float[,]
+        {
+            { 5, -3, -3 },
+            { 5,  0, -3 },
+            { 5, -3, -3 }
+        };
+    }
+}
---
+++ src/ImageProcessor/Filters/Convolution/LaplacianOfGaussian.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="LaplacianOfGaussian.cs" company="James South">
+// Copyright (c) James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// The Laplacian of Gaussian operator filter.
+    /// <see href="http://fourier.eng.hmc.edu/e161/lectures/gradient/node9.html"/>
+    /// </summary>
+    public class LaplacianOfGaussian : ConvolutionFilter
+    {
+        /// <summary>
+        /// Gets the horizontal gradient operator.
+        /// </summary>
+        public override float[,] KernelX => new float[,]
+        {
+            { 0, 0, -1,  0,  0 },
+            { 0, -1, -2, -1,  0 },
+            { -1, -2, 16, -2, -1 },
+            { 0, -1, -2, -1,  0 },
+            { 0, 0, -1,  0,  0 }
+        };
+    }
+}

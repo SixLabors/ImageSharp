b7280b561d8aa3c29da045bfc3909ce57041145f
James Jackson-South <james_south@hotmail.com>
2015-11-04 14:34:52 +1100

    Fix Saturation, tweak Polaroid.
    Former-commit-id: 70b09066081c21e2d2ea262c7bd9d3f5f4de6f4e
    Former-commit-id: 7ed1e1227ad5198646dd07bf636b6dd141e1f521
    Former-commit-id: 877b7c2fca734818e34ad024924d3b42b277d47f

--- src/ImageProcessor/Filters/ColorMatrix/Polaroid.cs
+++ src/ImageProcessor/Filters/ColorMatrix/Polaroid.cs
@@ -24,7 +24,7 @@ namespace ImageProcessor.Filters
             M21 = -0.022f,
             M22 = 1.578f,
             M23 = -0.022f,
-            M31 = .616f,
+            M31 = .216f,
             M32 = -.16f,
             M33 = 1.5831f,
             M41 = 0.02f,
---
+++ src/ImageProcessor/Filters/ColorMatrix/Saturation.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="Saturation.cs" company="James South">
+// Copyright (c) James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    using System;
+    using System.Numerics;
+
+    /// <summary>
+    /// An <see cref="IImageProcessor"/> to change the saturation of an <see cref="Image"/>.
+    /// </summary>
+    public class Saturation : ColorMatrixFilter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Saturation"/> class.
+        /// </summary>
+        /// <param name="saturation">The new saturation of the image. Must be between -100 and 100.</param>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="saturation"/> is less than -100 or is greater than 100.
+        /// </exception>
+        public Saturation(int saturation)
+        {
+            Guard.MustBeBetweenOrEqualTo(saturation, -100, 100, nameof(saturation));
+            float saturationFactor = saturation / 100f;
+
+            // Stop at -1 to prevent inversion.
+            saturationFactor++;
+
+            // The matrix is set up to "shear" the colour space using the following set of values.
+            // Note that each colour component has an effective luminance which contributes to the
+            // overall brightness of the pixel.
+            // See http://graficaobscura.com/matrix/index.html
+            float saturationComplement = 1.0f - saturationFactor;
+            float saturationComplementR = 0.3086f * saturationComplement;
+            float saturationComplementG = 0.6094f * saturationComplement;
+            float saturationComplementB = 0.0820f * saturationComplement;
+
+            Matrix4x4 matrix = new Matrix4x4()
+            {
+                M11 = saturationComplementR + saturationFactor,
+                M12 = saturationComplementR,
+                M13 = saturationComplementR,
+                M21 = saturationComplementG,
+                M22 = saturationComplementG + saturationFactor,
+                M23 = saturationComplementG,
+                M31 = saturationComplementB,
+                M32 = saturationComplementB,
+                M33 = saturationComplementB + saturationFactor,
+            };
+
+            this.Value = matrix;
+        }
+    }
+}

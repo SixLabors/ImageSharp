3f31a4f48f7c0dcc2ed83eff087df12ede06c30b
James Jackson-South <james_south@hotmail.com>
2015-11-05 00:43:05 +1100

    Add convolution filters
    Former-commit-id: c0578b87f4443ee3508e743b2ff56ce190bc9dbc
    Former-commit-id: fd223f0a81941eafdd858f5da687079bf7cf0116
    Former-commit-id: 1627f2d73ebb9b1067473fa82e2edd566299a0a4

---
+++ src/ImageProcessor/Filters/Convolution/RobertsCross.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="RobertsCross.cs" company="James South">
+// Copyright (c) James South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor.Filters
+{
+    /// <summary>
+    /// The Roberts Cross operator filter.
+    /// <see href="http://en.wikipedia.org/wiki/Roberts_cross"/>
+    /// </summary>
+    public class RobertsCross : Convolution2DFilter
+    {
+        /// <summary>
+        /// Gets the horizontal gradient operator.
+        /// </summary>
+        public override float[,] KernelX => new float[,]
+        {
+            { 1, 0 },
+            { 0, -1 }
+        };
+
+        /// <summary>
+        /// Gets the vertical gradient operator.
+        /// </summary>
+        public override float[,] KernelY => new float[,]
+        {
+            { 0, 1 },
+            { -1, 0 }
+        };
+    }
+}

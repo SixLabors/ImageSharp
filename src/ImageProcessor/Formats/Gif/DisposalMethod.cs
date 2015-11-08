8a8214173e010ce3bdba785dd803207856648bd4
James Jackson-South <james_south@hotmail.com>
2015-11-08 23:30:21 +1100

    Stylecop fixes
    Former-commit-id: 4ce25cbac837525bb98af107d3126f4cad6ed5e7
    Former-commit-id: 5f18082024474a701dc45c921aae1878dab85a37
    Former-commit-id: 1bd9cb859cc44a2bdc09a0ed66c1e389ed93ac80

--- src/ImageProcessor/Formats/Gif/DisposalMethod.cs
+++ src/ImageProcessor/Formats/Gif/DisposalMethod.cs
@@ -6,30 +6,30 @@
 namespace ImageProcessor.Formats
 {
     /// <summary>
-    /// Provides enumeration for instructing the decoder what to do with the last image 
+    /// Provides enumeration for instructing the decoder what to do with the last image
     /// in an animation sequence.
     /// </summary>
     public enum DisposalMethod
     {
         /// <summary>
-        /// No disposal specified. The decoder is not required to take any action. 
+        /// No disposal specified. The decoder is not required to take any action.
         /// </summary>
         Unspecified = 0,
 
         /// <summary>
-        /// Do not dispose. The graphic is to be left in place. 
+        /// Do not dispose. The graphic is to be left in place.
         /// </summary>
         NotDispose = 1,
 
         /// <summary>
         /// Restore to background color. The area used by the graphic must be restored to
-        /// the background color. 
+        /// the background color.
         /// </summary>
         RestoreToBackground = 2,
 
         /// <summary>
-        /// Restore to previous. The decoder is required to restore the area overwritten by the 
-        /// graphic with what was there prior to rendering the graphic. 
+        /// Restore to previous. The decoder is required to restore the area overwritten by the
+        /// graphic with what was there prior to rendering the graphic.
         /// </summary>
         RestoreToPrevious = 3
     }

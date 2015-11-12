54860af4722c13af7de035cb2736a4dbcbfa1150
James Jackson-South <james_south@hotmail.com>
2015-11-12 22:35:49 +1100

    Switch out List for ImmutableArray
    Use my full name.
    
    
    Former-commit-id: 329d5d6ae345d28b6c120966b77641cec4dce101
    Former-commit-id: 121cd1a695ad5d3da170725385a3e555b5536798
    Former-commit-id: c34c57087a21e44a404066329e2512d678eef567

--- src/ImageProcessor/Common/Extensions/ByteExtensions.cs
+++ src/ImageProcessor/Common/Extensions/ByteExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ByteExtensions.cs" company="James South">
-//   Copyright (c) James South and contributors.
+// <copyright file="ByteExtensions.cs" company="James Jackson-South">
+//   Copyright (c) James Jackson-South and contributors.
 //   Licensed under the Apache License, Version 2.0.
 // </copyright>
 // <summary>

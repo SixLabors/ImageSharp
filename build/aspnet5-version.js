cb2a412665466146f1cb2b96b20fafbe2aeb7fbf
Jeavon <jeavon@crumpled-dog.com>
2015-11-13 15:46:20 +0000

    Add a nodejs script to patch the project.json file with the AppVeyor version number
    Former-commit-id: 547399af66beb1b843ff0ef3d6540f8132ffaab1
    Former-commit-id: ba3b2143364e06c45e662284cc134634451bbe5b
    Former-commit-id: ab0b04b0e427d286d3611dd424fccda6138c3999

---
+++ build/aspnet5-version.js
@@ -0,0 +1,12 @@
+﻿var jsonfile = require('jsonfile');
+var util = require('util');
+
+var file = '../src/imageprocessor/project.json';
+var buildNumber = process.env.APPVEYOR_BUILD_NUMBER;
+
+jsonfile.readFile(file, function (err, project) {
+    project.version = buildNumber;
+    jsonfile.writeFile(file, project, {spaces: 2}, function(err) {
+        console.error(err);
+    });
+})

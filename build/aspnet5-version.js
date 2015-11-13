bff6614234db42edab3eb2bd9ccbb30b01e2b36a
Jeavon <jeavon@crumpled-dog.com>
2015-11-13 16:19:19 +0000

    Think I've got Semver versioning working but currently fixed with the "alpha" prefix, this needs to be changed to a Environmental variable
    Former-commit-id: 36db49c22a54ad2e5312c89382e6c23e384b688e
    Former-commit-id: 667ee9bd9f6fbee10341d6e62514c8a0666ae61b
    Former-commit-id: b9cf81be4888c902cd042b9d1434cfde14b386df

--- build/aspnet5-version.js
+++ build/aspnet5-version.js
@@ -1,11 +1,18 @@
 ﻿var jsonfile = require('jsonfile');
+var semver = require('semver');
 var util = require('util');
 
 var file = '../src/imageprocessor/project.json';
-var buildNumber = process.env.APPVEYOR_BUILD_VERSION.substring(1);
+var buildVersion = process.env.APPVEYOR_BUILD_VERSION.substring(1);
+//var buildVersion = '3.0.0.23';
+
+var findPoint       = buildVersion.lastIndexOf(".");
+var basePackageVer  = buildVersion.substring(0, findPoint);
+var buildNumber     = buildVersion.substring(findPoint + 1, buildVersion.length);
+var semversion 		= semver.valid(basePackageVer + '-alpha-' + buildNumber)
 
 jsonfile.readFile(file, function (err, project) {
-	project.version = buildNumber;
+	project.version = semversion;
 	jsonfile.writeFile(file, project, {spaces: 2}, function(err) {
 		console.error(err);
 	});

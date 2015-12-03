b999aa9dc37b48e2ed8e9c1552e16ff5f8fd2049
James Jackson-South <james_south@hotmail.com>
2015-12-04 01:15:52 +1100

    Add Hue Fixes #262
    Also fix sepia to conform to SVG spec
    
    
    Former-commit-id: ecdfb651ab5de60599c6284bf020e088be77864d
    Former-commit-id: ce0eed68bb7ee1d7a13be8f8f78bbdc70b723964
    Former-commit-id: 5021d51170f93ce260b2454071809968be629ecf

--- src/ImageProcessor/Filters/ColorMatrix/Hue.cs
+++ src/ImageProcessor/Filters/ColorMatrix/Hue.cs
@@ -37,30 +37,37 @@
         public override Matrix4x4 Matrix => this.matrix;
 
         /// <inheritdoc/>
+        public override bool Compand => false;
+
+        /// <inheritdoc/>
         protected override void OnApply(ImageBase source, ImageBase target, Rectangle targetRectangle, Rectangle sourceRectangle)
         {
-            float degrees = this.Angle;
-            double costheta = Math.Cos(degrees);
-            double sintheta = Math.Sin(degrees);
+            float radians = (float)ImageMaths.DegreesToRadians(this.Angle);
+            double cosradians = Math.Cos(radians);
+            double sinradians = Math.Sin(radians);
+
             float lumR = .213f;
             float lumG = .715f;
             float lumB = .072f;
 
+            float oneMinusLumR = 1 - lumR;
+            float oneMinusLumG = 1 - lumG;
+            float oneMinusLumB = 1 - lumB;
+
             // The matrix is set up to preserve the luminance of the image.
             // See http://graficaobscura.com/matrix/index.html
             // Number are taken from https://msdn.microsoft.com/en-us/library/jj192162(v=vs.85).aspx
-            // TODO: This isn't correct. Need to double check MS numbers against maths.
             Matrix4x4 matrix4X4 = new Matrix4x4()
             {
-                M11 = (float)(.213 + (costheta * .787) - (sintheta * .213)),
-                M12 = (float)(.715 - (costheta * .715) - (sintheta * .715)),
-                M13 = (float)(.072 - (costheta * .072) + (sintheta * .928)),
-                M21 = (float)(.213 - (costheta * .213) + (sintheta * .143)),
-                M22 = (float)(.715 + (costheta * .285) + (sintheta * .140)),
-                M23 = (float)(.072 - (costheta * .072) - (sintheta * .283)),
-                M31 = (float)(.213 - (costheta * .213) - (sintheta * .787)),
-                M32 = (float)(.715 - (costheta * .715) + (sintheta * .715)),
-                M33 = (float)(.072 + (costheta * .928) + (sintheta * .072))
+                M11 = (float)(+lumR + (cosradians * oneMinusLumR) - (sinradians * lumR)),
+                M12 = (float)(+lumR - (cosradians * lumR) - (sinradians * lumR)),
+                M13 = (float)(+lumR - (cosradians * lumR) - (sinradians * 0.787)),
+                M21 = (float)(+lumG - (cosradians * lumG) - (sinradians * lumG)),
+                M22 = (float)(+lumG + (cosradians * oneMinusLumG) + (sinradians * 0.140)),
+                M23 = (float)(+lumG - (cosradians * lumG) + (sinradians * lumG)),
+                M31 = (float)(+lumB - (cosradians * lumB) + (sinradians * oneMinusLumB)),
+                M32 = (float)(+lumB - (cosradians * lumB) - (sinradians * 0.283)),
+                M33 = (float)(+lumB + (cosradians * oneMinusLumB) + (sinradians * lumB))
             };
 
             this.matrix = matrix4X4;

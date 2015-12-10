e0208e114eebe79043d67fe8f0f605d20ddb2ca0
James Jackson-South <james_south@hotmail.com>
2015-12-10 18:10:16 +1100

    Begin Vignette.
    Former-commit-id: c5e1903acb354e047d73223867a04ae29f301354
    Former-commit-id: a86e30bf71e3eab130a4cc8d9879aa2d55db440b
    Former-commit-id: 272754ac9ccd2c97cfeb320d3e25ff431ac43de4

---
+++ src/ImageProcessor/Filters/Vignette.cs
@@ -0,0 +1,77 @@
+﻿namespace ImageProcessor.Filters
+{
+    using System.Numerics;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Creates a vignette
+    /// </summary>
+    public class Vignette : ParallelImageProcessor
+    {
+        /// <summary>
+        /// Used to hold a copy of the target image.
+        /// </summary>
+        private readonly Image targetCopy = new Image();
+
+        /// <summary>
+        /// Gets or sets the vignette color to apply.
+        /// </summary>
+        public Color Color { get; set; } = Color.Black;
+
+        /// <summary>
+        /// Gets or sets the the x-radius.
+        /// </summary>
+        public float RadiusX { get; set; }
+
+        /// <summary>
+        /// Gets or sets the the y-radius.
+        /// </summary>
+        public float RadiusY { get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnApply(ImageBase source, ImageBase target, Rectangle targetRectangle, Rectangle sourceRectangle)
+        {
+            this.targetCopy.SetPixels(target.Width, target.Height, target.Pixels);
+            target.SetPixels(target.Width, target.Height, new float[target.Pixels.Length]);
+        }
+
+        /// <inheritdoc/>
+        protected override void Apply(ImageBase target, ImageBase source, Rectangle targetRectangle, Rectangle sourceRectangle, int startY, int endY)
+        {
+            int startX = sourceRectangle.X;
+            int endX = sourceRectangle.Right;
+
+            Vector2 centre = Rectangle.Center(targetRectangle);
+            int centerX = (int)centre.X;
+            int centerY = (int)centre.Y;
+            float rX = this.RadiusX > 0 ? this.RadiusX : targetRectangle.Width / 2f;
+            float rY = this.RadiusY > 0 ? this.RadiusY : targetRectangle.Height / 2f;
+
+            Ellipse ellipse = new Ellipse(new Point(centerX, centerY), rX - 1, rY - 1);
+
+            Parallel.For(
+                startY,
+                endY,
+                y =>
+                    {
+                        for (int x = startX; x < endX; x++)
+                        {
+                            if (!ellipse.Contains(x, y))
+                            {
+                                target[x, y] = Color.Black;
+                            }
+                        }
+                    });
+        }
+
+        /// <inheritdoc/>
+        protected override void AfterApply(ImageBase source, ImageBase target, Rectangle targetRectangle, Rectangle sourceRectangle)
+        {
+            new GuassianBlur(30).Apply(target, target, targetRectangle);
+            Image temp = new Image(this.targetCopy);
+            new Blend(target, 40).Apply(this.targetCopy, temp, targetRectangle);
+            target.SetPixels(temp.Width, temp.Height, this.targetCopy.Pixels);
+        }
+    }
+}
+

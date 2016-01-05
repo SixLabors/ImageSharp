77d07694e693300c079b744e02ea4d742ac67791
James Jackson-South <james_south@hotmail.com>
2016-01-06 10:16:20 +1100

    A few Color tweaks
    Perf improvements + API clarity improvements.
    
    
    Former-commit-id: 219c3e9ea9c2b53b77a4d4ee3455a20bccc07e56
    Former-commit-id: 43460627a23abbaccb4d094de9d87a87b83d5752
    Former-commit-id: cfcae0b8648bf971cd3ffc93c9cf7e56f8f520b8

--- src/ImageProcessor/Filters/Brightness.cs
+++ src/ImageProcessor/Filters/Brightness.cs
@@ -66,12 +66,12 @@ namespace ImageProcessor.Filters
         /// </returns>
         private static Color AdjustBrightness(Color color, float brightness)
         {
-            color = Color.InverseCompand(color);
+            color = Color.Expand(color);
 
             Vector3 vector3 = color.ToVector3();
             vector3 += new Vector3(brightness, brightness, brightness);
 
-            return Color.Compand(new Color(vector3, color.A));
+            return Color.Compress(new Color(vector3, color.A));
         }
     }
 }

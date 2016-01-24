116fc1172b4ca049992e051509d82058e404bc7f
voidstar69 <github@ninemail.net>
2016-01-24 23:26:42 +0000

    Updated every filter and sampler to call ParallelImageProcessor.OnRowProcessed to enabled progress notifications for client code
    Former-commit-id: 3669c36371f1e113f64c927c352cffd8d083b2b9
    Former-commit-id: 3a338140eca0b31f75389c82b6a87c6b74a98db8
    Former-commit-id: b2e9b8dcc8bd8706056c8a23302b940de21bf1f6

--- src/ImageProcessor/Filters/Convolution/ConvolutionFilter.cs
+++ src/ImageProcessor/Filters/Convolution/ConvolutionFilter.cs
@@ -76,6 +76,7 @@ namespace ImageProcessor.Filters
 
                             target[x, y] = new Color(red, green, blue);
                         }
+                        this.OnRowProcessed();
                     }
                 });
         }

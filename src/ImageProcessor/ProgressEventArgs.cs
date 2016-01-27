06d5ce7e64c03bb6f2ecd1fa26e1acd528e07e4b
James Jackson-South <james_south@hotmail.com>
2016-01-27 13:22:16 +1100

    Complete progress messaging.
    Former-commit-id: 6a4ad9c1dc9ae7635bb4108e2840bcf88c4894e5
    Former-commit-id: 5fe2186a4fbc536b4d050ac4a5b2649b8532fe21
    Former-commit-id: 691721d5d4d6e3aa1d18deb7b2da7b521f497bff

---
+++ src/ImageProcessor/ProgressEventArgs.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="ProgressEventArgs.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessor
+{
+    /// <summary>
+    /// Contains event data related to the progress made processing an image.
+    /// </summary>
+    public class ProgressEventArgs : System.EventArgs
+    {
+        /// <summary>
+        /// Gets or sets the number of rows processed.
+        /// </summary>
+        public int RowsProcessed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total number of rows.
+        /// </summary>
+        public int TotalRows { get; set; }
+    }
+}

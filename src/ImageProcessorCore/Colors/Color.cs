dba6a7a704f8f05db32fa17da4defe03ed74a321
James Jackson-South <james_south@hotmail.com>
2016-02-01 21:04:48 +1100

    Color performance optimizations.
    Former-commit-id: 103031740de3d3b482d55800eac4910a29e882a0
    Former-commit-id: 258d7739fcdbb02d79aa52265f1af8d4a0d630c9
    Former-commit-id: 9ee758d7ccec4869fd0664f1379ec3b152be8d3e

--- src/ImageProcessorCore/Colors/Color.cs
+++ src/ImageProcessorCore/Colors/Color.cs
@@ -84,9 +84,7 @@ namespace ImageProcessorCore
                 float a = Convert.ToByte(hex.Substring(0, 2), 16);
 
                 // Do division of Vector4 instead of each component to utilize SIMD optimizations
-                this.backingVector = new Vector4(r, g, b, a) / 255f;
-
-                this.backingVector = FromNonPremultiplied(this.backingVector, this.A);
+                this.backingVector = FromNonPremultiplied(new Vector4(r, g, b, a) / 255f, this.A);
 
             }
             else if (hex.Length == 6)

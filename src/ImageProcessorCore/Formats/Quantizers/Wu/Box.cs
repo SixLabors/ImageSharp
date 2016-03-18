67ad4ae21e80fa2482a563753deafe8050023f65
James Jackson-South <james_south@hotmail.com>
2016-03-18 15:10:25 +1100

    Add Wu Quantizer
    R & B are reversed.
    
    
    Former-commit-id: 92632679d9c7a1440e51e42bb23c264c2b8c4dad
    Former-commit-id: 84f98b9f6124e93b61c43dafc28442f8fac92487
    Former-commit-id: 043e6b98103678de70bc41857c52b81d22daca6d

---
+++ src/ImageProcessorCore/Formats/Quantizers/Wu/Box.cs
@@ -0,0 +1,53 @@
+﻿namespace ImageProcessorCore.Formats
+{
+    /// <summary>
+    /// A box.
+    /// </summary>
+    internal sealed class Box
+    {
+        /// <summary>
+        /// Gets or sets the min red value, exclusive.
+        /// </summary>
+        public int R0 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max red value, inclusive.
+        /// </summary>
+        public int R1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the min green value, exclusive.
+        /// </summary>
+        public int G0 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max green value, inclusive.
+        /// </summary>
+        public int G1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the min blue value, exclusive.
+        /// </summary>
+        public int B0 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max green value, inclusive.
+        /// </summary>
+        public int B1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the min alpha value, exclusive.
+        /// </summary>
+        public int A0 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max alpha value, inclusive.
+        /// </summary>
+        public int A1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the volume.
+        /// </summary>
+        public int Volume { get; set; }
+    }
+}

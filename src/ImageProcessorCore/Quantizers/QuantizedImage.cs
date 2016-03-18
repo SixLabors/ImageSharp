cebef1542fc0d06adfb2f0ca6f6d05d9b4253bd6
James Jackson-South <james_south@hotmail.com>
2016-03-18 21:33:42 +1100

    Interchangeable quantizers :balloon:
    Former-commit-id: d9c1d3a41b639781c0e44ac921a6de9de59321b2
    Former-commit-id: 3b21b6cd0c539bf121d9f88fccf874793de71180
    Former-commit-id: 4510bab7605f17c4c85d7793daaa7e24ea2dc55f

--- src/ImageProcessorCore/Quantizers/QuantizedImage.cs
+++ src/ImageProcessorCore/Quantizers/QuantizedImage.cs
@@ -20,7 +20,8 @@ namespace ImageProcessorCore.Quantizers
         /// <param name="height">The image height.</param>
         /// <param name="palette">The color palette.</param>
         /// <param name="pixels">The quantized pixels.</param>
-        public QuantizedImage(int width, int height, Bgra32[] palette, byte[] pixels)
+        /// <param name="transparentIndex">The transparency index.</param>
+        public QuantizedImage(int width, int height, Bgra32[] palette, byte[] pixels, int transparentIndex = -1)
         {
             Guard.MustBeGreaterThan(width, 0, nameof(width));
             Guard.MustBeGreaterThan(height, 0, nameof(height));
@@ -37,6 +38,7 @@ namespace ImageProcessorCore.Quantizers
             this.Height = height;
             this.Palette = palette;
             this.Pixels = pixels;
+            this.TransparentIndex = transparentIndex;
         }
 
         /// <summary>
@@ -58,6 +60,11 @@ namespace ImageProcessorCore.Quantizers
         /// Gets the pixels of this <see cref="T:QuantizedImage"/>.
         /// </summary>
         public byte[] Pixels { get; }
+
+        /// <summary>
+        /// Gets the transparent index
+        /// </summary>
+        public int TransparentIndex { get; }
 
         /// <summary>
         /// Converts this quantized image to a normal image.

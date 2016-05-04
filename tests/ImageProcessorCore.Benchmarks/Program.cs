339ca3d86b5b4a9e2609459ceb750db5cb7432d9
James Jackson-South <james_south@hotmail.com>
2016-05-04 17:59:57 +1000

    Add benchmarking
    Former-commit-id: b49aa6d4012cc28d93b0f2c680d4de6d10aa09bb
    Former-commit-id: a59176e8bda26889d71fde5d239837bc0994ce79
    Former-commit-id: 2a7361abe1a31ab578c0b85d0492eacea12cb8a4

---
+++ tests/ImageProcessorCore.Benchmarks/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ImageProcessorCore.Benchmarks
+{
+    using System;
+    using System.Reflection;
+
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Running;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Use reflection for a more maintainable way of creating the benchmark switcher,
+            Type[] benchmarks = typeof(Program).Assembly.GetTypes()
+                .Where(t => t.GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                                .Any(m => m.GetCustomAttributes(typeof(BenchmarkAttribute), false).Any()))
+                .OrderBy(t => t.Namespace)
+                .ThenBy(t => t.Name)
+                .ToArray();
+
+            BenchmarkSwitcher benchmarkSwitcher = new BenchmarkSwitcher(benchmarks);
+            benchmarkSwitcher.Run(args);
+        }
+    }
+}
---
+++ tests/ImageProcessorCore.Benchmarks/Resize.cs
@@ -0,0 +1,44 @@
+﻿namespace ImageProcessorCore.Benchmarks
+{
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+
+    using BenchmarkDotNet.Attributes;
+
+    using ImageProcessorCore.Samplers;
+    using CoreImage = ImageProcessorCore.Image;
+    using CoreSize = ImageProcessorCore.Size;
+
+    public class Resize
+    {
+        [Benchmark(Baseline = true, Description = "System Drawing Resize")]
+        public Size ResizeSystemDrawing()
+        {
+            using (Bitmap source = new Bitmap(400, 400))
+            {
+                using (Bitmap destination = new Bitmap(100, 100))
+                {
+                    using (Graphics graphics = Graphics.FromImage(destination))
+                    {
+                        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                        graphics.CompositingQuality = CompositingQuality.HighQuality;
+                        graphics.DrawImage(source, 0, 0, 100, 100);
+                    }
+
+                    return destination.Size;
+                }
+            }
+        }
+
+        [Benchmark(Description = "ImageProcessor.Core Resize")]
+        public CoreSize ResizeCore()
+        {
+            using (CoreImage image = new CoreImage(400, 400))
+            {
+                image.Resize(100, 100);
+                return new CoreSize(image.Width, image.Height);
+            }
+        }
+    }
+}

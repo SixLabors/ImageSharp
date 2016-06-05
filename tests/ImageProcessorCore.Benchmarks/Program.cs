5f411a99bad3ab238402e769988f7753c05f8be1
James Jackson-South <james_south@hotmail.com>
2016-06-06 02:15:52 +1000

    Fix 1D resize + performance tweaks
    Former-commit-id: 6a3854cdfd8fa47b3e49dfdd5a4a116a7eace69d
    Former-commit-id: 620f3c76756cfc7176d05fe20ae108b2a7f0da6e
    Former-commit-id: 9829703cea2c316e5a6800b844450b0a9c14f301

--- tests/ImageProcessorCore.Benchmarks/Program.cs
+++ tests/ImageProcessorCore.Benchmarks/Program.cs
@@ -1,25 +1,36 @@
 ﻿namespace ImageProcessorCore.Benchmarks
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
 
     using BenchmarkDotNet.Attributes;
     using BenchmarkDotNet.Running;
-    using System.Linq;
 
     public class Program
     {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The arguments to pas to the program.
+        /// </param>
         public static void Main(string[] args)
         {
             // Use reflection for a more maintainable way of creating the benchmark switcher,
             Type[] benchmarks = typeof(Program).Assembly.GetTypes()
                 .Where(t => t.GetMethods(BindingFlags.Instance | BindingFlags.Public)
-                                .Any(m => m.GetCustomAttributes(typeof(BenchmarkAttribute), false).Any()))
+                             .Any(m => m.GetCustomAttributes(typeof(BenchmarkAttribute), false).Any()))
                 .OrderBy(t => t.Namespace)
                 .ThenBy(t => t.Name)
                 .ToArray();
 
+            // TODO: This throws an exception.
+            // List<string> x = new List<string>(args) { "diagnosers=MemoryDiagnoser,InliningDiagnoser" };
             BenchmarkSwitcher benchmarkSwitcher = new BenchmarkSwitcher(benchmarks);
+            
+            // benchmarkSwitcher.Run(x.ToArray());
             benchmarkSwitcher.Run(args);
         }
     }

b3cc81bfde4413626d3cd8bc42bee9ffaee8dab9
Sverre Rekvin <sre@markedspartner.no>
2016-06-27 03:20:26 +0200

    Inv matrix, traget size of image was wrong
    Former-commit-id: b62be965af469e6c365f57261b5790ac05475329
    Former-commit-id: 621c23abc663803c7b49e81ce3e576a2042dc740
    Former-commit-id: e133d3a733742ec6038f8fb9f2053731e6f44618

--- tests/ImageProcessorCore.Tests/Processors/Samplers/SamplerTests.cs
+++ tests/ImageProcessorCore.Tests/Processors/Samplers/SamplerTests.cs
@@ -500,7 +500,7 @@
                     using (Image image = new Image(stream))
                     using (FileStream output = File.OpenWrite($"TestOutput/Skew/{filename}"))
                     {
-                        image.Skew(15, 12, this.ProgressUpdate)
+                        image.Skew(-15, 12, this.ProgressUpdate)
                              .Save(output);
                     }
 

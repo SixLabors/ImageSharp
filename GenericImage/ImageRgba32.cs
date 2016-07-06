93b4617c7686ff8aad4d8cb6348c98730e79b586
James Jackson-South <james_south@hotmail.com>
2016-07-06 20:57:35 +1000

    ImageRgba32
    Former-commit-id: e8d174508130e99cb9152e3c6dad690232425075
    Former-commit-id: 4191408f0e2678dfb6937918c46a0d588da073d7
    Former-commit-id: 3dc34dfac9b7e02f3dca4e80b1d8cf41fe40ee35

---
+++ GenericImage/ImageRgba32.cs
@@ -0,0 +1,25 @@
+﻿namespace GenericImage
+{
+    using GenericImage.PackedVectors;
+
+    public class ImageRgba32 : IImageBase<Rgba32>
+    {
+        public ImageRgba32(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Pixels = new Rgba32[width * height];
+        }
+
+        public Rgba32[] Pixels { get; }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public IPixelAccessor Lock()
+        {
+            return new PixelAccessorRgba32(this);
+        }
+    }
+}

e57c6775ce35aef330841bc663ed41101a236fdd
James Jackson-South <james_south@hotmail.com>
2016-07-14 00:55:06 +1000

    Benchmark clamp
    Former-commit-id: ecfd869a3c6f282f03f38239b0dc0a204d3bf565
    Former-commit-id: 4ad46c7929a15caebfe440eb7610617e97872a0b
    Former-commit-id: dfdcfa618c48e66921be1ebb395183de6785323c

---
+++ tests/ImageProcessorCore.Benchmarks/Color/Clamp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ImageProcessorCore.Benchmarks.Color
+{
+    using BenchmarkDotNet.Attributes;
+
+    public class Clamp
+    {
+        [Benchmark(Baseline = true, Description = "Maths Clamp")]
+        public byte ClampMaths()
+        {
+            double value = 256;
+            return (byte)Math.Min(Math.Max(0, value), 255);
+        }
+
+        [Benchmark(Description = "No Maths Clamp")]
+        public byte ClampNoMaths()
+        {
+            double value = 256;
+            value = (value > 255) ? 255 : value;
+            value = (value < 0) ? 0 : value;
+            return (byte)value;
+        }
+
+    }
+}

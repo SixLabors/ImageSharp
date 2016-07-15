8756f2f11d6b18ea9fae700bd23080ace087b9b9
Adam Sitnik <adam.sitnik@gmail.com>
2016-07-15 17:16:52 +0200

    update to latest #BenchmarkDotNet version
    Former-commit-id: 6ca587c2d3f773d2f430030e2ca0d8d4bd7fe11b
    Former-commit-id: 01b7b1e4c8fb0146d5efa0d755d0d0f248df5eeb
    Former-commit-id: 20c535434e174f9adb525313989423a986df908e

---
+++ tests/ImageProcessorCore.Benchmarks/Config.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Configs;
+
+namespace ImageProcessorCore.Benchmarks
+{
+    public class Config : ManualConfig
+    {
+        public Config()
+        {
+            // uncomment if you want to use any of the diagnoser
+            //Add(new BenchmarkDotNet.Diagnostics.MemoryDiagnoser());
+            //Add(new BenchmarkDotNet.Diagnostics.InliningDiagnoser());
+        }
+    }
+}

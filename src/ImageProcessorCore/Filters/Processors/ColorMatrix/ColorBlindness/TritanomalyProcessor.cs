267d5f4da5480a5d89741c51a58ba64687a142de
James Jackson-South <james_south@hotmail.com>
2016-07-27 23:23:08 +1000

    Add ColorBlindness
    Former-commit-id: 5ce61ca1e508ed598370b5817fa54fa6aaf7ecff
    Former-commit-id: b04c8676420dad90a6cb9515bdc8b9db67752576
    Former-commit-id: 98da045e3b41b5369a5d8ef393b2b22b8c011665

---
+++ src/ImageProcessorCore/Filters/Processors/ColorMatrix/ColorBlindness/TritanomalyProcessor.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="TritanomalyProcessor.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Processors
+{
+    using System.Numerics;
+
+    /// <summary>
+    /// Converts the colors of the image recreating Tritanomaly (Blue-Weak) color blindness.
+    /// </summary>
+    /// <typeparam name="T">The pixel format.</typeparam>
+    /// <typeparam name="TP">The packed format. <example>long, float.</example></typeparam>
+    public class TritanomalyProcessor<T, TP> : ColorMatrixFilter<T, TP>
+        where T : IPackedVector<TP>
+        where TP : struct
+    {
+        /// <inheritdoc/>
+        public override Matrix4x4 Matrix => new Matrix4x4()
+        {
+            M11 = 0.967f,
+            M21 = 0.33f,
+            M22 = 0.733f,
+            M23 = 0.183f,
+            M32 = 0.267f,
+            M33 = 0.817f
+        };
+
+        /// <inheritdoc/>
+        public override bool Compand => false;
+    }
+}

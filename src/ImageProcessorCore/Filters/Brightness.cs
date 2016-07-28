a3c1d1302f9fa9ca86f4b4d2dbecc02b25d9f260
James Jackson-South <james_south@hotmail.com>
2016-07-28 13:29:01 +1000

    Hue, Brightness
    Former-commit-id: 390a36f397e55d0caeaca4d5353be432da1c96b9
    Former-commit-id: a802e31bbbfe18164aa7f17b057bcf69d3f495b3
    Former-commit-id: 6275da035cee61d2d7abf1da32acc7db5f6aae2d

--- src/ImageProcessorCore/Filters/Brightness.cs
+++ src/ImageProcessorCore/Filters/Brightness.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Saturation.cs" company="James Jackson-South">
+﻿// <copyright file="Brightness.cs" company="James Jackson-South">
 // Copyright (c) James Jackson-South and contributors.
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
@@ -13,38 +13,38 @@ namespace ImageProcessorCore
     public static partial class ImageExtensions
     {
         /// <summary>
-        /// Alters the saturation component of the image.
+        /// Alters the brightness component of the image.
         /// </summary>
         /// <typeparam name="T">The pixel format.</typeparam>
         /// <typeparam name="TP">The packed format. <example>long, float.</example></typeparam>
         /// <param name="source">The image this method extends.</param>
-        /// <param name="amount">The new saturation of the image. Must be between -100 and 100.</param>
+        /// <param name="amount">The new brightness of the image. Must be between -100 and 100.</param>
         /// <param name="progressHandler">A delegate which is called as progress is made processing the image.</param>
         /// <returns>The <see cref="Image{T,TP}"/>.</returns>
-        public static Image<T, TP> Saturation<T, TP>(this Image<T, TP> source, int amount, ProgressEventHandler progressHandler = null)
+        public static Image<T, TP> Brightness<T, TP>(this Image<T, TP> source, int amount, ProgressEventHandler progressHandler = null)
             where T : IPackedVector<TP>
             where TP : struct
         {
-            return Saturation(source, amount, source.Bounds, progressHandler);
+            return Brightness(source, amount, source.Bounds, progressHandler);
         }
 
         /// <summary>
-        /// Alters the saturation component of the image.
+        /// Alters the brightness component of the image.
         /// </summary>
         /// <typeparam name="T">The pixel format.</typeparam>
         /// <typeparam name="TP">The packed format. <example>long, float.</example></typeparam>
         /// <param name="source">The image this method extends.</param>
-        /// <param name="amount">The new saturation of the image. Must be between -100 and 100.</param>
+        /// <param name="amount">The new brightness of the image. Must be between -100 and 100.</param>
         /// <param name="rectangle">
         /// The <see cref="Rectangle"/> structure that specifies the portion of the image object to alter.
         /// </param>
         /// <param name="progressHandler">A delegate which is called as progress is made processing the image.</param>
         /// <returns>The <see cref="Image{T,TP}"/>.</returns>
-        public static Image<T, TP> Saturation<T, TP>(this Image<T, TP> source, int amount, Rectangle rectangle, ProgressEventHandler progressHandler = null)
+        public static Image<T, TP> Brightness<T, TP>(this Image<T, TP> source, int amount, Rectangle rectangle, ProgressEventHandler progressHandler = null)
             where T : IPackedVector<TP>
             where TP : struct
         {
-            SaturationProcessor<T, TP> processor = new SaturationProcessor<T, TP>(amount);
+            BrightnessProcessor<T, TP> processor = new BrightnessProcessor<T, TP>(amount);
             processor.OnProgress += progressHandler;
 
             try
---
+++ tests/ImageProcessorCore.Tests/Processors/Filters/HueTest.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="HueTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Tests
+{
+    using System.IO;
+
+    using Xunit;
+
+    public class HueTest : FileTestBase
+    {
+        public static readonly TheoryData<int> HueValues
+        = new TheoryData<int>
+        {
+            180 ,
+           -180 ,
+        };
+
+        [Theory]
+        [MemberData("HueValues")]
+        public void ImageShouldApplyHueFilter(int value)
+        {
+            const string path = "TestOutput/Hue";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.Hue(value)
+                             .Save(output);
+                    }
+                }
+            }
+        }
+    }
+}

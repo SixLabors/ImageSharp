b442c4a49176cc1cf9513ff0ea6ca876c34f86b5
James Jackson-South <james_south@hotmail.com>
2016-07-28 11:07:17 +1000

    Add Kodachrome
    Former-commit-id: 395bbf802c958615ed5e497675200a9868bd816d
    Former-commit-id: ccc6ea01f5e2cf203991f4425e88faab7c56ac4c
    Former-commit-id: 3ff1d2812e905ee76bdbf50c4ccd4c6e0ad62a82

---
+++ tests/ImageProcessorCore.Tests/Processors/Filters/KodachromeTest.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="KodachromeTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Tests
+{
+    using System.IO;
+
+    using Xunit;
+
+    public class KodachromeTest : FileTestBase
+    {
+        [Fact]
+        public void ImageShouldApplyKodachromeFilter()
+        {
+            const string path = "TestOutput/Kodachrome";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileName(file);
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.Kodachrome()
+                             .Save(output);
+                    }
+                }
+            }
+        }
+    }
+}

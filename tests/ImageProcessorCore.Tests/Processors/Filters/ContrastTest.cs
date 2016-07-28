ff30ab089e7f12910eeac899fdc86971df61832c
James Jackson-South <james_south@hotmail.com>
2016-07-28 13:53:55 +1000

    Contrast
    Former-commit-id: 9aae78349c3150f3dff653fe4b065d6c305d3a1a
    Former-commit-id: 23d52315bcd126733230bd6ed8cc4f9566b6bd9a
    Former-commit-id: 29dfaaec8e8759096af680fc188541574002d297

---
+++ tests/ImageProcessorCore.Tests/Processors/Filters/ContrastTest.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="ContrastTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Tests
+{
+    using System.IO;
+
+    using Xunit;
+
+    public class ContrastTest : FileTestBase
+    {
+        public static readonly TheoryData<int> ContrastValues
+        = new TheoryData<int>
+        {
+            50 ,
+           -50 ,
+        };
+
+        [Theory]
+        [MemberData("ContrastValues")]
+        public void ImageShouldApplyContrastFilter(int value)
+        {
+            const string path = "TestOutput/Contrast";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.Contrast(value)
+                             .Save(output);
+                    }
+                }
+            }
+        }
+    }
+}

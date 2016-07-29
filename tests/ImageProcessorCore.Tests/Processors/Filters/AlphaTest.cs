e46d3ff686f80b16065b3bc487060fcfa2759290
James Jackson-South <james_south@hotmail.com>
2016-07-29 16:50:37 +1000

    Alpha
    Former-commit-id: a4b0ac45cb416e863496495ff9ac3d71585a46e6
    Former-commit-id: ea2e5a6d2315d14f6ed566ff6c91b73c83b1feb5
    Former-commit-id: 8a3cf0d04075c5da03b5d1ab747013d1efdd6491

---
+++ tests/ImageProcessorCore.Tests/Processors/Filters/AlphaTest.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="AlphaTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Tests
+{
+    using System.IO;
+
+    using Xunit;
+
+    public class AlphaTest : FileTestBase
+    {
+        public static readonly TheoryData<int> AlphaValues
+        = new TheoryData<int>
+        {
+            20 ,
+            80 ,
+        };
+
+        [Theory]
+        [MemberData("AlphaValues")]
+        public void ImageShouldApplyAlphaFilter(int value)
+        {
+            const string path = "TestOutput/Alpha";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.Alpha(value)
+                             .Save(output);
+                    }
+                }
+            }
+        }
+    }
+}

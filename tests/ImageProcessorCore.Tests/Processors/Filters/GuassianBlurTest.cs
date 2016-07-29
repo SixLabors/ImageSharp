9ecc7510e301d49681f3f8d72a990e8537904f57
James Jackson-South <james_south@hotmail.com>
2016-07-29 11:29:47 +1000

    Guassian Blur/Sharpen
    Former-commit-id: 8a5b4cf3f2f77979e055f7244a07152f3e7911b3
    Former-commit-id: 3c88eca8695868dafeb4478865530b94a4fff195
    Former-commit-id: e31d42ca178b55324a3700280a67fc7acd90f0a8

---
+++ tests/ImageProcessorCore.Tests/Processors/Filters/GuassianBlurTest.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="GuassianBlurTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Tests
+{
+    using System.IO;
+
+    using Xunit;
+
+    public class GuassianBlurTest : FileTestBase
+    {
+        public static readonly TheoryData<int> GuassianBlurValues
+        = new TheoryData<int>
+        {
+            3 ,
+            5 ,
+        };
+
+        [Theory]
+        [MemberData("GuassianBlurValues")]
+        public void ImageShouldApplyGuassianBlurFilter(int value)
+        {
+            const string path = "TestOutput/GuassianBlur";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.GuassianBlur(value)
+                             .Save(output);
+                    }
+                }
+            }
+        }
+    }
+}

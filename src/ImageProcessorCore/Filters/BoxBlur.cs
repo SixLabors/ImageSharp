216d2af3b33cdb160c4993efee0c645bb7151e7f
James Jackson-South <james_south@hotmail.com>
2016-07-29 11:44:01 +1000

    BoxBlur missed file
    Former-commit-id: 958ce2c8cbd0ac27babbc39493ec77b42a926d85
    Former-commit-id: 07d296a044d712bd0390558aec74fafc643a390e
    Former-commit-id: 2a64e2f642fafc3e8689dfa3e01165b085057c35

--- src/ImageProcessorCore/Filters/BoxBlur.cs
+++ src/ImageProcessorCore/Filters/BoxBlur.cs
@@ -1,6 +1,7 @@
 ﻿// <copyright file="BoxBlur.cs" company="James Jackson-South">
 // Copyright (c) James Jackson-South and contributors.
 // Licensed under the Apache License, Version 2.0.
+// </copyright>
 
 namespace ImageProcessorCore
 {

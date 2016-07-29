400b62f9647bef6cd49cb733ea7a38513fc3575e
James Jackson-South <james_south@hotmail.com>
2016-07-29 11:43:42 +1000

    Box Blur
    Former-commit-id: 5ac93d8e7ce1bb397a8ee7a68307aa423a85882a
    Former-commit-id: 7b8b0c704f7f272867516203133bb1c3fbbbf5a5
    Former-commit-id: e495b72e24e1f723023cc537749e03a987b9eb8c

---
+++ tests/ImageProcessorCore.Tests/Processors/Filters/BoxBlurTest.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="BoxBlurTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Tests
+{
+    using System.IO;
+
+    using Xunit;
+
+    public class BoxBlurTest : FileTestBase
+    {
+        public static readonly TheoryData<int> BoxBlurValues
+        = new TheoryData<int>
+        {
+            3 ,
+            5 ,
+        };
+
+        [Theory]
+        [MemberData("BoxBlurValues")]
+        public void ImageShouldApplyBoxBlurFilter(int value)
+        {
+            const string path = "TestOutput/BoxBlur";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.BoxBlur(value)
+                             .Save(output);
+                    }
+                }
+            }
+        }
+    }
+}

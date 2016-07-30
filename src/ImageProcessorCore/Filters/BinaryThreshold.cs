1761a6620dc1445063d475ad0b06b2808cc44b5d
James Jackson-South <james_south@hotmail.com>
2016-07-30 11:55:31 +1000

    Better sampler tests
    Fixes thresholder.
    
    
    Former-commit-id: 38af3bd0e8028989997c45c12f865601e36e1368
    Former-commit-id: b9e84eb7f0a56c5f896498f71838f790651b3398
    Former-commit-id: 7c5d5789d6fca88d9b5f751124dbb9d24efd8ea2

---
+++ src/ImageProcessorCore/Filters/BinaryThreshold.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="BinaryThreshold.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore
+{
+    using Processors;
+
+    /// <summary>
+    /// Extension methods for the <see cref="Image{T,TP}"/> type.
+    /// </summary>
+    public static partial class ImageExtensions
+    {
+        /// <summary>
+        /// Applies binerization to the image splitting the pixels at the given threshold.
+        /// </summary>
+        /// <typeparam name="T">The pixel format.</typeparam>
+        /// <typeparam name="TP">The packed format. <example>long, float.</example></typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="threshold">The threshold to apply binerization of the image. Must be between 0 and 1.</param>
+        /// <param name="progressHandler">A delegate which is called as progress is made processing the image.</param>
+        /// <returns>The <see cref="Image{T,TP}"/>.</returns>
+        public static Image<T, TP> BinaryThreshold<T, TP>(this Image<T, TP> source, float threshold, ProgressEventHandler progressHandler = null)
+            where T : IPackedVector<TP>
+            where TP : struct
+        {
+            return BinaryThreshold(source, threshold, source.Bounds, progressHandler);
+        }
+
+        /// <summary>
+        /// Applies binerization to the image splitting the pixels at the given threshold.
+        /// </summary>
+        /// <typeparam name="T">The pixel format.</typeparam>
+        /// <typeparam name="TP">The packed format. <example>long, float.</example></typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="threshold">The threshold to apply binerization of the image. Must be between 0 and 1.</param>
+        /// <param name="rectangle">
+        /// The <see cref="Rectangle"/> structure that specifies the portion of the image object to alter.
+        /// </param>
+        /// <param name="progressHandler">A delegate which is called as progress is made processing the image.</param>
+        /// <returns>The <see cref="Image{T,TP}"/>.</returns>
+        public static Image<T, TP> BinaryThreshold<T, TP>(this Image<T, TP> source, float threshold, Rectangle rectangle, ProgressEventHandler progressHandler = null)
+            where T : IPackedVector<TP>
+            where TP : struct
+        {
+            BinaryThresholdProcessor<T, TP> processor = new BinaryThresholdProcessor<T, TP>(threshold);
+            processor.OnProgress += progressHandler;
+
+            try
+            {
+                return source.Process(rectangle, processor);
+            }
+            finally
+            {
+                processor.OnProgress -= progressHandler;
+            }
+        }
+    }
+}

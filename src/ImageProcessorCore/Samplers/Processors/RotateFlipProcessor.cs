b2912f86253f6c407287b448be7760656a53b4e0
James Jackson-South <james_south@hotmail.com>
2016-07-30 11:59:56 +1000

    Add missing class docs
    Former-commit-id: 74c642cce368d8e8a917a71b05e06c9f59a665b8
    Former-commit-id: 0b8453f330d8be2187f941ddbd4e9d81431e2cf0
    Former-commit-id: 4b4a7e399c6662f0646ddb144b28fe71084a9ca8

--- src/ImageProcessorCore/Samplers/Processors/RotateFlipProcessor.cs
+++ src/ImageProcessorCore/Samplers/Processors/RotateFlipProcessor.cs
@@ -11,6 +11,8 @@ namespace ImageProcessorCore.Processors
     /// <summary>
     /// Provides methods that allow the rotation and flipping of an image around its center point.
     /// </summary>
+    /// <typeparam name="T">The pixel format.</typeparam>
+    /// <typeparam name="TP">The packed format. <example>long, float.</example></typeparam>
     public class RotateFlipProcessor<T, TP> : ImageSampler<T, TP>
         where T : IPackedVector<TP>
         where TP : struct

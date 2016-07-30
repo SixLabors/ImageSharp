4e9e4d0db94306869b762fa617d6f12d64324236
James Jackson-South <james_south@hotmail.com>
2016-07-30 11:55:31 +1000

    Better sampler tests
    Fixes thresholder.
    
    
    Former-commit-id: 38af3bd0e8028989997c45c12f865601e36e1368
    Former-commit-id: b9e84eb7f0a56c5f896498f71838f790651b3398
    Former-commit-id: 7c5d5789d6fca88d9b5f751124dbb9d24efd8ea2

---
+++ tests/ImageProcessorCore.Tests/Processors/Filters/BinaryThreshold.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="BinaryThresholdTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Tests
+{
+    using System.IO;
+
+    using Xunit;
+
+    public class BinaryThresholdTest : FileTestBase
+    {
+        public static readonly TheoryData<float> BinaryThresholdValues
+        = new TheoryData<float>
+        {
+            .25f ,
+            .75f ,
+        };
+
+        [Theory]
+        [MemberData("BinaryThresholdValues")]
+        public void ImageShouldApplyBinaryThresholdFilter(float value)
+        {
+            const string path = "TestOutput/BinaryThreshold";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.BinaryThreshold(value)
+                             .Save(output);
+                    }
+                }
+            }
+        }
+    }
+}

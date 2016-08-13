3ce2d964e59a1bbe6e015a1eb26ee2d34bfa5259
dirk <dirk@git.imagemagick.org>
2016-08-13 15:48:40 +0200

    Added Tests for code that was moved.
    Former-commit-id: e9adf42e34c17e9518c927a32384b56b915dc43d
    Former-commit-id: 27a5087388b5222b321a13e5bf4931c6b59d56df
    Former-commit-id: 93f08ba739fd4d68d921817cddfae19d59cece59

--- tests/ImageProcessorCore.Tests/Processors/Samplers/RotateTest.cs
+++ tests/ImageProcessorCore.Tests/Processors/Samplers/RotateTest.cs
@@ -11,16 +11,52 @@ namespace ImageProcessorCore.Tests
 
     public class RotateTest : FileTestBase
     {
-        public static readonly TheoryData<float> RotateValues
-        = new TheoryData<float>
+        public static readonly TheoryData<float> RotateFloatValues
+            = new TheoryData<float>
         {
              170 ,
             -170 ,
+              90 ,
+        };
+
+        public static readonly TheoryData<RotateType> RotateEnumValues
+            = new TheoryData<RotateType>
+        {
+            RotateType.None,
+            RotateType.Rotate90,
+            RotateType.Rotate180,
+            RotateType.Rotate270
         };
 
         [Theory]
-        [MemberData("RotateValues")]
+        [MemberData("RotateFloatValues")]
         public void ImageShouldApplyRotateSampler(float value)
+        {
+            const string path = "TestOutput/Rotate";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.Rotate(value)
+                             .Save(output);
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData("RotateEnumValues")]
+        public void ImageShouldApplyRotateSampler(RotateType value)
         {
             const string path = "TestOutput/Rotate";
             if (!Directory.Exists(path))

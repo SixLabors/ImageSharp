3a2d001eaf1ce777fa27db17ba6737a9d995817c
dirk <dirk@git.imagemagick.org>
2016-08-13 15:31:12 +0200

    Moved the Flip and Rotate code from FlipRotate to their own Processors and call those inside RotateFlip.
    Former-commit-id: 59a0db810427fcfd9c7426392f5de94317d928bd
    Former-commit-id: 89d27c015fbc972372f3697c06864dd71f458574
    Former-commit-id: 160b946dcbfc868be3d014c2ef6410cbb40d4ef7

--- src/ImageProcessorCore/Samplers/Flip.cs
+++ src/ImageProcessorCore/Samplers/Flip.cs
@@ -13,7 +13,7 @@ namespace ImageProcessorCore
     public static partial class ImageExtensions
     {
         /// <summary>
-        /// Rotates and flips an image by the given instructions.
+        /// Flips an image by the given instructions.
         /// </summary>
         /// <typeparam name="T">The pixel format.</typeparam>
         /// <typeparam name="TP">The packed format. <example>long, float.</example></typeparam>
@@ -21,13 +21,21 @@ namespace ImageProcessorCore
         /// <param name="flipType">The <see cref="FlipType"/> to perform the flip.</param>
         /// <param name="progressHandler">A delegate which is called as progress is made processing the image.</param>
         /// <returns>The <see cref="Image"/></returns>
-        public static Image<T, TP> RotateFlip<T, TP>(this Image<T, TP> source, RotateType rotateType, FlipType flipType, ProgressEventHandler progressHandler = null)
+        public static Image<T, TP> Flip<T, TP>(this Image<T, TP> source, FlipType flipType, ProgressEventHandler progressHandler = null)
             where T : IPackedVector<TP>
             where TP : struct
         {
-            return source
-                .Rotate(rotateType, progressHandler)
-                .Flip(flipType, progressHandler);
+            FlipProcessor<T, TP> processor = new FlipProcessor<T, TP>(flipType);
+            processor.OnProgress += progressHandler;
+
+            try
+            {
+                return source.Process(source.Width, source.Height, source.Bounds, source.Bounds, processor);
+            }
+            finally
+            {
+                processor.OnProgress -= progressHandler;
+            }
         }
     }
 }

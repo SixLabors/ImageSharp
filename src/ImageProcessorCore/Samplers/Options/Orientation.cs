266aad33eb46d869375566e398bfcabc0802b5aa
dirk <dirk@git.imagemagick.org>
2016-08-13 18:54:04 +0200

    Added option to change the orientation of an image based on the information in the Exif profile.
    Former-commit-id: ae4de1addaed801930aea365f4bc0a87d03f6e3f
    Former-commit-id: bb1e39e2ef759b391e88b7f3fbfd98ea97913b3f
    Former-commit-id: 97d2de47e68c5dafc524656ec3d95c1c1f72e7c2

---
+++ src/ImageProcessorCore/Samplers/Options/Orientation.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="AnchorPosition.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore
+{
+    internal enum Orientation : ushort
+    {
+        Unknown = 0,
+        TopLeft = 1,
+        TopRight = 2,
+        BottomRight = 3,
+        BottomLeft = 4,
+        LeftTop = 5,
+        RightTop = 6,
+        RightBottom = 7,
+        LeftBottom = 8
+    }
+}

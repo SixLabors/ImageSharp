cade6077043bed88349b9ca15dda4ab9d0826fb0
James Jackson-South <james_south@hotmail.com>
2016-08-18 01:48:12 +1000

    Begin greater png color format support. [skip ci]
    Former-commit-id: ee12a350d066acd2580895f89d852ef3c579237b
    Former-commit-id: cdce687f7966c694d232e7591667106fb9358918
    Former-commit-id: 92ed20377ae53847bb43b2f1f8a6d88f74653f6f

---
+++ src/ImageProcessorCore/Formats/Png/Filters/PaethFilter.cs
@@ -0,0 +1,66 @@
+﻿namespace ImageProcessorCore.Formats
+{
+    using System;
+
+    internal static class PaethFilter
+    {
+        public static byte[] Decode(byte[] scanline, byte[] previousScanline, int bytesPerPixel)
+        {
+            byte[] result = new byte[scanline.Length];
+
+            for (int x = 1; x < scanline.Length; x++)
+            {
+                byte left = (x - bytesPerPixel < 1) ? (byte)0 : result[x - bytesPerPixel];
+                byte above = previousScanline[x];
+                byte upperLeft = (x - bytesPerPixel < 1) ? (byte)0 : previousScanline[x - bytesPerPixel];
+
+                result[x] = (byte)((scanline[x] + PaethPredictor(left, above, upperLeft)) % 256);
+            }
+
+            return result;
+        }
+
+        public static byte[] Encode(byte[] scanline, byte[] previousScanline, int bytesPerPixel)
+        {
+            var encodedScanline = new byte[scanline.Length + 1];
+
+            encodedScanline[0] = (byte)FilterType.Paeth;
+
+            for (int x = 0; x < scanline.Length; x++)
+            {
+                byte left = (x - bytesPerPixel < 0) ? (byte)0 : scanline[x - bytesPerPixel];
+                byte above = previousScanline[x];
+                byte upperLeft = (x - bytesPerPixel < 0) ? (byte)0 : previousScanline[x - bytesPerPixel];
+
+                encodedScanline[x + 1] = (byte)((scanline[x] - PaethPredictor(left, above, upperLeft)) % 256);
+            }
+
+            return encodedScanline;
+        }
+
+        private static int PaethPredictor(int a, int b, int c)
+        {
+            int p = a + b - c;
+            int pa = Math.Abs(p - a);
+            int pb = Math.Abs(p - b);
+            int pc = Math.Abs(p - c);
+
+            if ((pa <= pb) && (pa <= pc))
+            {
+                return a;
+            }
+            else
+            {
+                if (pb <= pc)
+                {
+                    return b;
+                }
+                else
+                {
+                    return c;
+                }
+            }
+        }
+    }
+
+}
---
+++ src/ImageProcessorCore/Formats/Png/Filters/UpFilter.cs
@@ -0,0 +1,35 @@
+﻿namespace ImageProcessorCore.Formats
+{
+    internal static class UpFilter
+    {
+        public static byte[] Decode(byte[] scanline, byte[] previousScanline)
+        {
+            byte[] result = new byte[scanline.Length];
+
+            for (int x = 1; x < scanline.Length; x++)
+            {
+                byte above = previousScanline[x];
+
+                result[x] = (byte)((scanline[x] + above) % 256);
+            }
+
+            return result;
+        }
+
+        public static byte[] Encode(byte[] scanline, byte[] previousScanline)
+        {
+            var encodedScanline = new byte[scanline.Length + 1];
+
+            encodedScanline[0] = (byte)FilterType.Up;
+
+            for (int x = 0; x < scanline.Length; x++)
+            {
+                byte above = previousScanline[x];
+
+                encodedScanline[x + 1] = (byte)((scanline[x] - above) % 256);
+            }
+
+            return encodedScanline;
+        }
+    }
+}

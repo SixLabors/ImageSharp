ec5bb8feb897dff0a2c31836bcb549dc715c0fef
James Jackson-South <james_south@hotmail.com>
2016-09-08 16:22:55 +1000

    Add  oil painting effect.
    Former-commit-id: c9c4f16067cbca67fd055702fa15e71995e8ff35
    Former-commit-id: 6ffebcceb66d94c8a7920cb02edfcd572db29ac3
    Former-commit-id: 09721ad14a254868241f53d43095212ab20fefc8

---
+++ tests/ImageProcessorCore.Tests/Processors/Samplers/OilPaintTest.cs
@@ -0,0 +1,74 @@
+﻿// <copyright file="OilPaintTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Tests
+{
+    using System;
+    using System.IO;
+
+    using Xunit;
+
+    public class OilPaintTest : FileTestBase
+    {
+        public static readonly TheoryData<Tuple<int, int>> OilPaintValues
+            = new TheoryData<Tuple<int, int>>
+            {
+                new Tuple<int, int>(15,10),
+                new Tuple<int, int>(6,5)
+            };
+
+        [Theory]
+        [MemberData(nameof(OilPaintValues))]
+        public void ImageShouldApplyOilPaintFilter(Tuple<int, int> value)
+        {
+            const string path = "TestOutput/OilPaint";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.OilPaint(value.Item1, value.Item2)
+                             .Save(output);
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(OilPaintValues))]
+        public void ImageShouldApplyOilPaintFilterInBox(Tuple<int, int> value)
+        {
+            const string path = "TestOutput/OilPaint";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (string file in Files)
+            {
+                using (FileStream stream = File.OpenRead(file))
+                {
+                    string filename = Path.GetFileNameWithoutExtension(file) + "-" + value + "-InBox" + Path.GetExtension(file);
+
+                    Image image = new Image(stream);
+                    using (FileStream output = File.OpenWrite($"{path}/{filename}"))
+                    {
+                        image.OilPaint(value.Item1, value.Item2, new Rectangle(10, 10, image.Width / 2, image.Height / 2))
+                             .Save(output);
+                    }
+                }
+            }
+        }
+    }
+}

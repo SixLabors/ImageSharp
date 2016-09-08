bab0be1dde3a833205cb2c086ac93ca147e0b117
James Jackson-South <james_south@hotmail.com>
2016-09-08 16:19:16 +1000

    Organise tests
    Former-commit-id: 8c43c6aba24aa6983422a8b9645634c6b09f3a6b
    Former-commit-id: b5152d24a3f764fa3525a9f65dbefecb65305db9
    Former-commit-id: ee28c35506740387c1d91aeb902ff017790a58d3

--- tests/ImageProcessorCore.Tests/TestImages.cs
+++ tests/ImageProcessorCore.Tests/TestImages.cs
@@ -14,43 +14,43 @@ namespace ImageProcessorCore.Tests
         {
             private static readonly string folder = "TestImages/Formats/Png/";
 
-            public static string P1 { get { return folder + "pl.png"; } }
-            public static string Pd { get { return folder + "pd.png"; } }
-            public static string Blur { get { return folder + "blur.png"; } }
-            public static string Indexed { get { return folder + "indexed.png"; } }
-            public static string Splash { get { return folder + "splash.png"; } }
+            public static string P1 => folder + "pl.png";
+            public static string Pd => folder + "pd.png";
+            public static string Blur => folder + "blur.png";
+            public static string Indexed => folder + "indexed.png";
+            public static string Splash => folder + "splash.png";
         }
 
         public static class Jpg
         {
             private static readonly string folder = "TestImages/Formats/Jpg/";
-            public static string Cmyk { get { return folder + "cmyk.jpg"; } }
-            public static string Exif { get { return folder + "exif.jpeg"; } }
-            public static string Floorplan { get { return folder + "Floorplan.jpeg"; } }
-            public static string Calliphora { get { return folder + "Calliphora.jpg"; } }
-            public static string Turtle { get { return folder + "turtle.jpg"; } }
-            public static string Fb { get { return folder + "fb.jpg"; } }
-            public static string Progress { get { return folder + "progress.jpg"; } }
-            public static string Gamma_dalai_lama_gray { get { return folder + "gamma_dalai_lama_gray.jpg"; } }
+            public static string Cmyk => folder + "cmyk.jpg";
+            public static string Exif => folder + "exif.jpeg";
+            public static string Floorplan => folder + "Floorplan.jpeg";
+            public static string Calliphora => folder + "Calliphora.jpg";
+            public static string Turtle => folder + "turtle.jpg";
+            public static string Fb => folder + "fb.jpg";
+            public static string Progress => folder + "progress.jpg";
+            public static string GammaDalaiLamaGray => folder + "gamma_dalai_lama_gray.jpg";
         }
 
         public static class Bmp
         {
             private static readonly string folder = "TestImages/Formats/Bmp/";
 
-            public static string Car { get { return folder + "Car.bmp"; } }
+            public static string Car => folder + "Car.bmp";
 
-            public static string F { get { return folder + "F.bmp"; } }
+            public static string F => folder + "F.bmp";
 
-            public static string Neg_height { get { return folder + "neg_height.bmp"; } }
+            public static string NegHeight => folder + "neg_height.bmp";
         }
 
         public static class Gif
         {
             private static readonly string folder = "TestImages/Formats/Gif/";
 
-            public static string Rings { get { return folder + "rings.gif"; } }
-            public static string Giphy { get { return folder + "giphy.gif"; } }
+            public static string Rings => folder + "rings.gif";
+            public static string Giphy => folder + "giphy.gif";
         }
     }
 }

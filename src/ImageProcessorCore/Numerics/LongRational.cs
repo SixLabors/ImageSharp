ea016a4af560f45632d419fea1e9250c744c07cf
dirk <dirk@git.imagemagick.org>
2016-10-21 16:58:26 +0200

    Removed invalid format specifier.
    Former-commit-id: 0d711034d8854d6c1c23a377fdb1033182981468
    Former-commit-id: 53d308be6aa2b18d0f5bb8ad2484ebccb96bf6cd
    Former-commit-id: 0901d2559abaca309f19932e602df5671c6389a3

--- src/ImageProcessorCore/Numerics/LongRational.cs
+++ src/ImageProcessorCore/Numerics/LongRational.cs
@@ -271,7 +271,7 @@ namespace ImageProcessorCore
             }
 
             StringBuilder sb = new StringBuilder();
-            sb.Append(this.Numerator.ToString("R", provider));
+            sb.Append(this.Numerator.ToString(provider));
             sb.Append("/");
             sb.Append(this.Denominator.ToString("R", provider));
             return sb.ToString();

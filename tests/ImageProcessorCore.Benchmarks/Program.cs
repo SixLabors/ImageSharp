3550bf1d673f685226fbcd59f30cdccb0e84535d
dirk <dirk@git.imagemagick.org>
2016-10-22 10:33:19 +0200

    Added headers.
    Former-commit-id: 636061f6fa83ad9d07703cde87587a8a92a41296
    Former-commit-id: a9051b9e985b7e449af99da11f57d6ff1104efc3
    Former-commit-id: 11bb7d89d672c12c4fa00bbe7f9826eca6dc189d

--- tests/ImageProcessorCore.Benchmarks/Program.cs
+++ tests/ImageProcessorCore.Benchmarks/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace ImageProcessorCore.Benchmarks
+﻿// <copyright file="Program.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore.Benchmarks
 {
     using BenchmarkDotNet.Running;
 

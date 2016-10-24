83582b8999eb2e06c204ce941c838e6d16ca8fbb
dirk <dirk@git.imagemagick.org>
2016-10-24 18:15:46 +0200

    Added new interface to convert an IPackedPixel to and from a set of bytes.
    Former-commit-id: 63481b92335c358cfeb10da5df3c4fb79abf7786
    Former-commit-id: 2e9a02d7896340fbd9610c358ba4cd472ac124a1
    Former-commit-id: bb5030a386fb749f779e5acddd462832d6412ee2

---
+++ src/ImageProcessorCore/Colors/PackedPixel/IPackedBytes.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="IPackedBytes.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageProcessorCore
+{
+    /// <summary>
+    /// An interface that converts packed vector types to and from <see cref="byte[]"/> values, 
+    /// allowing multiple encodings to be manipulated in a generic manner.
+    /// </summary>
+    public interface IPackedBytes
+    {
+        /// <summary>
+        /// Gets the packed representation from the gives bytes.
+        /// </summary>
+        /// <param name="x">The x-component.</param>
+        /// <param name="y">The y-component.</param>
+        /// <param name="z">The z-omponent.</param>
+        /// <param name="w">The w-component.</param>
+        void PackFromBytes(byte x, byte y, byte z, byte w);
+
+        /// <summary>
+        /// Sets the packed representation into the gives bytes.
+        /// </summary>
+        /// <param name="bytes">The bytes to set the color in.</param>
+        /// <param name="startIndex">The starting index of the <paramref name="bytes"/>.</param>
+        /// <param name="componentOrder">The order of the components.</param>
+        void ToBytes(byte[] bytes, int startIndex, ComponentOrder componentOrder);
+    }
+}

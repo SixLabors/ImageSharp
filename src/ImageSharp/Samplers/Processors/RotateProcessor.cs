1c60129c62b64477c605b2b5aa1ef552e3becfa0
dirk <dirk@git.imagemagick.org>
2016-10-29 23:33:09 +0200

    Moved using inside namespace.

--- src/ImageSharp/Samplers/Processors/RotateProcessor.cs
+++ src/ImageSharp/Samplers/Processors/RotateProcessor.cs
@@ -3,10 +3,9 @@
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
 
-using System;
-
 namespace ImageSharp.Processors
 {
+    using System;
     using System.Numerics;
     using System.Threading.Tasks;
 

fa9b74678d3433c7941998e71bfb19af714ec91b
James Jackson-South <james_south@hotmail.com>
2016-11-08 18:03:56 +1100

    Revert "Change build number configuration"
    This reverts commit 35b626714be47b0f3ba245670144d0050dc69be2.

--- build/appveyor-project-version-patch.js
+++ build/appveyor-project-version-patch.js
@@ -4,14 +4,14 @@ var semver = require("semver");
 var file = "../src/imagesharp/project.json";
 var buildVersion = process.env.APPVEYOR_BUILD_VERSION.substring(1);
 
-var findPoint = buildVersion.lastIndexOf(".");
-var basePackageVer = buildVersion.substring(0, findPoint);
-var buildNumber = buildVersion.substring(findPoint + 1, buildVersion.length);
-var semversion = semver.valid(basePackageVer + "-alpha" + buildNumber.length === 2 ? buildNumber : "0" + buildNumber);
+var findPoint       = buildVersion.lastIndexOf(".");
+var basePackageVer  = buildVersion.substring(0, findPoint);
+var buildNumber     = buildVersion.substring(findPoint + 1, buildVersion.length);
+var semversion = semver.valid(basePackageVer + "-alpha" + buildNumber);
 
 jsonfile.readFile(file, function (err, project) {
-    project.version = semversion;
-    jsonfile.writeFile(file, project, { spaces: 2 }, function (err) {
-        console.error(err);
-    });
+	project.version = semversion;
+	jsonfile.writeFile(file, project, {spaces: 2}, function(err) {
+		console.error(err);
+	});
 })

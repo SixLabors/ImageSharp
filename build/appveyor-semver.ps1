e21d9a90fa55f56ed969d40f92820aa969316b81
Jeavon <jeavon@crumpled-dog.com>
2016-11-08 09:45:22 +0000

    Add ps for generating semver version numbers

---
+++ build/appveyor-semver.ps1
@@ -0,0 +1,16 @@
+$version=[Version]$Env:APPVEYOR_BUILD_VERSION
+$version_suffix=$Env:version_suffix
+
+$basever=$version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString()
+
+$semver = $basever + "-" + $version_suffix + "." + $version.Revision.ToString().PadLeft(6,"0")
+$mssemver = $basever + "-" + $version_suffix + "-" + $version.Revision.ToString().PadLeft(6,"0")
+$appveyor_version = $mssemver
+
+$Env:semver = $semver
+$Env:mssemver = $mssemver
+$Env:appveyor_version = $appveyor_version
+
+"Envrionment variable 'semver' set:" + $Env:semver
+"Envrionment variable 'mssemver' set:" + $Env:mssemver
+"Envrionment variable 'appveyor_version' set:" + $Env:appveyor_version

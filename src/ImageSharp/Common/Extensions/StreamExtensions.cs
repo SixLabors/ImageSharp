c188e5aa7990598a46afbfe114da8ea57ee885d5
dirk <dirk@git.imagemagick.org>
2016-11-08 20:40:12 +0100

    Added Skip extension to support streams that cannot seek.

---
+++ src/ImageSharp/Common/Extensions/StreamExtensions.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="StreamExtensions.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp
+{
+    using System.IO;
+
+    internal static class StreamExtensions
+    {
+        public static void Skip(this Stream stream, int count)
+        {
+            if (stream.CanSeek)
+            {
+                stream.Position += count;
+            }
+            else
+            {
+                byte[] foo = new byte[count];
+                stream.Read(foo, 0, count);
+            }
+        }
+    }
+}

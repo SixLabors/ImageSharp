d49621c27982fa34c5e24861d4e37d2b213a7e1c
Jeavon <jeavon@crumpled-dog.com>
2016-11-08 09:02:24 +0000

    Add padded build numbers so that NuGet knows which is the latest version

--- build/appveyor-project-version-patch.js
+++ build/appveyor-project-version-patch.js
@@ -4,10 +4,12 @@ var semver = require("semver");
 var file = "../src/imagesharp/project.json";
 var buildVersion = process.env.APPVEYOR_BUILD_VERSION.substring(1);
 
-var findPoint       = buildVersion.lastIndexOf(".");
-var basePackageVer  = buildVersion.substring(0, findPoint);
-var buildNumber     = buildVersion.substring(findPoint + 1, buildVersion.length);
-var semversion = semver.valid(basePackageVer + "-alpha" + buildNumber);
+var findPoint = buildVersion.lastIndexOf(".");
+var basePackageVer = buildVersion.substring(0, findPoint);
+var buildNumber = buildVersion.substring(findPoint + 1, buildVersion.length);
+var pad = "000000";
+var paddedBuildNumber = pad.substring(0, pad.length - buildNumber.length) + buildNumber;
+var semversion = semver.valid(basePackageVer + "-alpha" + paddedBuildNumber);
 
 jsonfile.readFile(file, function (err, project) {
 	project.version = semversion;

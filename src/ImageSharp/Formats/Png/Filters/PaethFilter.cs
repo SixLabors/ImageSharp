f814dc3609c53d33ba42ac0bdf24f6b799f8420a
James Jackson-South <james_south@hotmail.com>
2016-11-10 12:58:09 +1100

    Pass filtered result scanline by reference.
    Halfs the memory usage

--- src/ImageSharp/Formats/Png/Filters/PaethFilter.cs
+++ src/ImageSharp/Formats/Png/Filters/PaethFilter.cs
@@ -52,11 +52,9 @@ namespace ImageSharp.Formats
         /// <param name="bytesPerPixel">The bytes per pixel.</param>
         /// <param name="bytesPerScanline">The number of bytes per scanline</param>
         /// <returns>The <see cref="T:byte[]"/></returns>
-        public static byte[] Encode(byte[] scanline, byte[] previousScanline, int bytesPerPixel, int bytesPerScanline)
+        public static byte[] Encode(byte[] scanline, byte[] previousScanline, byte[] result, int bytesPerPixel, int bytesPerScanline)
         {
             // Paeth(x) = Raw(x) - PaethPredictor(Raw(x-bpp), Prior(x), Prior(x - bpp))
-            byte[] result = new byte[bytesPerScanline + 1];
-
             fixed (byte* scan = scanline)
             fixed (byte* prev = previousScanline)
             fixed (byte* res = result)

2256fda58fc4db7a4ec48ba57b42c4fa7023e44e
Anton Firszov <anton.firszov@sapagroup.com>
2016-11-12 13:39:25 +0100

    Buffer64

---
+++ src/ImageSharp46/Formats/Jpg/Components/Span.cs
@@ -0,0 +1,100 @@
+﻿using System.Buffers;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace ImageSharp.Formats
+{
+    public struct Span<T>
+    {
+        public T[] Data;
+        public int Offset;
+
+        public int TotalCount => Data.Length - Offset;
+
+        public Span(int size, int offset = 0)
+        {
+            Data = new T[size];
+            Offset = offset;
+        }
+
+        public Span(T[] data, int offset = 0)
+        {
+            Data = data;
+            Offset = offset;
+        }
+
+        public T this[int idx]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)] get { return Data[idx + Offset]; }
+            [MethodImpl(MethodImplOptions.AggressiveInlining)] set { Data[idx + Offset] = value; }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> Slice(int offset)
+        {
+            return new Span<T>(Data, Offset + offset);
+        }
+
+        public static implicit operator Span<T>(T[] data) => new Span<T>(data, 0);
+
+        private static readonly ArrayPool<T> Pool = ArrayPool<T>.Create(128, 10);
+
+        public static Span<T> RentFromPool(int size, int offset = 0)
+        {
+            return new Span<T>(Pool.Rent(size), offset);
+        }
+
+        public void ReturnToPool()
+        {
+            Pool.Return(Data, true);
+            Data = null;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AddOffset(int offset)
+        {
+            Offset += offset;
+        }
+    }
+
+    public static class SpanExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SaveTo(this Span<float> data, ref Vector4 v)
+        {
+            v.X = data[0];
+            v.Y = data[1];
+            v.Z = data[2];
+            v.W = data[3];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SaveTo(this Span<int> data, ref Vector4 v)
+        {
+            v.X = data[0];
+            v.Y = data[1];
+            v.Z = data[2];
+            v.W = data[3];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LoadFrom(this Span<float> data, ref Vector4 v)
+        {
+            data[0] = v.X;
+            data[1] = v.Y;
+            data[2] = v.Z;
+            data[3] = v.W;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LoadFrom(this Span<int> data, ref Vector4 v)
+        {
+            data[0] = (int)v.X;
+            data[1] = (int)v.Y;
+            data[2] = (int)v.Z;
+            data[3] = (int)v.W;
+        }
+
+
+    }
+}
---
+++ tests/ImageSharp.Tests46/Formats/Jpg/DctTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+using ImageSharp.Formats;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ImageSharp.Tests.Formats.Jpg
+{
+    public class DctTests : UtilityTestClassBase
+    {
+        public DctTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact]
+        public void Mennyi()
+        {
+            Output.WriteLine(Vector.IsHardwareAccelerated.ToString());
+            Output.WriteLine(Vector<float>.Count.ToString());
+        }
+
+        [Fact]
+        public void CheckTestData()
+        {
+            var data = Create8x8FloatData();
+
+            Print8x8Data(data);
+        }
+
+        [Fact]
+        public void Load_Store()
+        {
+            var data = Create8x8FloatData();
+
+            var m = ReferenceDCT.Load(data, 1, 1);
+            m = Matrix4x4.Transpose(m);
+
+            ReferenceDCT.Store(m, data, 4, 4);
+
+            Print8x8Data(data);
+        }
+
+        [Fact]
+        public void Transpose8x8()
+        {
+            var data = Create8x8FloatData();
+
+            Span<float> result = new Span<float>(64);
+
+            ReferenceDCT.Transpose8x8(data, result);
+
+            Print8x8Data(result.Data);
+        }
+
+        [Fact]
+        public void Transpose8x8_Inplace()
+        {
+            var data = Create8x8FloatData();
+
+            ReferenceDCT.Transpose8x8(data);
+
+            Print8x8Data(data);
+        }
+
+        
+    }
+}

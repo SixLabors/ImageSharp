272761f2cfdf310e800d78012b6edf31cff6d222
James Jackson-South <james_south@hotmail.com>
2016-11-13 16:41:52 +1100

    Change filter method signature

--- src/ImageSharp/Formats/Png/Filters/PaethFilter.cs
+++ src/ImageSharp/Formats/Png/Filters/PaethFilter.cs
@@ -21,11 +21,9 @@ namespace ImageSharp.Formats
         /// <param name="scanline">The scanline to decode</param>
         /// <param name="previousScanline">The previous scanline.</param>
         /// <param name="bytesPerPixel">The bytes per pixel.</param>
-        /// <returns>The <see cref="T:byte[]"/></returns>
-        public static byte[] Decode(byte[] scanline, byte[] previousScanline, int bytesPerPixel)
+        public static void Decode(byte[] scanline, byte[] previousScanline, int bytesPerPixel)
         {
             // Paeth(x) + PaethPredictor(Raw(x-bpp), Prior(x), Prior(x-bpp))
-
             fixed (byte* scan = scanline)
             fixed (byte* prev = previousScanline)
             {
@@ -38,8 +36,6 @@ namespace ImageSharp.Formats
                     scan[x] = (byte)((scan[x] + PaethPredicator(left, above, upperLeft)) % 256);
                 }
             }
-
-            return scanline;
         }
 
         /// <summary>

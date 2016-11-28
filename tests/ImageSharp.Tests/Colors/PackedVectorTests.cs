1a2180957b520a393de27edbedf8e175d26fce51
James Jackson-South <james_south@hotmail.com>
2016-11-29 01:11:45 +1100

    Add Bgr565

--- tests/ImageSharp.Tests/Colors/PackedVectorTests.cs
+++ tests/ImageSharp.Tests/Colors/PackedVectorTests.cs
@@ -5,6 +5,7 @@
 
 namespace ImageSharp.Tests.Colors
 {
+    using System;
     using System.Numerics;
 
     using Xunit;
@@ -31,6 +32,7 @@ namespace ImageSharp.Tests.Colors
 
             // Test ordering
             Vector4 vector = new Alpha8(.5F).ToVector4();
+
             Assert.Equal(vector.X, 0);
             Assert.Equal(vector.Y, 0);
             Assert.Equal(vector.Z, 0);
@@ -52,6 +54,73 @@ namespace ImageSharp.Tests.Colors
 
             new Alpha8(.5F).ToBytes(rgb, 0, ComponentOrder.ZYXW);
             Assert.Equal(bgra, new byte[] { 0, 0, 0, 128 });
+        }
+
+        [Fact]
+        public void Bgr565()
+        {
+            // Test the limits.
+            Assert.Equal(0x0, new Bgr565(Vector3.Zero).PackedValue);
+            Assert.Equal(0xFFFF, new Bgr565(Vector3.One).PackedValue);
+
+            // Test ToVector3.
+            Assert.True(Equal(Vector3.One, new Bgr565(Vector3.One).ToVector3()));
+            Assert.True(Equal(Vector3.Zero, new Bgr565(Vector3.Zero).ToVector3()));
+            Assert.True(Equal(Vector3.UnitX, new Bgr565(Vector3.UnitX).ToVector3()));
+            Assert.True(Equal(Vector3.UnitY, new Bgr565(Vector3.UnitY).ToVector3()));
+            Assert.True(Equal(Vector3.UnitZ, new Bgr565(Vector3.UnitZ).ToVector3()));
+
+            // Test clamping.
+            Assert.True(Equal(Vector3.Zero, new Bgr565(Vector3.One * -1234F).ToVector3()));
+            Assert.True(Equal(Vector3.One, new Bgr565(Vector3.One * 1234F).ToVector3()));
+
+            // Make sure the swizzle is correct.
+            Assert.Equal(0xF800, new Bgr565(Vector3.UnitX).PackedValue);
+            Assert.Equal(0x07E0, new Bgr565(Vector3.UnitY).PackedValue);
+            Assert.Equal(0x001F, new Bgr565(Vector3.UnitZ).PackedValue);
+
+            float x = 0.1F;
+            float y = -0.3F;
+            float z = 0.5F;
+            Assert.Equal(6160, new Bgr565(x, y, z).PackedValue);
+
+            byte[] rgb = new byte[3];
+            byte[] rgba = new byte[4];
+            byte[] bgr = new byte[3];
+            byte[] bgra = new byte[4];
+
+            new Bgr565(x, y, z).ToBytes(rgb, 0, ComponentOrder.XYZ);
+            Assert.Equal(rgb, new byte[] { 24, 0, 131 });
+
+            new Bgr565(x, y, z).ToBytes(rgba, 0, ComponentOrder.XYZW);
+            Assert.Equal(rgba, new byte[] { 24, 0, 131, 255 });
+
+            new Bgr565(x, y, z).ToBytes(bgr, 0, ComponentOrder.ZYX);
+            Assert.Equal(bgr, new byte[] { 131, 0, 24 });
+
+            new Bgr565(x, y, z).ToBytes(bgra, 0, ComponentOrder.ZYXW);
+            Assert.Equal(bgra, new byte[] { 131, 0, 24, 255 });
+        }
+
+        // Comparison helpers with small tolerance to allow for floating point rounding during computations.
+        public static bool Equal(float a, float b)
+        {
+            return Math.Abs(a - b) < 1e-5;
+        }
+
+        public static bool Equal(Vector2 a, Vector2 b)
+        {
+            return Equal(a.X, b.X) && Equal(a.Y, b.Y);
+        }
+
+        public static bool Equal(Vector3 a, Vector3 b)
+        {
+            return Equal(a.X, b.X) && Equal(a.Y, b.Y) && Equal(a.Z, b.Z);
+        }
+
+        public static bool Equal(Vector4 a, Vector4 b)
+        {
+            return Equal(a.X, b.X) && Equal(a.Y, b.Y) && Equal(a.Z, b.Z) && Equal(a.W, b.W);
         }
     }
 }

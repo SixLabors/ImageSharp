14f879254a622c5b0028bf87b89cffca54109806
James Jackson-South <james_south@hotmail.com>
2016-12-01 19:04:17 +1100

    Add Rgba1010102, Make Bgr565 conversion more accurate.

--- src/ImageSharp/Colors/PackedPixel/Rgba1010102.cs
+++ src/ImageSharp/Colors/PackedPixel/Rgba1010102.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Bgr565.cs" company="James Jackson-South">
+﻿// <copyright file="Rgba1010102.cs" company="James Jackson-South">
 // Copyright (c) James Jackson-South and contributors.
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
@@ -9,84 +9,83 @@ namespace ImageSharp
     using System.Numerics;
 
     /// <summary>
-    /// Packed pixel type containing unsigned normalized values ranging from 0 to 1. The x and z components use 5 bits, and the y component uses 6 bits.
+    /// Packed vector type containing unsigned normalized values ranging from 0 to 1.
+    /// The x, y and z components use 10 bits, and the w component uses 2 bits.
     /// </summary>
-    public struct Bgr565 : IPackedPixel<ushort>, IEquatable<Bgr565>
+    public struct Rgba1010102 : IPackedPixel<uint>, IEquatable<Rgba1010102>, IPackedVector
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="Bgr565"/> struct. 
+        /// Initializes a new instance of the <see cref="Rgba1010102"/> struct.
         /// </summary>
         /// <param name="x">The x-component</param>
         /// <param name="y">The y-component</param>
         /// <param name="z">The z-component</param>
-        public Bgr565(float x, float y, float z)
+        /// <param name="w">The w-component</param>
+        public Rgba1010102(float x, float y, float z, float w)
         {
-            this.PackedValue = Pack(x, y, z);
+            this.PackedValue = Pack(x, y, z, w);
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Bgr565"/> struct. 
+        /// Initializes a new instance of the <see cref="Rgba1010102"/> struct.
         /// </summary>
-        /// <param name="vector">
-        /// The vector containing the components for the packed value.
-        /// </param>
-        public Bgr565(Vector3 vector)
+        /// <param name="vector">The vector containing the component values.</param>
+        public Rgba1010102(Vector4 vector)
         {
-            this.PackedValue = Pack(vector.X, vector.Y, vector.Z);
+            this.PackedValue = Pack(vector.X, vector.Y, vector.Z, vector.W);
         }
 
         /// <inheritdoc />
-        public ushort PackedValue { get; set; }
+        public uint PackedValue { get; set; }
 
         /// <summary>
-        /// Compares two <see cref="Bgr565"/> objects for equality.
+        /// Compares two <see cref="Rgba1010102"/> objects for equality.
         /// </summary>
-        /// <param name="left">The <see cref="Bgr565"/> on the left side of the operand.</param>
-        /// <param name="right">The <see cref="Bgr565"/> on the right side of the operand.</param>
+        /// <param name="left">
+        /// The <see cref="Rgba1010102"/> on the left side of the operand.
+        /// </param>
+        /// <param name="right">
+        /// The <see cref="Rgba1010102"/> on the right side of the operand.
+        /// </param>
         /// <returns>
         /// True if the <paramref name="left"/> parameter is equal to the <paramref name="right"/> parameter; otherwise, false.
         /// </returns>
-        public static bool operator ==(Bgr565 left, Bgr565 right)
+        public static bool operator ==(Rgba1010102 left, Rgba1010102 right)
         {
             return left.PackedValue == right.PackedValue;
         }
 
         /// <summary>
-        /// Compares two <see cref="Bgr565"/> objects for equality.
+        /// Compares two <see cref="Rgba1010102"/> objects for equality.
         /// </summary>
-        /// <param name="left">The <see cref="Bgr565"/> on the left side of the operand.</param>
-        /// <param name="right">The <see cref="Bgr565"/> on the right side of the operand.</param>
+        /// <param name="left">
+        /// The <see cref="Rgba1010102"/> on the left side of the operand.
+        /// </param>
+        /// <param name="right">
+        /// The <see cref="Rgba1010102"/> on the right side of the operand.
+        /// </param>
         /// <returns>
         /// True if the <paramref name="left"/> parameter is not equal to the <paramref name="right"/> parameter; otherwise, false.
         /// </returns>
-        public static bool operator !=(Bgr565 left, Bgr565 right)
+        public static bool operator !=(Rgba1010102 left, Rgba1010102 right)
         {
             return left.PackedValue != right.PackedValue;
-        }
-
-        /// <summary>
-        /// Expands the packed representation into a <see cref="Vector3"/>.
-        /// The vector components are typically expanded in least to greatest significance order.
-        /// </summary>
-        /// <returns>The <see cref="Vector3"/>.</returns>
-        public Vector3 ToVector3()
-        {
-            return new Vector3(
-                       ((this.PackedValue >> 11) & 0x1F) * (1F / 31F),
-                       ((this.PackedValue >> 5) & 0x3F) * (1F / 63F),
-                       (this.PackedValue & 0x1F) * (1F / 31F));
-        }
-
-        /// <inheritdoc />
-        public void PackFromVector4(Vector4 vector)
-        {
-            this.PackedValue = Pack(vector.X, vector.Y, vector.Z);
         }
 
         /// <inheritdoc />
         public Vector4 ToVector4()
         {
-            return new Vector4(this.ToVector3(), 1F);
+            return new Vector4(
+                ((this.PackedValue >> 0) & 0x03FF) / 1023F,
+                ((this.PackedValue >> 10) & 0x03FF) / 1023F,
+                ((this.PackedValue >> 20) & 0x03FF) / 1023F,
+                ((this.PackedValue >> 30) & 0x03) / 3F);
+        }
+
+        /// <inheritdoc />
+        public void PackFromVector4(Vector4 vector)
+        {
+            this.PackedValue = Pack(vector.X, vector.Y, vector.Z, vector.W);
         }
 
         /// <inheritdoc />
@@ -132,11 +131,11 @@ namespace ImageSharp
         /// <inheritdoc />
         public override bool Equals(object obj)
         {
-            return (obj is Bgr565) && this.Equals((Bgr565)obj);
+            return (obj is Rgba1010102) && this.Equals((Rgba1010102)obj);
         }
 
         /// <inheritdoc />
-        public bool Equals(Bgr565 other)
+        public bool Equals(Rgba1010102 other)
         {
             return this.PackedValue == other.PackedValue;
         }
@@ -144,7 +143,7 @@ namespace ImageSharp
         /// <inheritdoc />
         public override string ToString()
         {
-            return this.ToVector3().ToString();
+            return this.ToVector4().ToString();
         }
 
         /// <inheritdoc />
@@ -154,17 +153,20 @@ namespace ImageSharp
         }
 
         /// <summary>
-        /// Packs the <see cref="float"/> components into a <see cref="ushort"/>.
+        /// Packs the <see cref="float"/> components into a <see cref="uint"/>.
         /// </summary>
         /// <param name="x">The x-component</param>
         /// <param name="y">The y-component</param>
         /// <param name="z">The z-component</param>
-        /// <returns>The <see cref="ushort"/> containing the packed values.</returns>
-        private static ushort Pack(float x, float y, float z)
+        /// <param name="w">The w-component</param>
+        /// <returns>The <see cref="uint"/> containing the packed values.</returns>
+        private static uint Pack(float x, float y, float z, float w)
         {
-            return (ushort)((((int)Math.Round(x.Clamp(0, 1) * 31F) & 0x1F) << 11) |
-                   (((int)Math.Round(y.Clamp(0, 1) * 63F) & 0x3F) << 5) |
-                   ((int)Math.Round(z.Clamp(0, 1) * 31F) & 0x1F));
+            return (uint)(
+                (((int)Math.Round(x.Clamp(0, 1) * 1023F) & 0x03FF) << 0) |
+                (((int)Math.Round(y.Clamp(0, 1) * 1023F) & 0x03FF) << 10) |
+                (((int)Math.Round(z.Clamp(0, 1) * 1023F) & 0x03FF) << 20) |
+                (((int)Math.Round(w.Clamp(0, 1) * 3F) & 0x03) << 30));
         }
     }
 }

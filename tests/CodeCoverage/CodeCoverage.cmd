019c116e25477b6a496103ea6d6ff626e4ab5a3e
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-17 14:36:14 +0100

    Try to figure out dotnet location.

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -4,6 +4,8 @@ nuget restore -PackagesDirectory .
 
 cd ..\ImageSharp.Tests
 
+where dotnet
+
 ..\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"C:\Program Files\dotnet\bin\dotnet.exe" -targetargs:"test -c Release" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -oldStyle -output:.\ImageSharp.Coverage.xml
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%

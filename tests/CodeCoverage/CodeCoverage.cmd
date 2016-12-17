c44c75bebcd40a32c9218625cf538925148c7154
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-17 15:12:55 +0100

    Corrected batch file.

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -1,13 +1,18 @@
 @echo off
 
-nuget restore -PackagesDirectory .
+cd tests\CodeCoverage
+
+nuget restore packages.config -PackagesDirectory .
 
 cd ..\ImageSharp.Tests
 
-where dotnet
+dotnet restore
 
-..\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"C:\Program Files\dotnet\bin\dotnet.exe" -targetargs:"test -c Release" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -oldStyle -output:.\ImageSharp.Coverage.xml
+..\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test -c Release" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -oldStyle -output:.\ImageSharp.Coverage.xml
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov
 codecov -f "ImageSharp.Coverage.xml"
+
+cd ..
+cd ..

4d58e73229d9ebc88f50f8475137feb1b44220d8
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-17 14:32:21 +0100

    Corrected dotnet folder.

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -4,7 +4,7 @@ nuget restore -PackagesDirectory .
 
 cd ..\ImageSharp.Tests
 
-..\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test -c Release" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -oldStyle -output:.\ImageSharp.Coverage.xml
+..\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"C:\Program Files\dotnet\bin\dotnet.exe" -targetargs:"test -c Release" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -oldStyle -output:.\ImageSharp.Coverage.xml
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov

fc7e3ac2253fca7164c60a0466f003f2df2e4efb
Dirk Lemstra <dirk@git.imagemagick.org>
2016-12-18 23:11:35 +0100

    Run CodeCoverage from the root.

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -8,12 +8,12 @@ cd ..\ImageSharp.Tests
 
 dotnet restore
 
+cd ..
+cd ..
+
 rem The -threshold options prevents this taking ages...
-..\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test -c Release -f net451" -threshold:10 -register:user -filter:"+[ImageSharp*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -output:.\ImageSharp.Coverage.xml
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test tests\ImageSharp.Tests -c Release -f net451" -threshold:10 -register:user -filter:"+[ImageSharp*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -output:.\ImageSharp.Coverage.xml
 
 SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
 pip install codecov
 codecov -f "ImageSharp.Coverage.xml"
-
-cd ..
-cd ..

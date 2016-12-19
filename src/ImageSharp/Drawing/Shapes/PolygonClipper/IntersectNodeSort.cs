3975806c93d19e620b65a7caf4c1bf92de7d928a
Scott Williams <tocsoft@gmail.com>
2016-12-19 12:57:49 +0000

    Port Clipper.cs into ImageSharp style
    All the classes from Clipper.cs have been moved into there own folder/namespace.
    Added README.md to preserve the original Attribution.
    I've ported the code to use Vector2 instead of its own point classes.
    I've removed all the unused clipping options, and polygon styles that are not required and thus removed a lot of branching logic.

---
+++ src/ImageSharp/Drawing/Shapes/PolygonClipper/IntersectNodeSort.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="IntersectNodeSort.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Drawing.Shapes.PolygonClipper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Runtime.CompilerServices;
+
+    using Paths;
+
+    /// <summary>
+    /// Compares <see cref="IntersectNode"/>s
+    /// </summary>
+    internal class IntersectNodeSort : IComparer<IntersectNode>
+    {
+        /// <summary>
+        /// Compares the specified node1.
+        /// </summary>
+        /// <param name="node1">The node1.</param>
+        /// <param name="node2">The node2.</param>
+        /// <returns>
+        /// 1 if node2 %gt; node1
+        /// -1 if node2 $lt; node1
+        /// 0 if same
+        /// </returns>
+        public int Compare(IntersectNode node1, IntersectNode node2)
+        {
+            float i = node2.Pt.Y - node1.Pt.Y;
+            if (i > 0)
+            {
+                return 1;
+            }
+            else if (i < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}

c074b63a49737ee905619cc58543ff53fdde2747
Olivia <olivia_git@outlook.com>
2016-12-19 14:32:26 +0100

    Renaming files with generic types to match {T} pattern.

--- src/ImageSharp/Drawing/Brushes/Brushes{TColor,TPacked}.cs
+++ src/ImageSharp/Drawing/Brushes/Brushes{TColor,TPacked}.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Brushes`2.cs" company="James Jackson-South">
+﻿// <copyright file="Brushes{TColor,TPacked}.cs" company="James Jackson-South">
 // Copyright (c) James Jackson-South and contributors.
 // Licensed under the Apache License, Version 2.0.
 // </copyright>

97eece56d3452d7e6cc9e041027540f9217a4a26
James Jackson-South <james_south@hotmail.com>
2016-12-20 12:17:22 +1100

    Add non-generic IPackedPixel interface

--- src/ImageSharp/Colors/PackedPixel/IPackedPixel.cs
+++ src/ImageSharp/Colors/PackedPixel/IPackedPixel.cs
@@ -8,11 +8,18 @@ namespace ImageSharp
     using System;
 
     /// <summary>
-    /// An interface that represents a packed pixel type.
+    /// An interface that represents a generic packed pixel type.
     /// </summary>
     /// <typeparam name="TPacked">The packed format. <example>uint, long, float.</example></typeparam>
-    public interface IPackedPixel<TPacked> : IPackedVector<TPacked>, IPackedBytes
+    public interface IPackedPixel<TPacked> : IPackedPixel, IPackedVector<TPacked>
         where TPacked : struct, IEquatable<TPacked>
+    {
+    }
+
+    /// <summary>
+    /// An interface that represents a packed pixel type.
+    /// </summary>
+    public interface IPackedPixel : IPackedVector, IPackedBytes
     {
     }
 }

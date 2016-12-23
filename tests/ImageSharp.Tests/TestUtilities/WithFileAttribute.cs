66f6403d57f7ed2461d9e3e0039ee57c1e934f6d
antonfirsov <antonfir@gmail.com>
2016-12-23 02:41:22 +0100

    re-added & refactored TestUtilities

---
+++ tests/ImageSharp.Tests/TestUtilities/WithFileAttribute.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="WithFileAttribute.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests.TestUtilities
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// Triggers passing <see cref="TestImageFactory{TColor}"/> instances which read an image from the given file
+    /// One <see cref="TestImageFactory{TColor}"/> instance will be passed for each the pixel format defined by the pixelTypes parameter
+    /// </summary>
+    public class WithFileAttribute : ImageDataAttributeBase
+    {
+        private readonly string fileName;
+
+        /// <summary>
+        /// Triggers passing <see cref="TestImageFactory{TColor}"/> instances which read an image from the given file
+        /// One <see cref="TestImageFactory{TColor}"/> instance will be passed for each the pixel format defined by the pixelTypes parameter
+        /// </summary>
+        /// <param name="fileName">The name of the file</param>
+        /// <param name="pixelTypes">The requested pixel types</param>
+        /// <param name="additionalParameters">Additional theory parameter values</param>
+        public WithFileAttribute(string fileName, PixelTypes pixelTypes, params object[] additionalParameters)
+            : base(pixelTypes, additionalParameters)
+        {
+            this.fileName = fileName;
+        }
+
+        protected override object[] GetFactoryMethodArgs(MethodInfo testMethod, Type factoryType) => new object[] { this.fileName };
+
+        protected override string GetFactoryMethodName(MethodInfo testMethod) => "File";
+    }
+}

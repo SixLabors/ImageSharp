f3a3391cd610e48b58ea897fee7b84f0160e7ac3
antonfirsov <antonfir@gmail.com>
2016-12-25 01:37:31 +0100

    Style Stalin

--- src/ImageSharp/Formats/Jpg/Components/Encoder/HuffIndex.cs
+++ src/ImageSharp/Formats/Jpg/Components/Encoder/HuffIndex.cs
@@ -1,4 +1,8 @@
-﻿namespace ImageSharp.Formats.Jpg.Components.Encoder
+﻿// <copyright file="HuffIndex.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+namespace ImageSharp.Formats.Jpg.Components.Encoder
 {
     /// <summary>
     /// Enumerates the Huffman tables

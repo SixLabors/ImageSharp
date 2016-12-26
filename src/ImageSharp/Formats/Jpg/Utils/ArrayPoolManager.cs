2cef59d93a0abb4d3287893c76f4993874030e03
antonfirsov <antonfir@gmail.com>
2016-12-26 06:35:04 +0100

    refactored GrayImage class --to--> reusable JpegPixelArea cleaned some mess in JpegDecoderCore

---
+++ src/ImageSharp/Formats/Jpg/Utils/ArrayPoolManager.cs
@@ -0,0 +1,13 @@
+﻿namespace ImageSharp.Formats
+{
+    using System.Buffers;
+
+    internal class ArrayPoolManager<T>
+    {
+        private static readonly ArrayPool<T> Pool = ArrayPool<T>.Create();
+
+        public static T[] RentCleanArray(int minimumLength) => Pool.Rent(minimumLength);
+
+        public static void ReturnArray(T[] array) => Pool.Return(array, true);
+    }
+}

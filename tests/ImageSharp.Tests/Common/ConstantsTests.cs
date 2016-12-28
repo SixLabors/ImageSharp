123022e7b56d3d1647c4beea7bcedd4d3687014e
Olivia <olivia_git@outlook.com>
2016-12-28 13:12:05 +0200

    Adding a test to protect the epsilon value.

---
+++ tests/ImageSharp.Tests/Common/ConstantsTests.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="ConstantsTests.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests.Common
+{
+    using ImageSharp.Common;
+    using Xunit;
+
+    public class ConstantsTests
+    {
+        [Fact]
+        public void Epsilon()
+        {
+            Assert.Equal(Constants.Epsilon, 0.001f);
+        }
+    }
+}

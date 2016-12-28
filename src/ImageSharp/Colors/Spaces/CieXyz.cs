9208de1659b1dedafb8c9b364354225aa4b7c1df
Olivia <olivia_git@outlook.com>
2016-12-28 21:09:58 +0200

    Fixing bug with non-inclusive AlmostEquals.

--- src/ImageSharp/Colors/Spaces/CieXyz.cs
+++ src/ImageSharp/Colors/Spaces/CieXyz.cs
@@ -160,9 +160,9 @@ namespace ImageSharp.Colors.Spaces
         {
             Vector3 result = Vector3.Abs(this.backingVector - other.backingVector);
 
-            return result.X < precision
-                && result.Y < precision
-                && result.Z < precision;
+            return result.X <= precision
+                && result.Y <= precision
+                && result.Z <= precision;
         }
     }
 }

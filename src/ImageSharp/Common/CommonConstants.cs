65c848af7bf5162ab207ec2cda5b6ca8e5674ab9
Olivia <olivia_git@outlook.com>
2016-12-28 12:55:01 +0200

    Adding CommonConstants.

---
+++ src/ImageSharp/Common/CommonConstants.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="CommonConstants.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Common
+{
+    /// <summary>
+    /// Common constants used throughout the project
+    /// </summary>
+    public static class CommonConstants
+    {
+        /// <summary>
+        /// The epsilon for comparing floating point numbers.
+        /// </summary>
+        public static readonly float Epsilon = 0.001f;
+    }
+}

260349bfb1b62d617c6a675cb142a76daef4867e
Anton Firszov <anton.firszov@sapagroup.com>
2016-12-29 01:32:27 +0100

    ok StyleCop, this time I won!

--- src/ImageSharp/Formats/Jpg/Components/Decoder/Scan.cs
+++ src/ImageSharp/Formats/Jpg/Components/Decoder/Scan.cs
@@ -1,4 +1,9 @@
-﻿namespace ImageSharp.Formats.Jpg
+﻿// <copyright file="Scan.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Formats.Jpg
 {
     using System.Runtime.InteropServices;
 

b9f177bb4da1a95c7ed149f941a9a8bc1142a139
Scott Williams <tocsoft@gmail.com>
2017-01-02 09:54:21 +0000

    drop unwanted build cmds

--- build/config.cmd
+++
@@ -1,8 +0,0 @@
-@echo Off
-REM ====================== 
-REM = Configure settings =
-
-REM Space seperated list of projects to version and package
-SET projects=".\src\ImageSharp" ".\src\ImageSharp.Drawing" 
-
-REM ======================

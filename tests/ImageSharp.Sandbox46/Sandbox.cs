453f167177b81d1cb84f4af29fe64d3166677924
Anton Firszov <anton.firszov@sapagroup.com>
2017-01-14 22:08:03 +0100

    Initialized ImageSharp.Sandbox46.csproj

---
+++ tests/ImageSharp.Sandbox46/Sandbox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSharp
+{
+    using Xunit;
+
+    public class Sandbox
+    {
+        [Fact]
+        public void HelloImage()
+        {
+            Image img = new Image(10, 20);
+            Assert.Equal(10, img.Width);
+        }
+    }
+}

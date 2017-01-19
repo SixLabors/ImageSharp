3da4b43d906464cb64e6f788330f2a128a640781
James Jackson-South <james_south@hotmail.com>
2017-01-19 21:24:07 +1100

    Add modulus benchmark

---
+++ tests/ImageSharp.Benchmarks/General/Modulus.cs
@@ -0,0 +1,19 @@
+﻿namespace ImageSharp.Benchmarks.General
+{
+    using BenchmarkDotNet.Attributes;
+
+    public class Modulus
+    {
+        [Benchmark(Baseline = true, Description = "Standard Modulus using %")]
+        public int StandardModulus()
+        {
+            return 255 % 256;
+        }
+
+        [Benchmark(Description = "Bitwise Modulus using &")]
+        public int BitwiseModulus()
+        {
+            return 255 & 255;
+        }
+    }
+}

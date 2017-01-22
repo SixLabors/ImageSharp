a19edc543038d41dedc8aadfa23d57573185a1aa
Anton Firszov <anton.firszov@sapagroup.com>
2017-01-22 02:47:30 +0100

    further Jpeg cleanup & refactor

--- src/ImageSharp.Formats.Jpeg/Components/Decoder/ComponentScan.cs
+++ src/ImageSharp.Formats.Jpeg/Components/Decoder/ComponentScan.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Scan.cs" company="James Jackson-South">
+﻿// <copyright file="ComponentScan.cs" company="James Jackson-South">
 // Copyright (c) James Jackson-South and contributors.
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
@@ -11,12 +11,12 @@ namespace ImageSharp.Formats.Jpg
     /// Represents a component scan
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
-    internal struct Scan
+    internal struct ComponentScan
     {
         /// <summary>
         /// Gets or sets the component index.
         /// </summary>
-        public byte Index;
+        public byte ComponentIndex;
 
         /// <summary>
         /// Gets or sets the DC table selector

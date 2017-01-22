f3471dd36fdccbada02eaf8fa5b57d74e128841b
Anton Firszov <anton.firszov@sapagroup.com>
2017-01-22 14:57:39 +0100

    pooling JpegDecoderCore.DecodedBlocks + made Sandbox46 executable

---
+++ tests/ImageSharp.Sandbox46/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Sandbox46
+{
+    using System;
+
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point. Useful for executing benchmarks and performance unit tests manually,
+        /// when the IDE test runners lack some of the functionality. Eg.: it's not possible to run JetBrains memory profiler for unit tests.
+        /// </summary>
+        /// <param name="args">
+        /// The arguments to pass to the program.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.");
+        }
+    }
+}

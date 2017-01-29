0f900befe129e8275baa9e18f930529499502d59
Scott Williams <tocsoft@gmail.com>
2017-01-29 22:45:25 +0000

    disable style cop as it spams the build log
    stylecop.analyzers seems to not like the dotnet cli build system, and throws lots of errors.

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -12,7 +12,7 @@ cd ..
 cd ..
 
 rem The -threshold options prevents this taking ages...
-tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test tests\ImageSharp.Tests -c Release -f net451" -threshold:10 -register:user -filter:"+[ImageSharp*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\ImageSharp.Coverage.xml
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test tests\ImageSharp.Tests\ImageSharp.Tests.csproj -c Release -f net451" -threshold:10 -register:user -filter:"+[ImageSharp*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\ImageSharp.Coverage.xml
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 

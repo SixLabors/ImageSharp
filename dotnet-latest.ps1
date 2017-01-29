dcfd71280af69872242acd980b98dca2e9e4fae8
Scott Williams <tocsoft@gmail.com>
2017-01-29 20:56:11 +0000

    use RC3 version of dotnet cli

--- dotnet-latest.ps1
+++ dotnet-latest.ps1
@@ -10,19 +10,31 @@ if (Test-Path $ArtifactsPath) {
 New-Item $ArtifactsPath -ItemType Directory -ErrorAction Ignore | Out-Null
 
 Write-Host "Created artifacts folder '$ArtifactsPath'"
+$installRequired = $TRUE
 
 # Install the latest dotnet cli   
 if (Get-Command "dotnet.exe" -ErrorAction SilentlyContinue) {
     Write-Host "dotnet SDK already installed"
-	dotnet --version 
-} else {
+
+	$version = dotnet --version 2>&1
+    if($version -ne "1.0.0-rc3-004530"){
+        Write-Host "$version installed but require 1.0.0-rc3-004530"
+        $installRequired = $TRUE
+    }else{
+        Write-Host "$version already installed"
+        $installRequired = $FALSE
+    } 
+} 
+
+if($installRequired -eq $TRUE)
+{
     Write-Host "Installing dotnet SDK"
     
     $installScript = Join-Path $ArtifactsPath "dotnet-install.ps1"
     
 	Write-Host $installScript
 
-     Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.ps1" `
+     Invoke-WebRequest "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-rc3/scripts/obtain/dotnet-install.ps1" `
        -OutFile $installScript
         
      & $installScript

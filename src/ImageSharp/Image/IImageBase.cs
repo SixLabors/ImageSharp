6792f46b729cb378dfb6495e189192327a4a0aa9
Dirk Lemstra <dirk@git.imagemagick.org>
2017-02-03 11:32:15 +0100

    Moved IImageBase to a separate file.

---
+++ src/ImageSharp/Image/IImageBase.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="IImageBase{TColor}.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp
+{
+    /// <summary>
+    /// Encapsulates the basic properties and methods required to manipulate images.
+    /// </summary>
+    public interface IImageBase
+    {
+        /// <summary>
+        /// Gets the <see cref="Rectangle"/> representing the bounds of the image.
+        /// </summary>
+        Rectangle Bounds { get; }
+
+        /// <summary>
+        /// Gets or sets the quality of the image. This affects the output quality of lossy image formats.
+        /// </summary>
+        int Quality { get; set; }
+
+        /// <summary>
+        /// Gets or sets the frame delay for animated images.
+        /// If not 0, this field specifies the number of hundredths (1/100) of a second to
+        /// wait before continuing with the processing of the Data Stream.
+        /// The clock starts ticking immediately after the graphic is rendered.
+        /// </summary>
+        int FrameDelay { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum allowable width in pixels.
+        /// </summary>
+        int MaxWidth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum allowable height in pixels.
+        /// </summary>
+        int MaxHeight { get; set; }
+
+        /// <summary>
+        /// Gets the width in pixels.
+        /// </summary>
+        int Width { get; }
+
+        /// <summary>
+        /// Gets the height in pixels.
+        /// </summary>
+        int Height { get; }
+
+        /// <summary>
+        /// Gets the pixel ratio made up of the width and height.
+        /// </summary>
+        double PixelRatio { get; }
+    }
+}

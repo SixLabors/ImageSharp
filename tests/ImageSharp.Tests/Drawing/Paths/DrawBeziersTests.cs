6bd8848b48d0cafcae0ebb1e85495d3d88bd969e
Scott Williams <tocsoft@gmail.com>
2017-02-09 12:37:25 +0000

    Update SixLabors.Shapes

--- tests/ImageSharp.Tests/Drawing/Paths/DrawBeziersTests.cs
+++ tests/ImageSharp.Tests/Drawing/Paths/DrawBeziersTests.cs
@@ -50,9 +50,10 @@ namespace ImageSharp.Tests.Drawing.Paths
             Assert.Equal(GraphicsOptions.Default, processor.Options);
 
             ShapePath path = Assert.IsType<ShapePath>(processor.Path);
-            Assert.NotEmpty(path.Paths);
+            Assert.NotNull(path.Path);
 
-            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Paths[0]);
+            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Path);
+
             BezierLineSegment segment = Assert.IsType<BezierLineSegment>(vector.LineSegments[0]);
 
             Pen<Color> pen = Assert.IsType<Pen<Color>>(processor.Pen);
@@ -71,9 +72,8 @@ namespace ImageSharp.Tests.Drawing.Paths
             Assert.Equal(noneDefault, processor.Options);
 
             ShapePath path = Assert.IsType<ShapePath>(processor.Path);
-            Assert.NotEmpty(path.Paths);
 
-            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Paths[0]);
+            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Path);
             BezierLineSegment segment = Assert.IsType<BezierLineSegment>(vector.LineSegments[0]);
 
             Pen<Color> pen = Assert.IsType<Pen<Color>>(processor.Pen);
@@ -92,9 +92,8 @@ namespace ImageSharp.Tests.Drawing.Paths
             Assert.Equal(GraphicsOptions.Default, processor.Options);
 
             ShapePath path = Assert.IsType<ShapePath>(processor.Path);
-            Assert.NotEmpty(path.Paths);
 
-            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Paths[0]);
+            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Path);
             BezierLineSegment segment = Assert.IsType<BezierLineSegment>(vector.LineSegments[0]);
 
             Pen<Color> pen = Assert.IsType<Pen<Color>>(processor.Pen);
@@ -115,9 +114,8 @@ namespace ImageSharp.Tests.Drawing.Paths
             Assert.Equal(noneDefault, processor.Options);
 
             ShapePath path = Assert.IsType<ShapePath>(processor.Path);
-            Assert.NotEmpty(path.Paths);
 
-            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Paths[0]);
+            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Path);
             BezierLineSegment segment = Assert.IsType<BezierLineSegment>(vector.LineSegments[0]);
 
             Pen<Color> pen = Assert.IsType<Pen<Color>>(processor.Pen);
@@ -138,9 +136,8 @@ namespace ImageSharp.Tests.Drawing.Paths
             Assert.Equal(GraphicsOptions.Default, processor.Options);
 
             ShapePath path = Assert.IsType<ShapePath>(processor.Path);
-            Assert.NotEmpty(path.Paths);
 
-            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Paths[0]);
+            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Path);
             BezierLineSegment segment = Assert.IsType<BezierLineSegment>(vector.LineSegments[0]);
 
             Assert.Equal(pen, processor.Pen);
@@ -157,9 +154,8 @@ namespace ImageSharp.Tests.Drawing.Paths
             Assert.Equal(noneDefault, processor.Options);
 
             ShapePath path = Assert.IsType<ShapePath>(processor.Path);
-            Assert.NotEmpty(path.Paths);
 
-            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Paths[0]);
+            SixLabors.Shapes.Path vector = Assert.IsType<SixLabors.Shapes.Path>(path.Path);
             BezierLineSegment segment = Assert.IsType<BezierLineSegment>(vector.LineSegments[0]);
 
             Assert.Equal(pen, processor.Pen);

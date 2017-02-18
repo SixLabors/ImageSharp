e50825518058f2652e0d79160f001c2dd5c3d26b
Dirk Lemstra <dirk@git.imagemagick.org>
2017-02-18 18:59:59 +0100

    Implemented the IDecoderOptions inside the gif decoder.

--- src/ImageSharp/Formats/DecoderOptions.cs
+++ src/ImageSharp/Formats/DecoderOptions.cs
@@ -3,13 +3,27 @@
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
 
-namespace ImageSharp.Formats
+namespace ImageSharp
 {
     /// <summary>
     /// Encapsulates the shared decoder options.
     /// </summary>
     public class DecoderOptions : IDecoderOptions
     {
+        /// <summary>
+        /// Gets the default decoder options
+        /// </summary>
+        public static DecoderOptions Default
+        {
+            get
+            {
+                return new DecoderOptions()
+                {
+                    IgnoreMetadata = false
+                };
+            }
+        }
+
         /// <summary>
         /// Gets or sets a value indicating whether the metadata should be ignored when the image is being decoded.
         /// </summary>

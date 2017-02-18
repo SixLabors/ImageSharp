614aa597de76b949bd906b78e099b2320a42178c
Dirk Lemstra <dirk@git.imagemagick.org>
2017-02-18 13:16:49 +0100

    Added options for the decoder.

---
+++ src/ImageSharp/Formats/IDecoderOptions.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IDecoderOptions.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Formats
+{
+    /// <summary>
+    /// Encapsulates the shared decoder options.
+    /// </summary>
+    public interface IDecoderOptions
+    {
+        /// <summary>
+        /// Gets a value indicating whether the metadata should be ignored when the image is being decoded.
+        /// </summary>
+        bool IgnoreMetadata { get; }
+    }
+}

5b240da27ecd8cf73166df69c60ccab7dfe07ee2
Anton Firszov <anton.firszov@sapagroup.com>
2017-02-18 15:09:16 +0100

    IPixel

--- src/ImageSharp/Common/Memory/ArrayPointer{T}.cs
+++ src/ImageSharp/Common/Memory/ArrayPointer{T}.cs
@@ -16,7 +16,7 @@ namespace ImageSharp
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ArrayPointer(T[] array, void* pointerToArray, int offset)
         {
-            // TODO: Use Guard.NotNull() here after optimizing it with ThrowHelper!
+            // TODO: Use Guard.NotNull() here after optimizing it by eliminating the default argument case and applying ThrowHelper!
             if (array == null)
             {
                 ThrowHelper.ThrowArgumentNullException(nameof(array));
@@ -29,7 +29,7 @@ namespace ImageSharp
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public ArrayPointer(T[] array, void* pointerToArray)
         {
-            // TODO: Use Guard.NotNull() here after optimizing it with ThrowHelper!
+            // TODO: Use Guard.NotNull() here after optimizing it by eliminating the default argument case and applying ThrowHelper!
             if (array == null)
             {
                 ThrowHelper.ThrowArgumentNullException(nameof(array));

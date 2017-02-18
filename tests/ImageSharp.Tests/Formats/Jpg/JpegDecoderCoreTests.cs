c7ced5bcdfaac6569e94830032de2835a02844a2
Dirk Lemstra <dirk@git.imagemagick.org>
2017-02-18 20:42:10 +0100

    Implemented the IDecoderOptions inside the jpeg decoder.

---
+++ tests/ImageSharp.Tests/Formats/Jpg/JpegDecoderCoreTests.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="JpegDecoderCoreTests.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests
+{
+    using Xunit;
+
+    public class JpegDecoderCoreTests
+    {
+      [Fact]
+      public void Decode_IgnoreMetadataIsFalse_ExifProfileIsRead()
+      {
+          var options = new DecoderOptions()
+          {
+              IgnoreMetadata = false
+          };
+
+          TestFile testFile = TestFile.Create(TestImages.Jpeg.Baseline.Floorplan);
+
+          using (Image image = new Image(testFile.FilePath, options))
+          {
+              Assert.NotNull(image.MetaData.ExifProfile);
+          }
+      }
+
+      [Fact]
+      public void Decode_IgnoreMetadataIsTrue_ExifProfileIgnored()
+      {
+          var options = new DecoderOptions()
+          {
+              IgnoreMetadata = true
+          };
+
+          TestFile testFile = TestFile.Create(TestImages.Jpeg.Baseline.Floorplan);
+
+          using (Image image = new Image(testFile.FilePath, options))
+          {
+              Assert.Null(image.MetaData.ExifProfile);
+          }
+      }
+    }
+}

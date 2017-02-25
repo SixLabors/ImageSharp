07b5ff4e2d8553ebc6a54e45c22baa1524bde44e
Scott Williams <tocsoft@gmail.com>
2017-02-25 17:59:11 +0000

    Fix drawing on transparent background

--- src/ImageSharp.Drawing/Processors/FillRegionProcessor.cs
+++ src/ImageSharp.Drawing/Processors/FillRegionProcessor.cs
@@ -202,7 +202,6 @@ namespace ImageSharp.Drawing.Processors
                                 Vector4 sourceVector = applicator[x, y].ToVector4();
 
                                 Vector4 finalColor = Vector4BlendTransforms.PremultipliedLerp(backgroundVector, sourceVector, opacity);
-                                finalColor.W = backgroundVector.W;
 
                                 TColor packed = default(TColor);
                                 packed.PackFromVector4(finalColor);

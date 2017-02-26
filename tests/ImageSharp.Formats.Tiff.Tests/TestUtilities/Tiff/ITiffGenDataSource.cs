f65b492acf7d32daa422163fdb1a5295ab628879
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2017-02-26 20:47:20 +0000

    Add initial Tiff codec projects, and TiffGen

---
+++ tests/ImageSharp.Formats.Tiff.Tests/TestUtilities/Tiff/ITiffGenDataSource.cs
@@ -0,0 +1,18 @@
+// <copyright file="ITiffGenDataSource.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// An interface for any class within the Tiff generator that produces data to be included in the file.
+    /// </summary>
+    public interface ITiffGenDataSource
+    {
+        IEnumerable<TiffGenDataBlock> GetData(bool isLittleEndian);
+    }
+}

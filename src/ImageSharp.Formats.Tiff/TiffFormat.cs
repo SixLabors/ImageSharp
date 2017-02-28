8cbfc8e67a8893bfd693e1518b18271f341536ef
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2017-02-28 19:21:28 +0000

    Add Tiff implementation of IImageFormat

---
+++ src/ImageSharp.Formats.Tiff/TiffFormat.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="TiffFormat.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Formats
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Encapsulates the means to encode and decode Tiff images.
+    /// </summary>
+    public class TiffFormat //: IImageFormat
+    {
+        /// <inheritdoc/>
+        public string MimeType => "image/tiff";
+
+        /// <inheritdoc/>
+        public string Extension => "tif";
+
+        /// <inheritdoc/>
+        public IEnumerable<string> SupportedExtensions => new string[] { "tif", "tiff" };
+
+        /// <inheritdoc/>
+        //public IImageDecoder Decoder => new TiffDecoder();
+
+        /// <inheritdoc/>
+        //public IImageEncoder Encoder => throw new System.NotImplementedException();
+
+        /// <inheritdoc/>
+        public int HeaderSize => 4;
+
+        /// <inheritdoc/>
+        public bool IsSupportedFileFormat(byte[] header)
+        {
+            return header.Length >= this.HeaderSize &&
+                   ((header[0] == 0x49 && header[1] == 0x49 && header[2] == 0x2A && header[3] == 0x00) ||  // Little-endian
+                    (header[0] == 0x4D && header[1] == 0x4D && header[2] == 0x00 && header[3] == 0x2A));   // Big-endian
+        }
+    }
+}

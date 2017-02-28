e19eb5046cc8a1ccf43aae437ef80e0d0d2f124f
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2017-02-28 19:21:28 +0000

    Add Tiff implementation of IImageFormat

---
+++ src/ImageSharp.Formats.Tiff/TiffConstants.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="TiffConstants.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Formats
+{
+    using System.Text;
+
+    /// <summary>
+    /// Defines constants defined in the TIFF specification.
+    /// </summary>
+    internal static class GifConstants
+    {
+        /// <summary>
+        /// Byte order markers for indicating little endian encoding.
+        /// </summary>
+        public const ushort ByteOrderLittleEndian = 0x4949;
+
+        /// <summary>
+        /// Byte order markers for indicating big endian encoding.
+        /// </summary>
+        public const ushort ByteOrderBigEndian = 0x4D4D;
+
+        /// <summary>
+        /// Magic number used within the image file header to identify a TIFF format file.
+        /// </summary>
+        public const ushort HeaderMagicNumber = 42;
+    }
+}
---
+++ tests/ImageSharp.Formats.Tiff.Tests/Formats/Tiff/TiffFormatTests.cs
@@ -0,0 +1,88 @@
+﻿// <copyright file="TiffFormatTests.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests
+{
+    using System.Linq;
+    using Xunit;
+
+    using ImageSharp.Formats;
+
+    public class TiffFormatTests
+    {
+        public static object[][] IsLittleEndianValues = new[] { new object[] { false },
+                                                                new object[] { true } };
+
+        [Theory]
+        [MemberData(nameof(IsLittleEndianValues))]
+        public void IsSupportedFileFormat_ReturnsTrue_ForValidFile(bool isLittleEndian)
+        {
+            byte[] bytes = new TiffGenHeader()
+                            {
+                                FirstIfd = new TiffGenIfd()
+                            }
+                            .ToBytes(isLittleEndian);
+
+            TiffFormat tiffFormat = new TiffFormat();
+            byte[] headerBytes = bytes.Take(tiffFormat.HeaderSize).ToArray();
+            bool isSupported = tiffFormat.IsSupportedFileFormat(headerBytes);
+
+            Assert.True(isSupported);
+        }
+
+        [Theory]
+        [MemberData(nameof(IsLittleEndianValues))]
+        public void IsSupportedFileFormat_ReturnsFalse_WithInvalidByteOrderMarkers(bool isLittleEndian)
+        {
+            byte[] bytes = new TiffGenHeader()
+                            {
+                                FirstIfd = new TiffGenIfd(),
+                                ByteOrderMarker = 0x1234
+                            }
+                            .ToBytes(isLittleEndian);
+
+            TiffFormat tiffFormat = new TiffFormat();
+            byte[] headerBytes = bytes.Take(tiffFormat.HeaderSize).ToArray();
+            bool isSupported = tiffFormat.IsSupportedFileFormat(headerBytes);
+
+            Assert.False(isSupported);
+        }
+
+        [Theory]
+        [MemberData(nameof(IsLittleEndianValues))]
+        public void IsSupportedFileFormat_ReturnsFalse_WithIncorrectMagicNumber(bool isLittleEndian)
+        {
+            byte[] bytes = new TiffGenHeader()
+                            {
+                                FirstIfd = new TiffGenIfd(),
+                                MagicNumber = 32
+                            }
+                            .ToBytes(isLittleEndian);
+
+            TiffFormat tiffFormat = new TiffFormat();
+            byte[] headerBytes = bytes.Take(tiffFormat.HeaderSize).ToArray();
+            bool isSupported = tiffFormat.IsSupportedFileFormat(headerBytes);
+
+            Assert.False(isSupported);
+        }
+
+        [Theory]
+        [MemberData(nameof(IsLittleEndianValues))]
+        public void IsSupportedFileFormat_ReturnsFalse_WithShortHeader(bool isLittleEndian)
+        {
+            byte[] bytes = new TiffGenHeader()
+                            {
+                                FirstIfd = new TiffGenIfd()
+                            }
+                            .ToBytes(isLittleEndian);
+
+            TiffFormat tiffFormat = new TiffFormat();
+            byte[] headerBytes = bytes.Take(tiffFormat.HeaderSize - 1).ToArray();
+            bool isSupported = tiffFormat.IsSupportedFileFormat(headerBytes);
+
+            Assert.False(isSupported);
+        }
+    }
+}

b8fd250117031d1c274b16ae4440b0c55522508d
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2017-03-02 22:29:00 +0000

    Add stub Tiff encoders/decoders

---
+++ src/ImageSharp.Formats.Tiff/TiffDecoder.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="TiffDecoder.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Formats
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Image decoder for generating an image out of a TIFF stream.
+    /// </summary>
+    public class TiffDecoder : IImageDecoder
+    {
+        /// <inheritdoc/>
+        public void Decode<TColor>(Image<TColor> image, Stream stream, IDecoderOptions options)
+            where TColor : struct, IPixel<TColor>
+        {
+            Guard.NotNull(image, "image");
+            Guard.NotNull(stream, "stream");
+
+            using (TiffDecoderCore decoder = new TiffDecoderCore(options))
+            {
+                decoder.Decode(image, stream, false);
+            }
+        }
+    }
+}
---
+++ src/ImageSharp.Formats.Tiff/TiffEncoder.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="TiffEncoder.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Formats
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Encoder for writing the data image to a stream in TIFF format.
+    /// </summary>
+    public class TiffEncoder : IImageEncoder
+    {
+        /// <inheritdoc/>
+        public void Encode<TColor>(Image<TColor> image, Stream stream, IEncoderOptions options)
+            where TColor : struct, IPixel<TColor>
+        {
+            ITiffEncoderOptions tiffOptions = TiffEncoderOptions.Create(options);
+
+            this.Encode(image, stream, tiffOptions);
+        }
+
+        /// <summary>
+        /// Encodes the image to the specified stream from the <see cref="Image{TColor}"/>.
+        /// </summary>
+        /// <typeparam name="TColor">The pixel format.</typeparam>
+        /// <param name="image">The <see cref="Image{TColor}"/> to encode from.</param>
+        /// <param name="stream">The <see cref="Stream"/> to encode the image data to.</param>
+        /// <param name="options">The options for the encoder.</param>
+        public void Encode<TColor>(Image<TColor> image, Stream stream, ITiffEncoderOptions options)
+            where TColor : struct, IPixel<TColor>
+        {
+            throw new NotImplementedException();
+            // TiffEncoderCore encode = new TiffEncoderCore(options);
+            // encode.Encode(image, stream);
+        }
+    }
+}

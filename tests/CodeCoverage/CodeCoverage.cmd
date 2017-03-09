d1bb898336a4d541be377e99ace7339426203492
Scott Williams <tocsoft@gmail.com>
2017-03-09 20:16:01 +0000

    produce full pdb for codecov

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -4,15 +4,13 @@ cd tests\CodeCoverage
 
 nuget restore packages.config -PackagesDirectory .
 
-cd ..\ImageSharp.Tests
-
-dotnet restore
-
 cd ..
 cd ..
 
+dotnet restore  ImageSharp.sln
+dotnet build  ImageSharp.sln --no-incremental -c release /p:codecov=true
 rem The -threshold options prevents this taking ages...
-tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\ImageSharp.Tests\ImageSharp.Tests.csproj -c Release -f net45" -threshold:10 -register:user -filter:"+[ImageSharp*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\ImageSharp.Coverage.xml
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\ImageSharp.Tests\ImageSharp.Tests.csproj --no-build -c release" -searchdirs:"tests\ImageSharp\bin\Release\netcoreapp1.1" -register:user -output:.\ImageSharp.Coverage.xml -hideskipped:All -returntargetcode -oldStyle -filter:"+[ImageSharp*]*" 
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 

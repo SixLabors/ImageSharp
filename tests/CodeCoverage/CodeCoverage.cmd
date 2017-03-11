e8359d9eb9d37e58941ce9f046e19e2e176a9a01
Scott Williams <tocsoft@gmail.com>
2017-03-11 14:26:25 +0000

    allow codecov to finish

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+
 cd tests\CodeCoverage
 
 nuget restore packages.config -PackagesDirectory .
@@ -7,10 +8,10 @@ nuget restore packages.config -PackagesDirectory .
 cd ..
 cd ..
 
-dotnet restore  ImageSharp.sln
-dotnet build  ImageSharp.sln --no-incremental -c release /p:codecov=true
+dotnet restore ImageSharp.sln
+dotnet build ImageSharp.sln --no-incremental -c release /p:codecov=true
 rem The -threshold options prevents this taking ages...
-tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\ImageSharp.Tests\ImageSharp.Tests.csproj --no-build -c release" -searchdirs:"tests\ImageSharp\bin\Release\netcoreapp1.1" -register:user -output:.\ImageSharp.Coverage.xml -hideskipped:All -returntargetcode -oldStyle -filter:"+[ImageSharp*]*" 
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\ImageSharp.Tests\ImageSharp.Tests.csproj --no-build -c release /p:codecov=true" -register:user -threshold:10 -oldStyle -safemode:off -output:.\ImageSharp.Coverage.xml -hideskipped:All -returntargetcode -filter:"+[ImageSharp*]*" 
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 

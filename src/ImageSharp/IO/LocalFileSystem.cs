fdfc9c6996cb9654b7a2d2c9c7bbb510f5ab61b8
Scott Williams <tocsoft@gmail.com>
2017-03-14 19:09:02 +0000

    Add IFileSystem

---
+++ src/ImageSharp/IO/LocalFileSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ImageSharp.IO
+{
+#if !NETSTANDARD1_1
+    /// <summary>
+    /// A wrapper around the local File apis.
+    /// </summary>
+    public class LocalFileSystem : IFileSystem
+    {
+        /// <inheritdoc/>
+        public Stream OpenRead(string path)
+        {
+            return File.OpenRead(path);
+        }
+
+        /// <inheritdoc/>
+        public Stream OpenWrite(string path)
+        {
+            return File.OpenWrite(path);
+        }
+    }
+#endif
+}

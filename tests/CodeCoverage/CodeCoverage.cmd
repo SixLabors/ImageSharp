a874148e4fb2acbbecbd94d62ae767cafe0f1b96
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2017-03-18 16:12:27 +0000

    Fix project path for CI coverage script

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -12,7 +12,7 @@ dotnet restore ImageSharp.sln
 dotnet build ImageSharp.sln --no-incremental -c release /p:codecov=true
 rem The -threshold options prevents this taking ages...
 tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\ImageSharp.Tests\ImageSharp.Tests.csproj --no-build -c release /p:codecov=true" -register:user -threshold:10 -oldStyle -safemode:off -output:.\ImageSharp.Coverage.xml -hideskipped:All -returntargetcode -filter:"+[ImageSharp*]*" 
-tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\ImageSharp.Tests\ImageSharp.Formats.Tiff.Tests.csproj --no-build -c release /p:codecov=true" -register:user -threshold:10 -oldStyle -safemode:off -output:.\ImageSharp.Formats.Tiff.Coverage.xml -hideskipped:All -returntargetcode -filter:"+[ImageSharp*]*" 
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\ImageSharp.Formats.Tiff.Tests\ImageSharp.Formats.Tiff.Tests.csproj --no-build -c release /p:codecov=true" -register:user -threshold:10 -oldStyle -safemode:off -output:.\ImageSharp.Formats.Tiff.Coverage.xml -hideskipped:All -returntargetcode -filter:"+[ImageSharp*]*" 
 
 
 if %errorlevel% neq 0 exit /b %errorlevel%

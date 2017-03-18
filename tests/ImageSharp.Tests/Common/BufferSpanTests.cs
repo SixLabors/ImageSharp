8b91c02a274f833dfea9bd5dad577e86019970ad
Anton Firszov <anton.firszov@sapagroup.com>
2017-03-18 16:57:15 +0100

    PinnedBuffer<T> and PinnedImageBuffer<T> indexers

--- tests/ImageSharp.Tests/Common/BufferSpanTests.cs
+++ tests/ImageSharp.Tests/Common/BufferSpanTests.cs
@@ -7,62 +7,10 @@ namespace ImageSharp.Tests.Common
 
     using Xunit;
 
+    using static TestStructs;
+
     public unsafe class BufferSpanTests
     {
-        public struct Foo
-        {
-            public int A;
-
-            public double B;
-
-            public Foo(int a, double b)
-            {
-                this.A = a;
-                this.B = b;
-            }
-
-            internal static Foo[] CreateArray(int size)
-            {
-                Foo[] result = new Foo[size];
-                for (int i = 0; i < size; i++)
-                {
-                    result[i] = new Foo(i+1, i+1);
-                }
-                return result;
-            }
-        }
-
-        /// <summary>
-        /// sizeof(AlignedFoo) == sizeof(long)
-        /// </summary>
-        public struct AlignedFoo
-        {
-            public int A;
-
-            public int B;
-
-            static AlignedFoo()
-            {
-                Assert.Equal(sizeof(AlignedFoo), sizeof(long));
-            }
-
-            public AlignedFoo(int a, int b)
-            {
-                this.A = a;
-                this.B = b;
-            }
-
-            internal static AlignedFoo[] CreateArray(int size)
-            {
-                AlignedFoo[] result = new AlignedFoo[size];
-                for (int i = 0; i < size; i++)
-                {
-                    result[i] = new AlignedFoo(i + 1, i + 1);
-                }
-                return result;
-            }
-        }
-
         [Fact]
         public void AsBytes()
         {

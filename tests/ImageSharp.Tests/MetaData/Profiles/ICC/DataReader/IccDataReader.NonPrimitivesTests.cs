4d3bfc1c59ca4a8adf5b99c89ccfcd0b99f9db7d
Johannes Bildstein <johannes@bildstein.at>
2017-03-25 00:40:23 +0100

    add tests and test data for ICC reading/writing

---
+++ tests/ImageSharp.Tests/MetaData/Profiles/ICC/DataReader/IccDataReader.NonPrimitivesTests.cs
@@ -0,0 +1,118 @@
+﻿// <copyright file="IccDataReader.NonPrimitivesTests.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests.Icc
+{
+    using System;
+    using System.Numerics;
+    using Xunit;
+
+    public class IccDataReaderNonPrimitivesTests
+    {
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.DateTimeTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        public void ReadDateTime(byte[] data, DateTime expected)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            DateTime output = reader.ReadDateTime();
+
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.VersionNumberTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        public void ReadVersionNumber(byte[] data, Version expected)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            Version output = reader.ReadVersionNumber();
+
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.XyzNumberTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        public void ReadXyzNumber(byte[] data, Vector3 expected)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            Vector3 output = reader.ReadXyzNumber();
+
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.ProfileIdTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        internal void ReadProfileId(byte[] data, IccProfileId expected)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            IccProfileId output = reader.ReadProfileId();
+
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.PositionNumberTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        internal void ReadPositionNumber(byte[] data, IccPositionNumber expected)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            IccPositionNumber output = reader.ReadPositionNumber();
+
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.ResponseNumberTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        internal void ReadResponseNumber(byte[] data, IccResponseNumber expected)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            IccResponseNumber output = reader.ReadResponseNumber();
+
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.NamedColorTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        internal void ReadNamedColor(byte[] data, IccNamedColor expected, uint coordinateCount)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            IccNamedColor output = reader.ReadNamedColor(coordinateCount);
+
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.ProfileDescriptionTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        internal void ReadProfileDescription(byte[] data, IccProfileDescription expected)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            IccProfileDescription output = reader.ReadProfileDescription();
+
+            Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [MemberData(nameof(IccTestDataNonPrimitives.ColorantTableEntryTestData), MemberType = typeof(IccTestDataNonPrimitives))]
+        internal void ReadColorantTableEntry(byte[] data, IccColorantTableEntry expected)
+        {
+            IccDataReader reader = CreateReader(data);
+
+            IccColorantTableEntry output = reader.ReadColorantTableEntry();
+
+            Assert.Equal(expected, output);
+        }
+
+        private IccDataReader CreateReader(byte[] data)
+        {
+            return new IccDataReader(data);
+        }
+    }
+}
---
+++ tests/ImageSharp.Tests/TestUtilities/ArrayHelper.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="ExifValueTests.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests
+{
+    using System.Linq;
+
+    public static class ArrayHelper
+    {
+        /// <summary>
+        /// Concatenates multiple arrays of the same type into one
+        /// </summary>
+        /// <typeparam name="T">The array type</typeparam>
+        /// <param name="arrs">The arrays to concatenate. The order is kept</param>
+        /// <returns>The concatenated array</returns>
+        public static T[] Concat<T>(params T[][] arrs)
+        {
+            T[] result = new T[arrs.Sum(t => t.Length)];
+            int offset = 0;
+            for (int i = 0; i < arrs.Length; i++)
+            {
+                arrs[i].CopyTo(result, offset);
+                offset += arrs[i].Length;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Creates an array filled with the given value
+        /// </summary>
+        /// <typeparam name="T">The array type</typeparam>
+        /// <param name="value">The value to fill the array with</param>
+        /// <param name="length">The wanted length of the array</param>
+        /// <returns>The created array filled with the given value</returns>
+        public static T[] Fill<T>(T value, int length)
+        {
+            T[] result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = value;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Creates a string from a character with a given length
+        /// </summary>
+        /// <param name="value">The character to fill the string with</param>
+        /// <param name="length">The wanted length of the string</param>
+        /// <returns>The filled string</returns>
+        public static string Fill(char value, int length)
+        {
+            return "".PadRight(length, value);
+        }
+    }
+}

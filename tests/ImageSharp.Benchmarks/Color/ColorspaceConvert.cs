190d4a933dfd6a28ac20b53365641d1df97207b8
James Jackson-South <james_south@hotmail.com>
2017-03-25 18:00:57 +1100

    Add benchmark

---
+++ tests/ImageSharp.Benchmarks/Color/ColorspaceConvert.cs
@@ -0,0 +1,34 @@
+﻿namespace ImageSharp.Benchmarks.Color
+{
+    using BenchmarkDotNet.Attributes;
+
+    using Colourful;
+    using Colourful.Conversion;
+
+    using ImageSharp.Colors.Conversion;
+    using ImageSharp.Colors.Spaces;
+
+    public class ColorspaceConvert
+    {
+        private static readonly CieXyz CieXyz = new CieXyz(0.95047F, 1, 1.08883F);
+
+        private static readonly XYZColor XYZColor = new XYZColor(0.95047, 1, 1.08883);
+
+        private static readonly ColorSpaceConverter ColorSpaceConverter = new ColorSpaceConverter();
+
+        private static readonly ColourfulConverter ColourfulConverter = new ColourfulConverter();
+
+
+        [Benchmark(Baseline = true, Description = "Colourful Convert")]
+        public LMSColor SystemDrawingColorEqual()
+        {
+            return ColourfulConverter.ToLMS(XYZColor);
+        }
+
+        [Benchmark(Description = "ImageSharp Convert")]
+        public Lms ColorEqual()
+        {
+            return ColorSpaceConverter.ToLms(CieXyz);
+        }
+    }
+}

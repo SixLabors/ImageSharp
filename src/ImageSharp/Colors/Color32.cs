e340b3f10388d924fe50aaebebf89464771293db
James Jackson-South <james_south@hotmail.com>
2017-04-17 00:20:10 +1000

    Begin add vector based Color

--- src/ImageSharp/Colors/Color32.cs
+++ src/ImageSharp/Colors/Color32.cs
@@ -10,7 +10,7 @@ namespace ImageSharp
     using System.Runtime.InteropServices;
 
     /// <summary>
-    /// Packed pixel type containing four 8-bit unsigned normalized values ranging from 0 to 255.
+    /// Unpacked pixel type containing four 8-bit unsigned normalized values ranging from 0 to 255.
     /// The color components are stored in red, green, blue, and alpha order.
     /// </summary>
     /// <remarks>

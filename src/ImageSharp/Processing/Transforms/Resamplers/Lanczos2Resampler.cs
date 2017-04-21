f1fd99c83186a884cd09cf72dbd418da7de1c1ff
James Jackson-South <james_south@hotmail.com>
2017-04-21 22:28:35 +1000

    Move SinC to MathF [skip ci]

--- src/ImageSharp/Processing/Transforms/Resamplers/Lanczos2Resampler.cs
+++ src/ImageSharp/Processing/Transforms/Resamplers/Lanczos2Resampler.cs
@@ -25,7 +25,7 @@ namespace ImageSharp.Processing
 
             if (x < 2F)
             {
-                return ImageMaths.SinC(x) * ImageMaths.SinC(x / 2F);
+                return MathF.SinC(x) * MathF.SinC(x / 2F);
             }
 
             return 0F;

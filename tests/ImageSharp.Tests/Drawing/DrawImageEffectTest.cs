3a542eec74dd3f4bfa3b1a63018db6c98bccc31a
Vicente Penades <megaz0ne23@hotmail.com>
2017-04-29 00:25:28 +0200

    Added a DrawImage Effect test

---
+++ tests/ImageSharp.Tests/Drawing/DrawImageEffectTest.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="DrawImageEffectTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests
+{
+    using System.IO;
+    using ImageSharp.PixelFormats;
+    using Xunit;
+
+    public class DrawImageEffectTest : FileTestBase
+    {
+        [Fact]
+        public void ImageShouldApplyDrawImageFilter()
+        {
+            string path = this.CreateOutputDirectory("Drawing", "DrawImageEffect");
+
+            PixelTransformMode[] modes = (PixelTransformMode[])System.Enum.GetValues(typeof(PixelTransformMode));
+
+            using (Image blend = TestFile.Create(TestImages.Png.Blur).CreateImage())
+            {
+                foreach (TestFile file in Files)
+                {
+                    using (Image image = file.CreateImage())
+                    {
+                        foreach (PixelTransformMode mode in modes)
+                        {
+                            using (FileStream output = File.OpenWrite($"{path}/{mode}.{file.FileName}"))
+                            {
+                                Size size = new Size(image.Width / 2, image.Height / 2);
+                                Point loc = new Point(image.Width / 4, image.Height / 4);
+
+                                image.DrawImage(blend, mode, 75, size, loc).Save(output);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

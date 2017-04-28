3575264f0d73fcdb5dc005fc130bd0634eb7df19
Vicente Penades <megaz0ne23@hotmail.com>
2017-04-28 21:14:13 +0200

    Added Image drawing functions to use general porter duff modes

--- src/ImageSharp.Drawing/DrawImage.cs
+++ src/ImageSharp.Drawing/DrawImage.cs
@@ -5,6 +5,7 @@
 
 namespace ImageSharp
 {
+    using System;
     using Drawing.Processors;
     using ImageSharp.PixelFormats;
 
@@ -51,6 +52,53 @@ namespace ImageSharp
             }
 
             source.ApplyProcessor(new DrawImageProcessor<TPixel>(image, size, location, percent), source.Bounds);
+            return source;
+        }
+
+        /// <summary>
+        /// Draws the given image together with the current one by blending their pixels.
+        /// </summary>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="image">The image to blend with the currently processing image.</param>
+        /// <param name="mode">Pixel function effect to apply on every pixel</param>
+        /// <typeparam name="TPixel">The pixel format.</typeparam>
+        /// <param name="percent">The opacity of the image image to blend. Must be between 0 and 100.</param>
+        /// <param name="size">The size to draw the blended image.</param>
+        /// <param name="location">The location to draw the blended image.</param>
+        /// <returns>The <see cref="Image{TPixel}"/>.</returns>
+        public static Image<TPixel> DrawImage<TPixel>(this Image<TPixel> source, Image<TPixel> image, PixelTransformMode mode, int percent, Size size, Point location)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            Func<TPixel, TPixel, float, TPixel> pixelFunc = mode.GetPixelFunction<TPixel>();
+
+            return DrawImage(source, image, pixelFunc, percent, size, location);
+        }
+
+        /// <summary>
+        /// Draws the given image together with the current one by blending their pixels.
+        /// </summary>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="image">The image to blend with the currently processing image.</param>
+        /// <param name="pixelFunc">Pixel function effect to apply on every pixel</param>
+        /// <typeparam name="TPixel">The pixel format.</typeparam>
+        /// <param name="percent">The opacity of the image image to blend. Must be between 0 and 100.</param>
+        /// <param name="size">The size to draw the blended image.</param>
+        /// <param name="location">The location to draw the blended image.</param>
+        /// <returns>The <see cref="Image{TPixel}"/>.</returns>
+        public static Image<TPixel> DrawImage<TPixel>(this Image<TPixel> source, Image<TPixel> image, Func<TPixel, TPixel, float, TPixel> pixelFunc, int percent, Size size, Point location)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            if (size == default(Size))
+            {
+                size = new Size(image.Width, image.Height);
+            }
+
+            if (location == default(Point))
+            {
+                location = Point.Empty;
+            }
+
+            source.ApplyProcessor(new DrawImageEffectProcessor<TPixel>(image, size, location, pixelFunc, percent), source.Bounds);
             return source;
         }
     }

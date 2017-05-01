659d11d5ecb09ff34c1b015b3303627e1cfe4ddc
James Jackson-South <james_south@hotmail.com>
2017-05-01 15:23:47 +1000

    Fix summary classname

--- src/ImageSharp/Quantizers/IQuantizer{TPixel}.cs
+++ src/ImageSharp/Quantizers/IQuantizer{TPixel}.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="IDitheredQuantizer{TPixel}.cs" company="James Jackson-South">
+﻿// <copyright file="IQuantizer{TPixel}.cs" company="James Jackson-South">
 // Copyright (c) James Jackson-South and contributors.
 // Licensed under the Apache License, Version 2.0.
 // </copyright>

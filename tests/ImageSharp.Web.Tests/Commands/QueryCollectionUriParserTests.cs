505105fcf56f5c684be48c6ab7919b81e8c9f053
James Jackson-South <james_south@hotmail.com>
2017-05-22 21:39:04 +1000

    Add QueryCollectionUriParserTest #216

---
+++ tests/ImageSharp.Web.Tests/Commands/QueryCollectionUriParserTests.cs
@@ -0,0 +1,29 @@
+﻿namespace ImageSharp.Web.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+
+    using ImageSharp.Web.Commands;
+
+    using Microsoft.AspNetCore.Http;
+
+    using Xunit;
+
+    public class QueryCollectionUriParserTests
+    {
+        [Fact]
+        public void QueryCollectionParserExtractsCommands()
+        {
+            IDictionary<string, string> expected = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                {"width", "400" },
+                {"height", "200" }
+            };
+
+            HttpContext context = TestHelpers.CreateHttpContext();
+            IDictionary<string, string> actual = new QueryCollectionUriParser().ParseUriCommands(context);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ tests/ImageSharp.Web.Tests/TestHelpers.cs
@@ -0,0 +1,23 @@
+﻿namespace ImageSharp.Web.Tests
+{
+    using Microsoft.AspNetCore.Http;
+
+    public class TestHelpers
+    {
+        public static HttpContext CreateHttpContext()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Path = "/testwebsite.com/image-12345.jpeg";
+            httpContext.Request.QueryString = new QueryString("?width=400&height=200");
+            return httpContext;
+        }
+
+        public static HttpContext CreateHttpContext(string path, string query)
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Path = path;
+            httpContext.Request.QueryString = new QueryString(query);
+            return httpContext;
+        }
+    }
+}

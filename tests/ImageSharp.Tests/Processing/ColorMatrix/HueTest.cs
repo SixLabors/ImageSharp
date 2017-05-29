ac919121e03c7cadcc965eeabfd55f4d95ede850
James Jackson-South <james_south@hotmail.com>
2017-05-29 23:30:30 +1000

    Hue

---
+++ tests/ImageSharp.Tests/Processing/ColorMatrix/HueTest.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="HueTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests.Processing.ColorMatrix
+{
+    using ImageSharp.PixelFormats;
+
+    using Xunit;
+
+    public class HueTest : FileTestBase
+    {
+        public static readonly TheoryData<int> HueValues
+        = new TheoryData<int>
+        {
+            180,
+           -180
+        };
+
+        [Theory]
+        [WithFileCollection(nameof(AllBmpFiles), nameof(HueValues), StandardPixelTypes)]
+        public void ImageShouldApplyHueFilter<TPixel>(TestImageProvider<TPixel> provider, int value)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                image.Hue(value)
+                    .DebugSave(provider, null, Extensions.Bmp);
+            }
+        }
+
+        [Theory]
+        [WithFileCollection(nameof(AllBmpFiles), nameof(HueValues), StandardPixelTypes)]
+        public void ImageShouldApplyHueFilterInBox<TPixel>(TestImageProvider<TPixel> provider, int value)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> source = provider.GetImage())
+            using (var image = new Image<TPixel>(source))
+            {
+                var bounds = new Rectangle(10, 10, image.Width / 2, image.Height / 2);
+
+                image.Hue(value, bounds)
+                    .DebugSave(provider, null, Extensions.Bmp);
+
+                // Draw identical shapes over the bounded and compare to ensure changes are constrained.
+                image.Fill(NamedColors<TPixel>.HotPink, bounds);
+                source.Fill(NamedColors<TPixel>.HotPink, bounds);
+                ImageComparer.CheckSimilarity(image, source);
+            }
+        }
+    }
+}

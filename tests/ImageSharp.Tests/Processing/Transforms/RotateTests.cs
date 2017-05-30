43cb22c13943e91c43f64bdbfe4877e1e3f8c8b1
James Jackson-South <james_south@hotmail.com>
2017-05-30 23:52:06 +1000

    Rotate, Skew

---
+++ tests/ImageSharp.Tests/Processing/Transforms/RotateTests.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="RotateTests.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests.Processing.Transforms
+{
+    using ImageSharp.PixelFormats;
+    using ImageSharp.Processing;
+
+    using Xunit;
+
+    public class RotateTests : FileTestBase
+    {
+        public static readonly TheoryData<float> RotateFloatValues
+            = new TheoryData<float>
+        {
+             170,
+            -170
+        };
+
+        public static readonly TheoryData<RotateType> RotateEnumValues
+            = new TheoryData<RotateType>
+        {
+            RotateType.None,
+            RotateType.Rotate90,
+            RotateType.Rotate180,
+            RotateType.Rotate270
+        };
+
+        [Theory]
+        [WithFileCollection(nameof(DefaultFiles), nameof(RotateFloatValues), StandardPixelType)]
+        public void ImageShouldRotate<TPixel>(TestImageProvider<TPixel> provider, float value)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                image.Rotate(value)
+                    .DebugSave(provider, value, Extensions.Bmp);
+            }
+        }
+
+        [Theory]
+        [WithFileCollection(nameof(DefaultFiles), nameof(RotateEnumValues), StandardPixelType)]
+        public void ImageShouldRotateEnum<TPixel>(TestImageProvider<TPixel> provider, RotateType value)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                image.Rotate(value)
+                    .DebugSave(provider, value, Extensions.Bmp);
+            }
+        }
+    }
+}

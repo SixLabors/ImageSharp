e56d5f016f6e20e64051d834861c718df01bf46f
James Jackson-South <james_south@hotmail.com>
2017-05-30 23:52:06 +1000

    Rotate, Skew

---
+++ tests/ImageSharp.Tests/Processing/Transforms/SkewTest.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="SkewTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests.Processing.Transforms
+{
+    using ImageSharp.PixelFormats;
+
+    using Xunit;
+
+    public class SkewTest : FileTestBase
+    {
+        public static readonly TheoryData<float, float> SkewValues
+        = new TheoryData<float, float>
+        {
+            { 20, 10 },
+            { -20, -10 }
+        };
+
+        [Theory]
+        [WithFileCollection(nameof(DefaultFiles), nameof(SkewValues), StandardPixelType)]
+        public void ImageShouldSkew<TPixel>(TestImageProvider<TPixel> provider, float x, float y)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                image.Skew(x, y)
+                    .DebugSave(provider, string.Join("_", x, y), Extensions.Bmp);
+            }
+        }
+    }
+}

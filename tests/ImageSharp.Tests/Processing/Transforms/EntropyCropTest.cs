1d169b6f3dce042a9258aef1f083e4c48879152d
James Jackson-South <james_south@hotmail.com>
2017-05-30 17:18:00 +1000

    EntropyCrop

---
+++ tests/ImageSharp.Tests/Processing/Transforms/EntropyCropTest.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="EntropyCropTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests.Processing.Transforms
+{
+    using ImageSharp.PixelFormats;
+
+    using Xunit;
+
+    public class EntropyCropTest : FileTestBase
+    {
+        public static readonly TheoryData<float> EntropyCropValues
+        = new TheoryData<float>
+        {
+            .25F,
+            .75F
+        };
+
+        [Theory]
+        [WithFileCollection(nameof(DefaultFiles), nameof(EntropyCropValues), StandardPixelType)]
+        public void ImageShouldEntropyCrop<TPixel>(TestImageProvider<TPixel> provider, float value)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                image.EntropyCrop(value)
+                    .DebugSave(provider, value, Extensions.Bmp);
+            }
+        }
+    }
+}

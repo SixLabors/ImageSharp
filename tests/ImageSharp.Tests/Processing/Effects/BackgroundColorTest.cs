a8a4dc057ce04416b8d7e82515aa09e063e236a6
James Jackson-South <james_south@hotmail.com>
2017-05-30 16:09:53 +1000

    BackgroundColor

---
+++ tests/ImageSharp.Tests/Processing/Effects/BackgroundColorTest.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="BackgroundColorTest.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Tests.Processing.Effects
+{
+    using ImageSharp.PixelFormats;
+
+    using Xunit;
+
+    public class BackgroundColorTest : FileTestBase
+    {
+        [Theory]
+        [WithFileCollection(nameof(DefaultFiles), StandardPixelType)]
+        public void ImageShouldApplyBackgroundColorFilter<TPixel>(TestImageProvider<TPixel> provider)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                image.BackgroundColor(NamedColors<TPixel>.HotPink)
+                    .DebugSave(provider, null, Extensions.Bmp);
+            }
+        }
+
+        [Theory]
+        [WithFileCollection(nameof(DefaultFiles), StandardPixelType)]
+        public void ImageShouldApplyBackgroundColorFilterInBox<TPixel>(TestImageProvider<TPixel> provider)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> source = provider.GetImage())
+            using (var image = new Image<TPixel>(source))
+            {
+                var bounds = new Rectangle(10, 10, image.Width / 2, image.Height / 2);
+
+                image.BackgroundColor(NamedColors<TPixel>.HotPink, bounds)
+                    .DebugSave(provider, null, Extensions.Bmp);
+
+                // Draw identical shapes over the bounded and compare to ensure changes are constrained.
+                image.Fill(NamedColors<TPixel>.HotPink, bounds);
+                source.Fill(NamedColors<TPixel>.HotPink, bounds);
+                ImageComparer.CheckSimilarity(image, source);
+            }
+        }
+    }
+}

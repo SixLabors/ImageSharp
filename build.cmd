3f96eef751dc8d352751d12be6b0d28d0dbbf495
Scott Williams <tocsoft@gmail.com>
2017-06-02 23:13:20 +0100

    [SL.Core] initial primitive

---
+++ build.cmd
@@ -0,0 +1,38 @@
+@echo Off
+
+if not "%GitVersion_NuGetVersion%" == "" (
+    dotnet restore /p:packageversion=%GitVersion_NuGetVersion%
+)ELSE ( 
+	dotnet restore 
+)
+
+ECHO Building nuget packages
+if not "%GitVersion_NuGetVersion%" == "" (
+    dotnet build -c Release /p:packageversion=%GitVersion_NuGetVersion%
+)ELSE ( 
+    dotnet build -c Release
+)
+if not "%errorlevel%"=="0" goto failure
+
+dotnet test ./tests/SixLabors.Primitives.Tests/SixLabors.Primitives.Tests.csproj
+
+
+if not "%GitVersion_NuGetVersion%" == "" (
+    dotnet pack ./src/SixLabors.Primitives/ -c Release --output ../../artifacts --no-build /p:packageversion=%GitVersion_NuGetVersion%
+)ELSE ( 
+    dotnet pack ./src/SixLabors.Primitives/ -c Release --output ../../artifacts --no-build
+)
+
+if not "%errorlevel%"=="0" goto failure
+
+:success
+ECHO successfully built project
+REM exit 0
+goto end
+
+:failure
+ECHO failed to build.
+REM exit -1
+goto end
+
+:end

0d277b8b5d022bf8079acdeca8549275b02ea5ee
Scott Williams <tocsoft@gmail.com>
2017-06-02 23:13:20 +0100

    [SL.Core] initial primitive

---
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+cd tests\CodeCoverage
+
+nuget restore packages.config -PackagesDirectory .
+
+cd ..
+cd ..
+
+dotnet restore  SixLabors.Primitives.sln
+dotnet build  SixLabors.Primitives.sln --no-incremental -c debug /p:codecov=true
+ 
+rem The -threshold options prevents this taking ages...
+rem tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\SixLabors.Shapes.Tests\SixLabors.Shapes.Tests.csproj --no-build -c Release /p:codecov=true" -threshold:10 -register:user -filter:"+[SixLabors.Shapes*]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -returntargetcode -output:.\SixLabors.Shapes.Coverage.xml
+tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\SixLabors.Primitives.Tests\SixLabors.Primitives.Tests.csproj --no-build -c debug" -searchdirs:"tests\SixLabors.Shapes.Tests\bin\Release\netcoreapp1.1" -register:user -output:.\SixLabors.Shapes.Coverage.xml -hideskipped:All -returntargetcode -oldStyle -filter:"+[SixLabors.Primitives*]*" 
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+SET PATH=C:\\Python34;C:\\Python34\\Scripts;%PATH%
+pip install codecov
+codecov -f "SixLabors.Primitives.Coverage.xml"
---
+++ tests/CodeCoverage/OpenCover.4.6.519/SampleSln/BomTest/BomManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bom;
+using NUnit.Framework;
+
+namespace BomTest
+{
+    [TestFixture]
+    public class BomManagerTests
+    {
+        [Test]
+        public void Sum_Is_Zero_When_No_Entries()
+        {
+            var bomManager = new BomManager();
+            Assert.AreEqual(0, bomManager.MethodToTest(new Collection<int>()));
+        }
+
+        [Test]
+        [TestCase(new[] { 0 }, 0)]
+        [TestCase(new[] { 1 }, 1)]
+        [TestCase(new[] { 1, 2, 3 }, 6)]
+        public void Sum_Is_Calculated_Correctly_When_Entries_Supplied(int[] data, int expected)
+        {
+            var bomManager = new BomManager();
+            Assert.AreEqual(expected, bomManager.MethodToTest(new Collection<int>(data)));
+        }
+
+        [Test]
+        public void Sum_Is_Zero_When_Null_Collection()
+        {
+            var bomManager = new BomManager();
+            Assert.AreEqual(0, bomManager.MethodToTest(null));
+        }
+    }
+}

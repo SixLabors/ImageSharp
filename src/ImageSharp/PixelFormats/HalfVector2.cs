01ebd8e1d6a987b162fb07826e4eafca0786964b
Anton Firszov <anton.firszov@sapagroup.com>
2017-06-04 04:29:42 +0200

    fixed broken rounding

--- src/ImageSharp/PixelFormats/HalfVector2.cs
+++ src/ImageSharp/PixelFormats/HalfVector2.cs
@@ -128,8 +128,8 @@ namespace ImageSharp.PixelFormats
         public void ToRgb24(ref Rgb24 dest)
         {
             Vector4 vector = this.ToScaledVector4();
-            dest.R = (byte)MathF.Round(vector.X);
-            dest.G = (byte)MathF.Round(vector.Y);
+            dest.R = (byte)vector.X;
+            dest.G = (byte)vector.Y;
             dest.B = 0;
         }
 
@@ -138,8 +138,8 @@ namespace ImageSharp.PixelFormats
         public void ToRgba32(ref Rgba32 dest)
         {
             Vector4 vector = this.ToScaledVector4();
-            dest.R = (byte)MathF.Round(vector.X);
-            dest.G = (byte)MathF.Round(vector.Y);
+            dest.R = (byte)vector.X;
+            dest.G = (byte)vector.Y;
             dest.B = 0;
             dest.A = 255;
         }
@@ -149,8 +149,8 @@ namespace ImageSharp.PixelFormats
         public void ToBgr24(ref Bgr24 dest)
         {
             Vector4 vector = this.ToScaledVector4();
-            dest.R = (byte)MathF.Round(vector.X);
-            dest.G = (byte)MathF.Round(vector.Y);
+            dest.R = (byte)vector.X;
+            dest.G = (byte)vector.Y;
             dest.B = 0;
         }
 
@@ -159,8 +159,8 @@ namespace ImageSharp.PixelFormats
         public void ToBgra32(ref Bgra32 dest)
         {
             Vector4 vector = this.ToScaledVector4();
-            dest.R = (byte)MathF.Round(vector.X);
-            dest.G = (byte)MathF.Round(vector.Y);
+            dest.R = (byte)vector.X;
+            dest.G = (byte)vector.Y;
             dest.B = 0;
             dest.A = 255;
         }

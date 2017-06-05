2cf4b328ab17a6ce0bea5f46eec1a4a86df9e693
Anton Firszov <anton.firszov@sapagroup.com>
2017-06-05 23:46:49 +0200

    T4 Rgba32.PixelOperations

--- src/ImageSharp/PixelFormats/Rgba32.PixelOperations.cs
+++ src/ImageSharp/PixelFormats/Rgba32.PixelOperations.cs
@@ -21,7 +21,7 @@ namespace ImageSharp
         /// <summary>
         /// <see cref="PixelOperations{TPixel}"/> implementation optimized for <see cref="Rgba32"/>.
         /// </summary>
-        internal class PixelOperations : PixelOperations<Rgba32>
+        internal partial class PixelOperations : PixelOperations<Rgba32>
         {
             /// <summary>
             /// SIMD optimized bulk implementation of <see cref="IPixel.PackFromVector4(Vector4)"/>
@@ -121,43 +121,6 @@ namespace ImageSharp
             }
 
             /// <inheritdoc />
-            internal override void PackFromRgb24(Span<Rgb24> source, Span<Rgba32> destPixels, int count)
-            {
-                Guard.MustBeSizedAtLeast(source, count, nameof(source));
-                Guard.MustBeSizedAtLeast(destPixels, count, nameof(destPixels));
-
-                ref Rgb24 sourceRef = ref source.DangerousGetPinnableReference();
-                ref Rgba32 destRef = ref destPixels.DangerousGetPinnableReference();
-
-                for (int i = 0; i < count; i++)
-                {
-                    ref Rgb24 sp = ref Unsafe.Add(ref sourceRef, i);
-                    ref Rgba32 dp = ref Unsafe.Add(ref destRef, i);
-
-                    Unsafe.As<Rgba32, Rgb24>(ref dp) = sp;
-                    dp.A = 255;
-                }
-            }
-
-            /// <inheritdoc />
-            internal override void ToRgb24(Span<Rgba32> sourcePixels, Span<Rgb24> dest, int count)
-            {
-                Guard.MustBeSizedAtLeast(sourcePixels, count, nameof(sourcePixels));
-                Guard.MustBeSizedAtLeast(dest, count, nameof(dest));
-
-                ref Rgba32 sourceRef = ref sourcePixels.DangerousGetPinnableReference();
-                ref Rgb24 destRef = ref dest.DangerousGetPinnableReference();
-
-                for (int i = 0; i < count; i++)
-                {
-                    ref Rgba32 sp = ref Unsafe.Add(ref sourceRef, i);
-                    ref Rgb24 dp = ref Unsafe.Add(ref destRef, i);
-
-                    dp = Unsafe.As<Rgba32, Rgb24>(ref sp);
-                }
-            }
-
-            /// <inheritdoc />
             internal override void PackFromRgba32(Span<Rgba32> source, Span<Rgba32> destPixels, int count)
             {
                 GuardSpans(source, nameof(source), destPixels, nameof(destPixels), count);
@@ -171,71 +134,6 @@ namespace ImageSharp
                 GuardSpans(sourcePixels, nameof(sourcePixels), dest, nameof(dest), count);
 
                 SpanHelper.Copy(sourcePixels, dest, count);
-            }
-
-            /// <inheritdoc />
-            internal override void PackFromBgr24(Span<Bgr24> source, Span<Rgba32> destPixels, int count)
-            {
-                GuardSpans(source, nameof(source), destPixels, nameof(destPixels), count);
-
-                ref Bgr24 sourceRef = ref source.DangerousGetPinnableReference();
-                ref Rgba32 destRef = ref destPixels.DangerousGetPinnableReference();
-
-                for (int i = 0; i < count; i++)
-                {
-                    ref Bgr24 sp = ref Unsafe.Add(ref sourceRef, i);
-                    ref Rgba32 dp = ref Unsafe.Add(ref destRef, i);
-                    dp.Bgr = sp;
-                    dp.A = 255;
-                }
-            }
-
-            /// <inheritdoc />
-            internal override void ToBgr24(Span<Rgba32> sourcePixels, Span<Bgr24> dest, int count)
-            {
-                GuardSpans(sourcePixels, nameof(sourcePixels), dest, nameof(dest), count);
-
-                ref Rgba32 sourceRef = ref sourcePixels.DangerousGetPinnableReference();
-                ref Bgr24 destRef = ref dest.DangerousGetPinnableReference();
-
-                for (int i = 0; i < count; i++)
-                {
-                    ref Rgba32 sp = ref Unsafe.Add(ref sourceRef, i);
-                    ref Bgr24 dp = ref Unsafe.Add(ref destRef, i);
-                    dp = sp.Bgr;
-                }
-            }
-
-            /// <inheritdoc />
-            internal override void PackFromBgra32(Span<Bgra32> source, Span<Rgba32> destPixels, int count)
-            {
-                GuardSpans(source, nameof(source), destPixels, nameof(destPixels), count);
-
-                ref Bgra32 sourceRef = ref source.DangerousGetPinnableReference();
-                ref Rgba32 destRef = ref destPixels.DangerousGetPinnableReference();
-
-                for (int i = 0; i < count; i++)
-                {
-                    ref Bgra32 sp = ref Unsafe.Add(ref sourceRef, i);
-                    ref Rgba32 dp = ref Unsafe.Add(ref destRef, i);
-                    dp = sp.ToRgba32();
-                }
-            }
-
-            /// <inheritdoc />
-            internal override void ToBgra32(Span<Rgba32> sourcePixels, Span<Bgra32> dest, int count)
-            {
-                GuardSpans(sourcePixels, nameof(sourcePixels), dest, nameof(dest), count);
-
-                ref Rgba32 sourceRef = ref sourcePixels.DangerousGetPinnableReference();
-                ref Bgra32 destRef = ref dest.DangerousGetPinnableReference();
-
-                for (int i = 0; i < count; i++)
-                {
-                    ref Rgba32 sp = ref Unsafe.Add(ref sourceRef, i);
-                    ref Bgra32 dp = ref Unsafe.Add(ref destRef, i);
-                    dp = sp.ToBgra32();
-                }
             }
 
             /// <summary>

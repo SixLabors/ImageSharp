03da628ba2a57a344f147b07a7eecace61f0d09d
Scott Williams <tocsoft@gmail.com>
2017-06-08 18:01:47 +0100

    remove opencover

--- tests/CodeCoverage/OpenCover.4.6.519/SampleSln/coverage.bat
+++
@@ -1,5 +0,0 @@
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user "-filter:+[Bom]* -[*Test]*" "-target:.\packages\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe" "-targetargs:/noshadow .\BomTest\bin\Debug\BomTest.dll"
-
-.\packages\ReportGenerator.2.1.8.0\tools\ReportGenerator.exe "-reports:results.xml" "-targetdir:.\coverage"
-
-pause

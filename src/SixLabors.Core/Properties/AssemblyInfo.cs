a2349bf0b8ebe60e449031873d3cdad4f4afdefb
Dirk Lemstra <dirk@git.imagemagick.org>
2017-06-25 14:06:07 +0200

    [SL.Core] Moved files in the Shared folder.

--- src/SixLabors.Core/Properties/AssemblyInfo.cs
+++ src/SixLabors.Core/Properties/AssemblyInfo.cs
@@ -10,6 +10,7 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("SixLabors.Primitives")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Six Labors")]

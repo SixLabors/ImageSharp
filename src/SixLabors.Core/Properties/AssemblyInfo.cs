4a065fce1ff54f3fa7ddd68ac32c95683a2d49ee
Dirk Lemstra <dirk@git.imagemagick.org>
2017-06-25 15:08:07 +0200

    [SL.Core] Added guards from the ImageSharp project and added InternalsVisibleTo to make sure they can be used.

--- src/SixLabors.Core/Properties/AssemblyInfo.cs
+++ src/SixLabors.Core/Properties/AssemblyInfo.cs
@@ -34,3 +34,9 @@ using System.Runtime.CompilerServices;
 
 // Ensure the internals can be tested.
 [assembly: InternalsVisibleTo("SixLabors.Core.Tests")]
+
+// Ensure the internals are visible to the other projects.
+[assembly: InternalsVisibleTo("SixLabors.Exif")]
+[assembly: InternalsVisibleTo("SixLabors.Fonts")]
+[assembly: InternalsVisibleTo("SixLabors.ImageSharp")]
+[assembly: InternalsVisibleTo("SixLabors.Shapes")]

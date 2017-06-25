6e7c700e0c93a2ff15da92651006bdb853fc00fd
Dirk Lemstra <dirk@git.imagemagick.org>
2017-06-25 14:15:03 +0200

    Fixed version numbers

--- src/SixLabors.Core/Properties/AssemblyInfo.cs
+++ src/SixLabors.Core/Properties/AssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha02")]
 
 // Ensure the internals can be tested.
 [assembly: InternalsVisibleTo("SixLabors.Core.Tests")]

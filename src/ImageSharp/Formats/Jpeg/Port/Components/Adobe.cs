5683772532162b73e3b1e77c9765d4cbfa9c4d6e
James Jackson-South <james_south@hotmail.com>
2017-06-28 02:23:43 +1000

    Can now decode many images

--- src/ImageSharp/Formats/Jpeg/Port/Components/Adobe.cs
+++ src/ImageSharp/Formats/Jpeg/Port/Components/Adobe.cs
@@ -6,10 +6,12 @@
 // ReSharper disable InconsistentNaming
 namespace ImageSharp.Formats.Jpeg.Port.Components
 {
+    using System;
+
     /// <summary>
     /// Provides information about the Adobe marker segment
     /// </summary>
-    internal struct Adobe
+    internal struct Adobe : IEquatable<Adobe>
     {
         /// <summary>
         /// The DCT Encode Version
@@ -34,5 +36,39 @@ namespace ImageSharp.Formats.Jpeg.Port.Components
         /// 02 : YCCK
         /// </summary>
         public byte ColorTransform;
+
+        /// <inheritdoc/>
+        public bool Equals(Adobe other)
+        {
+            return this.DCTEncodeVersion == other.DCTEncodeVersion
+                && this.APP14Flags0 == other.APP14Flags0
+                && this.APP14Flags1 == other.APP14Flags1
+                && this.ColorTransform == other.ColorTransform;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            return obj is Adobe && this.Equals((Adobe)obj);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                // TODO: Merge and use HashCodeHelpers
+                int hashCode = this.DCTEncodeVersion.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.APP14Flags0.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.APP14Flags1.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.ColorTransform.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 }

d96392e0d2e351f26ee9ed41cad5d1525713dd23
Anton Firszov <anton.firszov@sapagroup.com>
2017-07-01 18:28:35 +0200

    ReferencePngDecoder

--- tests/ImageSharp.Tests/TestUtilities/Integration/IntegrationTestUtils.cs
+++ tests/ImageSharp.Tests/TestUtilities/Integration/IntegrationTestUtils.cs
@@ -29,6 +29,60 @@
             }
         }
 
+        private static void FromArgb32<TPixel>(Span<Argb32> source, Span<TPixel> dest)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            int length = source.Length;
+            Guard.MustBeSizedAtLeast(dest, length, nameof(dest));
+
+            using (var rgbaBuffer = new Buffer<Rgba32>(length))
+            {
+                PixelOperations<Argb32>.Instance.ToRgba32(source, rgbaBuffer, length);
+
+                for (int i = 0; i < length; i++)
+                {
+                    ref Rgba32 s = ref rgbaBuffer[i];
+                    ref TPixel d = ref dest[i];
+
+                    d.PackFromRgba32(s);
+                }
+            }
+        }
+
+        internal static unsafe Image<TPixel> FromSystemDrawingBitmap<TPixel>(System.Drawing.Bitmap bmp)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            int w = bmp.Width;
+            int h = bmp.Height;
+
+            var fullRect = new System.Drawing.Rectangle(0, 0, w, h);
+
+            BitmapData data = bmp.LockBits(fullRect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+            byte* sourcePtrBase = (byte*)data.Scan0;
+
+            long sourceRowByteCount = data.Stride;
+            long destRowByteCount = w * sizeof(Argb32);
+
+            var image = new Image<TPixel>(w, h);
+
+            using (var workBuffer = new Buffer<Argb32>(w))
+            {
+                var destPtr = (Argb32*)workBuffer.Pin();
+                for (int y = 0; y < h; y++)
+                {
+                    Span<TPixel> row = image.GetRowSpan(y);
+                    
+                    byte* sourcePtr = sourcePtrBase + data.Stride * y;
+
+                    Buffer.MemoryCopy(sourcePtr, destPtr, destRowByteCount, sourceRowByteCount);
+
+                    FromArgb32(workBuffer, row);
+                }
+            }
+
+            return image;
+        }
+
         internal static unsafe System.Drawing.Bitmap ToSystemDrawingBitmap<TPixel>(Image<TPixel> image)
             where TPixel : struct, IPixel<TPixel>
         {

8d73031229665a90121695c16da13eeef395cc00
Anton Firszov <anton.firszov@sapagroup.com>
2017-07-01 18:28:35 +0200

    ReferencePngDecoder

--- tests/ImageSharp.Tests/TestUtilities/Tests/IntegrationTestUtilsTests.cs
+++ tests/ImageSharp.Tests/TestUtilities/Tests/IntegrationTestUtilsTests.cs
@@ -29,5 +29,44 @@ namespace ImageSharp.Tests
                 }
             }
         }
+
+        [Theory]
+        [WithBlankImages(1, 1, PixelTypes.Rgba32 | PixelTypes.Bgra32)]
+        public void FromSystemDrawingBitmap<TPixel>(TestImageProvider<TPixel> dummyProvider)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            string path = TestFile.GetPath(TestImages.Png.Splash);
+
+            using (var sdBitmap = new System.Drawing.Bitmap(path))
+            {
+                using (Image<TPixel> image = IntegrationTestUtils.FromSystemDrawingBitmap<TPixel>(sdBitmap))
+                {
+                    image.DebugSave(dummyProvider);
+                }
+            }
+        }
+
+        [Theory]
+        [WithBlankImages(1, 1, PixelTypes.Rgba32 | PixelTypes.Bgra32)]
+        public void OpenWithReferenceDecoder<TPixel>(TestImageProvider<TPixel> dummyProvider)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            string path = TestFile.GetPath(TestImages.Png.Splash);
+            using (Image<TPixel> image = Image.Load<TPixel>(path, ReferencePngDecoder.Instance))
+            {
+                image.DebugSave(dummyProvider);
+            }
+        }
+
+        [Theory]
+        [WithTestPatternImages(20, 20, PixelTypes.Rgba32 | PixelTypes.Argb32)]
+        public void SaveWithReferenceEncoder<TPixel>(TestImageProvider<TPixel> provider)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                provider.Utility.SaveTestOutputFile(image, "png", ReferencePngEncoder.Instance);
+            }
+        }
     }
 }

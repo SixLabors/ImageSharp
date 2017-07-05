a61fcc95d9e37a63903aa2def3e59e475edae39d
Scott Williams <tocsoft@gmail.com>
2017-07-05 07:39:19 +0100

    Rename Generate -> Clone

--- src/ImageSharp.Drawing/Processors/DrawImageProcessor.cs
+++ src/ImageSharp.Drawing/Processors/DrawImageProcessor.cs
@@ -70,7 +70,7 @@ namespace ImageSharp.Drawing.Processors
             {
                 if (targetImage.Bounds.Size != this.Size)
                 {
-                    targetImage = disposableImage = this.Image.Generate(x => x.Resize(this.Size.Width, this.Size.Height));
+                    targetImage = disposableImage = this.Image.Clone(x => x.Resize(this.Size.Width, this.Size.Height));
                 }
 
                 // Align start/end positions.

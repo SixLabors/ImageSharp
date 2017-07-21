2af76f62b396005c4678517e95ed420197df0dd6
Anton Firszov <anton.firszov@sapagroup.com>
2017-07-22 01:27:09 +0200

    refactored GetFormatsDirectory() to GetInputImagesDirectoryFullPath()

---
+++ tests/ImageSharp.Tests/TestUtilities/Tests/TestEnvironmentTests.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="FlagsHelper.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+// ReSharper disable InconsistentNaming
+namespace ImageSharp.Tests
+{
+    using System.IO;
+
+    using Xunit;
+    using Xunit.Abstractions;
+    
+    public class TestEnvironmentTests
+    {
+        public TestEnvironmentTests(ITestOutputHelper output)
+        {
+            this.Output = output;
+        }
+
+        private ITestOutputHelper Output { get; }
+
+
+        [Fact]
+        public void GetSolutionDirectoryFullPath()
+        {
+            string path = TestEnvironment.GetSolutionDirectoryFullPath();
+            this.Output.WriteLine(path);
+
+            Assert.True(Directory.Exists(path));
+        }
+
+        [Fact]
+        public void GetInputImagesDirectoryFullPath()
+        {
+            string path = TestEnvironment.GetInputImagesDirectoryFullPath();
+            this.Output.WriteLine(path);
+
+            Assert.True(Directory.Exists(path));
+        }
+    }
+}

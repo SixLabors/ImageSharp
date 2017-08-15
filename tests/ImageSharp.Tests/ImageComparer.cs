0b0ba1369aee5f5e4d9eb1a7ed97134f0c22833c
Anton Firszov <anton.firszov@sapagroup.com>
2017-08-15 16:52:04 +0200

    implemented ExactImageComparer

--- tests/ImageSharp.Tests/ImageComparer.cs
+++ tests/ImageSharp.Tests/ImageComparer.cs
@@ -1,22 +1,10 @@
 namespace ImageSharp.Tests
 {
-    using System;
-
     using ImageSharp.PixelFormats;
 
     public abstract class ImageComparer
     {
         public abstract void Verify<TPixelA, TPixelB>(Image<TPixelA> expected, Image<TPixelB> actual)
             where TPixelA : struct, IPixel<TPixelA> where TPixelB : struct, IPixel<TPixelB>;
-    }
-
-    public class ExactComparer : ImageComparer
-    {
-        public static ExactComparer Instance { get; } = new ExactComparer();
-
-        public override void Verify<TPixelA, TPixelB>(Image<TPixelA> expected, Image<TPixelB> actual)
-        {
-            throw new NotImplementedException();
-        }
     }
 }

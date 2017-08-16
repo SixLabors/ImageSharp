c491cdd9739ac29c8dffef49915297bbcd0f0b97
Anton Firszov <anton.firszov@sapagroup.com>
2017-08-16 04:18:26 +0200

    JpegDecoderTests using reference images

--- tests/ImageSharp.Tests/TestUtilities/Tests/TestImageExtensionsTests.cs
+++ tests/ImageSharp.Tests/TestUtilities/Tests/TestImageExtensionsTests.cs
@@ -4,6 +4,9 @@ namespace ImageSharp.Tests
     using System;
 
     using ImageSharp.PixelFormats;
+    using ImageSharp.Tests.TestUtilities.ImageComparison;
+
+    using Moq;
 
     using Xunit;
 
@@ -41,6 +44,52 @@ namespace ImageSharp.Tests
             using (Image<TPixel> image = provider.GetImage())
             {
                 Assert.ThrowsAny<Exception>(() => image.CompareToReferenceOutput(provider));
+            }
+        }
+
+        [Theory]
+        [WithFile(TestImages.Png.CalliphoraPartial, PixelTypes.Rgba32)]
+        public void CompareToOriginal_WhenSimilar<TPixel>(TestImageProvider<TPixel> provider)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                using (Image<TPixel> clone = image.Clone())
+                {
+                    clone.CompareToOriginal(provider, ImageComparer.Exact);
+                }
+            }
+        }
+
+        [Theory]
+        [WithFile(TestImages.Png.CalliphoraPartial, PixelTypes.Rgba32)]
+        public void CompareToOriginal_WhenDifferent_Throws<TPixel>(TestImageProvider<TPixel> provider)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                ImagingTestCaseUtility.ModifyPixel(image, 3, 1, 1);
+
+                Assert.ThrowsAny<ImagePixelsAreDifferentException>(
+                    () =>
+                        {
+                            image.CompareToOriginal(provider, ImageComparer.Exact);
+                        });
+            }
+        }
+
+        [Theory]
+        [WithBlankImages(10, 10, PixelTypes.Rgba32)]
+        public void CompareToOriginal_WhenInputIsNotFromFile_Throws<TPixel>(TestImageProvider<TPixel> provider)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            using (Image<TPixel> image = provider.GetImage())
+            {
+                Assert.ThrowsAny<Exception>(
+                    () =>
+                        {
+                            image.CompareToOriginal(provider, Mock.Of<ImageComparer>());
+                        });
             }
         }
     }

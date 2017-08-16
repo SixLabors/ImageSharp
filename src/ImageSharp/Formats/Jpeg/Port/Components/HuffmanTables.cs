5f18f802dc06e0121c8d7ad81498e953cb5bdab1
Anton Firszov <anton.firszov@sapagroup.com>
2017-08-16 14:33:35 +0200

    copied pdfjs classes from the jpeg-port branch

---
+++ src/ImageSharp/Formats/Jpeg/Port/Components/HuffmanTables.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="HuffmanTables.cs" company="James Jackson-South">
+// Copyright (c) James Jackson-South and contributors.
+// Licensed under the Apache License, Version 2.0.
+// </copyright>
+
+namespace ImageSharp.Formats.Jpeg.Port.Components
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Defines a pair of huffman tables
+    /// </summary>
+    internal sealed class HuffmanTables : IDisposable
+    {
+        private readonly HuffmanTable[] tables = new HuffmanTable[4];
+
+        /// <summary>
+        /// Gets or sets the table at the given index.
+        /// </summary>
+        /// <param name="index">The index</param>
+        /// <returns>The <see cref="List{HuffmanBranch}"/></returns>
+        public ref HuffmanTable this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                return ref this.tables[index];
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            for (int i = 0; i < this.tables.Length; i++)
+            {
+                this.tables[i].Dispose();
+            }
+        }
+    }
+}

234b8303267386ecce1586e5dffd774770a294f5
Anton Firszov <anton.firszov@sapagroup.com>
2017-08-18 20:10:02 +0200

    TestImageProvider.FileProvider cache is now aware of decoder parameters

--- tests/ImageSharp.Tests/TestUtilities/ImageProviders/FileProvider.cs
+++ tests/ImageSharp.Tests/TestUtilities/ImageProviders/FileProvider.cs
@@ -7,6 +7,8 @@ namespace ImageSharp.Tests
 {
     using System;
     using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Reflection;
 
     using ImageSharp.Formats;
     using ImageSharp.PixelFormats;
@@ -20,11 +22,86 @@ namespace ImageSharp.Tests
         {
             // Need PixelTypes in the dictionary key, because result images of TestImageProvider<TPixel>.FileProvider
             // are shared between PixelTypes.Color & PixelTypes.Rgba32
-            private class Key : Tuple<PixelTypes, string, Type>
+            private class Key : IEquatable<Key>
             {
-                public Key(PixelTypes pixelType, string filePath, Type customDecoderType = null)
-                    : base(pixelType, filePath, customDecoderType)
+                private Tuple<PixelTypes, string, Type> commonValues;
+
+                private Dictionary<string, object> decoderParameters;
+
+                public Key(PixelTypes pixelType, string filePath, IImageDecoder customDecoder)
                 {
+                    Type customType = customDecoder?.GetType();
+                    this.commonValues = new Tuple<PixelTypes, string, Type>(pixelType, filePath, customType);
+                    this.decoderParameters = GetDecoderParameters(customDecoder);
+                }
+
+                private static Dictionary<string, object> GetDecoderParameters(IImageDecoder customDecoder)
+                {
+                    Type type = customDecoder.GetType();
+                    
+                    var data = new Dictionary<string, object>();
+
+                    while (type != null && type != typeof(object))
+                    {
+                        PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                        foreach (PropertyInfo p in properties)
+                        {
+                            string key = $"{type.FullName}.{p.Name}";
+                            object value = p.GetValue(customDecoder);
+                            data[key] = value;
+                        }
+                        type = type.GetTypeInfo().BaseType;
+                    }
+                    return data;
+                }
+
+                public bool Equals(Key other)
+                {
+                    if (ReferenceEquals(null, other)) return false;
+                    if (ReferenceEquals(this, other)) return true;
+                    if (!this.commonValues.Equals(other.commonValues)) return false;
+
+                    if (this.decoderParameters.Count != other.decoderParameters.Count)
+                    {
+                        return false;
+                    }
+                    
+                    foreach (KeyValuePair<string, object> kv in this.decoderParameters)
+                    {
+                        object otherVal;
+                        if (!other.decoderParameters.TryGetValue(kv.Key, out otherVal))
+                        {
+                            return false;
+                        }
+                        if (!object.Equals(kv.Value, otherVal))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+
+                public override bool Equals(object obj)
+                {
+                    if (ReferenceEquals(null, obj)) return false;
+                    if (ReferenceEquals(this, obj)) return true;
+                    if (obj.GetType() != this.GetType()) return false;
+                    return this.Equals((Key)obj);
+                }
+
+                public override int GetHashCode()
+                {
+                    return this.commonValues.GetHashCode();
+                }
+
+                public static bool operator ==(Key left, Key right)
+                {
+                    return Equals(left, right);
+                }
+
+                public static bool operator !=(Key left, Key right)
+                {
+                    return !Equals(left, right);
                 }
             }
 
@@ -53,7 +130,7 @@ namespace ImageSharp.Tests
             {
                 Guard.NotNull(decoder, nameof(decoder));
 
-                Key key = new Key(this.PixelType, this.FilePath, decoder.GetType());
+                Key key = new Key(this.PixelType, this.FilePath, decoder);
 
                 Image<TPixel> cachedImage = cache.GetOrAdd(
                     key,

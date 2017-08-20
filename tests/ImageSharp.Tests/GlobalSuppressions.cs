f7e2b81d541d661b2b9767c82bccfa48bb6f1257
Anton Firszov <anton.firszov@sapagroup.com>
2017-08-20 19:33:36 +0200

    suppress xUnit1013 for CI

--- tests/ImageSharp.Tests/GlobalSuppressions.cs
+++ tests/ImageSharp.Tests/GlobalSuppressions.cs
@@ -5,5 +5,6 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1013:Public method should be marked as test")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Assertions", "xUnit2013:Do not use equality check to check for collection size.")]
 

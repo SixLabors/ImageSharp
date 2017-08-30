4d85b30c98ef4253108fd953eb613d85ca256046
James Jackson-South <james_south@hotmail.com>
2017-08-31 00:27:34 +1000

    GrayScale & RGB + Tests

--- src/ImageSharp/Formats/Jpeg/Common/Decoder/JpegColorConverter.FromGrayScale.cs
+++ src/ImageSharp/Formats/Jpeg/Common/Decoder/JpegColorConverter.FromGrayScale.cs
@@ -5,10 +5,10 @@ namespace SixLabors.ImageSharp.Formats.Jpeg.Common.Decoder
 {
     internal abstract partial class JpegColorConverter
     {
-        private class FromYCbCr : JpegColorConverter
+        private class FromGrayScale : JpegColorConverter
         {
-            public FromYCbCr()
-                : base(JpegColorSpace.YCbCr)
+            public FromGrayScale()
+                : base(JpegColorSpace.GrayScale)
             {
             }
 
@@ -16,8 +16,6 @@ namespace SixLabors.ImageSharp.Formats.Jpeg.Common.Decoder
             {
                 // TODO: We can optimize a lot here with Vector<float> and SRCS.Unsafe()!
                 ReadOnlySpan<float> yVals = values.Component0;
-                ReadOnlySpan<float> cbVals = values.Component1;
-                ReadOnlySpan<float> crVals = values.Component2;
 
                 var v = new Vector4(0, 0, 0, 1);
 
@@ -26,12 +24,10 @@ namespace SixLabors.ImageSharp.Formats.Jpeg.Common.Decoder
                 for (int i = 0; i < result.Length; i++)
                 {
                     float y = yVals[i];
-                    float cb = cbVals[i] - 128F;
-                    float cr = crVals[i] - 128F;
 
-                    v.X = MathF.Round(y + (1.402F * cr), MidpointRounding.AwayFromZero);
-                    v.Y = MathF.Round(y - (0.344136F * cb) - (0.714136F * cr), MidpointRounding.AwayFromZero);
-                    v.Z = MathF.Round(y + (1.772F * cb), MidpointRounding.AwayFromZero);
+                    v.X = y;
+                    v.Y = y;
+                    v.Z = y;
 
                     v *= scale;
 

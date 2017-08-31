f5d1e3de844140c45c17b8251b5b2e129ddcc65d
José Caetano Silva <caetanator@hotmail.com>
2017-08-31 19:00:34 +0100

    Fixes

--- src/ImageSharp/Formats/Bmp/BmpCompression.cs
+++ src/ImageSharp/Formats/Bmp/BmpCompression.cs
@@ -7,6 +7,10 @@ namespace ImageSharp.Formats
 {
     /// <summary>
     /// The compression method used on the Microsoft Windows BMP v2 and IBM OS/2 BMP v1 (and later versions) image DIB or file.
+    /// <para>
+    /// Defines how the compression type of the image data
+    /// in the bitmap file.
+    /// </para>
     /// <para>See: https://en.wikipedia.org/wiki/BMP_file_format </para>
     /// </summary>
     internal enum BmpCompression
@@ -15,6 +19,14 @@ namespace ImageSharp.Formats
 
         /// <summary>
         /// Uncompressed format. The pixels are in plain RGB/RGBA.
+        /// <para>
+        /// Each image row has a multiple of four elements. If the
+        /// row has less elements, zeros will be added at the right side.
+        /// The format depends on the number of bits, stored in the info header.
+        /// If the number of bits are one, four or eight each pixel data is
+        /// a index to the palette. If the number of bits are sixteen,
+        /// twenty-four or thirty-two each pixel contains a color.
+        /// </para>
         /// <para>Supported by Windows 2.0x and OS/2 1.0x or later.</para>
         /// <para>From Windows BMP v2 and OS/2 BMP v1.</para>
         /// </summary>
@@ -22,6 +34,14 @@ namespace ImageSharp.Formats
 
         /// <summary>
         /// Compressed format using Run-Length Encoded (RLE) 8-bit/pixel. Only for 8 bpp bitmaps.
+        /// <para>
+        /// Two bytes are one data record. If the first byte is not zero, the
+        /// next byte will be repeated as much as the value of the first byte.
+        /// If the first byte is zero, the record has different meanings, depending
+        /// on the second byte. If the second byte is zero, it is the end of the row,
+        /// if it is one, it is the end of the image.
+        /// Not supported at the moment.
+        /// </para>
         /// <para>Supported by Windows 2.0x and OS/2 2.0x or later.</para>
         /// <para>From Windows BMP v2 and OS/2 BMP v1.</para>
         /// </summary>
@@ -29,6 +49,14 @@ namespace ImageSharp.Formats
 
         /// <summary>
         /// Compressed format using Run-Length Encoded (RLE) 4-bit/pixel. Only for 4 bpp bitmaps.
+        /// <para>
+        /// Two bytes are one data record. If the first byte is not zero, the
+        /// next byte will be repeated as much as the value of the first byte.
+        /// If the first byte is zero, the record has different meanings, depending
+        /// on the second byte. If the second byte is zero, it is the end of the row,
+        /// if it is one, it is the end of the image.
+        /// Not supported at the moment.
+        /// </para>
         /// <para>Supported by Windows 2.0x and OS/2 2.0x or later.</para>
         /// <para>From Windows BMP v2 and OS/2 BMP v1.</para>
         /// </summary>
@@ -39,6 +67,11 @@ namespace ImageSharp.Formats
         /// <summary>
         /// Uncompressed format. For Windows BMP v3 only, the color table consists of three DWORD color bitfield masks
         /// that specify the red, green, and blue components, respectively, of each pixel.
+        /// <para>
+        /// Each image row has a multiple of four elements. If the
+        /// row has less elements, zeros will be added at the right side.
+        /// Not supported at the moment.
+        /// </para>
         /// <para>This is valid when used with 16 and 32 bpp bitmaps.</para>
         /// <para>Supported by Windows 3.0x/Windows NT 3.1/Windows CE 2.0 or later.</para>
         /// <para>From Windows BMP v3.</para>
@@ -47,6 +80,10 @@ namespace ImageSharp.Formats
 
         /// <summary>
         /// The bitmap contains a JPG compressed image.
+        /// <para>
+        /// The bitmap contains a JPG image.
+        /// Not supported at the moment.
+        /// </para>
         /// <para>Supported by Windows 98/Windows 2000 or later.</para>
         /// <para>From Windows BMP v4.</para>
         /// </summary>
@@ -54,6 +91,10 @@ namespace ImageSharp.Formats
 
         /// <summary>
         /// The bitmap contains a PNG compressed image.
+        /// <para>
+        /// The bitmap contains a PNG image.
+        /// Not supported at the moment.
+        /// </para>
         /// <para>Supported by Windows 98/Windows 2000 or later.</para>
         /// <para>From Windows BMP v4.</para>
         /// </summary>
--- src/ImageSharp/Formats/Bmp/BmpInfoHeader.cs
+++ src/ImageSharp/Formats/Bmp/BmpInfoHeader.cs
@@ -13,12 +13,27 @@ namespace ImageSharp.Formats
     internal sealed class BmpInfoHeader
     {
         /// <summary>
-        /// Defines of the data structure in the bitmap file.
+        /// Defines the size of the BITMAPINFOHEADER data structure in the bitmap file.
         /// </summary>
-        public const int Size = 40;
+        public const int BitmapInfoHeaderSize = 40;
 
         /// <summary>
-        /// Gets or sets the size of this header (40 bytes)
+        /// Defines the size of the BITMAPCOREHEADER data structure in the bitmap file.
+        /// </summary>
+        public const int BitmapCoreHeaderSize = 12;
+
+        /// <summary>
+        /// Defines the size of the biggest supported header data structure in the bitmap file.
+        /// </summary>
+        public const int MaxHeaderSize = BitmapInfoHeaderSize;
+
+        /// <summary>
+        /// Defines the size of the <see cref="HeaderSize"/> field.
+        /// </summary>
+        public const int HeaderSizeSize = 4;
+
+        /// <summary>
+        /// Gets or sets the size of this header
         /// </summary>
         public int HeaderSize { get; set; }
 

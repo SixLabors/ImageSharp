dd7c9012df87d7bdad33f36e5f2f241cba30b9fb
Dirk Lemstra <dirk@git.imagemagick.org>
2017-09-04 20:14:28 +0200

    Renamed AutoRotateProcessor to AutoOrientProcessor.

--- tests/ImageSharp.Tests/Processing/Transforms/AutoOrientTests.cs
+++ tests/ImageSharp.Tests/Processing/Transforms/AutoOrientTests.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-using System;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Processing.Processors;
 using Xunit;
 
@@ -12,10 +9,10 @@ namespace SixLabors.ImageSharp.Tests.Processing.Transforms
     public class AutoOrientTests : BaseImageOperationsExtensionTest
     {
         [Fact]
-        public void AutoOrient_AutoRotateProcessor()
+        public void AutoOrient_AutoOrientProcessor()
         {
             this.operations.AutoOrient();
-            this.Verify<AutoRotateProcessor<Rgba32>>();
+            this.Verify<AutoOrientProcessor<Rgba32>>();
         }
     }
 }

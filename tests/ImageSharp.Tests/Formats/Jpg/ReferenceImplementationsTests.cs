44838d54e1e96f46ee1fa3cf5fd3bc22fafc5426
José Caetano Silva <caetanator@hotmail.com>
2017-09-15 01:22:44 +0100

    ok

---
+++ tests/ImageSharp.Tests/Formats/Jpg/ReferenceImplementationsTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.Formats.Jpeg.GolangPort.Utils;
+
+using Xunit.Abstractions;
+
+// ReSharper disable InconsistentNaming
+namespace SixLabors.ImageSharp.Tests.Formats.Jpg
+{
+    using SixLabors.ImageSharp.Tests.Formats.Jpg.Utils;
+
+    public partial class ReferenceImplementationsTests : JpegFixture
+    {
+        public ReferenceImplementationsTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+    }
+}

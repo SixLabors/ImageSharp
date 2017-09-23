93fe689f843eff3f2d1eca55cc9c71317c4737e6
José Caetano Silva <caetanator@hotmail.com>
2017-09-23 03:30:50 +0100

    Sync to SixLabors/ImageSharp GIT version 2c8d68ae3d65eacf9123aeedfc63335f6e936ff9
    New sample program "ImageSharp Picture Viewer" to show the images loaded from ImageSharp lib into .NET Standard Form application.
    
    Changed BMP file decoder/encoder for upgrading to read all BMP file formats from Windows 2.0 and OS/2 1.0 and above.
    
    Fixed some typos.

---
+++ samples/ImageSharp Picture Viewer/FormMain.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ImageSharp_Picture_Viewer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.DialogOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.DialogColorChooser = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBoxMain
+            // 
+            this.PictureBoxMain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.PictureBoxMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PictureBoxMain.Location = new System.Drawing.Point(0, 0);
+            this.PictureBoxMain.Margin = new System.Windows.Forms.Padding(0);
+            this.PictureBoxMain.MinimumSize = new System.Drawing.Size(20, 20);
+            this.PictureBoxMain.Name = "PictureBoxMain";
+            this.PictureBoxMain.Size = new System.Drawing.Size(284, 262);
+            this.PictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBoxMain.TabIndex = 0;
+            this.PictureBoxMain.TabStop = false;
+            this.PictureBoxMain.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.PictureBoxMain_MouseDoubleClick);
+            // 
+            // DialogOpenFile
+            //
+            this.DialogOpenFile.AutoUpgradeEnabled = false; 
+            this.DialogOpenFile.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.PictureBoxMain);
+            this.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Name = "FormMain";
+            this.Text = "ImageSharp Picture Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBoxMain;
+        private System.Windows.Forms.OpenFileDialog DialogOpenFile;
+        private System.Windows.Forms.ColorDialog DialogColorChooser;
+    }
+}
+
---
+++ samples/ImageSharp Picture Viewer/FormMain.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using SixLabors.ImageSharp;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ImageSharp_Picture_Viewer
+{
+    public partial class FormMain : Form
+    {
+        public FormMain(string imageFile = null)
+        {
+            InitializeComponent();
+
+            if(imageFile == null)
+            {
+                if(this.DialogOpenFile.ShowDialog(this) == DialogResult.OK)
+                {
+                    imageFile = this.DialogOpenFile.FileName;
+                }
+                
+                if((imageFile != null) && (imageFile.Trim().Length > 0))
+                {
+                    using (var image = SixLabors.ImageSharp.Image.Load(imageFile))
+                    {
+                        int w = image.Width;
+                        int h = image.Height;
+                        int ch = 4; // number of channels (ie. assuming 32 bit RGB in this case)
+
+                        byte[] imageData = image.Frames.RootFrame.SavePixelData().ToArray();
+                        var bitmap = new Bitmap(w, h, w * ch, PixelFormat.Format32bppArgb, Marshal.UnsafeAddrOfPinnedArrayElement(imageData, 0));
+
+                        System.Drawing.Image image2 = bitmap;
+                        this.PictureBoxMain.Image = image2;
+                    }
+                    //var bitmap = new Bitmap(this.DialogOpenFile.FileName);
+                    //Image image = bitmap;
+                    //this.PictureBoxMain.Image = image;
+                }
+            }
+        }
+
+        private void PictureBoxMain_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if(this.DialogColorChooser.ShowDialog(this) == DialogResult.OK)
+            {
+                this.PictureBoxMain.BackColor = this.DialogColorChooser.Color;
+            }
+        }
+    }
+}

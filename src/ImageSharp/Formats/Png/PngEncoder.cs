027a6bd23dead62f2b1d0ceba4b2e09ab8df69af
James Jackson-South <james_south@hotmail.com>
2017-09-27 23:36:32 +1000

    Reduce scanline looping

--- src/ImageSharp/Formats/Png/PngEncoder.cs
+++ src/ImageSharp/Formats/Png/PngEncoder.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-using System.Collections.Generic;
 using System.IO;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Quantizers;

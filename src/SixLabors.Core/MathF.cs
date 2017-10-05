c4824fa309da222e40f11bdec127b3b569b91936
JimBobSquarePants <james_south@hotmail.com>
2017-10-05 15:59:29 +1100

    [SL.Core] Use type forwarding as per example

--- src/SixLabors.Core/MathF.cs
+++ src/SixLabors.Core/MathF.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-#if !NETCOREAPP2_0
 using System.Runtime.CompilerServices;
 
+#if NETCOREAPP2_0
+[assembly: TypeForwardedTo(typeof(System.MathF))]
+#else
 namespace System
 {
     /// <summary>

81f8bdb8e80928c0055e0d5be1225fa543993228
James Jackson-South <james_south@hotmail.com>
2017-12-18 14:07:26 +1100

    Convert photo effects

--- src/ImageSharp/Processing/Processors/ColorMatrix/LomographProcessor.cs
+++ src/ImageSharp/Processing/Processors/ColorMatrix/LomographProcessor.cs
@@ -11,7 +11,7 @@ namespace SixLabors.ImageSharp.Processing.Processors
     /// Converts the colors of the image recreating an old Lomograph effect.
     /// </summary>
     /// <typeparam name="TPixel">The pixel format.</typeparam>
-    internal class LomographProcessor<TPixel> : ColorMatrixProcessor<TPixel>
+    internal class LomographProcessor<TPixel> : FilterProcessor<TPixel>
         where TPixel : struct, IPixel<TPixel>
     {
         private static readonly TPixel VeryDarkGreen = ColorBuilder<TPixel>.FromRGBA(0, 10, 0, 255);
@@ -22,21 +22,10 @@ namespace SixLabors.ImageSharp.Processing.Processors
         /// </summary>
         /// <param name="options">The options effecting blending and composition.</param>
         public LomographProcessor(GraphicsOptions options)
+            : base(Matrix4x4Extensions.LomographFilter)
         {
             this.options = options;
         }
-
-        /// <inheritdoc/>
-        public override Matrix4x4 Matrix => new Matrix4x4
-        {
-            M11 = 1.5F,
-            M22 = 1.45F,
-            M33 = 1.11F,
-            M41 = -.1F,
-            M42 = .0F,
-            M43 = -.08F,
-            M44 = 1
-        };
 
         /// <inheritdoc/>
         protected override void AfterApply(ImageFrame<TPixel> source, Rectangle sourceRectangle, Configuration configuration)
--- src/ImageSharp/Processing/Processors/ColorMatrix/PolaroidProcessor.cs
+++ src/ImageSharp/Processing/Processors/ColorMatrix/PolaroidProcessor.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-using System.Numerics;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.Primitives;
 
@@ -11,11 +10,11 @@ namespace SixLabors.ImageSharp.Processing.Processors
     /// Converts the colors of the image recreating an old Polaroid effect.
     /// </summary>
     /// <typeparam name="TPixel">The pixel format.</typeparam>
-    internal class PolaroidProcessor<TPixel> : ColorMatrixProcessor<TPixel>
+    internal class PolaroidProcessor<TPixel> : FilterProcessor<TPixel>
         where TPixel : struct, IPixel<TPixel>
     {
         private static readonly TPixel VeryDarkOrange = ColorBuilder<TPixel>.FromRGB(102, 34, 0);
-        private static readonly TPixel LightOrange = ColorBuilder<TPixel>.FromRGBA(255, 153, 102, 178);
+        private static readonly TPixel LightOrange = ColorBuilder<TPixel>.FromRGBA(255, 153, 102, 128);
         private readonly GraphicsOptions options;
 
         /// <summary>
@@ -23,27 +22,10 @@ namespace SixLabors.ImageSharp.Processing.Processors
         /// </summary>
         /// <param name="options">The options effecting blending and composition.</param>
         public PolaroidProcessor(GraphicsOptions options)
+            : base(Matrix4x4Extensions.PolaroidFilter)
         {
             this.options = options;
         }
-
-        /// <inheritdoc/>
-        public override Matrix4x4 Matrix => new Matrix4x4
-        {
-            M11 = 1.538F,
-            M12 = -0.062F,
-            M13 = -0.262F,
-            M21 = -0.022F,
-            M22 = 1.578F,
-            M23 = -0.022F,
-            M31 = .216F,
-            M32 = -.16F,
-            M33 = 1.5831F,
-            M41 = 0.02F,
-            M42 = -0.05F,
-            M43 = -0.05F,
-            M44 = 1
-        };
 
         /// <inheritdoc/>
         protected override void AfterApply(ImageFrame<TPixel> source, Rectangle sourceRectangle, Configuration configuration)

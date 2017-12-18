1cfe9bb56c7210af8a3c40c4e79e44fb9fb7ae3f
James Jackson-South <james_south@hotmail.com>
2017-12-18 15:42:20 +1100

    Add Filter extension method + rename

--- tests/ImageSharp.Tests/Processing/Processors/ColorMatrix/FilterTest.cs
+++ tests/ImageSharp.Tests/Processing/Processors/ColorMatrix/FilterTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using SixLabors.ImageSharp.Tests.TestUtilities.ImageComparison;
 
 using SixLabors.Primitives;
@@ -9,31 +10,31 @@ using Xunit;
 
 namespace SixLabors.ImageSharp.Tests.Processing.Processors.Filters
 {
-    public class PolaroidTest : FileTestBase
+    public class FilterTest : FileTestBase
     {
         [Theory]
         [WithFileCollection(nameof(DefaultFiles), DefaultPixelType)]
-        public void ImageShouldApplyPolaroidFilter<TPixel>(TestImageProvider<TPixel> provider)
+        public void ImageShouldApplyFilter<TPixel>(TestImageProvider<TPixel> provider)
             where TPixel : struct, IPixel<TPixel>
         {
             using (Image<TPixel> image = provider.GetImage())
             {
-                image.Mutate(x => x.Polaroid());
+                image.Mutate(x => x.Filter(MatrixFilters.CreateBrightnessFilter(1.2F) * MatrixFilters.CreateContrastFilter(1.2F)));
                 image.DebugSave(provider);
             }
         }
 
         [Theory]
         [WithFileCollection(nameof(DefaultFiles), DefaultPixelType)]
-        public void ImageShouldApplyPolaroidFilterInBox<TPixel>(TestImageProvider<TPixel> provider)
+        public void ImageShouldApplyFilterInBox<TPixel>(TestImageProvider<TPixel> provider)
             where TPixel : struct, IPixel<TPixel>
         {
             using (Image<TPixel> source = provider.GetImage())
-            using (var image = source.Clone())
+            using (Image<TPixel> image = source.Clone())
             {
                 var bounds = new Rectangle(10, 10, image.Width / 2, image.Height / 2);
 
-                image.Mutate(x => x.Polaroid(bounds));
+                image.Mutate(x => x.Filter(MatrixFilters.CreateBrightnessFilter(1.2F) * MatrixFilters.CreateContrastFilter(1.2F), bounds));
                 image.DebugSave(provider);
 
                 ImageComparer.Tolerant().VerifySimilarityIgnoreRegion(source, image, bounds);

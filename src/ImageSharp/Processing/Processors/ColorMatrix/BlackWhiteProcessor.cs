43a040f105ebc8e8aab16af458028dc0bfb346dd
James Jackson-South <james_south@hotmail.com>
2017-12-18 14:07:26 +1100

    Convert photo effects

--- src/ImageSharp/Processing/Processors/ColorMatrix/BlackWhiteProcessor.cs
+++ src/ImageSharp/Processing/Processors/ColorMatrix/BlackWhiteProcessor.cs
@@ -1,34 +1,23 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-using System.Numerics;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace SixLabors.ImageSharp.Processing.Processors
 {
     /// <summary>
-    /// Converts the colors of the image to their black and white equivalent.
+    /// Applies a black and white filter matrix to the image
     /// </summary>
     /// <typeparam name="TPixel">The pixel format.</typeparam>
-    internal class BlackWhiteProcessor<TPixel> : ColorMatrixProcessor<TPixel>
-        where TPixel : struct, IPixel<TPixel>
+    internal class BlackWhiteProcessor<TPixel> : FilterProcessor<TPixel>
+          where TPixel : struct, IPixel<TPixel>
     {
-        /// <inheritdoc/>
-        public override Matrix4x4 Matrix => new Matrix4x4()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BlackWhiteProcessor{TPixel}"/> class.
+        /// </summary>
+        public BlackWhiteProcessor()
+            : base(Matrix4x4Extensions.BlackWhiteFilter)
         {
-            M11 = 1.5F,
-            M12 = 1.5F,
-            M13 = 1.5F,
-            M21 = 1.5F,
-            M22 = 1.5F,
-            M23 = 1.5F,
-            M31 = 1.5F,
-            M32 = 1.5F,
-            M33 = 1.5F,
-            M41 = -1F,
-            M42 = -1F,
-            M43 = -1F,
-            M44 = 1
-        };
+        }
     }
 }

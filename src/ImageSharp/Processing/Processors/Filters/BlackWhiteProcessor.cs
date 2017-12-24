5041e5f54218573f20c215e148e22e9e587f7b2e
Anton Firszov <anton.firszov@sapagroup.com>
2017-12-24 04:33:20 +0100

    fixing StyleCop issue

--- src/ImageSharp/Processing/Processors/Filters/BlackWhiteProcessor.cs
+++ src/ImageSharp/Processing/Processors/Filters/BlackWhiteProcessor.cs
@@ -9,7 +9,7 @@ namespace SixLabors.ImageSharp.Processing.Processors
     /// Applies a black and white filter matrix to the image
     /// </summary>
     /// <typeparam name="TPixel">The pixel format.</typeparam>
-    internal class  BlackWhiteProcessor<TPixel> : FilterProcessor<TPixel>
+    internal class BlackWhiteProcessor<TPixel> : FilterProcessor<TPixel>
           where TPixel : struct, IPixel<TPixel>
     {
         /// <summary>

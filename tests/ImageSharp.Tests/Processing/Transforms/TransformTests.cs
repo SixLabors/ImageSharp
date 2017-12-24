0ce82c82505de747e11f7a556e47eb92c6b9d3b5
Anton Firszov <anton.firszov@sapagroup.com>
2017-12-25 00:26:12 +0100

    yet another test case for TransformTests

--- tests/ImageSharp.Tests/Processing/Transforms/TransformTests.cs
+++ tests/ImageSharp.Tests/Processing/Transforms/TransformTests.cs
@@ -137,6 +137,7 @@ namespace SixLabors.ImageSharp.Tests.Processing.Transforms
                     { 0, 0, 10, 10 },
                     { 0, 0, 5, 10 },
                     { 0, 0, 10, 5 },
+                    { 5, 0, 5, 10 },
                     {-5,-5, 15, 15 }
                 };
 

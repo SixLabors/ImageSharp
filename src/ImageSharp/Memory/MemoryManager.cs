4f0d06c9103911cbe80baa8c758414ce34c39049
Lauri Kotilainen <lauri@offbeat.fi>
2018-01-11 21:05:42 +0200

    - Add a minimum size threshold for array pool usage - Add a null memory manager that doesn't do any actual memory management

--- src/ImageSharp/Memory/MemoryManager.cs
+++ src/ImageSharp/Memory/MemoryManager.cs
@@ -14,7 +14,7 @@ namespace SixLabors.ImageSharp.Memory
         /// <summary>
         /// Gets or sets the <see cref="MemoryManager"/> that is currently in use.
         /// </summary>
-        public static MemoryManager Current { get; set; } = new ArrayPoolMemoryManager();
+        public static MemoryManager Current { get; set; } = new ArrayPoolMemoryManager(1024 * 80);
 
         /// <summary>
         /// Allocates a <see cref="Buffer{T}"/> of size <paramref name="size"/>, optionally

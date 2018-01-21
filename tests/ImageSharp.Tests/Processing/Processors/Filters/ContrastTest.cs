fdae7aa8aedff1bf49c3ac0b3fa06abaa685f805
Anton Firszov <antonfir@gmail.com>
2018-01-21 18:40:18 +0100

    there is no need for separate Apply****InBox tests,
    having one in FilterTest is sufficient

--- tests/ImageSharp.Tests/Processing/Processors/Filters/ContrastTest.cs
+++ tests/ImageSharp.Tests/Processing/Processors/Filters/ContrastTest.cs
@@ -2,9 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Tests.TestUtilities.ImageComparison;
 
-using SixLabors.Primitives;
 using Xunit;
 
 namespace SixLabors.ImageSharp.Tests.Processing.Processors.Effects
@@ -25,23 +23,6 @@ namespace SixLabors.ImageSharp.Tests.Processing.Processors.Effects
             where TPixel : struct, IPixel<TPixel>
         {
             provider.RunValidatingProcessorTest(x => x.Contrast(value), value);
-        }
-
-        [Theory]
-        [WithTestPatternImages(nameof(ContrastValues), 48, 48, PixelTypes.Rgba32)]
-        public void ApplyContrastFilterInBox<TPixel>(TestImageProvider<TPixel> provider, float value)
-            where TPixel : struct, IPixel<TPixel>
-        {
-            using (Image<TPixel> source = provider.GetImage())
-            using (Image<TPixel> image = source.Clone())
-            {
-                var bounds = new Rectangle(image.Width / 4, image.Width / 4, image.Width / 2, image.Height / 2);
-
-                image.Mutate(x => x.Contrast(value, bounds));
-                image.DebugSave(provider, value);
-
-                ImageComparer.Tolerant().VerifySimilarityIgnoreRegion(source, image, bounds);
-            }
         }
     }
 }

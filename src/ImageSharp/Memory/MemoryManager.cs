464ad147fad028b4dcedcab1169854a1ddabc892
Anton Firszov <antonfir@gmail.com>
2018-02-17 19:56:40 +0100

    moving common MemoryManager logic into extension methods

--- src/ImageSharp/Memory/MemoryManager.cs
+++ src/ImageSharp/Memory/MemoryManager.cs
@@ -12,17 +12,6 @@ namespace SixLabors.ImageSharp.Memory
     public abstract class MemoryManager
     {
         /// <summary>
-        /// Allocates a <see cref="Buffer{T}"/> of size <paramref name="size"/>.
-        /// Note: Depending on the implementation, the buffer may not cleared before
-        /// returning, so it may contain data from an earlier use.
-        /// </summary>
-        /// <typeparam name="T">Type of the data stored in the buffer</typeparam>
-        /// <param name="size">Size of the buffer to allocate</param>
-        /// <returns>A buffer of values of type <typeparamref name="T"/>.</returns>
-        internal abstract Buffer<T> Allocate<T>(int size)
-            where T : struct;
-
-        /// <summary>
         /// Allocates a <see cref="Buffer{T}"/> of size <paramref name="size"/>, optionally
         /// clearing the buffer before it gets returned.
         /// </summary>
@@ -41,13 +30,5 @@ namespace SixLabors.ImageSharp.Memory
         /// <param name="buffer">The buffer to release</param>
         internal abstract void Release<T>(Buffer<T> buffer)
             where T : struct;
-
-        internal Buffer2D<T> Allocate2D<T>(int width, int height, bool clear = false)
-            where T : struct
-        {
-            Buffer<T> buffer = this.Allocate<T>(width * height, clear);
-
-            return new Buffer2D<T>(buffer, width, height);
-        }
     }
 }
---
+++ src/ImageSharp/Memory/MemoryManagerExtensions.cs
@@ -0,0 +1,45 @@
+﻿namespace SixLabors.ImageSharp.Memory
+{
+    /// <summary>
+    /// Extension methods for <see cref="MemoryManager"/>.
+    /// </summary>
+    internal static class MemoryManagerExtensions
+    {
+        /// <summary>
+        /// Allocates a <see cref="Buffer{T}"/> of size <paramref name="size"/>.
+        /// Note: Depending on the implementation, the buffer may not cleared before
+        /// returning, so it may contain data from an earlier use.
+        /// </summary>
+        /// <typeparam name="T">Type of the data stored in the buffer</typeparam>
+        /// <param name="memoryManager">The <see cref="MemoryManager"/></param>
+        /// <param name="size">Size of the buffer to allocate</param>
+        /// <returns>A buffer of values of type <typeparamref name="T"/>.</returns>
+        public static Buffer<T> Allocate<T>(this MemoryManager memoryManager, int size)
+            where T : struct
+        {
+            return memoryManager.Allocate<T>(size, false);
+        }
+
+        public static Buffer<T> AllocateClean<T>(this MemoryManager memoryManager, int size)
+            where T : struct
+        {
+            return memoryManager.Allocate<T>(size, true);
+        }
+
+        public static Buffer2D<T> Allocate2D<T>(this MemoryManager memoryManager, int width, int height, bool clear)
+            where T : struct
+        {
+            Buffer<T> buffer = memoryManager.Allocate<T>(width * height, clear);
+
+            return new Buffer2D<T>(buffer, width, height);
+        }
+
+        public static Buffer2D<T> Allocate2D<T>(this MemoryManager memoryManager, int width, int height)
+            where T : struct =>
+            Allocate2D<T>(memoryManager, width, height, false);
+
+        public static Buffer2D<T> AllocateClean2D<T>(this MemoryManager memoryManager, int width, int height)
+            where T : struct =>
+            Allocate2D<T>(memoryManager, width, height, true);
+    }
+}

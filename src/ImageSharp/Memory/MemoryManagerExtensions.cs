a7689de20817c534193582d1b9d1cd1c5abcb73c
Anton Firszov <antonfir@gmail.com>
2018-02-19 19:00:34 +0100

    IManagedByteBuffer

--- src/ImageSharp/Memory/MemoryManagerExtensions.cs
+++ src/ImageSharp/Memory/MemoryManagerExtensions.cs
@@ -6,24 +6,29 @@
     internal static class MemoryManagerExtensions
     {
         /// <summary>
-        /// Allocates a <see cref="Buffer{T}"/> of size <paramref name="size"/>.
+        /// Allocates a <see cref="Buffer{T}"/> of size <paramref name="length"/>.
         /// Note: Depending on the implementation, the buffer may not cleared before
         /// returning, so it may contain data from an earlier use.
         /// </summary>
         /// <typeparam name="T">Type of the data stored in the buffer</typeparam>
         /// <param name="memoryManager">The <see cref="MemoryManager"/></param>
-        /// <param name="size">Size of the buffer to allocate</param>
+        /// <param name="length">Size of the buffer to allocate</param>
         /// <returns>A buffer of values of type <typeparamref name="T"/>.</returns>
-        public static Buffer<T> Allocate<T>(this MemoryManager memoryManager, int size)
+        public static Buffer<T> Allocate<T>(this MemoryManager memoryManager, int length)
             where T : struct
         {
-            return memoryManager.Allocate<T>(size, false);
+            return memoryManager.Allocate<T>(length, false);
         }
 
-        public static Buffer<T> AllocateClean<T>(this MemoryManager memoryManager, int size)
+        public static Buffer<T> AllocateClean<T>(this MemoryManager memoryManager, int length)
             where T : struct
         {
-            return memoryManager.Allocate<T>(size, true);
+            return memoryManager.Allocate<T>(length, true);
+        }
+
+        public static IManagedByteBuffer AllocateCleanManagedByteBuffer(this MemoryManager memoryManager, int length)
+        {
+            return memoryManager.AllocateManagedByteBuffer(length, true);
         }
 
         public static Buffer2D<T> Allocate2D<T>(this MemoryManager memoryManager, int width, int height, bool clear)

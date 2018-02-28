e1418f592b9ea5cd6fcb364475bdae623be96234
Anton Firszov <antonfir@gmail.com>
2018-02-28 01:40:58 +0100

    added failing tests for #464

--- tests/ImageSharp.Tests/Formats/Jpg/SpectralJpegTests.cs
+++ tests/ImageSharp.Tests/Formats/Jpg/SpectralJpegTests.cs
@@ -29,6 +29,7 @@ namespace SixLabors.ImageSharp.Tests.Formats.Jpg
                 TestImages.Jpeg.Baseline.Jpeg444, TestImages.Jpeg.Baseline.Testorig420,
                 TestImages.Jpeg.Baseline.Jpeg420Small, TestImages.Jpeg.Baseline.Bad.BadEOF,
                 TestImages.Jpeg.Baseline.Bad.ExifUndefType,
+                TestImages.Jpeg.Issues.CmykIssueBaseline464
             };
 
         public static readonly string[] ProgressiveTestJpegs = 

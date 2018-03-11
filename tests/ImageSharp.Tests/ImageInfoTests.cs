3d8f9c23e92f9b9b9983298b9714ed206444f177
James Jackson-South <james_south@hotmail.com>
2018-03-12 00:39:34 +1100

    Move Bounds/Size to IImageInfo extension methods

---
+++ tests/ImageSharp.Tests/ImageInfoTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.MetaData;
+using SixLabors.Primitives;
+
+using Xunit;
+
+namespace SixLabors.ImageSharp.Tests
+{
+    public class ImageInfoTests
+    {
+        [Fact]
+        public void ImageInfoInitializesCorrectly()
+        {
+            const int Width = 50;
+            const int Height = 60;
+            var size = new Size(Width, Height);
+            var rectangle = new Rectangle(0, 0, Width, Height);
+            var pixelType = new PixelTypeInfo(8);
+            var meta = new ImageMetaData();
+
+            var info = new ImageInfo(pixelType, Width, Height, meta);
+
+            Assert.Equal(pixelType, info.PixelType);
+            Assert.Equal(Width, info.Width);
+            Assert.Equal(Height, info.Height);
+            Assert.Equal(size, info.Size());
+            Assert.Equal(rectangle, info.Bounds());
+            Assert.Equal(meta, info.MetaData);
+        }
+    }
+}

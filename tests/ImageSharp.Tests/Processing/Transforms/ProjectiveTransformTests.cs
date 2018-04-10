815264d332bdfcc38720d1580fbd959d15673404
James Jackson-South <james_south@hotmail.com>
2018-04-10 11:14:49 +1000

    Init projective transform testing

---
+++ tests/ImageSharp.Tests/Processing/Transforms/ProjectiveTransformTests.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.Tests.TestUtilities.ImageComparison;
+using Xunit.Abstractions;
+
+namespace SixLabors.ImageSharp.Tests.Processing.Transforms
+{
+    public class ProjectiveTransformTests
+    {
+        private readonly ITestOutputHelper Output;
+
+        private static readonly ImageComparer ValidatorComparer = ImageComparer.TolerantPercentage(0.005f, 3);
+    }
+}

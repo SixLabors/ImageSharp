7210131548e0536b3d47de066340853735e50f4f
Jason Nelson <jason@carbonmade.com>
2018-04-14 20:38:58 -0700

    Write the BmpFileHeader directly to the output span

---
+++ tests/ImageSharp.Tests/Formats/Bmp/BmpFileHeaderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SixLabors.ImageSharp.Formats.Bmp;
+using Xunit;
+
+namespace SixLabors.ImageSharp.Tests.Formats.Bmp
+{
+    public class BmpFileHeaderTests
+    {
+        [Fact]
+        public void TestWrite()
+        {
+            var header = new BmpFileHeader(1, 2, 3, 4);
+
+            byte[] buffer = new byte[14];
+
+            header.WriteTo(buffer);
+
+            Assert.Equal("AQACAAAAAwAAAAQAAAA=", Convert.ToBase64String(buffer));
+        }
+    }
+}

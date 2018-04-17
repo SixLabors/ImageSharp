c77f612aabfc6d522ca1a3683a970eeddc3a5534
Jason Nelson <jason@carbonmade.com>
2018-04-17 13:59:41 -0700

    Move the png header to constants

--- src/ImageSharp/Formats/Png/PngConstants.cs
+++ src/ImageSharp/Formats/Png/PngConstants.cs
@@ -25,5 +25,16 @@ namespace SixLabors.ImageSharp.Formats.Png
         /// The list of file extensions that equate to a png.
         /// </summary>
         public static readonly IEnumerable<string> FileExtensions = new[] { "png" };
+
+        public static readonly byte[] HeaderBytes = {
+             0x89, // Set the high bit.
+             0x50, // P
+             0x4E, // N
+             0x47, // G
+             0x0D, // Line ending CRLF
+             0x0A, // Line ending CRLF
+             0x1A, // EOF
+             0x0A // LF
+        };
     }
 }

4b92801e3384e0c861d7ed2b622fd2aa15c82a63
Unknown <peter.wendorff@gmx.de>
2018-04-20 00:01:33 +0200

    #542: fix test: indices are 0-based, so bottom left pixel is one smaller

--- tests/ImageSharp.Tests/Drawing/FillLinearGradientBrushTests.cs
+++ tests/ImageSharp.Tests/Drawing/FillLinearGradientBrushTests.cs
@@ -1,21 +1,17 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-using System.Numerics;
+using System;
+
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Drawing.Brushes;
+
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing.Drawing;
 using Xunit;
 
 namespace SixLabors.ImageSharp.Tests.Drawing
 {
-    using System;
-
-    using SixLabors.ImageSharp.Processing;
-    using SixLabors.ImageSharp.Processing.Drawing.Brushes;
-    using SixLabors.ImageSharp.Processing.Overlays;
-
-    using Point = SixLabors.Primitives.Point;
-
     public class FillLinearGradientBrushTests : FileTestBase
     {
         [Fact]
@@ -26,8 +22,8 @@ namespace SixLabors.ImageSharp.Tests.Drawing
             {
                 LinearGradientBrush<Rgba32> unicolorLinearGradientBrush = 
                     new LinearGradientBrush<Rgba32>(
-                        new Point(0, 0),
-                        new Point(500, 0),
+                        new SixLabors.Primitives.Point(0, 0),
+                        new SixLabors.Primitives.Point(500, 0),
                         new Tuple<float, Rgba32>(0, Rgba32.Red),
                         new Tuple<float, Rgba32>(1, Rgba32.Red));
                 
@@ -39,7 +35,7 @@ namespace SixLabors.ImageSharp.Tests.Drawing
                     Assert.Equal(Rgba32.Red, sourcePixels[0, 0]);
                     Assert.Equal(Rgba32.Red, sourcePixels[9, 9]);
                     Assert.Equal(Rgba32.Red, sourcePixels[199, 149]);
-                    Assert.Equal(Rgba32.Red, sourcePixels[500, 500]);
+                    Assert.Equal(Rgba32.Red, sourcePixels[499, 499]);
                 }
             }
         }

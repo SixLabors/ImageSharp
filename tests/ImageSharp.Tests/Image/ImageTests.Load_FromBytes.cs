78bb139128f5ccb1b0276216f507fd541afc9634
Anton Firszov <antonfir@gmail.com>
2018-06-17 19:50:59 +0200

    common test cases for Image.Load(byte) and Image.Load(span) overloads

--- tests/ImageSharp.Tests/Image/ImageTests.Load_FromBytes.cs
+++ tests/ImageSharp.Tests/Image/ImageTests.Load_FromBytes.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
-
+using System;
 using System.IO;
+
 using Moq;
+
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.Primitives;
+
 using Xunit;
 
 // ReSharper disable InconsistentNaming
@@ -13,10 +17,22 @@ namespace SixLabors.ImageSharp.Tests
     {
         public class Load_FromBytes : ImageLoadTestBase
         {
-            [Fact]
-            public void BasicCase()
+            private byte[] ByteArray => this.DataStream.ToArray();
+
+            private ReadOnlySpan<byte> ByteSpan => this.ByteArray.AsSpan();
+
+            private byte[] ActualImageBytes => TestFile.Create(TestImages.Bmp.F).Bytes;
+
+            private ReadOnlySpan<byte> ActualImageSpan => this.ActualImageBytes.AsSpan();
+
+            [Theory]
+            [InlineData(false)]
+            [InlineData(true)]
+            public void BasicCase(bool useSpan)
             {
-                var img = Image.Load(this.TopLevelConfiguration, this.DataStream.ToArray());
+                Image<Rgba32> img = useSpan
+                                        ? Image.Load(this.TopLevelConfiguration, this.ByteSpan)
+                                        : Image.Load(this.TopLevelConfiguration, this.ByteArray);
 
                 Assert.NotNull(img);
                 Assert.Equal(this.TestFormat.Sample<Rgba32>(), img);
@@ -24,10 +40,14 @@ namespace SixLabors.ImageSharp.Tests
                 this.TestFormat.VerifyDecodeCall(this.Marker, this.TopLevelConfiguration);
             }
 
-            [Fact]
-            public void NonDefaultPixelType()
+            [Theory]
+            [InlineData(false)]
+            [InlineData(true)]
+            public void NonDefaultPixelType(bool useSpan)
             {
-                var img = Image.Load<Rgb24>(this.TopLevelConfiguration, this.DataStream.ToArray());
+                Image<Rgb24> img = useSpan
+                                       ? Image.Load<Rgb24>(this.TopLevelConfiguration, this.ByteSpan)
+                                       : Image.Load<Rgb24>(this.TopLevelConfiguration, this.ByteArray);
 
                 Assert.NotNull(img);
                 Assert.Equal(this.TestFormat.Sample<Rgb24>(), img);
@@ -35,10 +55,14 @@ namespace SixLabors.ImageSharp.Tests
                 this.TestFormat.VerifyDecodeCall(this.Marker, this.TopLevelConfiguration);
             }
 
-            [Fact]
-            public void UseLocalConfiguration()
+            [Theory]
+            [InlineData(false)]
+            [InlineData(true)]
+            public void UseLocalConfiguration(bool useSpan)
             {
-                var img = Image.Load<Rgba32>(this.LocalConfiguration, this.DataStream.ToArray());
+                Image<Rgba32> img = useSpan
+                                        ? Image.Load<Rgba32>(this.LocalConfiguration, this.ByteSpan)
+                                        : Image.Load<Rgba32>(this.LocalConfiguration, this.ByteArray);
 
                 Assert.NotNull(img);
 
@@ -47,17 +71,45 @@ namespace SixLabors.ImageSharp.Tests
                 Assert.Equal(this.DataStream.ToArray(), this.DecodedData);
             }
 
-            [Fact]
-            public void UseCustomDecoder()
+            [Theory]
+            [InlineData(false)]
+            [InlineData(true)]
+            public void UseCustomDecoder(bool useSpan)
             {
-                var img = Image.Load<Rgba32>(
-                    this.TopLevelConfiguration,
-                    this.DataStream.ToArray(),
-                    this.localDecoder.Object);
-
+                Image<Rgba32> img = useSpan
+                                        ? Image.Load<Rgba32>(
+                                            this.TopLevelConfiguration,
+                                            this.ByteSpan,
+                                            this.localDecoder.Object)
+                                        : Image.Load<Rgba32>(
+                                            this.TopLevelConfiguration,
+                                            this.ByteArray,
+                                            this.localDecoder.Object);
                 Assert.NotNull(img);
                 this.localDecoder.Verify(x => x.Decode<Rgba32>(this.TopLevelConfiguration, It.IsAny<Stream>()));
                 Assert.Equal(this.DataStream.ToArray(), this.DecodedData);
+            }
+
+            [Theory]
+            [InlineData(false)]
+            [InlineData(true)]
+            public void UseGlobalConfiguration(bool useSpan)
+            {
+                Image<Rgba32> img = useSpan ? Image.Load(this.ActualImageSpan) : Image.Load(this.ActualImageBytes);
+
+                Assert.Equal(new Size(108, 202), img.Size());
+            }
+
+            [Theory]
+            [InlineData(false)]
+            [InlineData(true)]
+            public void UseGlobalConfiguration_NonDefaultPixelType(bool useSpan)
+            {
+                Image<Rgb24> img = useSpan
+                                       ? Image.Load<Rgb24>(this.ActualImageSpan)
+                                       : Image.Load<Rgb24>(this.ActualImageBytes);
+
+                Assert.Equal(new Size(108, 202), img.Size());
             }
         }
     }

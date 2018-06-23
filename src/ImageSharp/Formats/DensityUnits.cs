2278789736ae646eb1d8195efbbf1d66b905733f
James Jackson-South <james_south@hotmail.com>
2018-06-24 01:46:23 +1000

    Create new DensityUnits enum.

---
+++ src/ImageSharp/Formats/DensityUnits.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.Formats
+{
+    /// <summary>
+    /// Provides enumeration of available pixel density units.
+    /// </summary>
+    public enum DensityUnits : byte
+    {
+        /// <summary>
+        /// No units; width:height pixel aspect ratio = Ydensity:Xdensity
+        /// </summary>
+        AspectRatio = 0,
+
+        /// <summary>
+        /// Pixels per inch (2.54 cm)
+        /// </summary>
+        PixelsPerInch = 1, // Other image formats would default to this.
+
+        /// <summary>
+        /// Pixels per centimeter.
+        /// </summary>
+        PixelsPerCentimeter = 2
+    }
+}

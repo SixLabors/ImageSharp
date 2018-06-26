d116d83ffb92c83fad31dad03af7c93f1b54213a
Jason Nelson <jason@carbonmade.com>
2018-06-26 15:54:55 -0700

    [ICC] Eliminate Version allocation using custom struct

---
+++ src/ImageSharp/MetaData/Profiles/ICC/Various/IccVersion.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.MetaData.Profiles.Icc
+{
+    /// <summary>
+    /// Represents the ICC profile version number.
+    /// </summary>
+    public readonly struct IccVersion
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IccVersion"/> struct.
+        /// </summary>
+        /// <param name="major">The major version number.</param>
+        /// <param name="minor">The minor version number.</param>
+        /// <param name="patch">The patch version number.</param>
+        public IccVersion(int major, int minor, int patch)
+        {
+            this.Major = major;
+            this.Minor = minor;
+            this.Patch = patch;
+        }
+
+        /// <summary>
+        /// Gets the major version number.
+        /// </summary>
+        public int Major { get; }
+
+        /// <summary>
+        /// Gets the minor version number.
+        /// </summary>
+        public int Minor { get; }
+
+        /// <summary>
+        /// Gets the patch number.
+        /// </summary>
+        public int Patch { get; }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return string.Join(".", this.Major, this.Minor, this.Patch);
+        }
+    }
+}

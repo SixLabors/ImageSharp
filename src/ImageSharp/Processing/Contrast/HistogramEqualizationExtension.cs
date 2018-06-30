d41ece14326d83c93edfc8b57166573953612d90
popow <popow@gmx.de>
2018-06-30 15:46:22 +0200

    added first attempt of histogram equalization

---
+++ src/ImageSharp/Processing/Contrast/HistogramEqualizationExtension.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace SixLabors.ImageSharp.Processing.Contrast
+{
+    /// <summary>
+    /// Adds extension that allows applying an HistogramEqualization to the image.
+    /// </summary>
+    public static class HistogramEqualizationExtension
+    {
+        /// <summary>
+        /// Equalizes the histogram of an image to increases the global contrast.
+        /// </summary>
+        /// <typeparam name="TPixel">The pixel format.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <returns>The <see cref="Image{TPixel}"/>.</returns>
+        public static IImageProcessingContext<TPixel> HistogramEqualization<TPixel>(this IImageProcessingContext<TPixel> source)
+            where TPixel : struct, IPixel<TPixel>
+            => source.ApplyProcessor(new HistogramEqualizationProcessor<TPixel>());
+    }
+}

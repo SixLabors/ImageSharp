1d8610b2c4f7b75c955648c23ba069e0630c2b1e
Scott Williams <tocsoft@gmail.com>
2018-07-01 14:13:08 +0100

    bake package version into dll

--- src/Shared/AssemblyInfo.Common.cs
+++ src/Shared/AssemblyInfo.Common.cs
@@ -28,8 +28,6 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("custom-build")]
 
 // Ensure the internals can be built and tested.
 [assembly: InternalsVisibleTo("SixLabors.ImageSharp.Drawing")]

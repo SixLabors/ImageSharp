72cbea7cc876611d7e91f830133de5be756ae8f5
Anton Firszov <antonfir@gmail.com>
2018-07-09 01:29:33 +0200

    LoadResizeSave benchmark + add EnableParallelExecution parameter to the Resize benchmark

--- tests/ImageSharp.Benchmarks/Samplers/Resize.cs
+++ tests/ImageSharp.Benchmarks/Samplers/Resize.cs
@@ -3,20 +3,38 @@
 // Licensed under the Apache License, Version 2.0.
 // </copyright>
 
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+using BenchmarkDotNet.Attributes;
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+using CoreSize = SixLabors.Primitives.Size;
+
 namespace SixLabors.ImageSharp.Benchmarks
 {
-    using System.Drawing;
-    using System.Drawing.Drawing2D;
+    using System.Threading.Tasks;
 
-    using BenchmarkDotNet.Attributes;
+    using SixLabors.ImageSharp.Formats.Jpeg;
 
-    using SixLabors.ImageSharp.PixelFormats;
-    using SixLabors.ImageSharp.Processing;
-
-    using CoreSize = SixLabors.Primitives.Size;
-
+    [Config(typeof(Config.ShortClr))]
     public class Resize : BenchmarkBase
     {
+        private readonly Configuration configuration = new Configuration(new JpegConfigurationModule());
+
+        [Params(false, true)]
+        public bool EnableParallelExecution { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            this.configuration.ParallelOptions.MaxDegreeOfParallelism =
+                this.EnableParallelExecution ? Environment.ProcessorCount : 1;
+        }
+
         [Benchmark(Baseline = true, Description = "System.Drawing Resize")]
         public Size ResizeSystemDrawing()
         {
@@ -40,7 +58,7 @@ namespace SixLabors.ImageSharp.Benchmarks
         [Benchmark(Description = "ImageSharp Resize")]
         public CoreSize ResizeCore()
         {
-            using (Image<Rgba32> image = new Image<Rgba32>(2000, 2000))
+            using (var image = new Image<Rgba32>(this.configuration, 2000, 2000))
             {
                 image.Mutate(x => x.Resize(400, 400));
                 return new CoreSize(image.Width, image.Height);

f30389f7d5fcfe6b7787c67cf610c4f3492fbf93
popow <popow@gmx.de>
2018-07-09 19:22:09 +0200

    removed unnecessary comments

--- src/ImageSharp/MetaData/Profiles/Exif/ExifReader.cs
+++ src/ImageSharp/MetaData/Profiles/Exif/ExifReader.cs
@@ -79,7 +79,6 @@ namespace SixLabors.ImageSharp.MetaData.Profiles.Exif
 
             if (this.ReadString(4) == "Exif")
             {
-                // two zeros are expected to follow the Exif Id code
                 if (this.ReadUInt16() != 0)
                 {
                     return values;

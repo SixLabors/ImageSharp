9b70bfe85c99e5da20022136a98b1fe784442e6d
Scott Williams <tocsoft@gmail.com>
2018-08-01 18:18:32 +0100

    fix build version number generation

--- build.cmd
+++ build.cmd
@@ -1,27 +1,6 @@
 @echo Off
 
-if not "%GitVersion_NuGetVersion%" == "" (
-    dotnet restore /p:packageversion=%GitVersion_NuGetVersion%
-)ELSE (
-    dotnet restore
-)
-
-ECHO Building nuget packages
-if not "%GitVersion_NuGetVersion%" == "" (
-    dotnet build -c Release /p:packageversion=%GitVersion_NuGetVersion%
-)ELSE (
-    dotnet build -c Release
-)
-if not "%errorlevel%"=="0" goto failure
-
-dotnet test ./tests/SixLabors.Core.Tests/SixLabors.Core.Tests.csproj
-
-
-if not "%GitVersion_NuGetVersion%" == "" (
-    dotnet pack ./src/SixLabors.Core/ -c Release --output ../../artifacts --no-build /p:packageversion=%GitVersion_NuGetVersion%
-)ELSE (
-    dotnet pack ./src/SixLabors.Core/ -c Release --output ../../artifacts --no-build
-)
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '.\build.ps1'"
 
 if not "%errorlevel%"=="0" goto failure
 

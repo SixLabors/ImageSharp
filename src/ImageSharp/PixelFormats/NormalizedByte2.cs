67e1445787f65e5ff4e9d8db2415291fe02dd2b5
Jason Nelson <jason@carbonmade.com>
2018-08-20 12:05:27 -0700

    Remove trailing semicolon

--- src/ImageSharp/PixelFormats/NormalizedByte2.cs
+++ src/ImageSharp/PixelFormats/NormalizedByte2.cs
@@ -257,7 +257,7 @@ namespace SixLabors.ImageSharp.PixelFormats
 
         /// <inheritdoc />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override int GetHashCode() => this.PackedValue.GetHashCode();;
+        public override int GetHashCode() => this.PackedValue.GetHashCode();
 
         /// <inheritdoc />
         public override string ToString()

5306eaf841ef92255fbed2cb16d483e1d256c421
Vicente Penades <megaz0ne23@hotmail.com>
2018-08-29 13:13:00 +0200

    removed a redundant opacity.Clamp(0,1) in the innermost level of pixel blenders. Pixel blenders are performance critical, so every tiny performance improvement counts.

--- src/ImageSharp/PixelFormats/PixelBlender{TPixel}.cs
+++ src/ImageSharp/PixelFormats/PixelBlender{TPixel}.cs
@@ -55,6 +55,22 @@ namespace SixLabors.ImageSharp.PixelFormats
         /// <summary>
         /// Blends 2 rows together
         /// </summary>
+        /// <param name="memoryManager">memory manager to use internally</param>
+        /// <param name="destination">the destination span</param>
+        /// <param name="background">the background span</param>
+        /// <param name="source">the source span</param>
+        /// <param name="amount">
+        /// A span with values between 0 and 1 indicating the weight of the second source vector.
+        /// At amount = 0, "from" is returned, at amount = 1, "to" is returned.
+        /// </param>
+        public void Blend(MemoryAllocator memoryManager, Span<TPixel> destination, ReadOnlySpan<TPixel> background, ReadOnlySpan<TPixel> source, ReadOnlySpan<float> amount)
+        {
+            this.Blend<TPixel>(memoryManager, destination, background, source, amount);
+        }
+
+        /// <summary>
+        /// Blends 2 rows together
+        /// </summary>
         /// <typeparam name="TPixelSrc">the pixel format of the source span</typeparam>
         /// <param name="memoryManager">memory manager to use internally</param>
         /// <param name="destination">the destination span</param>
@@ -113,38 +129,6 @@ namespace SixLabors.ImageSharp.PixelFormats
 
                 PixelOperations<TPixel>.Instance.ToVector4(background, backgroundSpan, destination.Length);
                 PixelOperations<TPixelSrc>.Instance.ToVector4(source, sourceSpan, destination.Length);
-
-                this.BlendFunction(destinationSpan, backgroundSpan, sourceSpan, amount);
-
-                PixelOperations<TPixel>.Instance.PackFromVector4(destinationSpan, destination, destination.Length);
-            }
-        }
-
-        /// <summary>
-        /// Blends 2 rows together
-        /// </summary>
-        /// <param name="memoryManager">memory manager to use internally</param>
-        /// <param name="destination">the destination span</param>
-        /// <param name="background">the background span</param>
-        /// <param name="source">the source span</param>
-        /// <param name="amount">
-        /// A span with values between 0 and 1 indicating the weight of the second source vector.
-        /// At amount = 0, "from" is returned, at amount = 1, "to" is returned.
-        /// </param>
-        public void Blend(MemoryAllocator memoryManager, Span<TPixel> destination, ReadOnlySpan<TPixel> background, ReadOnlySpan<TPixel> source, ReadOnlySpan<float> amount)
-        {
-            Guard.MustBeGreaterThanOrEqualTo(background.Length, destination.Length, nameof(background.Length));
-            Guard.MustBeGreaterThanOrEqualTo(source.Length, destination.Length, nameof(source.Length));
-            Guard.MustBeGreaterThanOrEqualTo(amount.Length, destination.Length, nameof(amount.Length));
-
-            using (IMemoryOwner<Vector4> buffer = memoryManager.Allocate<Vector4>(destination.Length * 3))
-            {
-                Span<Vector4> destinationSpan = buffer.Slice(0, destination.Length);
-                Span<Vector4> backgroundSpan = buffer.Slice(destination.Length, destination.Length);
-                Span<Vector4> sourceSpan = buffer.Slice(destination.Length * 2, destination.Length);
-
-                PixelOperations<TPixel>.Instance.ToVector4(background, backgroundSpan, destination.Length);
-                PixelOperations<TPixel>.Instance.ToVector4(source, sourceSpan, destination.Length);
 
                 this.BlendFunction(destinationSpan, backgroundSpan, sourceSpan, amount);
 

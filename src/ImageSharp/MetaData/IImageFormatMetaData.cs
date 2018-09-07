dc4ec30a14d82bd901ccb257a2de4277d0d52e80
James Jackson-South <james_south@hotmail.com>
2018-09-07 22:01:18 +0100

    Strong type meta query + format singletons

---
+++ src/ImageSharp/MetaData/IImageFormatMetaData.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.MetaData
+{
+    /// <summary>
+    /// Encapsulates the format specific metadata of an image.
+    /// This interface exists to allow type saftey and avoid the performance overhead of parsing attributes.
+    /// </summary>
+    public interface IImageFormatMetaData
+    {
+    }
+}

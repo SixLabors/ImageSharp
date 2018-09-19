7698f5d286bbfa4a0d0197eee03da75d82bb1ad7
James Jackson-South <james_south@hotmail.com>
2018-09-19 13:01:53 +0100

    Add metadata cloning tests

---
+++ tests/ImageSharp.Tests/Formats/Jpg/JpegMetaDataTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.Formats.Jpeg;
+using Xunit;
+
+namespace SixLabors.ImageSharp.Tests.Formats.Jpg
+{
+    public class JpegMetaDataTests
+    {
+        [Fact]
+        public void CloneIsDeep()
+        {
+            var meta = new JpegMetaData() { Quality = 50 };
+            var clone = (JpegMetaData)meta.DeepClone();
+
+            clone.Quality = 99;
+
+            Assert.False(meta.Quality.Equals(clone.Quality));
+        }
+    }
+}

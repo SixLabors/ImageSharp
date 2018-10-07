0c437c35a698a7f35b9e656fd3fd5f2a20252505
Anton Firszov <antonfir@gmail.com>
2018-10-07 21:35:23 +0200

    disable profiling-related stuff

--- src/ImageSharp/Common/Helpers/InliningOptions.cs
+++ src/ImageSharp/Common/Helpers/InliningOptions.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 // Uncomment this for verbose profiler results:
-#define PROFILING
+// #define PROFILING
 using System.Runtime.CompilerServices;
 
 namespace SixLabors.ImageSharp

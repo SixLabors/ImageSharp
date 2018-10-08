3d14b90b93c971211bb8fcda38758bc60dd0b8e2
Anton Firszov <antonfir@gmail.com>
2018-10-09 01:26:51 +0200

    intoduce PlanarColorBuffer4F

---
+++ src/ImageSharp/PixelFormats/PlanarColorBuffer4F.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Buffers;
+using System.Numerics;
+
+using SixLabors.Memory;
+
+namespace SixLabors.ImageSharp.PixelFormats
+{
+    /// <summary>
+    /// AOS (Array Of Structures) / Planar Image Layout representation of a 4-channel <see cref="float"/>/<see cref="Vector4"/> color buffer.
+    /// <see>https://en.wikipedia.org/wiki/AOS_and_SOA</see>
+    /// </summary>
+    internal class PlanarColorBuffer4F : IDisposable
+    {
+        private IMemoryOwner<float> data;
+
+        public PlanarColorBuffer4F(int length, MemoryAllocator allocator, AllocationOptions allocationOptions)
+        {
+            DebugGuard.MustBeGreaterThanOrEqualTo(length, 0, nameof(length));
+            DebugGuard.NotNull(allocator, nameof(allocator));
+
+            this.Length = length;
+            this.data = allocator.Allocate<float>(4 * length, allocationOptions);
+            this.Memory = this.data.Memory;
+
+            this.X = this.Memory.Slice(0, length);
+            this.Y = this.Memory.Slice(length, length);
+            this.Z = this.Memory.Slice(2 * length, length);
+            this.W = this.Memory.Slice(3 * length, length);
+        }
+
+        public int Length { get; }
+
+        public Memory<float> Memory { get; }
+
+        public Memory<float> X { get; }
+
+        public Memory<float> Y { get; }
+
+        public Memory<float> Z { get; }
+
+        public Memory<float> W { get; }
+
+        public void Dispose()
+        {
+            this.data?.Dispose();
+            this.data = null;
+        }
+    }
+}
---
+++ tests/ImageSharp.Tests/PixelFormats/PlanarColorBufferTests.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.Memory;
+
+using Xunit;
+
+namespace SixLabors.ImageSharp.Tests.PixelFormats
+{
+    public class PlanarColorBufferTests
+    {
+        private readonly MemoryAllocator allocator;
+
+        public PlanarColorBufferTests()
+        {
+            this.allocator = ArrayPoolMemoryAllocator.CreateDefault();
+        }
+
+        public static readonly TheoryData<int> ConstructLengthData = new TheoryData<int> { 0, 1, 17, 2047 };
+        
+        [Theory]
+        [MemberData(nameof(ConstructLengthData))]
+        public void Construct_CreatesCorrectBuffers(int length)
+        {
+            using (var buffer = new PlanarColorBuffer4F(length, this.allocator, AllocationOptions.None))
+            {
+                Assert.Equal(4 * length, buffer.Memory.Length);
+
+                buffer.X.Span.Fill(2.0f);
+                buffer.Y.Span.Fill(3.0f);
+                buffer.Z.Span.Fill(5.0f);
+                buffer.W.Span.Fill(7.0f);
+
+                int sum = 0;
+
+                foreach (float val in buffer.Memory.Span)
+                {
+                    sum += (int)val;
+                }
+
+                int expected = (2 + 3 + 5 + 7) * length;
+                Assert.Equal(expected, sum);
+            }
+        }
+
+        [Fact]
+        public void Construct_UsesPooledAllocation()
+        {
+            using (var buffer1 = new PlanarColorBuffer4F(10000, this.allocator, AllocationOptions.None))
+            {
+                buffer1.Memory.Span[0] = 666f;
+            }
+
+            using (var buffer2 = new PlanarColorBuffer4F(10000, this.allocator, AllocationOptions.None))
+            {
+                Assert.Equal(666f, buffer2.Memory.Span[0]);
+            }
+        }
+    }
+}

96d0ae80b9b743ca8e67435e40ba7ff9c0937b75
Anton Firszov <antonfir@gmail.com>
2018-10-23 20:14:05 +0200

    Rgba32 <-> Argb32 <-> Bgra32

--- src/ImageSharp/PixelFormats/PixelConverter.cs
+++ src/ImageSharp/PixelFormats/PixelConverter.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
+using System.Buffers.Binary;
 using System.Runtime.CompilerServices;
 
 namespace SixLabors.ImageSharp.PixelFormats
@@ -16,31 +17,90 @@ namespace SixLabors.ImageSharp.PixelFormats
     /// </remarks>
     internal static class PixelConverter
     {
-        public static class Rgba32
+        public static class FromRgba32
         {
             /// <summary>
-            /// Converts a packed <see cref="PixelFormats.Rgba32"/> to <see cref="Argb32"/>.
+            /// Converts a packed <see cref="Rgba32"/> to <see cref="Argb32"/>.
             /// </summary>
             [MethodImpl(InliningOptions.ShortMethod)]
             public static uint ToArgb32(uint packedRgba)
             {
-                // packedRgba = [aa bb gg rr]
-                // ROL(8, packedRgba):
+                // packedRgba          = [aa bb gg rr]
+                // ROL(8, packedRgba)  = [bb gg rr aa]
                 return (packedRgba << 8) | (packedRgba >> 24);
+            }
+
+            /// <summary>
+            /// Converts a packed <see cref="Rgba32"/> to <see cref="Bgra32"/>.
+            /// </summary>
+            [MethodImpl(InliningOptions.ShortMethod)]
+            public static uint ToBgra32(uint packedRgba)
+            {
+                // packedRgba          = [aa bb gg rr]
+                // tmp1                = [aa 00 gg 00]
+                // tmp2                = [00 bb 00 rr]
+                // tmp3=ROL(16, tmp2)  = [00 rr 00 bb]
+                // tmp1 + tmp3         = [aa rr gg bb]
+                uint tmp1 = packedRgba & 0xFF00FF00;
+                uint tmp2 = packedRgba & 0x00FF00FF;
+                uint tmp3 = (tmp2 << 16) | (tmp2 >> 16);
+                return tmp1 + tmp3;
             }
         }
 
-        public static class Argb32
+        public static class FromArgb32
         {
             /// <summary>
-            /// Converts a packed <see cref="Argb32"/> to <see cref="PixelFormats.Rgba32"/>.
+            /// Converts a packed <see cref="Argb32"/> to <see cref="Rgba32"/>.
             /// </summary>
             [MethodImpl(InliningOptions.ShortMethod)]
             public static uint ToRgba32(uint packedArgb)
             {
-                // packedArgb = [bb gg rr aa]
-                // ROR(8, packedArgb):
+                // packedArgb          = [bb gg rr aa]
+                // ROR(8, packedArgb)  = [aa bb gg rr]
                 return (packedArgb >> 8) | (packedArgb << 24);
+            }
+
+            /// <summary>
+            /// Converts a packed <see cref="Argb32"/> to <see cref="Bgra32"/>.
+            /// </summary>
+            [MethodImpl(InliningOptions.ShortMethod)]
+            public static uint ToBgra32(uint packedArgb)
+            {
+                // packedArgb          = [bb gg rr aa]
+                // REVERSE(packedArgb) = [aa rr gg bb]
+                return BinaryPrimitives.ReverseEndianness(packedArgb);
+            }
+        }
+
+        public static class FromBgra32
+        {
+            /// <summary>
+            /// Converts a packed <see cref="Bgra32"/> to <see cref="Argb32"/>.
+            /// </summary>
+            [MethodImpl(InliningOptions.ShortMethod)]
+            public static uint ToArgb32(uint packedBgra)
+            {
+                // packedBgra          = [aa rr gg bb]
+                // REVERSE(packedBgra) = [bb gg rr aa]
+                return BinaryPrimitives.ReverseEndianness(packedBgra);
+            }
+
+            /// <summary>
+            /// Converts a packed <see cref="Rgba32"/> to <see cref="Bgra32"/>.
+            /// </summary>
+            [MethodImpl(InliningOptions.ShortMethod)]
+            public static uint ToRgba32(uint packedBgra)
+            {
+                // packedRgba          = [aa rr gg bb]
+                // tmp1                = [aa 00 gg 00]
+                // tmp2                = [00 rr 00 bb]
+                // tmp3=ROL(16, tmp2)  = [00 bb 00 rr]
+                // tmp1 + tmp3         = [aa bb gg rr]
+                uint tmp1 = packedBgra & 0xFF00FF00;
+                uint tmp2 = packedBgra & 0x00FF00FF;
+                uint tmp3 = (tmp2 << 16) | (tmp2 >> 16);
+                return tmp1 + tmp3;
             }
         }
     }

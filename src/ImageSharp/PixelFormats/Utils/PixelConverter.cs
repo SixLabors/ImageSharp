3b59b9ac2ce3de68ae022c29a473206f83b8beb1
Anton Firszov <antonfir@gmail.com>
2018-10-26 19:43:00 +0200

    better docs, drop PixelExtensions, fix tests

--- src/ImageSharp/PixelFormats/Utils/PixelConverter.cs
+++ src/ImageSharp/PixelFormats/Utils/PixelConverter.cs
@@ -12,7 +12,9 @@ namespace SixLabors.ImageSharp.PixelFormats.Utils
     /// <remarks>
     /// Implementations are based on ideas in:
     /// https://github.com/dotnet/coreclr/blob/master/src/System.Private.CoreLib/shared/System/Buffers/Binary/Reader.cs#L84
-    /// The JIT should be able to detect and optimize ROL and ROR patterns.
+    /// The JIT can detect and optimize rotation idioms ROTL (Rotate Left)
+    /// and ROTR (Rotate Right) emitting efficient CPU instructions:
+    /// https://github.com/dotnet/coreclr/pull/1830
     /// </remarks>
     internal static class PixelConverter
     {
@@ -25,7 +27,7 @@ namespace SixLabors.ImageSharp.PixelFormats.Utils
             public static uint ToArgb32(uint packedRgba)
             {
                 // packedRgba          = [aa bb gg rr]
-                // ROL(8, packedRgba)  = [bb gg rr aa]
+                // ROTL(8, packedRgba) = [bb gg rr aa]
                 return (packedRgba << 8) | (packedRgba >> 24);
             }
 
@@ -38,7 +40,7 @@ namespace SixLabors.ImageSharp.PixelFormats.Utils
                 // packedRgba          = [aa bb gg rr]
                 // tmp1                = [aa 00 gg 00]
                 // tmp2                = [00 bb 00 rr]
-                // tmp3=ROL(16, tmp2)  = [00 rr 00 bb]
+                // tmp3=ROTL(16, tmp2)  = [00 rr 00 bb]
                 // tmp1 + tmp3         = [aa rr gg bb]
                 uint tmp1 = packedRgba & 0xFF00FF00;
                 uint tmp2 = packedRgba & 0x00FF00FF;
@@ -56,7 +58,7 @@ namespace SixLabors.ImageSharp.PixelFormats.Utils
             public static uint ToRgba32(uint packedArgb)
             {
                 // packedArgb          = [bb gg rr aa]
-                // ROR(8, packedArgb)  = [aa bb gg rr]
+                // ROTR(8, packedArgb) = [aa bb gg rr]
                 return (packedArgb >> 8) | (packedArgb << 24);
             }
 
@@ -94,7 +96,7 @@ namespace SixLabors.ImageSharp.PixelFormats.Utils
                 // packedRgba          = [aa rr gg bb]
                 // tmp1                = [aa 00 gg 00]
                 // tmp2                = [00 rr 00 bb]
-                // tmp3=ROL(16, tmp2)  = [00 bb 00 rr]
+                // tmp3=ROTL(16, tmp2) = [00 bb 00 rr]
                 // tmp1 + tmp3         = [aa bb gg rr]
                 uint tmp1 = packedBgra & 0xFF00FF00;
                 uint tmp2 = packedBgra & 0x00FF00FF;

014e5c928ee116fbc5530bfeb299806215765f00
Anton Firszov <antonfir@gmail.com>
2018-10-26 20:04:19 +0200

    refactor PixelOperationsTests

---
+++ tests/ImageSharp.Tests/PixelFormats/PixelOperations/PixelOperationsTests.Bgra32OperationsTests.cs
@@ -0,0 +1,21 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SixLabors.ImageSharp.Tests.PixelFormats.PixelOperations
+{
+    public partial class PixelOperationsTests
+    {
+        public class Bgra32OperationsTests : PixelOperationsTests<Bgra32>
+        {
+            public Bgra32OperationsTests(ITestOutputHelper output)
+                : base(output)
+            {
+            }
+
+            [Fact]
+            public void IsSpecialImplementation() => Assert.IsType<Bgra32.PixelOperations>(PixelOperations<Bgra32>.Instance);
+        }
+    }
+}

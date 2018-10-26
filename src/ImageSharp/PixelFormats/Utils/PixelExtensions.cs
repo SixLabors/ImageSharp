460675a1202318df114b2586264fd21e46acd9bd
Anton Firszov <antonfir@gmail.com>
2018-10-26 16:35:02 +0200

    refactor Vector4 implementation code from PixelOperation<T> to Vector4Converters

--- src/ImageSharp/PixelFormats/Utils/PixelExtensions.cs
+++ src/ImageSharp/PixelFormats/Utils/PixelExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-namespace SixLabors.ImageSharp.PixelFormats
+namespace SixLabors.ImageSharp.PixelFormats.Utils
 {
     /// <summary>
     /// Low-performance extension methods to help conversion syntax, suitable for testing purposes.

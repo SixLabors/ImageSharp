9279983127e96bb8ece236e090c682b118135cd1
Anton Firszov <antonfir@gmail.com>
2018-11-04 02:54:21 +0100

    Drop all GetBlockDataReference() usages

--- tests/ImageSharp.Benchmarks/Codecs/Jpeg/DecodeJpeg_ImageSpecific.cs
+++ tests/ImageSharp.Benchmarks/Codecs/Jpeg/DecodeJpeg_ImageSpecific.cs
@@ -46,14 +46,18 @@ namespace SixLabors.ImageSharp.Benchmarks.Codecs.Jpeg
 
         private string TestImageFullPath => Path.Combine(TestEnvironment.InputImagesDirectoryFullPath, this.TestImage);
 
+        // NOTE:
+        // The scaled result for very large image "TestImages.Jpeg.Issues.ExifGetString750Transform"
+        // is almost the same as the result for Jpeg420Exif,
+        // which proves that the execution time for the most common YCbCr 420 path scales linearly
         [Params(
             TestImages.Jpeg.Baseline.Lake,
             TestImages.Jpeg.Issues.BadRstProgressive518,
-            TestImages.Jpeg.Issues.ExifGetString750Transform,
 
             TestImages.Jpeg.Baseline.Jpeg420Exif
             )]
         public string TestImage { get; set; }
+
 
         [GlobalSetup]
         public void ReadImages()
@@ -110,16 +114,16 @@ namespace SixLabors.ImageSharp.Benchmarks.Codecs.Jpeg
         // RESULTS (2018 November 4):
         //                          Method |                                   TestImage |       Mean |      Error |    StdDev | Scaled | ScaledSD |     Gen 0 |    Gen 1 |    Gen 2 |   Allocated |
         // ------------------------------- |-------------------------------------------- |-----------:|-----------:|----------:|-------:|---------:|----------:|---------:|---------:|------------:|
-        //  'Decode Jpeg - System.Drawing' |                       Jpg/baseline/Lake.jpg |   6.291 ms |   1.200 ms | 0.0678 ms |   1.00 |     0.00 |   62.5000 |        - |        - |   205.83 KB |
-        //      'Decode Jpeg - ImageSharp' |                       Jpg/baseline/Lake.jpg |  18.493 ms |   3.025 ms | 0.1709 ms |   2.94 |     0.03 |         - |        - |        - |    19.97 KB |
+        //  'Decode Jpeg - System.Drawing' |                       Jpg/baseline/Lake.jpg |   6.117 ms |  0.3923 ms | 0.0222 ms |   1.00 |     0.00 |   62.5000 |        - |        - |   205.83 KB |
+        //      'Decode Jpeg - ImageSharp' |                       Jpg/baseline/Lake.jpg |  18.126 ms |  0.6023 ms | 0.0340 ms |   2.96 |     0.01 |         - |        - |        - |    19.97 KB |
         //                                 |                                             |            |            |           |        |          |           |          |          |             |
-        //  'Decode Jpeg - System.Drawing' |                Jpg/baseline/jpeg420exif.jpg |  16.962 ms |   1.446 ms | 0.0817 ms |   1.00 |     0.00 |  218.7500 |        - |        - |   757.04 KB |
-        //      'Decode Jpeg - ImageSharp' |                Jpg/baseline/jpeg420exif.jpg |  42.105 ms |   4.496 ms | 0.2540 ms |   2.48 |     0.02 |         - |        - |        - |    21.94 KB |
+        //  'Decode Jpeg - System.Drawing' |                Jpg/baseline/jpeg420exif.jpg |  17.063 ms |  2.6096 ms | 0.1474 ms |   1.00 |     0.00 |  218.7500 |        - |        - |   757.04 KB |
+        //      'Decode Jpeg - ImageSharp' |                Jpg/baseline/jpeg420exif.jpg |  41.366 ms |  1.0115 ms | 0.0572 ms |   2.42 |     0.02 |         - |        - |        - |    21.94 KB |
         //                                 |                                             |            |            |           |        |          |           |          |          |             |
-        //  'Decode Jpeg - System.Drawing' | Jpg/issues/Issue518-Bad-RST-Progressive.jpg | 432.344 ms |  89.746 ms | 5.0708 ms |   1.00 |     0.00 | 2375.0000 |        - |        - |  7403.76 KB |
-        //      'Decode Jpeg - ImageSharp' | Jpg/issues/Issue518-Bad-RST-Progressive.jpg | 421.292 ms | 128.587 ms | 7.2654 ms |   0.97 |     0.02 |  125.0000 | 125.0000 | 125.0000 | 35186.98 KB |
+        //  'Decode Jpeg - System.Drawing' | Jpg/issues/Issue518-Bad-RST-Progressive.jpg | 428.282 ms | 94.9163 ms | 5.3629 ms |   1.00 |     0.00 | 2375.0000 |        - |        - |  7403.76 KB |
+        //      'Decode Jpeg - ImageSharp' | Jpg/issues/Issue518-Bad-RST-Progressive.jpg | 386.698 ms | 33.0065 ms | 1.8649 ms |   0.90 |     0.01 |  125.0000 | 125.0000 | 125.0000 | 35186.97 KB |
         //                                 |                                             |            |            |           |        |          |           |          |          |             |
-        //  'Decode Jpeg - System.Drawing' |       Jpg/issues/issue750-exif-tranform.jpg |  94.723 ms |   4.663 ms | 0.2635 ms |   1.00 |     0.00 | 1750.0000 |        - |        - |  5492.63 KB |
-        //      'Decode Jpeg - ImageSharp' |       Jpg/issues/issue750-exif-tranform.jpg | 234.071 ms |  37.979 ms | 2.1459 ms |   2.47 |     0.02 |  312.5000 | 312.5000 | 312.5000 | 58834.45 KB |
+        //  'Decode Jpeg - System.Drawing' |       Jpg/issues/issue750-exif-tranform.jpg |  95.192 ms |  3.1762 ms | 0.1795 ms |   1.00 |     0.00 | 1750.0000 |        - |        - |  5492.63 KB |
+        //      'Decode Jpeg - ImageSharp' |       Jpg/issues/issue750-exif-tranform.jpg | 230.158 ms | 48.8128 ms | 2.7580 ms |   2.42 |     0.02 |  312.5000 | 312.5000 | 312.5000 | 58834.66 KB |
     }
 }

657f551642a3e2773cf72534a886ffc63241e9dc
Dan Manning <dmanning23@hotmail.com>
2018-11-10 17:25:39 -0600

    ImageSharp-762_Aot-compiling: added simple unit test to check no exceptions are thrown by AotCompiler

---
+++ tests/ImageSharp.Tests/Advanced/AotCompilerTests.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+using Xunit;
+
+namespace SixLabors.ImageSharp.Tests.Advanced
+{
+    public class AotCompilerTests
+    {
+        [Fact]
+        public void AotCompiler_NoExceptions()
+        {
+            AotCompiler.Seed<Rgba32>();
+        }
+    }
+}

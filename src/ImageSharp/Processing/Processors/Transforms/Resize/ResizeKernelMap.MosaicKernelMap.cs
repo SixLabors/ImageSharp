f83da45025e1a3d5f16818c313d8c72be3ac93b9
Anton Firszov <antonfir@gmail.com>
2018-11-28 02:16:01 +0100

    MosaicKernelMap works!

--- src/ImageSharp/Processing/Processors/Transforms/Resize/ResizeKernelMap.MosaicKernelMap.cs
+++ src/ImageSharp/Processing/Processors/Transforms/Resize/ResizeKernelMap.MosaicKernelMap.cs
@@ -1,5 +1,8 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
+
+using System;
+
 using SixLabors.Memory;
 
 namespace SixLabors.ImageSharp.Processing.Processors.Transforms
@@ -42,9 +45,36 @@ namespace SixLabors.ImageSharp.Processing.Processors.Transforms
                 this.period = period;
             }
 
+            internal override string Info => base.Info + $"|period:{this.period}|cornerInterval:{this.cornerInterval}";
+
             protected override void Initialize()
             {
-                base.Initialize();
+                // Build top corner data + one period of the mosaic data:
+                int startOfFirstRepeatedMosaic = this.cornerInterval + this.period;
+
+                for (int i = 0; i < startOfFirstRepeatedMosaic; i++)
+                {
+                    ResizeKernel kernel = this.BuildKernel(i, i);
+                    this.kernels[i] = kernel;
+                }
+
+                // Copy the mosaics:
+                int bottomStartDest = this.DestinationSize - this.cornerInterval;
+                for (int i = startOfFirstRepeatedMosaic; i < bottomStartDest; i++)
+                {
+                    float center = ((i + .5F) * this.ratio) - .5F;
+                    int left = (int)MathF.Ceiling(center - this.radius);
+                    ResizeKernel kernel = this.kernels[i - this.period];
+                    this.kernels[i] = kernel.AlterLeftValue(left);
+                }
+
+                // Build bottom corner data:
+                int bottomStartData = this.cornerInterval + this.period;
+                for (int i = 0; i < this.cornerInterval; i++)
+                {
+                    ResizeKernel kernel = this.BuildKernel(bottomStartDest + i, bottomStartData + i);
+                    this.kernels[bottomStartDest + i] = kernel;
+                }
             }
         }
     }

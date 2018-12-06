3104a3e23fe50b6974275900f960ec7a9f52ea91
Jason Nelson <jason@carbonmade.com>
2018-12-06 12:42:32 -0800

    Cleanup assembly properties

--- src/Shared/AssemblyInfo.Common.cs
+++ src/Shared/AssemblyInfo.Common.cs
@@ -5,32 +5,6 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyDescription("A cross-platform library for processing of image files; written in C#")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Six Labors")]
-[assembly: AssemblyProduct("SixLabors.ImageSharp")]
-[assembly: AssemblyCopyright("Copyright (c) Six Labors and contributors.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
-
 // Ensure the internals can be built and tested.
 [assembly: InternalsVisibleTo("SixLabors.ImageSharp.Drawing")]
 [assembly: InternalsVisibleTo("ImageSharp.Benchmarks")]

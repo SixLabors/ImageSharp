2849fd9c1dc19a7338235f6c33f4970e5a12dee3
James Jackson-South <james_south@hotmail.com>
2019-01-25 16:32:45 +1100

    Update dependencies + fix stylecop (#816)

--- src/ImageSharp/Formats/Png/Chunks/PhysicalChunkData.cs
+++ src/ImageSharp/Formats/Png/Chunks/PhysicalChunkData.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
 using System.Buffers.Binary;
 using SixLabors.ImageSharp.Common.Helpers;
 using SixLabors.ImageSharp.MetaData;

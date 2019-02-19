e649369f97539b04ba159aea94983993b0486377
Sergio Pedri <sergio0694@live.com>
2019-02-19 15:59:47 +0100

    Switched to float, added method to create the 1D kernels

--- src/ImageSharp/Processing/Processors/Convolution/BokehBlurProcessor.cs
+++ src/ImageSharp/Processing/Processors/Convolution/BokehBlurProcessor.cs
@@ -3,8 +3,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Primitives;
 using SixLabors.Primitives;
 
 namespace SixLabors.ImageSharp.Processing.Processors.Convolution
@@ -27,11 +28,39 @@ namespace SixLabors.ImageSharp.Processing.Processors.Convolution
         private readonly int componentsCount;
 
         /// <summary>
+        /// The kernel components to use for the current instance
+        /// </summary>
+        private readonly IReadOnlyList<IReadOnlyDictionary<char, float>> kernelComponents;
+
+        /// <summary>
+        /// The scaling factor for kernel values
+        /// </summary>
+        private readonly float kernelsScale;
+
+        /// <summary>
+        /// The complex kernels to use to apply the blur for the current instance
+        /// </summary>
+        private readonly IReadOnlyList<Complex[]> complexKernels;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="SixLabors.ImageSharp.Processing.Processors.Convolution.BokehBlurProcessor{TPixel}"/> class.
         /// </summary>
-        public BokehBlurProcessor(int components = 2)
+        /// <param name="radius">
+        /// The 'radius' value representing the size of the area to sample.
+        /// </param>
+        /// <param name="components">
+        /// The number of components to use to approximate the original 2D bokeh blur convolution kernel.
+        /// </param>
+        public BokehBlurProcessor(int radius = 32, int components = 2)
         {
+            this.Radius = radius;
+            this.kernelSize = (radius * 2) + 1;
             this.componentsCount = components;
+
+            (this.kernelComponents, this.kernelsScale) = this.GetParameters();
+            this.complexKernels = (
+                from component in this.kernelComponents
+                select this.CreateComplex1DKernel(component['a'], component['b'])).ToArray();
         }
 
         /// <summary>
@@ -40,86 +69,76 @@ namespace SixLabors.ImageSharp.Processing.Processors.Convolution
         public int Radius { get; }
 
         /// <summary>
-        /// Gets the horizontal gradient operator.
-        /// </summary>
-        public DenseMatrix<float> KernelX { get; }
-
-        /// <summary>
-        /// Gets the vertical gradient operator.
-        /// </summary>
-        public DenseMatrix<float> KernelY { get; }
-
-        /// <summary>
         /// Gets the kernel scales to adjust the component values in each kernel
         /// </summary>
-        private static IReadOnlyList<double> KernelScales { get; } = new[] { 1.4, 1.2, 1.2, 1.2, 1.2, 1.2 };
+        private static IReadOnlyList<float> KernelScales { get; } = new[] { 1.4f, 1.2f, 1.2f, 1.2f, 1.2f, 1.2f };
 
         /// <summary>
         /// Gets the available bokeh blur kernel parameters
         /// </summary>
-        private static IReadOnlyList<double[,]> KernelParameters { get; } = new[]
+        private static IReadOnlyList<float[,]> KernelParameters { get; } = new[]
         {
             // 1 component
-            new[,] { { 0.862325, 1.624835, 0.767583, 1.862321 } },
+            new[,] { { 0.862325f, 1.624835f, 0.767583f, 1.862321f } },
 
             // 2 components
             new[,]
             {
-                { 0.886528, 5.268909, 0.411259, -0.548794 },
-                { 1.960518, 1.558213, 0.513282, 4.56111 }
+                { 0.886528f, 5.268909f, 0.411259f, -0.548794f },
+                { 1.960518f, 1.558213f, 0.513282f, 4.56111f }
             },
 
             // 3 components
             new[,]
             {
-                { 2.17649, 5.043495, 1.621035, -2.105439 },
-                { 1.019306, 9.027613, -0.28086, -0.162882 },
-                { 2.81511, 1.597273, -0.366471, 10.300301 }
+                { 2.17649f, 5.043495f, 1.621035f, -2.105439f },
+                { 1.019306f, 9.027613f, -0.28086f, -0.162882f },
+                { 2.81511f, 1.597273f, -0.366471f, 10.300301f }
             },
 
             // 4 components
             new[,]
             {
-                { 4.338459, 1.553635, -5.767909, 46.164397 },
-                { 3.839993, 4.693183, 9.795391, -15.227561 },
-                { 2.791880, 8.178137, -3.048324, 0.302959 },
-                { 1.342190, 12.328289, 0.010001, 0.244650 }
+                { 4.338459f, 1.553635f, -5.767909f, 46.164397f },
+                { 3.839993f, 4.693183f, 9.795391f, -15.227561f },
+                { 2.791880f, 8.178137f, -3.048324f, 0.302959f },
+                { 1.342190f, 12.328289f, 0.010001f, 0.244650f }
             },
 
             // 5 components
             new[,]
             {
-                { 4.892608, 1.685979, -22.356787, 85.91246 },
-                { 4.71187, 4.998496, 35.918936, -28.875618 },
-                { 4.052795, 8.244168, -13.212253, -1.578428 },
-                { 2.929212, 11.900859, 0.507991, 1.816328 },
-                { 1.512961, 16.116382, 0.138051, -0.01 }
+                { 4.892608f, 1.685979f, -22.356787f, 85.91246f },
+                { 4.71187f, 4.998496f, 35.918936f, -28.875618f },
+                { 4.052795f, 8.244168f, -13.212253f, -1.578428f },
+                { 2.929212f, 11.900859f, 0.507991f, 1.816328f },
+                { 1.512961f, 16.116382f, 0.138051f, -0.01f }
             },
 
             // 6 components
             new[,]
             {
-                { 5.143778, 2.079813, -82.326596, 111.231024 },
-                { 5.612426, 6.153387, 113.878661, 58.004879 },
-                { 5.982921, 9.802895, 39.479083, -162.028887 },
-                { 6.505167, 11.059237, -71.286026, 95.027069 },
-                { 3.869579, 14.81052, 1.405746, -3.704914 },
-                { 2.201904, 19.032909, -0.152784, -0.107988 }
+                { 5.143778f, 2.079813f, -82.326596f, 111.231024f },
+                { 5.612426f, 6.153387f, 113.878661f, 58.004879f },
+                { 5.982921f, 9.802895f, 39.479083f, -162.028887f },
+                { 6.505167f, 11.059237f, -71.286026f, 95.027069f },
+                { 3.869579f, 14.81052f, 1.405746f, -3.704914f },
+                { 2.201904f, 19.032909f, -0.152784f, -0.107988f }
             }
         };
 
         /// <summary>
         /// Gets the kernel parameters and scaling factor for the current count value in the current instance
         /// </summary>
-        private (IReadOnlyList<IReadOnlyDictionary<char, double>> Components, double Scale) GetParameters()
+        private (IReadOnlyList<IReadOnlyDictionary<char, float>> Components, float Scale) GetParameters()
         {
             // Prepare the kernel components
             int index = Math.Max(0, Math.Min(this.componentsCount - 1, KernelParameters.Count));
-            double[,] parameters = KernelParameters[index];
-            var mapping = new IReadOnlyDictionary<char, double>[parameters.GetLength(0)];
+            float[,] parameters = KernelParameters[index];
+            var mapping = new IReadOnlyDictionary<char, float>[parameters.GetLength(0)];
             for (int i = 0; i < parameters.GetLength(0); i++)
             {
-                mapping[i] = new Dictionary<char, double>
+                mapping[i] = new Dictionary<char, float>
                 {
                     ['a'] = parameters[i, 0],
                     ['b'] = parameters[i, 1],
@@ -130,6 +149,34 @@ namespace SixLabors.ImageSharp.Processing.Processors.Convolution
 
             // Return the components and the adjustment scale
             return (mapping, KernelScales[index]);
+        }
+
+        /// <summary>
+        /// Creates a complex 1D kernel with the specified parameters
+        /// </summary>
+        /// <param name="a">The exponential parameter for each complex component</param>
+        /// <param name="b">The angle component for each complex component</param>
+        private Complex[] CreateComplex1DKernel(float a, float b)
+        {
+            // Precompute the range values
+            float[] ax = Enumerable.Range(-this.Radius, this.Radius + 1).Select(
+                i =>
+                    {
+                        float value = i * this.kernelsScale * (1f / this.Radius);
+                        return value * value;
+                    }).ToArray();
+
+            // Compute the complex kernels
+            var kernel = new Complex[this.kernelSize];
+            for (int i = 0; i < this.kernelSize; i++)
+            {
+                double
+                    real = Math.Exp(-a * ax[i]) * Math.Cos(b * ax[i]),
+                    imaginary = Math.Exp(-a * ax[i]) * Math.Sin(b * ax[i]);
+                kernel[i] = new Complex(real, imaginary);
+            }
+
+            return kernel;
         }
 
         /// <inheritdoc/>

50100b9edf63f1803ccfef8ec27a76164ffe50aa
Robin <robin.krom@gmail.com>
2019-02-20 07:36:45 +0100

    Added overloads to the FillRectangleExtensions which take a Rectangle and color or brush to quickly fill a rectangle via the FillProcessor instead of the FillRegionProcessor.

--- src/ImageSharp.Drawing/Processing/FillRectangleExtensions.cs
+++ src/ImageSharp.Drawing/Processing/FillRectangleExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing.Processors.Drawing;
 using SixLabors.Primitives;
 using SixLabors.Shapes;
 
@@ -38,7 +39,7 @@ namespace SixLabors.ImageSharp.Processing
             => source.Fill(brush, new RectangularPolygon(shape.X, shape.Y, shape.Width, shape.Height));
 
         /// <summary>
-        /// Flood fills the image in the shape of the provided rectangle with the specified brush.
+        /// Flood fills the image in the shape of the provided rectangle with the specified color.
         /// </summary>
         /// <typeparam name="TPixel">The type of the color.</typeparam>
         /// <param name="source">The image this method extends.</param>
@@ -51,7 +52,7 @@ namespace SixLabors.ImageSharp.Processing
             => source.Fill(options, new SolidBrush<TPixel>(color), shape);
 
         /// <summary>
-        /// Flood fills the image in the shape of the provided rectangle with the specified brush.
+        /// Flood fills the image in the shape of the provided rectangle with the specified color.
         /// </summary>
         /// <typeparam name="TPixel">The type of the color.</typeparam>
         /// <param name="source">The image this method extends.</param>
@@ -61,5 +62,55 @@ namespace SixLabors.ImageSharp.Processing
         public static IImageProcessingContext<TPixel> Fill<TPixel>(this IImageProcessingContext<TPixel> source, TPixel color, RectangleF shape)
             where TPixel : struct, IPixel<TPixel>
             => source.Fill(new SolidBrush<TPixel>(color), shape);
+
+        /// <summary>
+        /// Fills the image in the shape of the provided rectangle with the specified color.
+        /// </summary>
+        /// <typeparam name="TPixel">The type of the color.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="rectangle">The rectangle shape.</param>
+        /// <returns>The <see cref="Image{TPixel}"/>.</returns>
+        public static IImageProcessingContext<TPixel> Fill<TPixel>(this IImageProcessingContext<TPixel> source, TPixel color, Rectangle rectangle)
+            where TPixel : struct, IPixel<TPixel>
+            => source.Fill(GraphicsOptions.Default, new SolidBrush<TPixel>(color), rectangle);
+
+        /// <summary>
+        /// Fills the image in the shape of the provided rectangle with the specified brush.
+        /// </summary>
+        /// <typeparam name="TPixel">The type of the color.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="brush">The details how to fill the region of interest.</param>
+        /// <param name="rectangle">The rectangle shape.</param>
+        /// <returns>The <see cref="Image{TPixel}"/>.</returns>
+        public static IImageProcessingContext<TPixel> Fill<TPixel>(this IImageProcessingContext<TPixel> source, IBrush<TPixel> brush, Rectangle rectangle)
+            where TPixel : struct, IPixel<TPixel>
+            => source.Fill(GraphicsOptions.Default, brush, rectangle);
+
+        /// <summary>
+        /// Fills the image in the shape of the provided rectangle with the specified color.
+        /// </summary>
+        /// <typeparam name="TPixel">The type of the color.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="options">The graphics options.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="rectangle">The rectangle shape.</param>
+        /// <returns>The <see cref="Image{TPixel}"/>.</returns>
+        public static IImageProcessingContext<TPixel> Fill<TPixel>(this IImageProcessingContext<TPixel> source, GraphicsOptions options, TPixel color, Rectangle rectangle)
+            where TPixel : struct, IPixel<TPixel>
+            => source.Fill(options, new SolidBrush<TPixel>(color), rectangle);
+
+        /// <summary>
+        /// Fills the image in the shape of the provided rectangle with the specified brush.
+        /// </summary>
+        /// <typeparam name="TPixel">The type of the color.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="options">The graphics options.</param>
+        /// <param name="brush">The details how to fill the region of interest.</param>
+        /// <param name="rectangle">The rectangle shape.</param>
+        /// <returns>The <see cref="Image{TPixel}"/>.</returns>
+        public static IImageProcessingContext<TPixel> Fill<TPixel>(this IImageProcessingContext<TPixel> source, GraphicsOptions options, IBrush<TPixel> brush, Rectangle rectangle)
+            where TPixel : struct, IPixel<TPixel>
+            => source.ApplyProcessor(new FillProcessor<TPixel>(brush, options), rectangle);
     }
 }

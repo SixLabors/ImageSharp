a64636e82b11d33b6f4b78e3f0013359f26987ef
Sergio Pedri <sergio0694@live.com>
2019-02-24 00:03:29 +0100

    Added BokehBlur overload to just specify the target bounds

--- src/ImageSharp/Processing/BokehBlurExtensions.cs
+++ src/ImageSharp/Processing/BokehBlurExtensions.cs
@@ -40,6 +40,19 @@ namespace SixLabors.ImageSharp.Processing
         /// </summary>
         /// <typeparam name="TPixel">The pixel format.</typeparam>
         /// <param name="source">The image this method extends.</param>
+        /// <param name="rectangle">
+        /// The <see cref="Rectangle"/> structure that specifies the portion of the image object to alter.
+        /// </param>
+        /// <returns>The <see cref="Image{TPixel}"/>.</returns>
+        public static IImageProcessingContext<TPixel> BokehBlur<TPixel>(this IImageProcessingContext<TPixel> source, Rectangle rectangle)
+            where TPixel : struct, IPixel<TPixel>
+            => source.ApplyProcessor(new BokehBlurProcessor<TPixel>(), rectangle);
+
+        /// <summary>
+        /// Applies a bokeh blur to the image.
+        /// </summary>
+        /// <typeparam name="TPixel">The pixel format.</typeparam>
+        /// <param name="source">The image this method extends.</param>
         /// <param name="radius">The 'radius' value representing the size of the area to sample.</param>
         /// <param name="components">The 'components' value representing the number of kernels to use to approximate the bokeh effect.</param>
         /// <param name="gamma">The gamma highlight factor to use to emphasize bright spots in the source image</param>

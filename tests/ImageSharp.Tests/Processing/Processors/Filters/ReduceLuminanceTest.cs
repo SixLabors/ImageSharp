c8ae2dcff6d99f747fade88fe1ed7181878eaf1a
Sergio Pedri <sergio0694@live.com>
2019-02-23 17:08:59 +0100

    Added luminance reduction tests

--- tests/ImageSharp.Tests/Processing/Processors/Filters/ReduceLuminanceTest.cs
+++ tests/ImageSharp.Tests/Processing/Processors/Filters/ReduceLuminanceTest.cs
@@ -10,25 +10,26 @@ using Xunit;
 
 namespace SixLabors.ImageSharp.Tests.Processing.Processors.Convolution
 {
-    public class BoxBlurTest : FileTestBase
+    [GroupOutput("Filters")]
+    public class ReduceLuminanceTest : FileTestBase
     {
-        public static readonly TheoryData<int> BoxBlurValues = new TheoryData<int> { 3, 5 };
+        public static readonly TheoryData<float> ReduceLuminanceValues = new TheoryData<float> { 0.4f, 0.6f };
 
         [Theory]
-        [WithFileCollection(nameof(DefaultFiles), nameof(BoxBlurValues), DefaultPixelType)]
-        public void ImageShouldApplyBoxBlurFilter<TPixel>(TestImageProvider<TPixel> provider, int value)
+        [WithFileCollection(nameof(DefaultFiles), nameof(ReduceLuminanceValues), DefaultPixelType)]
+        public void ImageShouldApplyBoxBlurFilter<TPixel>(TestImageProvider<TPixel> provider, float value)
             where TPixel : struct, IPixel<TPixel>
         {
             using (Image<TPixel> image = provider.GetImage())
             {
-                image.Mutate(x => x.BoxBlur(value));
+                image.Mutate(x => x.ReduceLuminance(value));
                 image.DebugSave(provider, value);
             }
         }
 
         [Theory]
-        [WithFileCollection(nameof(DefaultFiles), nameof(BoxBlurValues), DefaultPixelType)]
-        public void ImageShouldApplyBoxBlurFilterInBox<TPixel>(TestImageProvider<TPixel> provider, int value)
+        [WithFileCollection(nameof(DefaultFiles), nameof(ReduceLuminanceValues), DefaultPixelType)]
+        public void ImageShouldApplyBoxBlurFilterInBox<TPixel>(TestImageProvider<TPixel> provider, float value)
             where TPixel : struct, IPixel<TPixel>
         {
             using (Image<TPixel> source = provider.GetImage())
@@ -36,7 +37,7 @@ namespace SixLabors.ImageSharp.Tests.Processing.Processors.Convolution
             {
                 var bounds = new Rectangle(10, 10, image.Width / 2, image.Height / 2);
 
-                image.Mutate(x => x.BoxBlur(value, bounds));
+                image.Mutate(x => x.ReduceLuminance(value, bounds));
                 image.DebugSave(provider, value);
 
                 ImageComparer.Tolerant().VerifySimilarityIgnoreRegion(source, image, bounds);

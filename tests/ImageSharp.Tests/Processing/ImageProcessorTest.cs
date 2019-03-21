491aa9a9e9cc870cd84cf70563678d9b8f6a0eef
Kjetil Klaussen <kjetil.klaussen@gmail.com>
2019-03-21 22:38:34 +0100

    test: Added tests asserting the usage of custom configuration for the image processor

---
+++ tests/ImageSharp.Tests/Processing/ImageProcessorTest.cs
@@ -0,0 +1,37 @@
+﻿// // Copyright (c) Six Labors and contributors.
+// // Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing.Processors;
+using SixLabors.Primitives;
+
+using Xunit;
+
+namespace SixLabors.ImageSharp.Tests.Processing
+{
+    public class ImageProcessorTest
+    {
+        [Fact]
+        public void Apply_ShouldUseCustomConfiguration()
+        {
+            Configuration expected = Configuration.Default;
+            var source = new Image<Rgba32>(91 + 324, 123 + 56);
+            var processor = new FakeProcessor<Rgba32>();
+
+            processor.Apply(source, source.Bounds(), expected);
+
+            Assert.Equal(expected, processor.AppliedConfiguration);
+        }
+    }
+
+    public class FakeProcessor<TPixel> : IImageProcessor<TPixel>
+        where TPixel : struct, IPixel<TPixel>
+    {
+        public void Apply(Image<TPixel> source, Rectangle sourceRectangle, Configuration configuration = null)
+        {
+            this.AppliedConfiguration = configuration;
+        }
+
+        public Configuration AppliedConfiguration { get; private set; }
+    }
+}

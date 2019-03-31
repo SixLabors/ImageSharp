b0ff7cf6b6929f35de310b7d61c24ae839fd95d7
Anton Firszov <antonfir@gmail.com>
2019-03-31 23:40:56 +0200

    copy companding refactor from #847

---
+++ src/ImageSharp/PixelFormats/PixelConversionModifiers.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+
+namespace SixLabors.ImageSharp.PixelFormats
+{
+    [Flags]
+    internal enum PixelConversionModifiers
+    {
+        None = 0,
+        Scale = 1 << 0,
+        Premultiply = 1 << 1,
+        SRgbCompand = 1 << 2,
+    }
+}

a8a9ff543d39e5eec064850a9f3a783c0ced3271
Jason Nelson <jason@carbonmade.com>
2019-04-01 23:16:04 -0500

    Nits (#870)
    * Format conversion models
    
    * Format color spaces
    
    * Add missing periods
    
    * Replace Endianness enum with isBigEndian
    
    * Seal DoubleBufferedStreamReader
    
    * Remove unused namespaces
    
    * Simplify variable names
    
    * Remove unused namespace
    
    * Format Block8x8F
    
    * Use MathF
    
    * Simplify exception when an encoder is not found
    
    * Format ICC models
    
    * Simplify excption messages in ExifValue (reduces IL size)
    
    * Add missing periods to documentation.
    
    * Format ImageProperty
    
    * Format BmpImageFormatDetector
    
    * Seal LocalFileSystem
    
    * Seal JpegColorConverter implementations
    
    * Add missing periods to jpeg documentation
    
    * Format ValueSize
    
    * Fix stylecop violation
    
    * Revert "Simplify excption messages in ExifValue (reduces IL size)"
    
    This reverts commit d22f2a679f4d9b1d5c3e41a56d1a7ea8e895f6c7.

--- src/ImageSharp/MetaData/ImageProperty.cs
+++ src/ImageSharp/MetaData/ImageProperty.cs
@@ -107,10 +107,7 @@ namespace SixLabors.ImageSharp.MetaData
         /// <returns>
         /// A <see cref="T:System.String"/> containing a fully qualified type name.
         /// </returns>
-        public override string ToString()
-        {
-            return $"ImageProperty [ Name={this.Name}, Value={this.Value} ]";
-        }
+        public override string ToString() => $"ImageProperty [ Name={this.Name}, Value={this.Value} ]";
 
         /// <summary>
         /// Indicates whether the current object is equal to another object of the same type.

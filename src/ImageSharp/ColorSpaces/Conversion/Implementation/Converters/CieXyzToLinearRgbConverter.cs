890165316e98b061999fab9bbf50cca1d656df86
Jason Nelson <jason@carbonmade.com>
2019-04-11 10:19:00 -0700

    Only perform matrix inversion once in CieXyzToLinearRgbConverter

--- src/ImageSharp/ColorSpaces/Conversion/Implementation/Converters/CieXyzToLinearRgbConverter.cs
+++ src/ImageSharp/ColorSpaces/Conversion/Implementation/Converters/CieXyzToLinearRgbConverter.cs
@@ -28,11 +28,15 @@ namespace SixLabors.ImageSharp.ColorSpaces.Conversion.Implementation
         public CieXyzToLinearRgbConverter(RgbWorkingSpaceBase workingSpace)
         {
             this.TargetWorkingSpace = workingSpace;
-            this.conversionMatrix = GetRgbToCieXyzMatrix(workingSpace);
+
+            // Gets the inverted Rgb -> Xyz matrix 
+            Matrix4x4.Invert(GetRgbToCieXyzMatrix(workingSpace), out Matrix4x4 inverted);
+
+            this.conversionMatrix = inverted;
         }
 
         /// <summary>
-        /// Gets the target working space
+        /// Gets the target working space.
         /// </summary>
         public RgbWorkingSpaceBase TargetWorkingSpace { get; }
 
@@ -44,8 +48,8 @@ namespace SixLabors.ImageSharp.ColorSpaces.Conversion.Implementation
         [MethodImpl(InliningOptions.ShortMethod)]
         public LinearRgb Convert(in CieXyz input)
         {
-            Matrix4x4.Invert(this.conversionMatrix, out Matrix4x4 inverted);
-            var vector = Vector3.Transform(input.ToVector3(), inverted);
+            var vector = Vector3.Transform(input.ToVector3(), this.conversionMatrix);
+
             return new LinearRgb(vector, this.TargetWorkingSpace);
         }
     }

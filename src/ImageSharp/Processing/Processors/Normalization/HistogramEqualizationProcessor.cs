5283c6db6d7c01f2d01a84547afe1d44cee26c72
Brian Popow <38701097+brianpopow@users.noreply.github.com>
2019-04-27 16:36:23 +0200

    Feature: adaptive histogram equalization (#673)
    * first version of sliding window adaptive histogram equalization
    
    * going now from top to bottom of the image, added more comments
    
    * using memory allocator to create the histogram and the cdf
    
    * mirroring rows which exceeds the borders
    
    * mirroring also left and right borders
    
    * gridsize and cliplimit are now parameters of the constructor
    
    * using Parallel.For
    
    * only applying clipping once, effect applying it multiple times is neglectable
    
    * added abstract base class for histogram equalization, added option to enable / disable clipping
    
    * small improvements
    
    * clipLimit now in percent of the total number of pixels in the grid
    
    * optimization: only calculating the cdf until the maximum histogram index
    
    * fix: using configuration from the parameter instead of the default
    
    * removed unnecessary loops in CalculateCdf, fixed typo in method name AddPixelsToHistogram
    
    * added different approach for ahe: image is split up in tiles, cdf is computed for each tile. Grey value will be determined by interpolating between 4 tiles
    
    * simplified interpolation between the tiles
    
    * number of tiles is now fixed and depended on the width and height of the image
    
    * moved calculating LUT's into separate method
    
    * number of tiles is now part of the options and will be used with the sliding window approach also, so both methods are comparable
    
    * removed no longer valid xml comment
    
    * attempt fixing the borders
    
    * refactoring to improve readability
    
    * linear interpolation in the border tiles
    
    * refactored processing the borders into separate methods
    
    * fixing corner tiles
    
    * fixed build errors
    
    * fixing mistake during merge from upstream: setting test images to "update Resize reference output because of improved ResizeKernelMap calculations"
    
    * using Parallel.ForEach for all inner tile calculations
    
    * using Parallel.ForEach to calculate the lookup tables
    
    * re-using pre allocated pixel row in GetPixelRow
    
    * fixed issue with the border tiles, when tile width != tile height
    
    * changed default value for ClipHistogram to false again
    
    * alpha channel from the original image is now preserved
    
    * added unit tests for adaptive histogram equalization
    
    * Update External
    
    * 2x faster adaptive tiled processor
    
    * Remove double indexing and bounds checks
    
    * Begin optimizing the global histogram
    
    * Parallelize GlobalHistogramEqualizationProcessor
    
    * Moving sliding window from left to right instead of from top to bottom
    
    * The tile width and height is again depended on the image width: image.Width / Tiles
    
    * Removed keeping track of the maximum histogram position
    
    * Updated reference image for sliding window AHE for moving the sliding window from left to right
    
    * Removed unnecessary call to Span.Clear(), all values are overwritten anyway
    
    * Revert "Moving sliding window from left to right instead of from top to bottom"
    
    This reverts commit 8f19e5edd23f13fd1ddf93b4e795f82e7f1334bb.
    
    # Conflicts:
    #	src/ImageSharp/Processing/Processors/Normalization/AdaptiveHistEqualizationSWProcessor.cs
    
    * Split GetPixelRow in two version: one which mirrors the edges (only needed in the borders of the images) and one which does not
    
    * Refactoring and cleanup sliding window processor
    
    * Added an upper limit of 100 tiles
    
    * Performance tweaks
    
    * Update External

--- src/ImageSharp/Processing/Processors/Normalization/HistogramEqualizationProcessor.cs
+++ src/ImageSharp/Processing/Processors/Normalization/HistogramEqualizationProcessor.cs
@@ -2,34 +2,38 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
-using System.Buffers;
 using System.Numerics;
 using System.Runtime.CompilerServices;
-using SixLabors.ImageSharp.Advanced;
-using SixLabors.ImageSharp.Memory;
+using System.Runtime.InteropServices;
 using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.Memory;
-using SixLabors.Primitives;
 
 namespace SixLabors.ImageSharp.Processing.Processors.Normalization
 {
     /// <summary>
-    /// Applies a global histogram equalization to the image.
+    /// Defines a processor that normalizes the histogram of an image.
     /// </summary>
     /// <typeparam name="TPixel">The pixel format.</typeparam>
-    internal class HistogramEqualizationProcessor<TPixel> : ImageProcessor<TPixel>
+    internal abstract class HistogramEqualizationProcessor<TPixel> : ImageProcessor<TPixel>
         where TPixel : struct, IPixel<TPixel>
     {
+        private readonly float luminanceLevelsFloat;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="HistogramEqualizationProcessor{TPixel}"/> class.
         /// </summary>
         /// <param name="luminanceLevels">The number of different luminance levels. Typical values are 256 for 8-bit grayscale images
         /// or 65536 for 16-bit grayscale images.</param>
-        public HistogramEqualizationProcessor(int luminanceLevels)
+        /// <param name="clipHistogram">Indicates, if histogram bins should be clipped.</param>
+        /// <param name="clipLimitPercentage">Histogram clip limit in percent of the total pixels in the tile. Histogram bins which exceed this limit, will be capped at this value.</param>
+        protected HistogramEqualizationProcessor(int luminanceLevels, bool clipHistogram, float clipLimitPercentage)
         {
             Guard.MustBeGreaterThan(luminanceLevels, 0, nameof(luminanceLevels));
+            Guard.MustBeGreaterThan(clipLimitPercentage, 0F, nameof(clipLimitPercentage));
 
             this.LuminanceLevels = luminanceLevels;
+            this.luminanceLevelsFloat = luminanceLevels;
+            this.ClipHistogramEnabled = clipHistogram;
+            this.ClipLimitPercentage = clipLimitPercentage;
         }
 
         /// <summary>
@@ -37,77 +41,77 @@ namespace SixLabors.ImageSharp.Processing.Processors.Normalization
         /// </summary>
         public int LuminanceLevels { get; }
 
-        /// <inheritdoc/>
-        protected override void OnFrameApply(ImageFrame<TPixel> source, Rectangle sourceRectangle, Configuration configuration)
-        {
-            MemoryAllocator memoryAllocator = configuration.MemoryAllocator;
-            int numberOfPixels = source.Width * source.Height;
-            Span<TPixel> pixels = source.GetPixelSpan();
+        /// <summary>
+        /// Gets a value indicating whether to clip the histogram bins at a specific value.
+        /// </summary>
+        public bool ClipHistogramEnabled { get; }
 
-            // Build the histogram of the grayscale levels.
-            using (IMemoryOwner<int> histogramBuffer = memoryAllocator.Allocate<int>(this.LuminanceLevels, AllocationOptions.Clean))
-            using (IMemoryOwner<int> cdfBuffer = memoryAllocator.Allocate<int>(this.LuminanceLevels, AllocationOptions.Clean))
-            {
-                Span<int> histogram = histogramBuffer.GetSpan();
-                for (int i = 0; i < pixels.Length; i++)
-                {
-                    TPixel sourcePixel = pixels[i];
-                    int luminance = this.GetLuminance(sourcePixel, this.LuminanceLevels);
-                    histogram[luminance]++;
-                }
-
-                // Calculate the cumulative distribution function, which will map each input pixel to a new value.
-                Span<int> cdf = cdfBuffer.GetSpan();
-                int cdfMin = this.CalculateCdf(cdf, histogram);
-
-                // Apply the cdf to each pixel of the image
-                float numberOfPixelsMinusCdfMin = numberOfPixels - cdfMin;
-                for (int i = 0; i < pixels.Length; i++)
-                {
-                    TPixel sourcePixel = pixels[i];
-
-                    int luminance = this.GetLuminance(sourcePixel, this.LuminanceLevels);
-                    float luminanceEqualized = cdf[luminance] / numberOfPixelsMinusCdfMin;
-
-                    pixels[i].FromVector4(new Vector4(luminanceEqualized));
-                }
-            }
-        }
+        /// <summary>
+        /// Gets the histogram clip limit in percent of the total pixels in the tile. Histogram bins which exceed this limit, will be capped at this value.
+        /// </summary>
+        public float ClipLimitPercentage { get; }
 
         /// <summary>
         /// Calculates the cumulative distribution function.
         /// </summary>
-        /// <param name="cdf">The array holding the cdf.</param>
-        /// <param name="histogram">The histogram of the input image.</param>
+        /// <param name="cdfBase">The reference to the array holding the cdf.</param>
+        /// <param name="histogramBase">The reference to the histogram of the input image.</param>
+        /// <param name="maxIdx">Index of the maximum of the histogram.</param>
         /// <returns>The first none zero value of the cdf.</returns>
-        private int CalculateCdf(Span<int> cdf, Span<int> histogram)
+        public int CalculateCdf(ref int cdfBase, ref int histogramBase, int maxIdx)
         {
-            // Calculate the cumulative histogram
             int histSum = 0;
-            for (int i = 0; i < histogram.Length; i++)
-            {
-                histSum += histogram[i];
-                cdf[i] = histSum;
-            }
-
-            // Get the first none zero value of the cumulative histogram
             int cdfMin = 0;
-            for (int i = 0; i < histogram.Length; i++)
-            {
-                if (cdf[i] != 0)
-                {
-                    cdfMin = cdf[i];
-                    break;
-                }
-            }
+            bool cdfMinFound = false;
 
-            // Creating the lookup table: subtracting cdf min, so we do not need to do that inside the for loop
-            for (int i = 0; i < histogram.Length; i++)
+            for (int i = 0; i <= maxIdx; i++)
             {
-                cdf[i] = Math.Max(0, cdf[i] - cdfMin);
+                histSum += Unsafe.Add(ref histogramBase, i);
+                if (!cdfMinFound && histSum != 0)
+                {
+                    cdfMin = histSum;
+                    cdfMinFound = true;
+                }
+
+                // Creating the lookup table: subtracting cdf min, so we do not need to do that inside the for loop.
+                Unsafe.Add(ref cdfBase, i) = Math.Max(0, histSum - cdfMin);
             }
 
             return cdfMin;
+        }
+
+        /// <summary>
+        /// AHE tends to over amplify the contrast in near-constant regions of the image, since the histogram in such regions is highly concentrated.
+        /// Clipping the histogram is meant to reduce this effect, by cutting of histogram bin's which exceed a certain amount and redistribute
+        /// the values over the clip limit to all other bins equally.
+        /// </summary>
+        /// <param name="histogram">The histogram to apply the clipping.</param>
+        /// <param name="clipLimitPercentage">Histogram clip limit in percent of the total pixels in the tile. Histogram bins which exceed this limit, will be capped at this value.</param>
+        /// <param name="pixelCount">The numbers of pixels inside the tile.</param>
+        public void ClipHistogram(Span<int> histogram, float clipLimitPercentage, int pixelCount)
+        {
+            int clipLimit = (int)MathF.Round(pixelCount * clipLimitPercentage);
+            int sumOverClip = 0;
+            ref int histogramBase = ref MemoryMarshal.GetReference(histogram);
+
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                ref int histogramLevel = ref Unsafe.Add(ref histogramBase, i);
+                if (histogramLevel > clipLimit)
+                {
+                    sumOverClip += histogramLevel - clipLimit;
+                    histogramLevel = clipLimit;
+                }
+            }
+
+            int addToEachBin = sumOverClip > 0 ? (int)MathF.Floor(sumOverClip / this.luminanceLevelsFloat) : 0;
+            if (addToEachBin > 0)
+            {
+                for (int i = 0; i < histogram.Length; i++)
+                {
+                    Unsafe.Add(ref histogramBase, i) += addToEachBin;
+                }
+            }
         }
 
         /// <summary>
@@ -116,13 +120,19 @@ namespace SixLabors.ImageSharp.Processing.Processors.Normalization
         /// <param name="sourcePixel">The pixel to get the luminance from</param>
         /// <param name="luminanceLevels">The number of luminance levels (256 for 8 bit, 65536 for 16 bit grayscale images)</param>
         [MethodImpl(InliningOptions.ShortMethod)]
-        private int GetLuminance(TPixel sourcePixel, int luminanceLevels)
+        public static int GetLuminance(TPixel sourcePixel, int luminanceLevels)
         {
-            // Convert to grayscale using ITU-R Recommendation BT.709
             var vector = sourcePixel.ToVector4();
-            int luminance = Convert.ToInt32(((.2126F * vector.X) + (.7152F * vector.Y) + (.0722F * vector.Y)) * (luminanceLevels - 1));
-
-            return luminance;
+            return GetLuminance(ref vector, luminanceLevels);
         }
+
+        /// <summary>
+        /// Convert the pixel values to grayscale using ITU-R Recommendation BT.709.
+        /// </summary>
+        /// <param name="vector">The vector to get the luminance from</param>
+        /// <param name="luminanceLevels">The number of luminance levels (256 for 8 bit, 65536 for 16 bit grayscale images)</param>
+        [MethodImpl(InliningOptions.ShortMethod)]
+        public static int GetLuminance(ref Vector4 vector, int luminanceLevels)
+            => (int)MathF.Round(((.2126F * vector.X) + (.7152F * vector.Y) + (.0722F * vector.Y)) * (luminanceLevels - 1));
     }
 }

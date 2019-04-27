570849085be1f3a449eb3aad73d5aab75778b783
BorisTheBrave <boris@boristhebrave.com>
2019-04-27 23:27:04 +0100

    ImageBrush shouldn't Dispose of the image it is using. (#883)
    Fixes #881

---
+++ tests/ImageSharp.Tests/Drawing/FillImageBrushTests.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Primitives;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Tests.TestUtilities.ImageComparison;
+using SixLabors.Primitives;
+using SixLabors.Shapes;
+
+using Xunit;
+
+// ReSharper disable InconsistentNaming
+
+namespace SixLabors.ImageSharp.Tests.Drawing
+{
+    [GroupOutput("Drawing")]
+    public class FillImageBrushTests
+    {
+        [Fact]
+        public void DoesNotDisposeImage()
+        {
+            using (var src = new Image<Rgba32>(5, 5))
+            {
+                var brush = new ImageBrush<Rgba32>(src);
+                using (var dest = new Image<Rgba32>(10, 10))
+                {
+                    dest.Mutate(c => c.Fill(brush, new Rectangle(0, 0, 10, 10)));
+                    dest.Mutate(c => c.Fill(brush, new Rectangle(0, 0, 10, 10)));
+                }
+            }
+        }
+    }
+}

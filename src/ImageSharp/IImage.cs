599765a7c60349dd868211c84de50f8c6d0e27f8
Anton Firszov <antonfir@gmail.com>
2019-05-01 17:41:36 +0200

    define IImageVisitor

--- src/ImageSharp/IImage.cs
+++ src/ImageSharp/IImage.cs
@@ -4,7 +4,7 @@
 using System;
 
 namespace SixLabors.ImageSharp
-{
+{   
     /// <summary>
     /// Encapsulates the properties and methods that describe an image.
     /// </summary>
---
+++ src/ImageSharp/Image.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Metadata;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace SixLabors.ImageSharp
+{
+    internal interface IImageVisitor
+    {
+        void Visit<TPixel>(Image<TPixel> image)
+            where TPixel : struct, IPixel<TPixel>;
+    }
+    
+    public abstract partial class Image : IImage
+    {
+        /// <inheritdoc/>
+        public PixelTypeInfo PixelType { get; }
+
+        /// <inheritdoc />
+        public abstract int Width { get; }
+
+        /// <inheritdoc />
+        public abstract int Height { get; }
+        
+        /// <inheritdoc/>
+        public ImageMetadata Metadata { get; }
+
+        protected Image(PixelTypeInfo pixelType, ImageMetadata metadata)
+        {
+            this.PixelType = pixelType;
+            this.Metadata = metadata ?? new ImageMetadata();
+        }
+
+        public abstract void Dispose();
+
+        internal abstract void ApplyVisitor(IImageVisitor visitor);
+    }
+}

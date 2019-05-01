e6fad9b6de486911d80d193cca96931094497df7
Anton Firszov <antonfir@gmail.com>
2019-05-01 23:41:36 +0200

    more cleanup

--- src/ImageSharp/Processing/Processors/Filters/FilterProcessor.cs
+++ src/ImageSharp/Processing/Processors/Filters/FilterProcessor.cs
@@ -1,5 +1,5 @@
-// // Copyright (c) Six Labors and contributors.
-// // Licensed under the Apache License, Version 2.0.
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
 
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Primitives;
@@ -9,7 +9,7 @@ namespace SixLabors.ImageSharp.Processing.Processors.Filters
     public class FilterProcessor : IImageProcessor
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="FilterProcessorImplementation{TPixel}"/> class.
+        /// Initializes a new instance of the <see cref="FilterProcessor"/> class.
         /// </summary>
         /// <param name="matrix">The matrix used to apply the image filter</param>
         public FilterProcessor(ColorMatrix matrix) => this.Matrix = matrix;

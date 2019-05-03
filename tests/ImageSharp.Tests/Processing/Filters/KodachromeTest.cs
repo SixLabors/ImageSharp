134d4845e237d1864a56b0b77c13ecc1a66e5a3f
Anton Firszov <antonfir@gmail.com>
2019-05-04 01:26:10 +0200

    re-add and fix filter invocation tests

---
+++ tests/ImageSharp.Tests/Processing/Filters/KodachromeTest.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Filters;
+using Xunit;
+
+namespace SixLabors.ImageSharp.Tests.Processing.Filters
+{
+    public class KodachromeTest : BaseImageOperationsExtensionTest
+    {
+        [Fact]
+        public void Kodachrome_amount_KodachromeProcessorDefaultsSet()
+        {
+            this.operations.Kodachrome();
+            var processor = this.Verify<KodachromeProcessor>();
+        }
+
+        [Fact]
+        public void Kodachrome_amount_rect_KodachromeProcessorDefaultsSet()
+        {
+            this.operations.Kodachrome(this.rect);
+            var processor = this.Verify<KodachromeProcessor>(this.rect);
+        }
+    }
+}
---
+++ tests/ImageSharp.Tests/Processing/Filters/OpacityTest.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Filters;
+using Xunit;
+
+namespace SixLabors.ImageSharp.Tests.Processing.Effects
+{
+    public class OpacityTest : BaseImageOperationsExtensionTest
+    {
+        [Fact]
+        public void Alpha_amount_AlphaProcessorDefaultsSet()
+        {
+            this.operations.Opacity(0.2f);
+            OpacityProcessor processor = this.Verify<OpacityProcessor>();
+
+            Assert.Equal(.2f, processor.Amount);
+        }
+
+        [Fact]
+        public void Alpha_amount_rect_AlphaProcessorDefaultsSet()
+        {
+            this.operations.Opacity(0.6f, this.rect);
+            OpacityProcessor processor = this.Verify<OpacityProcessor>(this.rect);
+
+            Assert.Equal(.6f, processor.Amount);
+        }
+    }
+}

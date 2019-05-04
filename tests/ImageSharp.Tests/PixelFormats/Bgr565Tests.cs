f21056e6c4f1a8e5a7c9a372177c69808e3394c3
Brian Popow <popow@gmx.de>
2019-05-04 21:05:25 +0200

    Add more pixel conversion tests

--- tests/ImageSharp.Tests/PixelFormats/Bgr565Tests.cs
+++ tests/ImageSharp.Tests/PixelFormats/Bgr565Tests.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-using System;
 using System.Numerics;
 using SixLabors.ImageSharp.PixelFormats;
 using Xunit;

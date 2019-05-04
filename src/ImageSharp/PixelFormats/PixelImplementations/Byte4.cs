65dc0ab53a6e053768089806b87638fe6504d26f
Brian Popow <popow@gmx.de>
2019-05-04 21:05:25 +0200

    Add more pixel conversion tests

--- src/ImageSharp/PixelFormats/PixelImplementations/Byte4.cs
+++ src/ImageSharp/PixelFormats/PixelImplementations/Byte4.cs
@@ -149,7 +149,7 @@ namespace SixLabors.ImageSharp.PixelFormats
         public override string ToString()
         {
             var vector = this.ToVector4();
-            return FormattableString.Invariant($"Bgra5551({vector.X:#0.##}, {vector.Y:#0.##}, {vector.Z:#0.##}, {vector.W:#0.##})");
+            return FormattableString.Invariant($"Byte4({vector.X:#0.##}, {vector.Y:#0.##}, {vector.Z:#0.##}, {vector.W:#0.##})");
         }
 
         /// <summary>

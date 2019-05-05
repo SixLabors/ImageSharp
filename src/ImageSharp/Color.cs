a2ca7da03d4eaf36a513bc6aab7b9121ac7ed1dd
Anton Firszov <antonfir@gmail.com>
2019-05-06 00:41:19 +0200

    basic Color methods

--- src/ImageSharp/Color.cs
+++ src/ImageSharp/Color.cs
@@ -7,9 +7,9 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace SixLabors.ImageSharp
 {
-    public struct Color
+    public readonly struct Color
     {
-        private Rgba64 data;
+        private readonly Rgba64 data;
 
         public Color(Rgba64 pixel)
         {
@@ -21,11 +21,54 @@ namespace SixLabors.ImageSharp
             this.data = new Rgba64(pixel);
         }
 
+        public Color(Argb32 pixel)
+        {
+            this.data = new Rgba64(pixel);
+        }
+
+        public Color(Bgra32 pixel)
+        {
+            this.data = new Rgba64(pixel);
+        }
+
+        public Color(Rgb24 pixel)
+        {
+            this.data = new Rgba64(pixel);
+        }
+
+        public Color(Bgr24 pixel)
+        {
+            this.data = new Rgba64(pixel);
+        }
+
         public Color(Vector4 vector)
         {
-            this.data = default;
-            this.data.FromVector4(vector);
+            this.data = new Rgba64(vector);
         }
+
+        public static implicit operator Color(Rgba64 source) => new Color(source);
+
+        public static implicit operator Color(Rgba32 source) => new Color(source);
+
+        public static implicit operator Color(Bgra32 source) => new Color(source);
+
+        public static implicit operator Color(Argb32 source) => new Color(source);
+
+        public static implicit operator Color(Rgb24 source) => new Color(source);
+
+        public static implicit operator Color(Bgr24 source) => new Color(source);
+
+        public static implicit operator Rgba64(Color color) => color.data;
+
+        public static implicit operator Rgba32(Color color) => color.data.ToRgba32();
+
+        public static implicit operator Bgra32(Color color) => color.data.ToBgra32();
+
+        public static implicit operator Argb32(Color color) => color.data.ToArgb32();
+
+        public static implicit operator Rgb24(Color color) => color.data.ToRgb24();
+
+        public static implicit operator Bgr24(Color color) => color.data.ToBgr24();
 
         public static Color FromRgba(byte r, byte g, byte b, byte a) => new Color(new Rgba32(r, g, b, a));
 

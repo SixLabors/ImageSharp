e5e3768f5a369e35cb965313128912d98c92db6f
Anton Firszov <antonfir@gmail.com>
2019-05-06 22:58:17 +0200

    Implement WernerPalette and WebSafePalette for Color

--- src/ImageSharp/Color/Color.NamedColors.cs
+++ src/ImageSharp/Color/Color.NamedColors.cs
@@ -1,5 +1,6 @@
 // // Copyright (c) Six Labors and contributors.
 // // Licensed under the Apache License, Version 2.0.
+
 namespace SixLabors.ImageSharp
 {
     public readonly partial struct Color

3f24ceedf7830133306a5c2e2f9fd3fab35263e1
Sergio Pedri <sergio0694@live.com>
2019-08-01 23:36:42 +0200

    Added BokehBlurExecutionMode enum

---
+++ src/ImageSharp/Processing/Processors/Convolution/Parameters/BokehBlurExecutionMode.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.Processing.Processors.Convolution.Parameters
+{
+    /// <summary>
+    /// An <see langword="enum"/> that indicates execution options for the <see cref="BokehBlurProcessor"/>.
+    /// </summary>
+    public enum BokehBlurExecutionMode
+    {
+        /// <summary>
+        /// Indicates that the maximum performance should be prioritized over memory usage.
+        /// </summary>
+        PreferMaximumPerformance,
+
+        /// <summary>
+        /// Indicates that the memory usage should be prioritized over raw performance.
+        /// </summary>
+        PreferLowMemoryUsage
+    }
+}

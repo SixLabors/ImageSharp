1cb6c7810c87dec9f4b50b1262fc62310d459d79
Simon Cropp <simon.cropp@gmail.com>
2019-08-15 19:31:53 +1000

    re add ImageProcessingException ctor
    only used in release

--- src/ImageSharp/Common/Exceptions/ImageProcessingException.cs
+++ src/ImageSharp/Common/Exceptions/ImageProcessingException.cs
@@ -19,5 +19,17 @@ namespace SixLabors.ImageSharp
             : base(errorMessage)
         {
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageProcessingException"/> class with a specified
+        /// error message and the exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="errorMessage">The error message that explains the reason for this exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic)
+        /// if no inner exception is specified.</param>
+        public ImageProcessingException(string errorMessage, Exception innerException)
+            : base(errorMessage, innerException)
+        {
+        }
     }
 }

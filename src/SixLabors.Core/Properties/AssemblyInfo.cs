b2091dde048fcef823ef0cdb8c1901c959a8cef8
Anton Firszov <antonfir@gmail.com>
2019-08-18 18:08:43 +0200

    [SL.Core] do not share internals across SixLabors projects

--- src/SixLabors.Core/Properties/AssemblyInfo.cs
+++ src/SixLabors.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,3 @@ using System.Runtime.CompilerServices;
 
 // Ensure the internals can be tested.
 [assembly: InternalsVisibleTo("SixLabors.Core.Tests")]
-
-// Ensure the internals are visible to the other projects.
-[assembly: InternalsVisibleTo("SixLabors.Exif")]
-[assembly: InternalsVisibleTo("SixLabors.Fonts")]
-[assembly: InternalsVisibleTo("SixLabors.ImageSharp")]
-[assembly: InternalsVisibleTo("SixLabors.ImageSharp.Drawing")]
-[assembly: InternalsVisibleTo("SixLabors.Shapes")]
-[assembly: InternalsVisibleTo("SixLabors.Shapes.Text")]

f95b8b972f39198ac4f8448a3eb816a4a96d702c
Sergio Pedri <sergio0694@live.com>
2019-08-19 12:52:13 +0200

    Fixed leftover dithering processor constructor

--- src/ImageSharp/Processing/Processors/Dithering/ErrorDiffusionPaletteProcessor{TPixel}.cs
+++ src/ImageSharp/Processing/Processors/Dithering/ErrorDiffusionPaletteProcessor{TPixel}.cs
@@ -16,8 +16,14 @@ namespace SixLabors.ImageSharp.Processing.Processors.Dithering
     internal class ErrorDiffusionPaletteProcessor<TPixel> : PaletteDitherProcessor<TPixel>
         where TPixel : struct, IPixel<TPixel>
     {
-        public ErrorDiffusionPaletteProcessor(ErrorDiffusionPaletteProcessor definition)
-            : base(definition)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorDiffusionPaletteProcessor{TPixel}"/> class.
+        /// </summary>
+        /// <param name="definition">The <see cref="ErrorDiffusionPaletteProcessor"/> defining the processor parameters.</param>
+        /// <param name="image">The target <see cref="Image{T}"/> for the current processor instance.</param>
+        /// <param name="rectangle">The target area to process for the current processor instance.</param>
+        public ErrorDiffusionPaletteProcessor(ErrorDiffusionPaletteProcessor definition, Image<TPixel> image, Rectangle rectangle)
+            : base(definition, image, rectangle)
         {
         }
 

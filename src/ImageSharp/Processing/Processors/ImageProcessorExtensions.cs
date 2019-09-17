e609108545a540e0152cb7a088b2d767d29ecb51
James Jackson-South <james_south@hotmail.com>
2019-09-17 16:45:05 +1000

    Expose visitor through advanced namespace.

--- src/ImageSharp/Processing/Processors/ImageProcessorExtensions.cs
+++ src/ImageSharp/Processing/Processors/ImageProcessorExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
+using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.Primitives;
 

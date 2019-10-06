38589a0eb825c89c8b589294cce17b571341f2a1
Brian Popow <popow@gmx.de>
2019-10-06 16:32:32 +0200

    Add support for decoding 8, 16, 32 bit tga files

---
+++ src/ImageSharp/Formats/Tga/TgaThrowHelper.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace SixLabors.ImageSharp.Formats.Tga
+{
+    internal static class TgaThrowHelper
+    {
+        /// <summary>
+        /// Cold path optimization for throwing <see cref="NotSupportedException"/>-s
+        /// </summary>
+        /// <param name="errorMessage">The error message for the exception.</param>
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowNotSupportedException(string errorMessage)
+        {
+            throw new NotSupportedException(errorMessage);
+        }
+    }
+}

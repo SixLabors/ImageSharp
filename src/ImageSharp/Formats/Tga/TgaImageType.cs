f6cce786ea0f8d66b42acc122304443e454f30b5
Brian Popow <popow@gmx.de>
2019-10-08 19:13:01 +0200

    Add decoding of 24bit RLE tga images

--- src/ImageSharp/Formats/Tga/TgaImageType.cs
+++ src/ImageSharp/Formats/Tga/TgaImageType.cs
@@ -1,7 +1,8 @@
 // Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-namespace SixLabors.ImageSharp.Formats.Tga
+namespace SixLabors.
+    ImageSharp.Formats.Tga
 {
     /// <summary>
     /// Defines the tga image type. The TGA File Format can be used to store Pseudo-Color,
@@ -44,5 +45,26 @@ namespace SixLabors.ImageSharp.Formats.Tga
         /// Run length encoded, black and white (grayscale) image.
         /// </summary>
         RleBlackAndWhite = 11,
+    }
+
+    /// <summary>
+    /// Extension methods for TgaImageType enum.
+    /// </summary>
+    public static class TgaImageTypeExtensions
+    {
+        /// <summary>
+        /// Checks if this tga image type is run length encoded.
+        /// </summary>
+        /// <param name="imageType">The tga image type.</param>
+        /// <returns>True, if this image type is run length encoded, otherwise false.</returns>
+        public static bool IsRunLengthEncoded(this TgaImageType imageType)
+        {
+            if (imageType is TgaImageType.RleColorMapped || imageType is TgaImageType.RleBlackAndWhite || imageType is TgaImageType.RleTrueColor)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }

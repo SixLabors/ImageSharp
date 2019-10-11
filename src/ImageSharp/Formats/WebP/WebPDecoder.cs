4e7b722d791e0449cda09a462391b10ac7c502e0
Brian Popow <popow@gmx.de>
2019-10-12 00:03:10 +0200

    Add empty implementation of IImageDecoder to test github colaboration

--- src/ImageSharp/Formats/WebP/WebPDecoder.cs
+++ src/ImageSharp/Formats/WebP/WebPDecoder.cs
@@ -1,10 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System.IO;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace SixLabors.ImageSharp.Formats.WebP
 {
+    /// <summary>
+    /// Image decoder for generating an image out of a webp stream.
+    /// </summary>
     public sealed class WebPDecoder : IImageDecoder
     {
+        /// <inheritdoc/>
+        public Image<TPixel> Decode<TPixel>(Configuration configuration, Stream stream)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public Image Decode(Configuration configuration, Stream stream)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }

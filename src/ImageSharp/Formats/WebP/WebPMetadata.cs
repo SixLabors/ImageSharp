1462dd22516d3e572fd24128f4ed701fccfa8008
Peter Amrehn <peter.wendorff@gmx.de>
2019-10-11 22:39:01 +0200

    Initial Commit towards WebP

---
+++ src/ImageSharp/Formats/WebP/WebPMetadata.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+
+namespace SixLabors.ImageSharp.Formats.WebP
+{
+    /// <summary>
+    /// Provides WebP specific metadata information for the image.
+    /// </summary>
+    public class WebPMetadata : IDeepCloneable
+    {
+        /// <inheritdoc/>
+        public IDeepCloneable DeepClone() => throw new NotImplementedException();
+    }
+}

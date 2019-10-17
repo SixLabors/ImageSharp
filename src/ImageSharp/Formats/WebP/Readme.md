93131afe45f82e394fd009341dd5fea8c02c9e1a
Lajos Marton <lajosmarton@hotmail.com>
2019-10-17 22:38:47 +0200

    WebP Identify test added, webp sample files copied from official samples repository

--- src/ImageSharp/Formats/WebP/Readme.md
+++ src/ImageSharp/Formats/WebP/Readme.md
@@ -6,3 +6,4 @@ Reference implementation, specification and stuff like that:
 - [WebP Spec 1.0.3](https://chromium.googlesource.com/webm/libwebp/+/v1.0.3/doc/webp-container-spec.txt)
 - [WebP VP8 chunk Spec](http://tools.ietf.org/html/rfc6386)
 - [WebP filefront](https://wiki.fileformat.com/image/webp/)
+- [WebP test data](https://github.com/webmproject/libwebp-test-data/)
--- tests/ImageSharp.Tests/Formats/WebP/WebPDecoderTests.cs
+++ tests/ImageSharp.Tests/Formats/WebP/WebPDecoderTests.cs
@@ -2,25 +2,18 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System.IO;
-using SixLabors.ImageSharp.PixelFormats;
 using Xunit;
 
 // ReSharper disable InconsistentNaming
 
 namespace SixLabors.ImageSharp.Tests.Formats.WebP
 {
-    using SixLabors.ImageSharp.Formats.WebP;
     using SixLabors.ImageSharp.Metadata;
+    using static SixLabors.ImageSharp.Tests.TestImages.WebP;
     using static TestImages.Bmp;
 
     public class WebPDecoderTests
     {
-        public const PixelTypes CommonNonDefaultPixelTypes = PixelTypes.Rgba32 | PixelTypes.Bgra32 | PixelTypes.RgbaVector;
-
-        public static readonly string[] MiscBmpFiles = Miscellaneous;
-
-        public static readonly string[] BitfieldsBmpFiles = BitFields;
-
         public static readonly TheoryData<string, int, int, PixelResolutionUnit> RatioFiles =
         new TheoryData<string, int, int, PixelResolutionUnit>
         {
@@ -30,72 +23,16 @@ namespace SixLabors.ImageSharp.Tests.Formats.WebP
         };
 
         [Theory]
-        [WithFileCollection(nameof(MiscBmpFiles), PixelTypes.Rgba32)]
-        public void BmpDecoder_CanDecode_MiscellaneousBitmaps<TPixel>(TestImageProvider<TPixel> provider)
-            where TPixel : struct, IPixel<TPixel>
-        {
-            using (Image<TPixel> image = provider.GetImage(new WebPDecoder()))
-            {
-                image.DebugSave(provider);
-                if (TestEnvironment.IsWindows)
-                {
-                    image.CompareToOriginal(provider);
-                }
-            }
-        }
-
-        [Theory]
-        [WithFile(Bit16Inverted, PixelTypes.Rgba32)]
-        [WithFile(Bit8Inverted, PixelTypes.Rgba32)]
-        public void BmpDecoder_CanDecode_Inverted<TPixel>(TestImageProvider<TPixel> provider)
-            where TPixel : struct, IPixel<TPixel>
-        {
-            using (Image<TPixel> image = provider.GetImage(new WebPDecoder()))
-            {
-                image.DebugSave(provider);
-                image.CompareToOriginal(provider);
-            }
-        }
-
-        [Theory]
-        [InlineData(Bit32Rgb, 32)]
-        [InlineData(Bit32Rgba, 32)]
-        [InlineData(Car, 24)]
-        [InlineData(F, 24)]
-        [InlineData(NegHeight, 24)]
-        [InlineData(Bit16, 16)]
-        [InlineData(Bit16Inverted, 16)]
-        [InlineData(Bit8, 8)]
-        [InlineData(Bit8Inverted, 8)]
-        [InlineData(Bit4, 4)]
-        [InlineData(Bit1, 1)]
-        [InlineData(Bit1Pal1, 1)]
-        public void Identify_DetectsCorrectPixelType(string imagePath, int expectedPixelSize)
+        [InlineData(Lossless.Lossless1, 1000, 307)]
+        public void Identify_DetectsCorrectDimensions(string imagePath, int expectedWidth, int expectedHeight)
         {
             var testFile = TestFile.Create(imagePath);
             using (var stream = new MemoryStream(testFile.Bytes, false))
             {
                 IImageInfo imageInfo = Image.Identify(stream);
                 Assert.NotNull(imageInfo);
-                Assert.Equal(expectedPixelSize, imageInfo.PixelType?.BitsPerPixel);
-            }
-        }
-
-        [Theory]
-        [MemberData(nameof(RatioFiles))]
-        public void Decode_VerifyRatio(string imagePath, int xResolution, int yResolution, PixelResolutionUnit resolutionUnit)
-        {
-            var testFile = TestFile.Create(imagePath);
-            using (var stream = new MemoryStream(testFile.Bytes, false))
-            {
-                var decoder = new WebPDecoder();
-                using (Image<Rgba32> image = decoder.Decode<Rgba32>(Configuration.Default, stream))
-                {
-                    ImageMetadata meta = image.Metadata;
-                    Assert.Equal(xResolution, meta.HorizontalResolution);
-                    Assert.Equal(yResolution, meta.VerticalResolution);
-                    Assert.Equal(resolutionUnit, meta.ResolutionUnits);
-                }
+                Assert.Equal(expectedWidth, imageInfo.Width);
+                Assert.Equal(expectedHeight, imageInfo.Height);
             }
         }
     }

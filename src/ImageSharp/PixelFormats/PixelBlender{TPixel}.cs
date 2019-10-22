c57012eb2fb16e6220a274f1995d383c81366039
James Jackson-South <james_south@hotmail.com>
2019-10-22 18:01:05 +1100

    Fix ordering

--- src/ImageSharp/PixelFormats/PixelBlender{TPixel}.cs
+++ src/ImageSharp/PixelFormats/PixelBlender{TPixel}.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-using SixLabors.ImageSharp.Memory;
 using System;
 using System.Buffers;
 using System.Numerics;
+using SixLabors.ImageSharp.Memory;
 
 namespace SixLabors.ImageSharp.PixelFormats
 {

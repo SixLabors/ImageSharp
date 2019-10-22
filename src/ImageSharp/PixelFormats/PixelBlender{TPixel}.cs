87b9de471d8ec879dfc9bbf62a21a652adc9abec
James Jackson-South <james_south@hotmail.com>
2019-10-22 15:59:38 +1100

    Update PixelBlender{TPixel}.cs

--- src/ImageSharp/PixelFormats/PixelBlender{TPixel}.cs
+++ src/ImageSharp/PixelFormats/PixelBlender{TPixel}.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
+using SixLabors.ImageSharp.Memory;
 using System;
 using System.Buffers;
 using System.Numerics;
-using SixLabors.ImageSharp.Memory;
 
 namespace SixLabors.ImageSharp.PixelFormats
 {
@@ -26,6 +26,49 @@ namespace SixLabors.ImageSharp.PixelFormats
         /// </param>
         /// <returns>The final pixel value after composition.</returns>
         public abstract TPixel Blend(TPixel background, TPixel source, float amount);
+
+        /// <summary>
+        /// Blends 2 rows together
+        /// </summary>
+        /// <typeparam name="TPixelSrc">the pixel format of the source span</typeparam>
+        /// <param name="configuration"><see cref="Configuration"/> to use internally</param>
+        /// <param name="destination">the destination span</param>
+        /// <param name="background">the background span</param>
+        /// <param name="source">the source span</param>
+        /// <param name="amount">
+        /// A value between 0 and 1 indicating the weight of the second source vector.
+        /// At amount = 0, "background" is returned, at amount = 1, "source" is returned.
+        /// </param>
+        public void Blend<TPixelSrc>(
+            Configuration configuration,
+            Span<TPixel> destination,
+            ReadOnlySpan<TPixel> background,
+            ReadOnlySpan<TPixelSrc> source,
+            float amount)
+            where TPixelSrc : struct, IPixel<TPixelSrc>
+        {
+            Guard.MustBeGreaterThanOrEqualTo(background.Length, destination.Length, nameof(background.Length));
+            Guard.MustBeGreaterThanOrEqualTo(source.Length, destination.Length, nameof(source.Length));
+            Guard.MustBeBetweenOrEqualTo(amount, 0, 1, nameof(amount));
+
+            using (IMemoryOwner<Vector4> buffer =
+                configuration.MemoryAllocator.Allocate<Vector4>(destination.Length * 3))
+            {
+                Span<Vector4> destinationSpan = buffer.Slice(0, destination.Length);
+                Span<Vector4> backgroundSpan = buffer.Slice(destination.Length, destination.Length);
+                Span<Vector4> sourceSpan = buffer.Slice(destination.Length * 2, destination.Length);
+
+                ReadOnlySpan<TPixel> sourcePixels = background.Slice(0, background.Length);
+                PixelOperations<TPixel>.Instance.ToVector4(configuration, sourcePixels, backgroundSpan, PixelConversionModifiers.Scale);
+                ReadOnlySpan<TPixelSrc> sourcePixels1 = source.Slice(0, background.Length);
+                PixelOperations<TPixelSrc>.Instance.ToVector4(configuration, sourcePixels1, sourceSpan, PixelConversionModifiers.Scale);
+
+                this.BlendFunction(destinationSpan, backgroundSpan, sourceSpan, amount);
+
+                Span<Vector4> sourceVectors = destinationSpan.Slice(0, background.Length);
+                PixelOperations<TPixel>.Instance.FromVector4Destructive(configuration, sourceVectors, destination, PixelConversionModifiers.Scale);
+            }
+        }
 
         /// <summary>
         /// Blends 2 rows together
@@ -69,49 +112,6 @@ namespace SixLabors.ImageSharp.PixelFormats
             Guard.MustBeGreaterThanOrEqualTo(background.Length, destination.Length, nameof(background.Length));
             Guard.MustBeGreaterThanOrEqualTo(source.Length, destination.Length, nameof(source.Length));
             Guard.MustBeGreaterThanOrEqualTo(amount.Length, destination.Length, nameof(amount.Length));
-
-            using (IMemoryOwner<Vector4> buffer =
-                configuration.MemoryAllocator.Allocate<Vector4>(destination.Length * 3))
-            {
-                Span<Vector4> destinationSpan = buffer.Slice(0, destination.Length);
-                Span<Vector4> backgroundSpan = buffer.Slice(destination.Length, destination.Length);
-                Span<Vector4> sourceSpan = buffer.Slice(destination.Length * 2, destination.Length);
-
-                ReadOnlySpan<TPixel> sourcePixels = background.Slice(0, background.Length);
-                PixelOperations<TPixel>.Instance.ToVector4(configuration, sourcePixels, backgroundSpan, PixelConversionModifiers.Scale);
-                ReadOnlySpan<TPixelSrc> sourcePixels1 = source.Slice(0, background.Length);
-                PixelOperations<TPixelSrc>.Instance.ToVector4(configuration, sourcePixels1, sourceSpan, PixelConversionModifiers.Scale);
-
-                this.BlendFunction(destinationSpan, backgroundSpan, sourceSpan, amount);
-
-                Span<Vector4> sourceVectors = destinationSpan.Slice(0, background.Length);
-                PixelOperations<TPixel>.Instance.FromVector4Destructive(configuration, sourceVectors, destination, PixelConversionModifiers.Scale);
-            }
-        }
-
-        /// <summary>
-        /// Blends 2 rows together
-        /// </summary>
-        /// <typeparam name="TPixelSrc">the pixel format of the source span</typeparam>
-        /// <param name="configuration"><see cref="Configuration"/> to use internally</param>
-        /// <param name="destination">the destination span</param>
-        /// <param name="background">the background span</param>
-        /// <param name="source">the source span</param>
-        /// <param name="amount">
-        /// A value between 0 and 1 indicating the weight of the second source vector.
-        /// At amount = 0, "background" is returned, at amount = 1, "source" is returned.
-        /// </param>
-        public void Blend<TPixelSrc>(
-            Configuration configuration,
-            Span<TPixel> destination,
-            ReadOnlySpan<TPixel> background,
-            ReadOnlySpan<TPixelSrc> source,
-            float amount)
-            where TPixelSrc : struct, IPixel<TPixelSrc>
-        {
-            Guard.MustBeGreaterThanOrEqualTo(background.Length, destination.Length, nameof(background.Length));
-            Guard.MustBeGreaterThanOrEqualTo(source.Length, destination.Length, nameof(source.Length));
-            Guard.MustBeBetweenOrEqualTo(amount, 0, 1, nameof(amount));
 
             using (IMemoryOwner<Vector4> buffer =
                 configuration.MemoryAllocator.Allocate<Vector4>(destination.Length * 3))

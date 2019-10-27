862018228d0e519838ab70161397af3682f5f660
Brian Popow <popow@gmx.de>
2019-10-27 12:29:12 +0100

    Add tests for bmp and tga header to throw UnknownImageFormatException when there is insufficient data

--- tests/ImageSharp.Tests/Formats/Tga/TgaFileHeaderTests.cs
+++ tests/ImageSharp.Tests/Formats/Tga/TgaFileHeaderTests.cs
@@ -1,32 +1,19 @@
 // Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
-using System;
 using System.IO;
 
 using SixLabors.ImageSharp.Formats;
-using SixLabors.ImageSharp.Formats.Bmp;
+
 using Xunit;
 
-namespace SixLabors.ImageSharp.Tests.Formats.Bmp
+namespace SixLabors.ImageSharp.Tests.Formats.Tga
 {
-    public class BmpFileHeaderTests
+    public class TgaFileHeaderTests
     {
-        private static readonly byte[] Data = BitConverter.GetBytes(BmpConstants.TypeMarkers.Bitmap);
+        private static readonly byte[] Data = { 0, 0, 0 };
 
         private MemoryStream Stream { get; } = new MemoryStream(Data);
-
-        [Fact]
-        public void TestWrite()
-        {
-            var header = new BmpFileHeader(1, 2, 3, 4);
-
-            var buffer = new byte[14];
-
-            header.WriteTo(buffer);
-
-            Assert.Equal("AQACAAAAAwAAAAQAAAA=", Convert.ToBase64String(buffer));
-        }
 
         [Fact]
         public void ImageLoad_WithoutEnoughData_Throws_UnknownImageFormatException()

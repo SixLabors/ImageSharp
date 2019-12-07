bd8feda6697491c314dd4f285008530707093f9a
Brian Popow <popow@gmx.de>
2019-12-07 17:18:11 +0100

    Fix more decoding bugs, lossless_vec_2_0.webp.webp now decodes without error

---
+++ src/ImageSharp/Formats/WebP/Vp8LDecoder.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.Formats.WebP
+{
+    internal class Vp8LDecoder
+    {
+        public Vp8LDecoder(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Metadata = new Vp8LMetadata();
+        }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public Vp8LMetadata Metadata { get; set; }
+    }
+}

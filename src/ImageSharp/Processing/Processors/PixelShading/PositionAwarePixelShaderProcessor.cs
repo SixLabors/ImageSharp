4a193aa5ea2e0ca3fe19ca43218f2831864a817d
Sergio Pedri <sergio0694@live.com>
2019-12-20 14:20:23 +0100

    Added PositionAwarePixelShader types

---
+++ src/ImageSharp/Processing/Processors/PixelShading/PositionAwarePixelShaderProcessor.cs
@@ -0,0 +1,38 @@
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing.Delegates;
+using SixLabors.Primitives;
+
+namespace SixLabors.ImageSharp.Processing.Processors.PixelShading
+{
+    /// <summary>
+    /// Applies a user defined pixel shader effect through a given delegate.
+    /// </summary>
+    public sealed class PositionAwarePixelShaderProcessor : IImageProcessor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PositionAwarePixelShaderProcessor"/> class.
+        /// </summary>
+        /// <param name="pixelShader">
+        /// The user defined pixel shader to use to modify images.
+        /// </param>
+        public PositionAwarePixelShaderProcessor(PositionAwarePixelShader pixelShader)
+        {
+            this.PixelShader = pixelShader;
+        }
+
+        /// <summary>
+        /// Gets the user defined pixel shader.
+        /// </summary>
+        public PositionAwarePixelShader PixelShader { get; }
+
+        /// <inheritdoc />
+        public IImageProcessor<TPixel> CreatePixelSpecificProcessor<TPixel>(Image<TPixel> source, Rectangle sourceRectangle)
+            where TPixel : struct, IPixel<TPixel>
+        {
+            return new PositionAwarePixelShader<TPixel>(this, source, sourceRectangle);
+        }
+    }
+}

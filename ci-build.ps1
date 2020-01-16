8ec58b07908cf7d775b1a55e9ff4758d4c20476b
James Jackson-South <james_south@hotmail.com>
2020-01-16 16:51:48 +1100

    Cleanup

---
+++ ci-build.ps1
@@ -0,0 +1,19 @@
+param(
+  [Parameter(Mandatory, Position = 0)]
+  [string]$version,
+  [string]$targetFramework = 'ALL'
+)
+
+dotnet clean -c Release
+
+$repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
+if ($targetFramework -ne 'ALL') {
+
+  # Building for a specific framework.
+  dotnet build -c Release -f $targetFramework /p:packageversion=$version /p:RepositoryUrl=$repositoryUrl
+}
+else {
+
+  # Building for packing and publishing.
+  dotnet pack -c Release --output "$PSScriptRoot/artifacts" /p:packageversion=$version /p:RepositoryUrl=$repositoryUrl
+}

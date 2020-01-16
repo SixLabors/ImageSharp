a9a4b9ea92c2db5e9f876c006cec7059aab37b28
Peter Amrehn <peter.wendorff@gmx.de>
2020-01-16 19:54:54 +0100

    code cleanup

--- src/ImageSharp/Formats/WebP/WebPDecoderBase.cs
+++ src/ImageSharp/Formats/WebP/WebPDecoderBase.cs
@@ -1,5 +1,5 @@
-﻿// // Copyright (c) Six Labors and contributors.
-// // Licensed under the Apache License, Version 2.0.
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
 
 using System;
 using System.Buffers;

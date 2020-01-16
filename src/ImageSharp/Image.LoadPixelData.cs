27bf6ca3a478c5e68945a55be7dedc678f139b45
Peter Tribe <peter.tribe@ukoolabs.com>
2020-01-16 15:00:42 -0800

    Update: added row skip property

--- src/ImageSharp/Image.LoadPixelData.cs
+++ src/ImageSharp/Image.LoadPixelData.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Six Labors and contributors.
+// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
 using System;
@@ -19,11 +19,12 @@ namespace SixLabors.ImageSharp
         /// <param name="data">The byte array containing image data.</param>
         /// <param name="width">The width of the final image.</param>
         /// <param name="height">The height of the final image.</param>
+        /// <param name="rowSkip">Pixels to skip for each row, defaults to 0.</param>
         /// <typeparam name="TPixel">The pixel format.</typeparam>
         /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
-        public static Image<TPixel> LoadPixelData<TPixel>(TPixel[] data, int width, int height)
+        public static Image<TPixel> LoadPixelData<TPixel>(TPixel[] data, int width, int height, int rowSkip)
             where TPixel : struct, IPixel<TPixel>
-            => LoadPixelData(Configuration.Default, data, width, height);
+            => LoadPixelData(Configuration.Default, data, width, height, rowSkip);
 
         /// <summary>
         /// Create a new instance of the <see cref="Image{TPixel}"/> class from the raw <typeparamref name="TPixel"/> data.
@@ -31,11 +32,12 @@ namespace SixLabors.ImageSharp
         /// <param name="data">The byte array containing image data.</param>
         /// <param name="width">The width of the final image.</param>
         /// <param name="height">The height of the final image.</param>
+        /// <param name="rowSkip">Pixels to skip for each row, defaults to 0.</param>
         /// <typeparam name="TPixel">The pixel format.</typeparam>
         /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
-        public static Image<TPixel> LoadPixelData<TPixel>(ReadOnlySpan<TPixel> data, int width, int height)
+        public static Image<TPixel> LoadPixelData<TPixel>(ReadOnlySpan<TPixel> data, int width, int height, int rowSkip)
             where TPixel : struct, IPixel<TPixel>
-            => LoadPixelData(Configuration.Default, data, width, height);
+            => LoadPixelData(Configuration.Default, data, width, height, rowSkip);
 
         /// <summary>
         /// Create a new instance of the <see cref="Image{TPixel}"/> class from the given byte array in <typeparamref name="TPixel"/> format.
@@ -43,11 +45,12 @@ namespace SixLabors.ImageSharp
         /// <param name="data">The byte array containing image data.</param>
         /// <param name="width">The width of the final image.</param>
         /// <param name="height">The height of the final image.</param>
+        /// <param name="rowSkip">Pixels to skip for each row, defaults to 0.</param>
         /// <typeparam name="TPixel">The pixel format.</typeparam>
         /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
-        public static Image<TPixel> LoadPixelData<TPixel>(byte[] data, int width, int height)
+        public static Image<TPixel> LoadPixelData<TPixel>(byte[] data, int width, int height, int rowSkip)
             where TPixel : struct, IPixel<TPixel>
-            => LoadPixelData<TPixel>(Configuration.Default, data, width, height);
+            => LoadPixelData<TPixel>(Configuration.Default, data, width, height, rowSkip);
 
         /// <summary>
         /// Create a new instance of the <see cref="Image{TPixel}"/> class from the given byte array in <typeparamref name="TPixel"/> format.
@@ -55,24 +58,12 @@ namespace SixLabors.ImageSharp
         /// <param name="data">The byte array containing image data.</param>
         /// <param name="width">The width of the final image.</param>
         /// <param name="height">The height of the final image.</param>
+        /// <param name="rowSkip">Pixels to skip for each row, defaults to 0.</param>
         /// <typeparam name="TPixel">The pixel format.</typeparam>
         /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
-        public static Image<TPixel> LoadPixelData<TPixel>(ReadOnlySpan<byte> data, int width, int height)
+        public static Image<TPixel> LoadPixelData<TPixel>(ReadOnlySpan<byte> data, int width, int height, int rowSkip)
             where TPixel : struct, IPixel<TPixel>
-            => LoadPixelData<TPixel>(Configuration.Default, data, width, height);
-
-        /// <summary>
-        /// Create a new instance of the <see cref="Image{TPixel}"/> class from the given byte array in <typeparamref name="TPixel"/> format.
-        /// </summary>
-        /// <param name="config">The config for the decoder.</param>
-        /// <param name="data">The byte array containing image data.</param>
-        /// <param name="width">The width of the final image.</param>
-        /// <param name="height">The height of the final image.</param>
-        /// <typeparam name="TPixel">The pixel format.</typeparam>
-        /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
-        public static Image<TPixel> LoadPixelData<TPixel>(Configuration config, byte[] data, int width, int height)
-            where TPixel : struct, IPixel<TPixel>
-            => LoadPixelData(config, MemoryMarshal.Cast<byte, TPixel>(new ReadOnlySpan<byte>(data)), width, height);
+            => LoadPixelData<TPixel>(Configuration.Default, data, width, height, rowSkip);
 
         /// <summary>
         /// Create a new instance of the <see cref="Image{TPixel}"/> class from the given byte array in <typeparamref name="TPixel"/> format.
@@ -81,11 +72,26 @@ namespace SixLabors.ImageSharp
         /// <param name="data">The byte array containing image data.</param>
         /// <param name="width">The width of the final image.</param>
         /// <param name="height">The height of the final image.</param>
+        /// <param name="rowSkip">Pixels to skip for each row, defaults to 0.</param>
         /// <typeparam name="TPixel">The pixel format.</typeparam>
         /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
-        public static Image<TPixel> LoadPixelData<TPixel>(Configuration config, ReadOnlySpan<byte> data, int width, int height)
+        public static Image<TPixel> LoadPixelData<TPixel>(Configuration config, byte[] data, int width, int height, int rowSkip)
             where TPixel : struct, IPixel<TPixel>
-            => LoadPixelData(config, MemoryMarshal.Cast<byte, TPixel>(data), width, height);
+            => LoadPixelData(config, MemoryMarshal.Cast<byte, TPixel>(new ReadOnlySpan<byte>(data)), width, height, rowSkip);
+
+        /// <summary>
+        /// Create a new instance of the <see cref="Image{TPixel}"/> class from the given byte array in <typeparamref name="TPixel"/> format.
+        /// </summary>
+        /// <param name="config">The config for the decoder.</param>
+        /// <param name="data">The byte array containing image data.</param>
+        /// <param name="width">The width of the final image.</param>
+        /// <param name="height">The height of the final image.</param>
+        /// <param name="rowSkip">Pixels to skip for each row, defaults to 0.</param>
+        /// <typeparam name="TPixel">The pixel format.</typeparam>
+        /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
+        public static Image<TPixel> LoadPixelData<TPixel>(Configuration config, ReadOnlySpan<byte> data, int width, int height, int rowSkip)
+            where TPixel : struct, IPixel<TPixel>
+            => LoadPixelData(config, MemoryMarshal.Cast<byte, TPixel>(data), width, height, rowSkip);
 
         /// <summary>
         /// Create a new instance of the <see cref="Image{TPixel}"/> class from the raw <typeparamref name="TPixel"/> data.
@@ -94,12 +100,13 @@ namespace SixLabors.ImageSharp
         /// <param name="data">The Span containing the image Pixel data.</param>
         /// <param name="width">The width of the final image.</param>
         /// <param name="height">The height of the final image.</param>
+        /// <param name="rowSkip">Pixels to skip for each row, defaults to 0.</param>
         /// <typeparam name="TPixel">The pixel format.</typeparam>
         /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
-        public static Image<TPixel> LoadPixelData<TPixel>(Configuration config, TPixel[] data, int width, int height)
+        public static Image<TPixel> LoadPixelData<TPixel>(Configuration config, TPixel[] data, int width, int height, int rowSkip)
             where TPixel : struct, IPixel<TPixel>
         {
-            return LoadPixelData(config, new ReadOnlySpan<TPixel>(data), width, height);
+            return LoadPixelData(config, new ReadOnlySpan<TPixel>(data), width, height, rowSkip);
         }
 
         /// <summary>
@@ -109,17 +116,31 @@ namespace SixLabors.ImageSharp
         /// <param name="data">The Span containing the image Pixel data.</param>
         /// <param name="width">The width of the final image.</param>
         /// <param name="height">The height of the final image.</param>
+        /// <param name="rowSkip">Pixels to skip for each row, defaults to 0.</param>
         /// <typeparam name="TPixel">The pixel format.</typeparam>
         /// <returns>A new <see cref="Image{TPixel}"/>.</returns>
-        public static Image<TPixel> LoadPixelData<TPixel>(Configuration config, ReadOnlySpan<TPixel> data, int width, int height)
+        public static Image<TPixel> LoadPixelData<TPixel>(Configuration config, ReadOnlySpan<TPixel> data, int width, int height, int rowSkip)
             where TPixel : struct, IPixel<TPixel>
         {
-            int count = width * height;
+            int rowCount = width + rowSkip;
+            int count = rowCount * height;
             Guard.MustBeGreaterThanOrEqualTo(data.Length, count, nameof(data));
 
             var image = new Image<TPixel>(config, width, height);
 
-            data.Slice(0, count).CopyTo(image.Frames.RootFrame.GetPixelSpan());
+            if (rowSkip == 0)
+            {
+                data.Slice(0, count).CopyTo(image.Frames.RootFrame.GetPixelSpan());
+            }
+            else
+            {
+                int offset = 0;
+                for (int y = 0; y < height; y++)
+                {
+                    data.Slice(offset, width).CopyTo(image.Frames.RootFrame.GetPixelRowSpan(y));
+                    offset += rowCount;
+                }
+            }
 
             return image;
         }

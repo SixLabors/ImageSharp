03aa2fb77588fb0db96f6981992f202fc2e0629d
James Jackson-South <james_south@hotmail.com>
2020-01-16 21:29:22 +1100

    Add new target frameworks

--- tests/ImageSharp.Tests/TestUtilities/Tests/TestEnvironmentTests.cs
+++ tests/ImageSharp.Tests/TestUtilities/Tests/TestEnvironmentTests.cs
@@ -33,29 +33,6 @@ namespace SixLabors.ImageSharp.Tests
             Assert.True(Directory.Exists(path));
         }
 
-        ///// <summary>
-        ///// We need this test to make sure that the netcoreapp2.1 test execution actually covers the netcoreapp2.1 build configuration of ImageSharp.
-        ///// </summary>
-        //[Fact]
-        //public void ImageSharpAssemblyUnderTest_MatchesExpectedTargetFramework()
-        //{
-        //    this.Output.WriteLine("NetCoreVersion: " + TestEnvironment.NetCoreVersion);
-        //    this.Output.WriteLine("ImageSharpBuiltAgainst: " + TestHelpers.ImageSharpBuiltAgainst);
-
-        //    if (string.IsNullOrEmpty(TestEnvironment.NetCoreVersion))
-        //    {
-        //        this.Output.WriteLine("Not running under .NET Core!");
-        //    }
-        //    else if (TestEnvironment.NetCoreVersion.StartsWith("2.1"))
-        //    {
-        //        Assert.Equal("netcoreapp2.1", TestHelpers.ImageSharpBuiltAgainst);
-        //    }
-        //    else
-        //    {
-        //        Assert.Equal("netstandard2.0", TestHelpers.ImageSharpBuiltAgainst);
-        //    }
-        //}
-
         [Fact]
         public void SolutionDirectoryFullPath()
         {

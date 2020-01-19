46ba50241f549909b2fa6a0e9dd3b14a688561d7
Anton Firszov <antonfir@gmail.com>
2020-01-19 23:48:17 +0100

    Keep only necessary stuff from SL.Core

--- src/SixLabors.Core/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Six Labors and contributors.
-// Licensed under the Apache License, Version 2.0.
-
-using System.Runtime.CompilerServices;
-
-// Ensure the internals can be tested.
-[assembly: InternalsVisibleTo("SixLabors.Core.Tests")]

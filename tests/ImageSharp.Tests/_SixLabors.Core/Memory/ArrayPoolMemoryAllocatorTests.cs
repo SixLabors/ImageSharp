8b88c3b6b5720948665e49c81221876385e2b37d
Anton Firszov <antonfir@gmail.com>
2020-01-19 04:55:20 +0100

    fix build for tests

--- tests/ImageSharp.Tests/_SixLabors.Core/Memory/ArrayPoolMemoryAllocatorTests.cs
+++ tests/ImageSharp.Tests/_SixLabors.Core/Memory/ArrayPoolMemoryAllocatorTests.cs
@@ -6,6 +6,7 @@ using System;
 using System.Buffers;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using SixLabors.ImageSharp.Tests;
 using SixLabors.Tests;
 using Xunit;
 

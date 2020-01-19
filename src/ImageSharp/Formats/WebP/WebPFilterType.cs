f74988035cffe6c0bff6567d927196923273f1c9
Peter Amrehn <peter.wendorff@gmx.de>
2020-01-19 17:38:06 +0100

    introduce WebPFilterType enum

---
+++ src/ImageSharp/Formats/WebP/WebPFilterType.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.Formats.WebP
+{
+    // TODO from dsp.h
+    public enum WebPFilterType
+    {
+        None = 0,
+        Horizontal,
+        Vertical,
+        Gradient,
+        Last = Gradient + 1, // end marker
+        Best, // meta types
+        Fast
+    }
+}

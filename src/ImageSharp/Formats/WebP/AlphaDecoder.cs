fcd5a374049924317c092a28f98a4b414f57b2c0
Peter Amrehn <peter.wendorff@gmx.de>
2020-01-20 20:08:10 +0100

    WebP: implement AlphaDecoder and AlphaApplyFilter, start implementing VP8Io

---
+++ src/ImageSharp/Formats/WebP/AlphaDecoder.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+
+namespace SixLabors.ImageSharp.Formats.WebP
+{
+    internal ref struct AlphaDecoder
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int Method { get; set; }
+
+        public WebPFilterBase Filter { get; set; }
+
+        public int PreProcessing { get; set; }
+
+        public Vp8LDecoder Vp8LDec { get; set; }
+
+        public Vp8Io Io { get; set; }
+
+        /// <summary>
+        /// Although Alpha Channel requires only 1 byte per pixel,
+        /// sometimes Vp8LDecoder may need to allocate
+        /// 4 bytes per pixel internally during decode.
+        /// </summary>
+        public bool Use8BDecode { get; set; }
+
+        // last output row (or null)
+        private Span<byte> PrevLine { get; set; }
+
+        private int PrevLineOffset { get; set; }
+
+        // Taken from vp8l_dec.c AlphaApplyFilter
+        public void AlphaApplyFilter(
+            int firstRow, int lastRow,
+            Span<byte> output, int outputOffset,
+            int stride)
+        {
+            if (!(this.Filter is WebPFilterNone))
+            {
+                Span<byte> prevLine = this.PrevLine;
+                int prevLineOffset = this.PrevLineOffset;
+
+                for (int y = firstRow; y < lastRow; y++)
+                {
+                    this.Filter
+                        .Unfilter(
+                            prevLine, prevLineOffset,
+                            output, outputOffset,
+                            output, outputOffset,
+                            stride);
+                    prevLineOffset = outputOffset;
+                    outputOffset += stride;
+                }
+
+                this.PrevLine = prevLine;
+            }
+        }
+    }
+}

460ca626bd5a1694eea776500b7e852f34c64384
James Jackson-South <james_south@hotmail.com>
2020-01-22 13:44:14 +1100

    Update codecov calc and use inbuild ref source/targets

--- ci-build.ps1
+++ ci-build.ps1
@@ -1,20 +1,13 @@
 param(
   [Parameter(Mandatory, Position = 0)]
   [string]$version,
-  [Parameter(Mandatory = $false, Position = 1)]
-  [string]$targetFramework = 'ALL'
+  [Parameter(Mandatory = $true, Position = 1)]
+  [string]$targetFramework
 )
 
 dotnet clean -c Release
 
 $repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
-if ($targetFramework -ne 'ALL') {
 
-  # Building for a specific framework.
-  dotnet build -c Release -f $targetFramework /p:packageversion=$version /p:RepositoryUrl=$repositoryUrl
-}
-else {
-
-  # Building for packing and publishing.
-  dotnet pack -c Release --output "$PSScriptRoot/artifacts" /p:packageversion=$version /p:RepositoryUrl=$repositoryUrl
-}
+# Building for a specific framework.
+dotnet build -c Release -f $targetFramework /p:packageversion=$version /p:RepositoryUrl=$repositoryUrl

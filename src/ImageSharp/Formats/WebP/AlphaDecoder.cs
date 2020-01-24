10459dc0239da860691c1fcfc0c55f81057211b2
Brian Popow <popow@gmx.de>
2020-01-24 18:55:34 +0100

    Paragraph 9.6: Dequantization Indices

--- src/ImageSharp/Formats/WebP/AlphaDecoder.cs
+++ src/ImageSharp/Formats/WebP/AlphaDecoder.cs
@@ -1,7 +1,9 @@
-﻿// Copyright (c) Six Labors and contributors.
+// Copyright (c) Six Labors and contributors.
 // Licensed under the Apache License, Version 2.0.
 
 using System;
+
+using SixLabors.ImageSharp.Formats.WebP.Filters;
 
 namespace SixLabors.ImageSharp.Formats.WebP
 {
--- src/ImageSharp/Formats/WebP/WebPConstants.cs
+++ src/ImageSharp/Formats/WebP/WebPConstants.cs
@@ -70,7 +70,7 @@ namespace SixLabors.ImageSharp.Formats.WebP
         public const int MaxColorCacheBits = 11;
 
         /// <summary>
-        /// The maximum number of allowed transforms in a bitstream.
+        /// The maximum number of allowed transforms in a VP8L bitstream.
         /// </summary>
         public const int MaxNumberOfTransforms = 4;
 
@@ -103,6 +103,52 @@ namespace SixLabors.ImageSharp.Formats.WebP
                                                          NumLiteralCodes + NumLengthCodes,
                                                          NumLiteralCodes, NumLiteralCodes, NumLiteralCodes,
                                                          NumDistanceCodes
+        };
+
+        // VP8 constants
+        public const int NumMbSegments = 4;
+
+        public const int MaxNumPartitions = 8;
+
+        // Paragraph 14.1
+        public static readonly int[] DcTable =
+        {
+            4,     5,   6,   7,   8,   9,  10,  10,
+            11,   12,  13,  14,  15,  16,  17,  17,
+            18,   19,  20,  20,  21,  21,  22,  22,
+            23,   23,  24,  25,  25,  26,  27,  28,
+            29,   30,  31,  32,  33,  34,  35,  36,
+            37,   37,  38,  39,  40,  41,  42,  43,
+            44,   45,  46,  46,  47,  48,  49,  50,
+            51,   52,  53,  54,  55,  56,  57,  58,
+            59,   60,  61,  62,  63,  64,  65,  66,
+            67,   68,  69,  70,  71,  72,  73,  74,
+            75,   76,  76,  77,  78,  79,  80,  81,
+            82,   83,  84,  85,  86,  87,  88,  89,
+            91,   93,  95,  96,  98, 100, 101, 102,
+            104, 106, 108, 110, 112, 114, 116, 118,
+            122, 124, 126, 128, 130, 132, 134, 136,
+            138, 140, 143, 145, 148, 151, 154, 157
+        };
+
+        public static readonly int[] AcTable =
+        {
+            4,     5,   6,   7,   8,   9,  10,  11,
+            12,   13,  14,  15,  16,  17,  18,  19,
+            20,   21,  22,  23,  24,  25,  26,  27,
+            28,   29,  30,  31,  32,  33,  34,  35,
+            36,   37,  38,  39,  40,  41,  42,  43,
+            44,   45,  46,  47,  48,  49,  50,  51,
+            52,   53,  54,  55,  56,  57,  58,  60,
+            62,   64,  66,  68,  70,  72,  74,  76,
+            78,   80,  82,  84,  86,  88,  90,  92,
+            94,   96,  98, 100, 102, 104, 106, 108,
+            110, 112, 114, 116, 119, 122, 125, 128,
+            131, 134, 137, 140, 143, 146, 149, 152,
+            155, 158, 161, 164, 167, 170, 173, 177,
+            181, 185, 189, 193, 197, 201, 205, 209,
+            213, 217, 221, 225, 229, 234, 239, 245,
+            249, 254, 259, 264, 269, 274, 279, 284
         };
     }
 }

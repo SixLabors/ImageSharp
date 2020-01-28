1e663d72de8ce9bb6831b79965d179a06af7ebc9
Anton Firszov <antonfir@gmail.com>
2020-01-28 02:43:34 +0100

    better comments

--- src/ImageSharp/Memory/DiscontinuousProto/IUniformMemoryGroup{T}.cs
+++ src/ImageSharp/Memory/DiscontinuousProto/IUniformMemoryGroup{T}.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace SixLabors.ImageSharp.Memory.DiscontinuousProto
 {
     /// <summary>
-    /// Represents a group of one or more uniformly-sized discontinuous memory segments.
+    /// Represents discontinuous group of multiple uniformly-sized memory segments.
     /// The last segment can be smaller than the preceding ones.
     /// </summary>
     /// <typeparam name="T">The element type.</typeparam>

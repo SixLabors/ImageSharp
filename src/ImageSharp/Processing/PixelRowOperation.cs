ada16abb497873b0ab4e485f5c638eaa894b1fd7
James Jackson-South <james_south@hotmail.com>
2020-01-28 23:33:46 +1100

    rename pixel shader extensions and delegates

--- src/ImageSharp/Processing/PixelRowOperation.cs
+++ src/ImageSharp/Processing/PixelRowOperation.cs
@@ -7,9 +7,9 @@ using System.Numerics;
 namespace SixLabors.ImageSharp.Processing
 {
     /// <summary>
-    /// A <see langword="delegate"/> representing a user defined pixel shader.
+    /// A <see langword="delegate"/> representing a user defined processing delegate to use to modify image rows.
     /// </summary>
     /// <param name="span">The target row of <see cref="Vector4"/> pixels to process.</param>
     /// <remarks>The <see cref="Vector4.X"/>, <see cref="Vector4.Y"/>, <see cref="Vector4.Z"/>, and <see cref="Vector4.W"/> fields map the RGBA channels respectively.</remarks>
-    public delegate void PixelShader(Span<Vector4> span);
+    public delegate void PixelRowOperation(Span<Vector4> span);
 }

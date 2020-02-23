6bac83838baf0f577da3920944582185a65610c4
James Jackson-South <james_south@hotmail.com>
2020-02-23 17:22:48 +1100

    Simplify Resamplers

--- src/ImageSharp/Processing/Processors/Transforms/Resamplers/LanczosResampler.cs
+++ src/ImageSharp/Processing/Processors/Transforms/Resamplers/LanczosResampler.cs
@@ -8,22 +8,56 @@ using SixLabors.ImageSharp.PixelFormats;
 namespace SixLabors.ImageSharp.Processing.Processors.Transforms
 {
     /// <summary>
-    /// The function implements the Robidoux algorithm.
-    /// <see href="http://www.imagemagick.org/Usage/filter/#robidoux"/>
+    /// The function implements the Lanczos kernel algorithm as described on
+    /// <see href="https://en.wikipedia.org/wiki/Lanczos_resampling#Algorithm">Wikipedia</see>.
     /// </summary>
-    public readonly struct RobidouxResampler : IResampler
+    public readonly struct LanczosResampler : IResampler
     {
+        /// <summary>
+        /// Implements the Lanczos kernel algorithm with a radius of 2.
+        /// </summary>
+        public static LanczosResampler Lanczos2 = new LanczosResampler(2);
+
+        /// <summary>
+        /// Implements the Lanczos kernel algorithm with a radius of 3.
+        /// </summary>
+        public static LanczosResampler Lanczos3 = new LanczosResampler(3);
+
+        /// <summary>
+        /// Implements the Lanczos kernel algorithm with a radius of 5.
+        /// </summary>
+        public static LanczosResampler Lanczos5 = new LanczosResampler(5);
+
+        /// <summary>
+        /// Implements the Lanczos kernel algorithm with a radius of 8.
+        /// </summary>
+        public static LanczosResampler Lanczos8 = new LanczosResampler(8);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LanczosResampler"/> struct.
+        /// </summary>
+        /// <param name="radius">The sampling radius.</param>
+        public LanczosResampler(float radius) => this.Radius = radius;
+
         /// <inheritdoc/>
-        public float Radius => 2;
+        public float Radius { get; }
 
         /// <inheritdoc/>
         [MethodImpl(InliningOptions.ShortMethod)]
         public float GetValue(float x)
         {
-            const float B = 0.37821575509399867F;
-            const float C = 0.31089212245300067F;
+            if (x < 0F)
+            {
+                x = -x;
+            }
 
-            return ImageMaths.GetBcValue(x, B, C);
+            float radius = this.Radius;
+            if (x < radius)
+            {
+                return ImageMaths.SinC(x) * ImageMaths.SinC(x / radius);
+            }
+
+            return 0F;
         }
 
         /// <inheritdoc/>

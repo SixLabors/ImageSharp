73450b93533af8dd4c6a030ede90471072d7e61f
James Jackson-South <james_south@hotmail.com>
2020-02-23 17:22:48 +1100

    Simplify Resamplers

--- src/ImageSharp/Processing/KnownResamplers.cs
+++ src/ImageSharp/Processing/KnownResamplers.cs
@@ -24,43 +24,43 @@ namespace SixLabors.ImageSharp.Processing
         /// <summary>
         /// Gets the Catmull-Rom sampler, a well known standard Cubic Filter often used as a interpolation function
         /// </summary>
-        public static IResampler CatmullRom { get; } = default(CatmullRomResampler);
+        public static IResampler CatmullRom { get; } = CubicResampler.CatmullRom;
 
         /// <summary>
         /// Gets the Hermite sampler. A type of smoothed triangular interpolation filter that rounds off strong edges while
         /// preserving flat 'color levels' in the original image.
         /// </summary>
-        public static IResampler Hermite { get; } = default(HermiteResampler);
+        public static IResampler Hermite { get; } = CubicResampler.Hermite;
 
         /// <summary>
         /// Gets the Lanczos kernel sampler that implements smooth interpolation with a radius of 2 pixels.
         /// This algorithm provides sharpened results when compared to others when downsampling.
         /// </summary>
-        public static IResampler Lanczos2 { get; } = default(Lanczos2Resampler);
+        public static IResampler Lanczos2 { get; } = LanczosResampler.Lanczos2;
 
         /// <summary>
         /// Gets the Lanczos kernel sampler that implements smooth interpolation with a radius of 3 pixels
         /// This algorithm provides sharpened results when compared to others when downsampling.
         /// </summary>
-        public static IResampler Lanczos3 { get; } = default(Lanczos3Resampler);
+        public static IResampler Lanczos3 { get; } = LanczosResampler.Lanczos3;
 
         /// <summary>
         /// Gets the Lanczos kernel sampler that implements smooth interpolation with a radius of 5 pixels
         /// This algorithm provides sharpened results when compared to others when downsampling.
         /// </summary>
-        public static IResampler Lanczos5 { get; } = default(Lanczos5Resampler);
+        public static IResampler Lanczos5 { get; } = LanczosResampler.Lanczos5;
 
         /// <summary>
         /// Gets the Lanczos kernel sampler that implements smooth interpolation with a radius of 8 pixels
         /// This algorithm provides sharpened results when compared to others when downsampling.
         /// </summary>
-        public static IResampler Lanczos8 { get; } = default(Lanczos8Resampler);
+        public static IResampler Lanczos8 { get; } = LanczosResampler.Lanczos8;
 
         /// <summary>
         /// Gets the Mitchell-Netravali sampler. This seperable cubic algorithm yields a very good equilibrium between
         /// detail preservation (sharpness) and smoothness.
         /// </summary>
-        public static IResampler MitchellNetravali { get; } = default(MitchellNetravaliResampler);
+        public static IResampler MitchellNetravali { get; } = CubicResampler.MitchellNetravali;
 
         /// <summary>
         /// Gets the Nearest-Neighbour sampler that implements the nearest neighbor algorithm. This uses a very fast, unscaled filter
@@ -72,17 +72,17 @@ namespace SixLabors.ImageSharp.Processing
         /// Gets the Robidoux sampler. This algorithm developed by Nicolas Robidoux providing a very good equilibrium between
         /// detail preservation (sharpness) and smoothness comparable to <see cref="MitchellNetravali"/>.
         /// </summary>
-        public static IResampler Robidoux { get; } = default(RobidouxResampler);
+        public static IResampler Robidoux { get; } = CubicResampler.Robidoux;
 
         /// <summary>
         /// Gets the Robidoux Sharp sampler. A sharpened form of the <see cref="Robidoux"/> sampler
         /// </summary>
-        public static IResampler RobidouxSharp { get; } = default(RobidouxSharpResampler);
+        public static IResampler RobidouxSharp { get; } = CubicResampler.RobidouxSharp;
 
         /// <summary>
-        /// Gets the Spline sampler. A seperable cubic algorithm similar to <see cref="MitchellNetravali"/> but yielding smoother results.
+        /// Gets the Spline sampler. A separable cubic algorithm similar to <see cref="MitchellNetravali"/> but yielding smoother results.
         /// </summary>
-        public static IResampler Spline { get; } = default(SplineResampler);
+        public static IResampler Spline { get; } = CubicResampler.Spline;
 
         /// <summary>
         /// Gets the Triangle sampler, otherwise known as Bilinear. This interpolation algorithm can be used where perfect image transformation

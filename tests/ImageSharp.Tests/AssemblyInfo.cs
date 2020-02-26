608de76e93d053a5945a51a07991746daae7a65c
Anton Firszov <antonfir@gmail.com>
2020-02-27 00:21:57 +0100

    strong name all assemblies in repository

--- tests/ImageSharp.Tests/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Six Labors and contributors.
-// Licensed under the Apache License, Version 2.0.
-
-using System.Runtime.CompilerServices;
-
-[assembly:InternalsVisibleTo("ImageSharp.Tests.ProfilingSandbox")]

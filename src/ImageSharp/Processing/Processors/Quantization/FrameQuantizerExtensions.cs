d9596ef33b213fd65210e2c6789be3d61188d2c0
James Jackson-South <james_south@hotmail.com>
2020-03-04 00:25:22 +1100

    Cleanup and update references.

--- src/ImageSharp/Processing/Processors/Quantization/FrameQuantizerExtensions.cs
+++ src/ImageSharp/Processing/Processors/Quantization/FrameQuantizerExtensions.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using SixLabors.ImageSharp.Advanced;
 using SixLabors.ImageSharp.Memory;
 using SixLabors.ImageSharp.PixelFormats;

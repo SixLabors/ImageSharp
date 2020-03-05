773f773f6d203db38553cabd0ec6554bb9248ea6
James Jackson-South <james_south@hotmail.com>
2020-03-05 17:33:51 +1100

    Cleanup

--- src/ImageSharp/Formats/Png/PngEncoderCore.cs
+++ src/ImageSharp/Formats/Png/PngEncoderCore.cs
@@ -567,7 +567,7 @@ namespace SixLabors.ImageSharp.Formats.Png
 
             // Bulk convert our palette to RGBA to allow assignment to tables.
             // Palette length maxes out at 256 so safe to stackalloc.
-            Span<Rgba32> rgbaPaletteSpan = stackalloc Rgba32[palette.Length];
+            Span<Rgba32> rgbaPaletteSpan = stackalloc Rgba32[paletteLength];
             PixelOperations<TPixel>.Instance.ToRgba32(quantized.Configuration, quantized.Palette.Span, rgbaPaletteSpan);
             ref Rgba32 rgbaPaletteRef = ref MemoryMarshal.GetReference(rgbaPaletteSpan);
 

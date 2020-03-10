d806e75c7f48dc895c01ae3376009acd71608fd1
Brian Popow <popow@gmx.de>
2020-03-10 18:28:15 +0100

    Vp8Decoder reserves only needed memory

--- src/ImageSharp/Formats/WebP/LoopFilter.cs
+++ src/ImageSharp/Formats/WebP/LoopFilter.cs
@@ -3,12 +3,24 @@
 
 namespace SixLabors.ImageSharp.Formats.WebP
 {
+    /// <summary>
+    /// Enum for the different loop filters used. VP8 supports two types of loop filters.
+    /// </summary>
     internal enum LoopFilter
     {
+        /// <summary>
+        /// No filter is used.
+        /// </summary>
         None = 0,
 
+        /// <summary>
+        /// Simple loop filter.
+        /// </summary>
         Simple = 1,
 
+        /// <summary>
+        /// Complex loop filter.
+        /// </summary>
         Complex = 2,
     }
 }

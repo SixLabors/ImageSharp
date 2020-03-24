33678bcb65272b592f62140f55b89a444a382bae
Brian Popow <popow@gmx.de>
2020-03-24 21:13:21 +0100

    Add missing copyright note in ClampVector4

--- tests/ImageSharp.Benchmarks/General/BasicMath/ClampVector4.cs
+++ tests/ImageSharp.Benchmarks/General/BasicMath/ClampVector4.cs
@@ -1,8 +1,9 @@
-using System;
-using System.Collections.Generic;
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
 using System.Numerics;
 using System.Runtime.CompilerServices;
-using System.Text;
+
 using BenchmarkDotNet.Attributes;
 
 namespace SixLabors.ImageSharp.Benchmarks.General.BasicMath

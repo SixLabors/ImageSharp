aaa8e82e0cd61635840ba4b7040064f0d58083e8
Sergio Pedri <sergio0694@live.com>
2020-04-06 19:29:08 +0200

    Added new extension APIs

--- src/ImageSharp/Processing/Extensions/Effects/PixelRowDelegateExtensions.cs
+++ src/ImageSharp/Processing/Extensions/Effects/PixelRowDelegateExtensions.cs
@@ -14,6 +14,106 @@ namespace SixLabors.ImageSharp.Processing
         /// <summary>
         /// Applies a user defined processing delegate to the image.
         /// </summary>
+        /// <typeparam name="TDelegate">The type of user defined, row processing delegate to use.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
+        public static IImageProcessingContext ProcessPixelRowsAsVector4<TDelegate>(this IImageProcessingContext source)
+            where TDelegate : struct, IPixelRowDelegate
+            => source.ApplyProcessor(new PixelRowDelegateProcessor<TDelegate>(PixelConversionModifiers.None));
+
+        /// <summary>
+        /// Applies a user defined processing delegate to the image.
+        /// </summary>
+        /// <typeparam name="TDelegate">The type of user defined, row processing delegate to use.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="modifiers">The <see cref="PixelConversionModifiers"/> to apply during the pixel conversions.</param>
+        /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
+        public static IImageProcessingContext ProcessPixelRowsAsVector4<TDelegate>(this IImageProcessingContext source, PixelConversionModifiers modifiers)
+            where TDelegate : struct, IPixelRowDelegate
+            => source.ApplyProcessor(new PixelRowDelegateProcessor<TDelegate>(modifiers));
+
+        /// <summary>
+        /// Applies a user defined processing delegate to the image.
+        /// </summary>
+        /// <typeparam name="TDelegate">The type of user defined, row processing delegate to use.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="rectangle">
+        /// The <see cref="Rectangle"/> structure that specifies the portion of the image object to alter.
+        /// </param>
+        /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
+        public static IImageProcessingContext ProcessPixelRowsAsVector4<TDelegate>(this IImageProcessingContext source, Rectangle rectangle)
+            where TDelegate : struct, IPixelRowDelegate
+            => source.ApplyProcessor(new PixelRowDelegateProcessor<TDelegate>(PixelConversionModifiers.None), rectangle);
+
+        /// <summary>
+        /// Applies a user defined processing delegate to the image.
+        /// </summary>
+        /// <typeparam name="TDelegate">The type of user defined, row processing delegate to use.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="rectangle">
+        /// The <see cref="Rectangle"/> structure that specifies the portion of the image object to alter.
+        /// </param>
+        /// <param name="modifiers">The <see cref="PixelConversionModifiers"/> to apply during the pixel conversions.</param>
+        /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
+        public static IImageProcessingContext ProcessPixelRowsAsVector4<TDelegate>(this IImageProcessingContext source, Rectangle rectangle, PixelConversionModifiers modifiers)
+            where TDelegate : struct, IPixelRowDelegate
+            => source.ApplyProcessor(new PixelRowDelegateProcessor<TDelegate>(modifiers), rectangle);
+
+        /// <summary>
+        /// Applies a user defined processing delegate to the image.
+        /// </summary>
+        /// <typeparam name="TDelegate">The type of user defined, row processing delegate to use.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="rowDelegate">The user defined processing delegate to use to modify image rows.</param>
+        /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
+        public static IImageProcessingContext ProcessPixelRowsAsVector4<TDelegate>(this IImageProcessingContext source, TDelegate rowDelegate)
+            where TDelegate : struct, IPixelRowDelegate
+            => source.ApplyProcessor(new PixelRowDelegateProcessor<TDelegate>(rowDelegate, PixelConversionModifiers.None));
+
+        /// <summary>
+        /// Applies a user defined processing delegate to the image.
+        /// </summary>
+        /// <typeparam name="TDelegate">The type of user defined, row processing delegate to use.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="rowDelegate">The user defined processing delegate to use to modify image rows.</param>
+        /// <param name="modifiers">The <see cref="PixelConversionModifiers"/> to apply during the pixel conversions.</param>
+        /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
+        public static IImageProcessingContext ProcessPixelRowsAsVector4<TDelegate>(this IImageProcessingContext source, TDelegate rowDelegate, PixelConversionModifiers modifiers)
+            where TDelegate : struct, IPixelRowDelegate
+            => source.ApplyProcessor(new PixelRowDelegateProcessor<TDelegate>(rowDelegate, modifiers));
+
+        /// <summary>
+        /// Applies a user defined processing delegate to the image.
+        /// </summary>
+        /// <typeparam name="TDelegate">The type of user defined, row processing delegate to use.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="rowDelegate">The user defined processing delegate to use to modify image rows.</param>
+        /// <param name="rectangle">
+        /// The <see cref="Rectangle"/> structure that specifies the portion of the image object to alter.
+        /// </param>
+        /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
+        public static IImageProcessingContext ProcessPixelRowsAsVector4<TDelegate>(this IImageProcessingContext source, TDelegate rowDelegate, Rectangle rectangle)
+            where TDelegate : struct, IPixelRowDelegate
+            => source.ApplyProcessor(new PixelRowDelegateProcessor<TDelegate>(rowDelegate, PixelConversionModifiers.None), rectangle);
+
+        /// <summary>
+        /// Applies a user defined processing delegate to the image.
+        /// </summary>
+        /// <typeparam name="TDelegate">The type of user defined, row processing delegate to use.</typeparam>
+        /// <param name="source">The image this method extends.</param>
+        /// <param name="rowDelegate">The user defined processing delegate to use to modify image rows.</param>
+        /// <param name="rectangle">
+        /// The <see cref="Rectangle"/> structure that specifies the portion of the image object to alter.
+        /// </param>
+        /// <param name="modifiers">The <see cref="PixelConversionModifiers"/> to apply during the pixel conversions.</param>
+        /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
+        public static IImageProcessingContext ProcessPixelRowsAsVector4<TDelegate>(this IImageProcessingContext source, TDelegate rowDelegate, Rectangle rectangle, PixelConversionModifiers modifiers)
+            where TDelegate : struct, IPixelRowDelegate
+            => source.ApplyProcessor(new PixelRowDelegateProcessor<TDelegate>(rowDelegate, modifiers), rectangle);
+
+        /// <summary>
+        /// Applies a user defined processing delegate to the image.
+        /// </summary>
         /// <param name="source">The image this method extends.</param>
         /// <param name="rowOperation">The user defined processing delegate to use to modify image rows.</param>
         /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>

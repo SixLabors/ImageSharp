c1f6890d677f30b012dcc57c4732efc64db8d48b
Sergio Pedri <sergio0694@live.com>
2020-04-13 15:32:08 +0200

    Removed unnecessary using directives

--- src/ImageSharp/Memory/DiscontiguousBuffers/MemoryGroup{T}.Consumed.cs
+++ src/ImageSharp/Memory/DiscontiguousBuffers/MemoryGroup{T}.Consumed.cs
@@ -2,9 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
-using System.Buffers;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace SixLabors.ImageSharp.Memory
 {

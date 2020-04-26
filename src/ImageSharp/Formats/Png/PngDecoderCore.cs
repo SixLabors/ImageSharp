05bbc743c12e32fe7b38a1f52adc9c24745dd858
Brian Popow <popow@gmx.de>
2020-04-26 16:19:50 +0200

    Identify now parses zTXt and iTXt chunks

--- src/ImageSharp/Formats/Png/PngDecoderCore.cs
+++ src/ImageSharp/Formats/Png/PngDecoderCore.cs
@@ -273,6 +273,12 @@ namespace SixLabors.ImageSharp.Formats.Png
                             case PngChunkType.Text:
                                 this.ReadTextChunk(pngMetadata, chunk.Data.Array.AsSpan(0, chunk.Length));
                                 break;
+                            case PngChunkType.CompressedText:
+                                this.ReadCompressedTextChunk(pngMetadata, chunk.Data.Array.AsSpan(0, chunk.Length));
+                                break;
+                            case PngChunkType.InternationalText:
+                                this.ReadInternationalTextChunk(pngMetadata, chunk.Data.Array.AsSpan(0, chunk.Length));
+                                break;
                             case PngChunkType.End:
                                 this.isEndChunkReached = true;
                                 break;

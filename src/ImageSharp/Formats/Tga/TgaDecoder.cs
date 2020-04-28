08b5c4cb8331fbba13a51ac7139975db83853851
Brian Popow <popow@gmx.de>
2020-04-28 10:26:32 +0200

    Change ImageFormatException to InvalidImageContentException

--- src/ImageSharp/Formats/Tga/TgaDecoder.cs
+++ src/ImageSharp/Formats/Tga/TgaDecoder.cs
@@ -28,9 +28,7 @@ namespace SixLabors.ImageSharp.Formats.Tga
             {
                 Size dims = decoder.Dimensions;
 
-                // TODO: use InvalidImageContentException here, if we decide to define it
-                // https://github.com/SixLabors/ImageSharp/issues/1110
-                throw new ImageFormatException($"Can not decode image. Failed to allocate buffers for possibly degenerate dimensions: {dims.Width}x{dims.Height}.", ex);
+                throw new InvalidImageContentException($"Can not decode image. Failed to allocate buffers for possibly degenerate dimensions: {dims.Width}x{dims.Height}.", ex);
             }
         }
 

fed9cc013558d7a58f0d8bc11c49affdae295547
Brian Popow <popow@gmx.de>
2020-05-30 17:45:01 +0200

    StoreHuffmanCode

--- src/ImageSharp/Formats/WebP/Lossless/HuffmanTreeToken.cs
+++ src/ImageSharp/Formats/WebP/Lossless/HuffmanTreeToken.cs
@@ -1,21 +1,24 @@
 // Copyright (c) Six Labors and contributors.
 // Licensed under the GNU Affero General Public License, Version 3.
 
+using System.Diagnostics;
+
 namespace SixLabors.ImageSharp.Formats.WebP.Lossless
 {
     /// <summary>
     /// Holds the tree header in coded form.
     /// </summary>
+    [DebuggerDisplay("Code = {Code}, ExtraBits = {ExtraBits}")]
     internal class HuffmanTreeToken
     {
         /// <summary>
-        /// Gets the code. Value (0..15) or escape code (16, 17, 18).
+        /// Gets or sets the code. Value (0..15) or escape code (16, 17, 18).
         /// </summary>
-        public byte Code { get; }
+        public byte Code { get; set; }
 
         /// <summary>
-        /// Gets extra bits for escape codes.
+        /// Gets or sets the extra bits for escape codes.
         /// </summary>
-        public byte ExtraBits { get; }
+        public byte ExtraBits { get; set; }
     }
 }

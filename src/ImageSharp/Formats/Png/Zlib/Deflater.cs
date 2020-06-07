874115bd0a382a922ddfea5dd33fe67ecef245f6
James Jackson-South <james_south@hotmail.com>
2020-06-07 23:09:22 +0100

    Update license

--- src/ImageSharp/Formats/Png/Zlib/Deflater.cs
+++ src/ImageSharp/Formats/Png/Zlib/Deflater.cs
@@ -1,5 +1,5 @@
-// Copyright (c) Six Labors and contributors.
-// Licensed under the GNU Affero General Public License, Version 3.
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
 
 using System;
 using System.Runtime.CompilerServices;

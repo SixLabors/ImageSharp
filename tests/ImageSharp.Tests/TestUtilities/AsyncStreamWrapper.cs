73cc7964e87fecc7cc71bae56e11dd05164376c1
James Jackson-South <james_south@hotmail.com>
2020-06-09 21:04:35 +0100

    Use named tuple

--- tests/ImageSharp.Tests/TestUtilities/AsyncStreamWrapper.cs
+++ tests/ImageSharp.Tests/TestUtilities/AsyncStreamWrapper.cs
@@ -1,10 +1,8 @@
-// Copyright (c) Six Labors and contributors.
-// Licensed under the GNU Affero General Public License, Version 3.
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 

5f78f1960588efb8aff40927628fc669d09c4244
James Jackson-South <james_south@hotmail.com>
2020-06-15 20:53:06 +0100

    Add tests

---
+++ tests/ImageSharp.Tests/TestUtilities/TestEnvironment.Features.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.Tests
+{
+    public static partial class TestEnvironment
+    {
+        internal static class Features
+        {
+            public const string On = "1";
+            public const string Off = "0";
+
+            public const string EnableAES = "COMPlus_EnableAES";
+            public const string EnableAVX = "COMPlus_EnableAVX";
+            public const string EnableAVX2 = "COMPlus_EnableAVX2";
+            public const string EnableBMI1 = "COMPlus_EnableBMI1";
+            public const string EnableBMI2 = "COMPlus_EnableBMI2";
+            public const string EnableFMA = "COMPlus_EnableFMA";
+            public const string EnableHWIntrinsic = "COMPlus_EnableHWIntrinsic";
+            public const string EnableIncompleteISAClass = "COMPlus_EnableIncompleteISAClass";
+            public const string EnableLZCNT = "COMPlus_EnableLZCNT";
+            public const string EnablePCLMULQDQ = "COMPlus_EnablePCLMULQDQ";
+            public const string EnablePOPCNT = "COMPlus_EnablePOPCNT";
+            public const string EnableSSE = "COMPlus_EnableSSE";
+            public const string EnableSSE2 = "COMPlus_EnableSSE2";
+            public const string EnableSSE3 = "COMPlus_EnableSSE3";
+            public const string EnableSSE3_4 = "COMPlus_EnableSSE3_4";
+            public const string EnableSSE41 = "COMPlus_EnableSSE41";
+            public const string EnableSSE42 = "COMPlus_EnableSSE42";
+            public const string EnableSSSE3 = "COMPlus_EnableSSSE3";
+            public const string FeatureSIMD = "COMPlus_FeatureSIMD";
+        }
+    }
+}

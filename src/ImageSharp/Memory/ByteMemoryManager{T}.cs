77a6d08a463265897bc1239de6d1413cbc7ead10
Sergio Pedri <sergio0694@live.com>
2020-08-13 17:15:16 +0200

    Fix relative offsetting in Pin() methods

--- src/ImageSharp/Memory/ByteMemoryManager{T}.cs
+++ src/ImageSharp/Memory/ByteMemoryManager{T}.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 using System;
 using System.Buffers;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace SixLabors.ImageSharp.Memory
@@ -42,7 +43,10 @@ namespace SixLabors.ImageSharp.Memory
         /// <inheritdoc/>
         public override MemoryHandle Pin(int elementIndex = 0)
         {
-            return this.memory.Slice(elementIndex).Pin();
+            // We need to adjust the offset into the wrapped byte segment,
+            // as the input index refers to the target-cast memory of T.
+            // We just have to shift this index by the byte size of T.
+            return this.memory.Slice(elementIndex * Unsafe.SizeOf<T>()).Pin();
         }
 
         /// <inheritdoc/>

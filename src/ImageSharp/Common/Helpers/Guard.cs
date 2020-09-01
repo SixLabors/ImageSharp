34522eb62cdecaa18069493bad9d14e59bd810e0
Sergio Pedri <sergio0694@live.com>
2020-09-01 13:18:45 +0200

    Improved Guard.MustBeValueType<T> codegen
    Can now be JITted to a constant on .NET 5, see https://github.com/dotnet/runtime/pull/1157

--- src/ImageSharp/Common/Helpers/Guard.cs
+++ src/ImageSharp/Common/Helpers/Guard.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
+#if NETSTANDARD1_3
 using System.Reflection;
+#endif
 using System.Runtime.CompilerServices;
 using SixLabors.ImageSharp;
 
@@ -20,7 +22,11 @@ namespace SixLabors
         [MethodImpl(InliningOptions.ShortMethod)]
         public static void MustBeValueType<TValue>(TValue value, string parameterName)
         {
-            if (value.GetType().GetTypeInfo().IsValueType)
+            if (value.GetType()
+#if NETSTANDARD1_3
+                .GetTypeInfo()
+#endif
+                .IsValueType)
             {
                 return;
             }

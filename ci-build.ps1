f871dd96fc1ab46f73a2b8703d0290d219847e5d
James Jackson-South <james_south@hotmail.com>
2020-10-18 21:41:47 +0100

    Add 32 bit tests for all windows targets

--- ci-build.ps1
+++ ci-build.ps1
@@ -1,11 +1,23 @@
 param(
-  [Parameter(Mandatory = $true, Position = 0)]
+  [Parameter(Mandatory, Position = 0)]
+  [string]$os,
+  [Parameter(Mandatory = $true, Position = 1)]
   [string]$targetFramework
+  [Parameter(Mandatory, Position = 2)]
+  [string]$platform,
 )
 
 dotnet clean -c Release
 
 $repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
 
-# Building for a specific framework.
-dotnet build -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
+if ($os -eq 'windows-latest' -and  $platform -eq 'x86') {
+
+  # Only Windows supports setting the target platform for now.
+  dotnet build -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl -- RunConfiguration.TargetPlatform=$platform
+}
+else {
+
+  # Default to 64 bit
+  dotnet build -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
+}
--- ci-test.ps1
+++ ci-test.ps1
@@ -11,27 +11,19 @@ param(
   [string]$codecovProfile = 'Release'
 )
 
-$netFxRegex = '^net\d+'
-
 if ($codecov -eq 'true') {
 
   # Allow toggling of profile to workaround any potential JIT errors caused by code injection.
   dotnet clean -c $codecovProfile
   dotnet test --collect "XPlat Code Coverage" --settings .\tests\coverlet.runsettings -c $codecovProfile -f $targetFramework /p:CodeCov=true
 }
-elseif ($platform -eq '-x86' -and $targetFramework -match $netFxRegex) {
+elseif ($os -eq 'windows-latest' -and  $platform -eq 'x86') {
 
-  # xunit doesn't run on core with NET SDK 3.1+.
-  # xunit doesn't actually understand -x64 as an option.
-  #
-  # xunit requires explicit path.
-  Set-Location $env:XUNIT_PATH
-
-  dotnet xunit --no-build -c Release -f $targetFramework ${fxVersion} $platform
-
-  Set-Location $PSScriptRoot
+  # Only Windows supports setting the target platform for now.
+  dotnet test --no-build -c Release -f $targetFramework -- RunConfiguration.TargetPlatform=$platform
 }
 else {
 
+  # Default to 64 bit
   dotnet test --no-build -c Release -f $targetFramework
 }

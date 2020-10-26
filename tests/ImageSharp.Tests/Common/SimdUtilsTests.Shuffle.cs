f659bc39501f89d16ec22937f1312beb6f3e424c
James Jackson-South <james_south@hotmail.com>
2020-10-26 20:04:32 +0000

    Add 4 channel byte shuffling

--- tests/ImageSharp.Tests/Common/SimdUtilsTests.Shuffle.cs
+++ tests/ImageSharp.Tests/Common/SimdUtilsTests.Shuffle.cs
@@ -14,7 +14,10 @@ namespace SixLabors.ImageSharp.Tests.Common
             {
                 SimdUtils.Shuffle.WXYZ,
                 SimdUtils.Shuffle.XYZW,
-                SimdUtils.Shuffle.ZYXW
+                SimdUtils.Shuffle.ZYXW,
+                SimdUtils.Shuffle.MmShuffle(2, 1, 3, 0),
+                SimdUtils.Shuffle.MmShuffle(1, 1, 1, 1),
+                SimdUtils.Shuffle.MmShuffle(3, 3, 3, 3)
             };
 
         [Theory]
@@ -28,7 +31,7 @@ namespace SixLabors.ImageSharp.Tests.Common
                 {
                     foreach (var count in item)
                     {
-                        TestShuffle(
+                        TestShuffleFloat4Channel(
                             (int)count,
                             (s, d) => SimdUtils.Shuffle4Channel(s.Span, d.Span, ctrl),
                             ctrl);
@@ -42,7 +45,32 @@ namespace SixLabors.ImageSharp.Tests.Common
                 HwIntrinsics.AllowAll | HwIntrinsics.DisableAVX2 | HwIntrinsics.DisableSSE);
         }
 
-        private static void TestShuffle(
+        [Theory]
+        [MemberData(nameof(ShuffleControls))]
+        public void BulkShuffleByte4Channel(byte control)
+        {
+            static void RunTest(string serialized)
+            {
+                byte ctrl = FeatureTestRunner.Deserialize<byte>(serialized);
+                foreach (var item in ArraySizesDivisibleBy4)
+                {
+                    foreach (var count in item)
+                    {
+                        TestShuffleByte4Channel(
+                            (int)count,
+                            (s, d) => SimdUtils.Shuffle4Channel(s.Span, d.Span, ctrl),
+                            ctrl);
+                    }
+                }
+            }
+
+            FeatureTestRunner.RunWithHwIntrinsicsFeature(
+                RunTest,
+                control,
+                HwIntrinsics.AllowAll | HwIntrinsics.DisableAVX2 | HwIntrinsics.DisableSSE);
+        }
+
+        private static void TestShuffleFloat4Channel(
             int count,
             Action<Memory<float>, Memory<float>> convert,
             byte control)
@@ -70,6 +98,37 @@ namespace SixLabors.ImageSharp.Tests.Common
             convert(source, result);
 
             Assert.Equal(expected, result, new ApproximateFloatComparer(1e-5F));
+        }
+
+        private static void TestShuffleByte4Channel(
+            int count,
+            Action<Memory<byte>, Memory<byte>> convert,
+            byte control)
+        {
+            byte[] source = new byte[count];
+            new Random(count).NextBytes(source);
+            var result = new byte[count];
+
+            byte[] expected = new byte[count];
+
+            SimdUtils.Shuffle.InverseMmShuffle(
+                control,
+                out int p3,
+                out int p2,
+                out int p1,
+                out int p0);
+
+            for (int i = 0; i < expected.Length; i += 4)
+            {
+                expected[i] = source[p0 + i];
+                expected[i + 1] = source[p1 + i];
+                expected[i + 2] = source[p2 + i];
+                expected[i + 3] = source[p3 + i];
+            }
+
+            convert(source, result);
+
+            Assert.Equal(expected, result);
         }
     }
 }

2548d2d31b179a9bee6e0c183dadbb1a5f3f17cf
Nicolas Portmann <ndportmann@gmail.com>
2020-11-04 21:14:54 +0100

    Add benchmarks

---
+++ tests/ImageSharp.Benchmarks/Codecs/Jpeg/GrayscaleColorConversion.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using SixLabors.ImageSharp.Formats.Jpeg.Components.Decoder.ColorConverters;
+
+namespace SixLabors.ImageSharp.Benchmarks.Codecs.Jpeg
+{
+    [Config(typeof(Config.ShortClr))]
+    public class GrayscaleColorConversion : ColorConversionBenchmark
+    {
+        public GrayscaleColorConversion()
+            : base(1)
+        {
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Scalar()
+        {
+            var values = new JpegColorConverter.ComponentValues(this.input, 0);
+
+            new JpegColorConverter.FromGrayscaleBasic(8).ConvertToRgba(values, this.output);
+        }
+
+        [Benchmark]
+        public void SimdVector8()
+        {
+            var values = new JpegColorConverter.ComponentValues(this.input, 0);
+
+            new JpegColorConverter.FromGrayscaleVector8(8).ConvertToRgba(values, this.output);
+        }
+
+        [Benchmark]
+        public void SimdVectorAvx2()
+        {
+            var values = new JpegColorConverter.ComponentValues(this.input, 0);
+
+            new JpegColorConverter.FromGrayscaleAvx2(8).ConvertToRgba(values, this.output);
+        }
+    }
+}

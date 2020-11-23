5941c1cfb487c3d6616b8acb771264cf9bc95d1c
James Jackson-South <james_south@hotmail.com>
2020-11-23 01:19:26 +0000

    Add benchmark results.

--- tests/ImageSharp.Benchmarks/General/BasicMath/ClampSpan.cs
+++ tests/ImageSharp.Benchmarks/General/BasicMath/ClampSpan.cs
@@ -40,3 +40,18 @@ namespace SixLabors.ImageSharp.Benchmarks.General.BasicMath
         }
     }
 }
+
+// 23-11-2020
+// ##########
+//
+// BenchmarkDotNet = v0.12.1, OS = Windows 10.0.19041.630(2004 /?/ 20H1)
+// Intel Core i7-8650U CPU 1.90GHz (Kaby Lake R), 1 CPU, 8 logical and 4 physical cores
+// .NET Core SDK=5.0.100
+//  [Host]     : .NET Core 3.1.10 (CoreCLR 4.700.20.51601, CoreFX 4.700.20.51901), X64 RyuJIT
+//  DefaultJob : .NET Core 3.1.10 (CoreCLR 4.700.20.51601, CoreFX 4.700.20.51901), X64 RyuJIT
+//
+//
+// |                Method |       Mean |     Error |     StdDev |  Ratio |
+// |---------------------- |-----------:| ---------:| ----------:| ------:|
+// |     ClampNoIntrinsics | 3,629.9 ns |  70.80 ns |  129.47 ns |   1.00 |
+// | ClampVectorIntrinsics |   131.9 ns |   2.68 ns |  6.66 ns   |   0.04 |

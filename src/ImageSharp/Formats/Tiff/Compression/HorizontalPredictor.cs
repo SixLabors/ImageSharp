9c05a3a30a16ad5e1550b817054d835395965cb7
Brian Popow <popow@gmx.de>
2020-12-04 12:47:57 +0100

    Move apply horizontal prediction to appropriate class

--- src/ImageSharp/Formats/Tiff/Compression/HorizontalPredictor.cs
+++ src/ImageSharp/Formats/Tiff/Compression/HorizontalPredictor.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 using SixLabors.ImageSharp.PixelFormats;
@@ -28,6 +29,41 @@ namespace SixLabors.ImageSharp.Formats.Tiff.Compression
             else if (bitsPerPixel == 24)
             {
                 Undo24Bit(pixelBytes, width);
+            }
+        }
+
+        /// <summary>
+        /// Applies a horizontal predictor to the rgb row.
+        /// Make use of the fact that many continuous-tone images rarely vary much in pixel value from one pixel to the next.
+        /// In such images, if we replace the pixel values by differences between consecutive pixels, many of the differences should be 0, plus
+        /// or minus 1, and so on.This reduces the apparent information content and allows LZW to encode the data more compactly.
+        /// </summary>
+        /// <param name="rowSpan">The rgb pixel row.</param>
+        [MethodImpl(InliningOptions.ShortMethod)]
+        public static void ApplyHorizontalPrediction24Bit(Span<byte> rowSpan)
+        {
+            Span<Rgb24> rowRgb = MemoryMarshal.Cast<byte, Rgb24>(rowSpan);
+
+            for (int x = rowRgb.Length - 1; x >= 1; x--)
+            {
+                byte r = (byte)(rowRgb[x].R - rowRgb[x - 1].R);
+                byte g = (byte)(rowRgb[x].G - rowRgb[x - 1].G);
+                byte b = (byte)(rowRgb[x].B - rowRgb[x - 1].B);
+                var rgb = new Rgb24(r, g, b);
+                rowRgb[x].FromRgb24(rgb);
+            }
+        }
+
+        /// <summary>
+        /// Applies a horizontal predictor to a gray pixel row.
+        /// </summary>
+        /// <param name="rowSpan">The gray pixel row.</param>
+        [MethodImpl(InliningOptions.ShortMethod)]
+        public static void ApplyHorizontalPrediction8Bit(Span<byte> rowSpan)
+        {
+            for (int x = rowSpan.Length - 1; x >= 1; x--)
+            {
+                rowSpan[x] -= rowSpan[x - 1];
             }
         }
 

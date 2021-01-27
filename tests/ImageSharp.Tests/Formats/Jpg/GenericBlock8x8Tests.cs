23a917a2d51e8dee36d40673eb1027c160278c10
James Jackson-South <james_south@hotmail.com>
2021-01-27 01:34:49 +0000

    Use less expensive update for RowOctet

--- tests/ImageSharp.Tests/Formats/Jpg/GenericBlock8x8Tests.cs
+++ tests/ImageSharp.Tests/Formats/Jpg/GenericBlock8x8Tests.cs
@@ -43,7 +43,7 @@ namespace SixLabors.ImageSharp.Tests.Formats.Jpg
             {
                 var d = default(GenericBlock8x8<TPixel>);
                 var rowOctet = new RowOctet<TPixel>(s.GetRootFramePixelBuffer(), 0);
-                d.LoadAndStretchEdges(s.Frames.RootFrame.PixelBuffer, 0, 0, rowOctet);
+                d.LoadAndStretchEdges(s.Frames.RootFrame.PixelBuffer, 0, 0, ref rowOctet);
 
                 TPixel a = s.Frames.RootFrame[0, 0];
                 TPixel b = d[0, 0];
@@ -68,7 +68,7 @@ namespace SixLabors.ImageSharp.Tests.Formats.Jpg
             {
                 var d = default(GenericBlock8x8<TPixel>);
                 var rowOctet = new RowOctet<TPixel>(s.GetRootFramePixelBuffer(), 7);
-                d.LoadAndStretchEdges(s.Frames.RootFrame.PixelBuffer, 6, 7, rowOctet);
+                d.LoadAndStretchEdges(s.Frames.RootFrame.PixelBuffer, 6, 7, ref rowOctet);
 
                 Assert.Equal(s[6, 7], d[0, 0]);
                 Assert.Equal(s[6, 8], d[0, 1]);

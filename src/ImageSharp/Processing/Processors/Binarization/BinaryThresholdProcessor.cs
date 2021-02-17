2e09bb4a3c05b1c7d489391c0d373bd7dce83ee9
Anton Firszov <antonfir@gmail.com>
2021-02-17 16:04:58 +0100

    BinaryThresholdColorComponent -> BinaryThresholdMode

--- src/ImageSharp/Processing/Processors/Binarization/BinaryThresholdProcessor.cs
+++ src/ImageSharp/Processing/Processors/Binarization/BinaryThresholdProcessor.cs
@@ -14,9 +14,9 @@ namespace SixLabors.ImageSharp.Processing.Processors.Binarization
         /// Initializes a new instance of the <see cref="BinaryThresholdProcessor"/> class.
         /// </summary>
         /// <param name="threshold">The threshold to split the image. Must be between 0 and 1.</param>
-        /// <param name="colorComponent">The color component to be compared to threshold.</param>
-        public BinaryThresholdProcessor(float threshold, BinaryThresholdColorComponent colorComponent)
-            : this(threshold, Color.White, Color.Black, colorComponent)
+        /// <param name="mode">The color component to be compared to threshold.</param>
+        public BinaryThresholdProcessor(float threshold, BinaryThresholdMode mode)
+            : this(threshold, Color.White, Color.Black, mode)
         {
         }
 
@@ -26,7 +26,7 @@ namespace SixLabors.ImageSharp.Processing.Processors.Binarization
         /// </summary>
         /// <param name="threshold">The threshold to split the image. Must be between 0 and 1.</param>
         public BinaryThresholdProcessor(float threshold)
-            : this(threshold, Color.White, Color.Black, BinaryThresholdColorComponent.Luminance)
+            : this(threshold, Color.White, Color.Black, BinaryThresholdMode.Luminance)
         {
         }
 
@@ -36,14 +36,14 @@ namespace SixLabors.ImageSharp.Processing.Processors.Binarization
         /// <param name="threshold">The threshold to split the image. Must be between 0 and 1.</param>
         /// <param name="upperColor">The color to use for pixels that are above the threshold.</param>
         /// <param name="lowerColor">The color to use for pixels that are below the threshold.</param>
-        /// <param name="colorComponent">The color component to be compared to threshold.</param>
-        public BinaryThresholdProcessor(float threshold, Color upperColor, Color lowerColor, BinaryThresholdColorComponent colorComponent)
+        /// <param name="mode">The color component to be compared to threshold.</param>
+        public BinaryThresholdProcessor(float threshold, Color upperColor, Color lowerColor, BinaryThresholdMode mode)
         {
             Guard.MustBeBetweenOrEqualTo(threshold, 0, 1, nameof(threshold));
             this.Threshold = threshold;
             this.UpperColor = upperColor;
             this.LowerColor = lowerColor;
-            this.ColorComponent = colorComponent;
+            this.Mode = mode;
         }
 
         /// <summary>
@@ -54,7 +54,7 @@ namespace SixLabors.ImageSharp.Processing.Processors.Binarization
         /// <param name="upperColor">The color to use for pixels that are above the threshold.</param>
         /// <param name="lowerColor">The color to use for pixels that are below the threshold.</param>
         public BinaryThresholdProcessor(float threshold, Color upperColor, Color lowerColor)
-            : this(threshold, upperColor, lowerColor, BinaryThresholdColorComponent.Luminance)
+            : this(threshold, upperColor, lowerColor, BinaryThresholdMode.Luminance)
         {
         }
 
@@ -73,10 +73,10 @@ namespace SixLabors.ImageSharp.Processing.Processors.Binarization
         /// </summary>
         public Color LowerColor { get; }
 
-         /// <summary>
-        /// Gets a value indicating whether to use saturation value instead of luminance.
+        /// <summary>
+        /// Gets a value indicating the value to be compared to threshold.
         /// </summary>
-        public BinaryThresholdColorComponent ColorComponent { get; }
+        public BinaryThresholdMode Mode { get; }
 
        /// <inheritdoc />
         public IImageProcessor<TPixel> CreatePixelSpecificProcessor<TPixel>(Configuration configuration, Image<TPixel> source, Rectangle sourceRectangle)

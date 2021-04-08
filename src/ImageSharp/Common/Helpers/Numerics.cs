35065c7f479fed1411fed2eff66fee692e5f1fc1
James Jackson-South <james_south@hotmail.com>
2021-04-08 17:20:15 +0100

    Detect runtime to determine pipeline.

--- src/ImageSharp/Common/Helpers/Numerics.cs
+++ src/ImageSharp/Common/Helpers/Numerics.cs
@@ -497,7 +497,13 @@ namespace SixLabors.ImageSharp
         public static void Premultiply(Span<Vector4> vectors)
         {
 #if SUPPORTS_RUNTIME_INTRINSICS
-            if (Avx2.IsSupported && vectors.Length >= 2)
+
+            // There's an issue with .NET Core 3.1 when calling this method on MacOS
+            // https://github.com/SixLabors/ImageSharp/pull/1591#discussion_r609118003
+            if (Avx2.IsSupported
+                && vectors.Length >= 2
+                && !RuntimeEnvironment.IsOSPlatform(OSPlatform.OSX)
+                && RuntimeEnvironment.IsNetCore)
             {
                 // Divide by 2 as 4 elements per Vector4 and 8 per Vector256<float>
                 ref Vector256<float> vectorsBase = ref Unsafe.As<Vector4, Vector256<float>>(ref MemoryMarshal.GetReference(vectors));
---
+++ src/ImageSharp/Common/Helpers/RuntimeEnvironment.cs
@@ -0,0 +1,75 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace SixLabors.ImageSharp
+{
+    /// <summary>
+    /// Provides information about the .NET runtime installation.
+    /// Many methods defer to <see cref="RuntimeInformation"/> when available.
+    /// </summary>
+    internal static class RuntimeEnvironment
+    {
+        private const string FrameworkName = ".NET";
+        private static bool isNetCoreChecked;
+        private static bool isNetCore;
+        private static string frameworkDescription;
+
+        /// <summary>
+        /// Gets a value indicating whether the .NET installation is .NET Core
+        /// </summary>
+        public static bool IsNetCore
+        {
+            get
+            {
+                if (isNetCoreChecked)
+                {
+                    return IsNetCore;
+                }
+
+                isNetCore = FrameworkDescription.StartsWith(".NET Core", StringComparison.OrdinalIgnoreCase);
+                isNetCoreChecked = true;
+                return isNetCore;
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the .NET installation on which an app is running.
+        /// </summary>
+        public static string FrameworkDescription
+        {
+            get
+            {
+                // Adapted from:
+                // https://source.dot.net/#System.Runtime.InteropServices.RuntimeInformation/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.cs
+                if (frameworkDescription == null)
+                {
+                    string versionString = typeof(object).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+                    // Strip the git hash if there is one
+                    if (versionString != null)
+                    {
+                        int plusIndex = versionString.IndexOf('+');
+                        if (plusIndex != -1)
+                        {
+                            versionString = versionString.Substring(0, plusIndex);
+                        }
+                    }
+
+                    frameworkDescription = !string.IsNullOrWhiteSpace(versionString) ? $"{FrameworkName} {versionString}" : FrameworkName;
+                }
+
+                return frameworkDescription;
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether the current application is running on the specified platform.
+        /// </summary>
+        public static bool IsOSPlatform(OSPlatform osPlatform)
+            => RuntimeInformation.IsOSPlatform(osPlatform);
+    }
+}

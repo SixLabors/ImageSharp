3b8bed5e9966d007d72275052431c39a76265702
Brian Popow <popow@gmx.de>
2021-06-12 16:53:59 +0200

    Remove not used constants

--- src/ImageSharp/Formats/Tiff/Constants/TiffConstants.cs
+++ src/ImageSharp/Formats/Tiff/Constants/TiffConstants.cs
@@ -41,39 +41,9 @@ namespace SixLabors.ImageSharp.Formats.Tiff.Constants
         public const int RowsPerStripInfinity = 2147483647;
 
         /// <summary>
-        /// Size (in bytes) of the TIFF file header.
-        /// </summary>
-        public const int SizeOfTiffHeader = 8;
-
-        /// <summary>
-        /// Size (in bytes) of each individual TIFF IFD entry
-        /// </summary>
-        public const int SizeOfIfdEntry = 12;
-
-        /// <summary>
-        /// Size (in bytes) of the Short and SShort data types
-        /// </summary>
-        public const int SizeOfShort = 2;
-
-        /// <summary>
-        /// Size (in bytes) of the Long and SLong data types
-        /// </summary>
-        public const int SizeOfLong = 4;
-
-        /// <summary>
         /// Size (in bytes) of the Rational and SRational data types
         /// </summary>
         public const int SizeOfRational = 8;
-
-        /// <summary>
-        /// Size (in bytes) of the Float data type
-        /// </summary>
-        public const int SizeOfFloat = 4;
-
-        /// <summary>
-        /// Size (in bytes) of the Double data type
-        /// </summary>
-        public const int SizeOfDouble = 8;
 
         /// <summary>
         /// The default strip size is 8k.
@@ -86,19 +56,9 @@ namespace SixLabors.ImageSharp.Formats.Tiff.Constants
         public static readonly TiffBitsPerSample BitsPerSample1Bit = new TiffBitsPerSample(1, 0, 0);
 
         /// <summary>
-        /// The bits per sample for images with a 2 color palette.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSample2Bit = new TiffBitsPerSample(2, 0, 0);
-
-        /// <summary>
         /// The bits per sample for images with a 4 color palette.
         /// </summary>
         public static readonly TiffBitsPerSample BitsPerSample4Bit = new TiffBitsPerSample(4, 0, 0);
-
-        /// <summary>
-        /// The bits per sample for 6 bit gray images.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSample6Bit = new TiffBitsPerSample(6, 0, 0);
 
         /// <summary>
         /// The bits per sample for 8 bit images.
@@ -106,59 +66,9 @@ namespace SixLabors.ImageSharp.Formats.Tiff.Constants
         public static readonly TiffBitsPerSample BitsPerSample8Bit = new TiffBitsPerSample(8, 0, 0);
 
         /// <summary>
-        /// The bits per sample for 10 bit gray images.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSample10Bit = new TiffBitsPerSample(10, 0, 0);
-
-        /// <summary>
-        /// The bits per sample for 12 bit gray images.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSample12Bit = new TiffBitsPerSample(12, 0, 0);
-
-        /// <summary>
-        /// The bits per sample for 14 bit gray images.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSample14Bit = new TiffBitsPerSample(14, 0, 0);
-
-        /// <summary>
-        /// The bits per sample for 16 bit gray images.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSample16Bit = new TiffBitsPerSample(16, 0, 0);
-
-        /// <summary>
-        /// The bits per sample for color images with 2 bits for each color channel.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSampleRgb2Bit = new TiffBitsPerSample(2, 2, 2);
-
-        /// <summary>
-        /// The bits per sample for color images with 4 bits for each color channel.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSampleRgb4Bit = new TiffBitsPerSample(4, 4, 4);
-
-        /// <summary>
         /// The bits per sample for color images with 8 bits for each color channel.
         /// </summary>
         public static readonly TiffBitsPerSample BitsPerSampleRgb8Bit = new TiffBitsPerSample(8, 8, 8);
-
-        /// <summary>
-        /// The bits per sample for color images with 10 bits for each color channel.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSampleRgb10Bit = new TiffBitsPerSample(10, 10, 10);
-
-        /// <summary>
-        /// The bits per sample for color images with 12 bits for each color channel.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSampleRgb12Bit = new TiffBitsPerSample(12, 12, 12);
-
-        /// <summary>
-        /// The bits per sample for color images with 14 bits for each color channel.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSampleRgb14Bit = new TiffBitsPerSample(14, 14, 14);
-
-        /// <summary>
-        /// The bits per sample for color images with 14 bits for each color channel.
-        /// </summary>
-        public static readonly TiffBitsPerSample BitsPerSampleRgb16Bit = new TiffBitsPerSample(16, 16, 16);
 
         /// <summary>
         /// The list of mimetypes that equate to a tiff.

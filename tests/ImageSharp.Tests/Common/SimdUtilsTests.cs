f725cc3957708294522d36598eaac2a60834caf8
Dmitry Pentin <riotbr3aker@gmail.com>
2021-06-15 00:19:16 +0300

    Added test output for test check with CI

--- tests/ImageSharp.Tests/Common/SimdUtilsTests.cs
+++ tests/ImageSharp.Tests/Common/SimdUtilsTests.cs
@@ -200,6 +200,8 @@ namespace SixLabors.ImageSharp.Tests.Common
         public void HwIntrinsics_BulkConvertByteToNormalizedFloat_Avx2(int count)
         {
 #if SUPPORTS_RUNTIME_INTRINSICS
+            this.Output.WriteLine($"Avx2.IsSupported: {System.Runtime.Intrinsics.X86.Avx2.IsSupported}");
+
             static void RunTest(string serialized)
             {
                 TestImpl_BulkConvertByteToNormalizedFloat(

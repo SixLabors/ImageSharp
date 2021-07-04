2566879ff5be41a573c6c22a04432576512dbad8
Anton Firszov <antonfir@gmail.com>
2021-07-04 23:45:19 +0200

    workaround NuGet restore issues

--- tests/ImageSharp.Tests.ProfilingSandbox/LoadResizeSaveParallelMemoryStress.cs
+++ tests/ImageSharp.Tests.ProfilingSandbox/LoadResizeSaveParallelMemoryStress.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Diagnostics;
 using System.Text;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using SixLabors.ImageSharp.Benchmarks.LoadResizeSave;
 
 namespace SixLabors.ImageSharp.Tests.ProfilingSandbox

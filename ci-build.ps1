a525a42b60dca33fddfbee7aabb49ebd915f2d8d
James Jackson-South <james_south@hotmail.com>
2021-08-19 23:49:39 +1000

    Use environment compiler version to build

--- ci-build.ps1
+++ ci-build.ps1
@@ -1,11 +1,6 @@
-param(
-  [Parameter(Mandatory = $true, Position = 0)]
-  [string]$targetFramework
-)
-
 dotnet clean -c Release
 
 $repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
 
 # Building for a specific framework.
-dotnet build -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
+dotnet build -c Release /p:RepositoryUrl=$repositoryUrl

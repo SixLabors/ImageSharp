f95102e340e34cfb5f22fec4c436db8b1321fc4f
Ildar Khayrutdinov <ildarkhairutdin@gmail.com>
2021-10-02 10:30:38 +0300

    Update src/ImageSharp/Metadata/Profiles/Exif/Values/ExifNumberArray.cs
    Co-authored-by: Günther Foidl <gue@korporal.at>

--- src/ImageSharp/Metadata/Profiles/Exif/Values/ExifNumberArray.cs
+++ src/ImageSharp/Metadata/Profiles/Exif/Values/ExifNumberArray.cs
@@ -56,7 +56,7 @@ namespace SixLabors.ImageSharp.Metadata.Profiles.Exif
                 case int[] array:
                 {
                     // workaround for inconsistent covariance of value-typed arrays
-                    if (value.GetType().Equals(typeof(uint[])))
+                    if (value.GetType() == typeof(uint[]))
                     {
                         return this.SetArray((uint[])value);
                     }

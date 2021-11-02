dec9036fa91f92ab11b1410554089ff1691a3e6e
Dmitry Pentin <riotbr3aker@gmail.com>
2021-11-02 12:26:55 +0300

    Added auto vectorized converter base class

--- src/ImageSharp/Formats/Jpeg/Components/Decoder/ColorConverters/JpegColorConverter.AutoVectorizedJpegColorConverter.cs
+++ src/ImageSharp/Formats/Jpeg/Components/Decoder/ColorConverters/JpegColorConverter.AutoVectorizedJpegColorConverter.cs
@@ -1,20 +1,40 @@
 // Copyright (c) Six Labors.
 // Licensed under the Apache License, Version 2.0.
 
+using System;
 using System.Numerics;
 
 namespace SixLabors.ImageSharp.Formats.Jpeg.Components.Decoder.ColorConverters
 {
     internal abstract partial class JpegColorConverter
     {
-        internal abstract class AutoVectorizedJpegColorConverter : VectorizedJpegColorConverter
+        internal abstract class AutoVectorizedJpegColorConverter : JpegColorConverter
         {
             protected AutoVectorizedJpegColorConverter(JpegColorSpace colorSpace, int precision)
-                : base(colorSpace, precision, Vector<float>.Count)
+                : base(colorSpace, precision)
             {
             }
 
             protected sealed override bool IsAvailable => Vector.IsHardwareAccelerated;
+
+            public sealed override void ConvertToRgbInplace(in ComponentValues values)
+            {
+                DebugGuard.IsTrue(this.IsAvailable, $"Used unsupported color converter: {this.GetType()}");
+
+                int length = values.Component0.Length;
+                int remainder = values.Component0.Length % Vector<float>.Count;
+                int simdCount = length - remainder;
+                if (simdCount > 0)
+                {
+                    this.ConvertCoreVectorizedInplace(values.Slice(0, simdCount));
+                }
+
+                this.ConvertCoreInplace(values.Slice(simdCount, remainder));
+            }
+
+            protected virtual void ConvertCoreVectorizedInplace(in ComponentValues values) => throw new NotImplementedException();
+
+            protected virtual void ConvertCoreInplace(in ComponentValues values) => throw new NotImplementedException();
         }
     }
 }

7191acaf34f535bc883b99114eb811708ddb5064
Brian Popow <popow@gmx.de>
2021-11-17 10:58:52 +0100

    Move UpSample to YuvConversion class

--- src/ImageSharp/Formats/Webp/Lossy/YuvConversion.cs
+++ src/ImageSharp/Formats/Webp/Lossy/YuvConversion.cs
@@ -18,6 +18,60 @@ namespace SixLabors.ImageSharp.Formats.Webp.Lossy
 
         private const int YuvHalf = 1 << (YuvFix - 1);
 
+        public static void UpSample(Span<byte> topY, Span<byte> bottomY, Span<byte> topU, Span<byte> topV, Span<byte> curU, Span<byte> curV, Span<byte> topDst, Span<byte> bottomDst, int len)
+        {
+            int xStep = 3;
+            int lastPixelPair = (len - 1) >> 1;
+            uint tluv = LoadUv(topU[0], topV[0]); // top-left sample
+            uint luv = LoadUv(curU[0], curV[0]); // left-sample
+            uint uv0 = ((3 * tluv) + luv + 0x00020002u) >> 2;
+            YuvToBgr(topY[0], (int)(uv0 & 0xff), (int)(uv0 >> 16), topDst);
+
+            if (bottomY != null)
+            {
+                uv0 = ((3 * luv) + tluv + 0x00020002u) >> 2;
+                YuvToBgr(bottomY[0], (int)uv0 & 0xff, (int)(uv0 >> 16), bottomDst);
+            }
+
+            for (int x = 1; x <= lastPixelPair; x++)
+            {
+                uint tuv = LoadUv(topU[x], topV[x]); // top sample
+                uint uv = LoadUv(curU[x], curV[x]); // sample
+
+                // Precompute invariant values associated with first and second diagonals.
+                uint avg = tluv + tuv + luv + uv + 0x00080008u;
+                uint diag12 = (avg + (2 * (tuv + luv))) >> 3;
+                uint diag03 = (avg + (2 * (tluv + uv))) >> 3;
+                uv0 = (diag12 + tluv) >> 1;
+                uint uv1 = (diag03 + tuv) >> 1;
+                int xMul2 = x * 2;
+                YuvToBgr(topY[xMul2 - 1], (int)(uv0 & 0xff), (int)(uv0 >> 16), topDst.Slice((xMul2 - 1) * xStep));
+                YuvToBgr(topY[xMul2 - 0], (int)(uv1 & 0xff), (int)(uv1 >> 16), topDst.Slice((xMul2 - 0) * xStep));
+
+                if (bottomY != null)
+                {
+                    uv0 = (diag03 + luv) >> 1;
+                    uv1 = (diag12 + uv) >> 1;
+                    YuvToBgr(bottomY[xMul2 - 1], (int)(uv0 & 0xff), (int)(uv0 >> 16), bottomDst.Slice((xMul2 - 1) * xStep));
+                    YuvToBgr(bottomY[xMul2 + 0], (int)(uv1 & 0xff), (int)(uv1 >> 16), bottomDst.Slice((xMul2 + 0) * xStep));
+                }
+
+                tluv = tuv;
+                luv = uv;
+            }
+
+            if ((len & 1) == 0)
+            {
+                uv0 = ((3 * tluv) + luv + 0x00020002u) >> 2;
+                YuvToBgr(topY[len - 1], (int)(uv0 & 0xff), (int)(uv0 >> 16), topDst.Slice((len - 1) * xStep));
+                if (bottomY != null)
+                {
+                    uv0 = ((3 * luv) + tluv + 0x00020002u) >> 2;
+                    YuvToBgr(bottomY[len - 1], (int)(uv0 & 0xff), (int)(uv0 >> 16), bottomDst.Slice((len - 1) * xStep));
+                }
+            }
+        }
+
         /// <summary>
         /// Converts the RGB values of the image to YUV.
         /// </summary>

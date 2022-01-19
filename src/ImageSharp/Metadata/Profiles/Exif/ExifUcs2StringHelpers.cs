918db05637db4a9fb1ea28fc24628d8b9e5d6232
Ildar Khayrutdinov <ildarkhairutdin@gmail.com>
2022-01-19 12:30:57 +0300

    add missing file

---
+++ src/ImageSharp/Metadata/Profiles/Exif/ExifUcs2StringHelpers.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Text;
+
+namespace SixLabors.ImageSharp.Metadata.Profiles.Exif
+{
+    internal static class ExifUcs2StringHelpers
+    {
+        private static Encoding Ucs2Encoding => Encoding.GetEncoding("UCS-2");
+
+        public static bool IsUcs2Tag(ExifTagValue tag)
+        {
+            switch (tag)
+            {
+                case ExifTagValue.XPAuthor:
+                case ExifTagValue.XPComment:
+                case ExifTagValue.XPKeywords:
+                case ExifTagValue.XPSubject:
+                case ExifTagValue.XPTitle:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static string ConvertToString(ReadOnlySpan<byte> buffer) => Ucs2Encoding.GetString(buffer);
+    }
+}

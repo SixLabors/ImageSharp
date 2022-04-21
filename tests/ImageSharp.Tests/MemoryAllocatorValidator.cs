297b7375211369c5fdfae20c9f8e81e7830befb8
Scott Williams <tocsoft@gmail.com>
2022-04-21 10:22:40 +0100

    track stack traces while debugging

--- tests/ImageSharp.Tests/MemoryAllocatorValidator.cs
+++ tests/ImageSharp.Tests/MemoryAllocatorValidator.cs
@@ -2,6 +2,9 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using SixLabors.ImageSharp.Diagnostics;
 using Xunit;
@@ -10,12 +13,19 @@ namespace SixLabors.ImageSharp.Tests
 {
     public static class MemoryAllocatorValidator
     {
+        private static bool _trackstacktraces = false;
+
         private static readonly AsyncLocal<TestMemoryDiagnostics> LocalInstance = new();
 
         public static bool MonitoringAllocations => LocalInstance.Value != null;
 
         static MemoryAllocatorValidator()
         {
+            if (bool.TryParse(Environment.GetEnvironmentVariable("TESTING_TRACK_STACK_TRACES"), out var trackStackTraces))
+            {
+                _trackstacktraces = trackStackTraces;
+            }
+
             MemoryDiagnostics.MemoryAllocated += MemoryDiagnostics_MemoryAllocated;
             MemoryDiagnostics.MemoryReleased += MemoryDiagnostics_MemoryReleased;
         }
@@ -43,6 +53,14 @@ namespace SixLabors.ImageSharp.Tests
         {
             var diag = new TestMemoryDiagnostics();
             LocalInstance.Value = diag;
+
+            // if we have the debugger attached or we have the environment variable set then track stacktraces of allocations
+            if ((Debugger.IsAttached || _trackstacktraces) && !MemoryDiagnostics.UndisposedAllocationSubscribed)
+            {
+                // noop - just needed to force the allocations to track stack traces
+                MemoryDiagnostics.UndisposedAllocation += (t) => { };
+            }
+
             return diag;
         }
 
@@ -61,7 +79,42 @@ namespace SixLabors.ImageSharp.Tests
             {
                 var count = this.TotalRemainingAllocated;
                 var pass = expectedAllocationCount == count;
-                Assert.True(pass, $"Expected a {expectedAllocationCount} undisposed buffers but found {count}");
+
+                if (!pass)
+                {
+                    if (MemoryDiagnostics.UndisposedAllocationSubscribed)
+                    {
+                        var stackTraces = new List<string>();
+                        void Callback(string trace)
+                        {
+                            while (trace.StartsWith("   at System.Environment.get_StackTrace") ||
+                            trace.StartsWith("   at SixLabors.ImageSharp.Memory"))
+                            {
+                                trace = trace.Substring(trace.IndexOf(Environment.NewLine) + Environment.NewLine.Length);
+                            }
+
+                            // strip prefix to get 'real' trace
+                            stackTraces.Add(trace);
+                        }
+
+                        MemoryDiagnostics.UndisposedAllocation += Callback;
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                        MemoryDiagnostics.UndisposedAllocation -= Callback;
+
+                        if (stackTraces.Any())
+                        {
+                            throw new AllocationException(stackTraces);
+                        }
+
+                        Assert.Empty(stackTraces);
+                        Assert.True(false, $"Expected {expectedAllocationCount} undisposed buffers but found {count}");
+                    }
+                    else
+                    {
+                        Assert.True(false, $"Expected {expectedAllocationCount} undisposed buffers but found {count}: ");
+                    }
+                }
             }
 
             public void Dispose()
@@ -72,6 +125,19 @@ namespace SixLabors.ImageSharp.Tests
                     StopMonitoringAllocations();
                 }
             }
+        }
+
+        public class AllocationException : Exception
+        {
+            public AllocationException(List<string> stackTraces)
+                : base($"Unexpected allocations")
+            {
+                this.StackTraces = stackTraces;
+            }
+
+            public List<string> StackTraces { get; }
+
+            public override string StackTrace => this.StackTraces.FirstOrDefault();
         }
     }
 }

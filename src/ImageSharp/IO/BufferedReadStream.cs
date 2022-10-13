544c14d12140a9ef421a47cfb9aba54d12df5081
James Jackson-South <james_south@hotmail.com>
2022-10-13 20:09:24 +1000

    Update src/ImageSharp/IO/BufferedReadStream.cs
    Co-authored-by: Günther Foidl <gue@korporal.at>

--- src/ImageSharp/IO/BufferedReadStream.cs
+++ src/ImageSharp/IO/BufferedReadStream.cs
@@ -172,7 +172,7 @@ internal sealed class BufferedReadStream : Stream
 
         // Too big for remaining buffer but less than entire buffer length
         // Copy to buffer then read from there.
-        if (this.readBufferIndex < 0 || (count + this.readBufferIndex > this.BufferSize))
+        if (this.readBufferIndex < 0 || (this.readBufferIndex > this.BufferSize - count))
         {
             return this.ReadToBufferViaCopySlow(buffer);
         }

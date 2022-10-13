d63a42c2935e54649cfc41360911c2b67ba5e313
James Jackson-South <james_south@hotmail.com>
2022-10-13 13:03:10 +1000

    Define base classes and options

---
+++ src/ImageSharp/Formats/ImageEncoder.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Six Labors.
+// Licensed under the Six Labors Split License.
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Quantization;
+
+namespace SixLabors.ImageSharp.Formats;
+
+/// <summary>
+/// The base class for all image encoders.
+/// </summary>
+public abstract class ImageEncoder : IImageEncoder, IEncoderOptions
+{
+    /// <inheritdoc/>
+    public bool SkipMetadata { get; set; }
+
+    /// <inheritdoc/>
+    public abstract void Encode<TPixel>(Image<TPixel> image, Stream stream)
+        where TPixel : unmanaged, IPixel<TPixel>;
+
+    /// <inheritdoc/>
+    public abstract Task EncodeAsync<TPixel>(Image<TPixel> image, Stream stream, CancellationToken cancellationToken)
+        where TPixel : unmanaged, IPixel<TPixel>;
+}
+
+/// <summary>
+/// The base class for all image encoders that allow color palette generation via quantization.
+/// </summary>
+public abstract class QuantizingImageEncoder : ImageEncoder, IQuantizingEncoderOptions
+{
+    /// <inheritdoc/>
+    public IQuantizer Quantizer { get; set; } = KnownQuantizers.Octree;
+
+    /// <inheritdoc/>
+    public IPixelSamplingStrategy GlobalPixelSamplingStrategy { get; set; } = new DefaultPixelSamplingStrategy();
+}

9f8bcc464db227bfc570c98ea6d23583f38fd4a7
James Jackson-South <james_south@hotmail.com>
2023-02-19 14:41:55 +1000

    Respond to feedback

--- src/ImageSharp/Common/Constants.cs
+++ src/ImageSharp/Common/Constants.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Six Labors.
 // Licensed under the Six Labors Split License.
 
-using System.Runtime.Intrinsics;
-
 namespace SixLabors.ImageSharp;
 
 /// <summary>
@@ -14,11 +12,6 @@ internal static class Constants
     /// The epsilon value for comparing floating point numbers.
     /// </summary>
     public static readonly float Epsilon = 0.001F;
-
-    /// <summary>
-    /// The epsilon value for comparing floating point numbers.
-    /// </summary>
-    public static readonly Vector256<float> Epsilon256 = Vector256.Create(0.001F);
 
     /// <summary>
     /// The epsilon squared value for comparing floating point numbers.

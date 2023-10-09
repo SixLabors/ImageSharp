a80ae33e4cac6acd60c0303f6740ce85c85cccad
Brian Popow <popow@gmx.de>
2023-10-09 18:31:25 +0200

    Add webp decoder option BackgroundColorHandling to decide howto handle the background color in the ANIM chunk

---
+++ src/ImageSharp/Formats/Webp/BackgroundColorHandling.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Six Labors.
+// Licensed under the Six Labors Split License.
+
+namespace SixLabors.ImageSharp.Formats.Webp;
+
+/// <summary>
+/// Enum to decide how to handle the background color of the Animation chunk during decoding.
+/// </summary>
+public enum BackgroundColorHandling
+{
+    /// <summary>
+    /// The background color of the ANIM chunk will be used to initialize the canvas to fill the unused space on the canvas around the frame.
+    /// Also, if AnimationDisposalMethod.Dispose is used, this color will be used to restore the canvas background.
+    /// </summary>
+    Standard = 0,
+
+    /// <summary>
+    /// The background color of the ANIM chunk is ignored and instead the canvas is initialized with black, BGRA(0, 0, 0, 0).
+    /// </summary>
+    Ignore = 1
+}

3d298db2cde6155e0b58f85e2b8e0a80b2d6702e
antonfirsov <antonfir@gmail.com>
2024-04-11 00:13:49 +0200

    Adapt BmpDecoder_ThrowsException_Issue2696 for V2

--- tests/ImageSharp.Tests/Formats/Bmp/BmpDecoderTests.cs
+++ tests/ImageSharp.Tests/Formats/Bmp/BmpDecoderTests.cs
@@ -624,9 +624,13 @@ namespace SixLabors.ImageSharp.Tests.Formats.Bmp
         [WithFile(Issue2696, PixelTypes.Rgba32)]
         public void BmpDecoder_ThrowsException_Issue2696<TPixel>(TestImageProvider<TPixel> provider)
             where TPixel : unmanaged, IPixel<TPixel>
-            => Assert.Throws<InvalidImageContentException>(() =>
-            {
-                using Image<TPixel> image = provider.GetImage(BmpDecoder);
-            });
+        {
+            // On V2 this is throwing InvalidOperationException,
+            // because of the validation logic in BmpInfoHeader.VerifyDimensions().
+            Assert.Throws<InvalidOperationException>(() =>
+                {
+                    using Image<TPixel> image = provider.GetImage(BmpDecoder);
+                });
+        }
     }
 }

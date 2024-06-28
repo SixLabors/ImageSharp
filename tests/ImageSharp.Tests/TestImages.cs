fae857fb1cd3f97df9831b9bc06ac2c33f2245b6
James Jackson-South <james_south@hotmail.com>
2024-06-28 13:46:47 +1000

    Clamp JPEG quality estimation results.

--- tests/ImageSharp.Tests/TestImages.cs
+++ tests/ImageSharp.Tests/TestImages.cs
@@ -320,6 +320,7 @@ public static class TestImages
             public const string HangBadScan = "Jpg/issues/Hang_C438A851.jpg";
             public const string Issue2517 = "Jpg/issues/issue2517-bad-d7.jpg";
             public const string Issue2638 = "Jpg/issues/Issue2638.jpg";
+            public const string Issue2758 = "Jpg/issues/issue-2758.jpg";
 
             public static class Fuzz
             {

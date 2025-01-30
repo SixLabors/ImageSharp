09ca511cccddabc1210d77e88a2eab5cc03bc56b
James Jackson-South <james_south@hotmail.com>
2025-01-30 14:14:01 +1000

    Try using setup-dotnet v4

--- ci-build.ps1
+++ ci-build.ps1
@@ -8,7 +8,4 @@ dotnet clean -c Release
 $repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
 
 # Building for a specific framework.
-# dotnet build -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl
-#
-# CI is now throwing build errors when none were present previously.
-dotnet build -c Release -f /p:RepositoryUrl=$repositoryUrl
+dotnet build -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl

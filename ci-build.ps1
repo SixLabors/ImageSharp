80a1432d997398ec2ceae776303e4cf252033d90
James Jackson-South <james_south@hotmail.com>
2025-01-30 14:39:16 +1000

    Check the version within the powershell script

--- ci-build.ps1
+++ ci-build.ps1
@@ -3,9 +3,16 @@ param(
   [string]$targetFramework
 )
 
+#$env:DOTNET_ROOT = "/usr/share/dotnet"
+#$env:PATH = "$env:DOTNET_ROOT" + [System.IO.Path]::PathSeparator + $env:PATH
+
+# Confirm dotnet version.
+dotnet --version
+
 dotnet clean -c Release
 
 $repositoryUrl = "https://github.com/$env:GITHUB_REPOSITORY"
+
 
 # Building for a specific framework.
 dotnet build -c Release -f $targetFramework /p:RepositoryUrl=$repositoryUrl

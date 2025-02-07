7cecea9020efba12bb29260db7335fe8251de981
James Jackson-South <james_south@hotmail.com>
2025-02-07 15:50:27 +1000

    Revert breaking change

--- src/ImageSharp/Formats/Webp/WebpBlendMethod.cs
+++ src/ImageSharp/Formats/Webp/WebpBlendMethod.cs
@@ -9,14 +9,14 @@ namespace SixLabors.ImageSharp.Formats.Webp;
 public enum WebpBlendMethod
 {
     /// <summary>
-    /// Do not blend. After disposing of the previous frame,
-    /// render the current frame on the canvas by overwriting the rectangle covered by the current frame.
-    /// </summary>
-    Source = 1,
-
-    /// <summary>
     /// Use alpha blending. After disposing of the previous frame, render the current frame on the canvas using alpha-blending.
     /// If the current frame does not have an alpha channel, assume alpha value of 255, effectively replacing the rectangle.
     /// </summary>
     Over = 0,
+
+    /// <summary>
+    /// Do not blend. After disposing of the previous frame,
+    /// render the current frame on the canvas by overwriting the rectangle covered by the current frame.
+    /// </summary>
+    Source = 1,
 }

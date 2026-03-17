d3b1cb2d2026406184fffaa94d9f26540238744e
antonfirsov <antonfir@gmail.com>
2026-03-17 23:17:01 +0100

    backport #3075 to 3.1

--- src/ImageSharp/Formats/Bmp/BmpDecoderCore.cs
+++ src/ImageSharp/Formats/Bmp/BmpDecoderCore.cs
@@ -1438,6 +1438,12 @@ internal sealed class BmpDecoderCore : ImageDecoderCore
                 switch (this.fileMarkerType)
                 {
                     case BmpFileMarkerType.Bitmap:
+                        if (this.fileHeader.Offset > stream.Length)
+                        {
+                            BmpThrowHelper.ThrowInvalidImageContentException(
+                                $"Pixel data offset {this.fileHeader.Offset} exceeds file size {stream.Length}.");
+                        }
+
                         colorMapSizeBytes = this.fileHeader.Offset - BmpFileHeader.Size - this.infoHeader.HeaderSize;
                         int colorCountForBitDepth = ColorNumerics.GetColorCountForBitDepth(this.infoHeader.BitsPerPixel);
                         bytesPerColorMapEntry = colorMapSizeBytes / colorCountForBitDepth;
